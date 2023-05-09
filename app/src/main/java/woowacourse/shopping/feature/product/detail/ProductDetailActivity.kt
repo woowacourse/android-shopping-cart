78f405cc2f38e3f66f4e761b05e8d0873f4d1118
otter <a33397397@gmail.com>
2023-05-09 20:51:12 +0900

    feat(product): 상품 상세 페이지 정보 출력 기능 작성

--- app/src/main/java/woowacourse/shopping/feature/product/detail/ProductDetailActivity.kt
+++ app/src/main/java/woowacourse/shopping/feature/product/detail/ProductDetailActivity.kt
@@ -4,21 +4,45 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import woowacourse.shopping.R
+import woowacourse.shopping.databinding.ActivityProductDetailBinding
 import woowacourse.shopping.feature.extension.showToast
 import woowacourse.shopping.feature.model.ProductState
+import java.text.DecimalFormat
 
 class ProductDetailActivity : AppCompatActivity() {
+    private var _binding: ActivityProductDetailBinding? = null
+    private val binding: ActivityProductDetailBinding
+        get() = _binding!!
+
     private val product: ProductState? by lazy { intent.getParcelableExtra(PRODUCT_KEY) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_product_detail)
+        _binding = ActivityProductDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         if (product == null) {
             showToast(getString(R.string.error_intent_message))
             finish()
         }
+
+        binding.product = product
+
+        product?.let { product ->
+            Glide.with(this)
+                .load(product.imageUrl)
+                .error(R.drawable.ic_launcher_background)
+                .into(binding.productImage)
+
+            binding.productPrice.text = "${DecimalFormat("#,###").format(product.price)}원"
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
     companion object {
