9d6bcf924fc683c5c63d1ee4d76c49c297b81b25
changhwan <chang5hustle@gmail.com>
2023-05-09 14:13:06 +0900

    feat: 기초 세팅

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("kotlin-parcelize")
 }
 
 android {
@@ -33,9 +35,18 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    dataBinding{
+        enable = true
+    }
+    viewBinding{
+        enable = true
+    }
 }
 
 dependencies {
+    // 프로젝트내 의존성
+    implementation(project(":domain"))
+
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.0")
     implementation("com.google.android.material:material:1.7.0")
@@ -43,4 +54,10 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Glide
+    implementation("com.github.bumptech.glide:glide:4.15.1")
+
+    // Mockk
+    testImplementation("io.mockk:mockk:1.13.5")
 }
