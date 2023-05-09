fc65528572affffaa0d1aaddbbce780d4d2742d3
boogieWoogie <kimjinuk1999@gmail.com>
2023-05-09 14:32:49 +0900

    feat: 기본 도메인 구현

---
+++ domain/src/main/java/model/Product.kt
@@ -0,0 +1,6 @@
+package model
+
+data class Product(
+    val name: String,
+    val price: Int,
+)
---
+++ domain/src/main/java/model/ProductRepository.kt
@@ -0,0 +1,14 @@
+package model
+
+object ProductRepository {
+    val products = listOf(
+        Product(0, "one", 1000),
+        Product(1, "two", 1000),
+        Product(2, "three", 1000),
+        Product(3, "fpir", 10009),
+        Product(4, "onddde", 10800),
+        Product(5, "ondffdfde", 1000),
+        Product(6, "eeonffffe", 1000),
+        Product(7, "edfone", 1000),
+    )
+}
