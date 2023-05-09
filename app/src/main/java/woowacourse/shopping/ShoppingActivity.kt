5e23362d787405d7264c2a0420a1140f3ff8c584
changhwan <chang5hustle@gmail.com>
2023-05-09 16:40:59 +0900

    feat(ShoppingActivity): Toolbar 제작

--- app/src/main/java/woowacourse/shopping/ShoppingActivity.kt
+++ app/src/main/java/woowacourse/shopping/ShoppingActivity.kt
@@ -3,9 +3,9 @@ package woowacourse.shopping
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class ShoppingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_shopping)
     }
 }
