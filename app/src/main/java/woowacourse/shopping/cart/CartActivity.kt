143e31cb61688ff6d43aa4bae5cf5cb632e75355
hyunji1203 <hj27720@gmail.com>
2023-05-09 17:34:24 +0900

    feat: 장바구니 화면의 액션바 안의 백버튼을 누르면 뒤로 이동하는 기능 추가

--- app/src/main/java/woowacourse/shopping/cart/CartActivity.kt
+++ app/src/main/java/woowacourse/shopping/cart/CartActivity.kt
@@ -1,6 +1,7 @@
 package woowacourse.shopping.cart
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import woowacourse.shopping.R
 
@@ -8,5 +9,15 @@ class CartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cart)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
