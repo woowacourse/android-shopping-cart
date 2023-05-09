72e3dcd18e41042f1788f532b80a1e9929eefc63
chws0508 <chws0508@naver.com>
2023-05-09 16:16:07 +0900

    feat(ProductListActivity): 화면 xml 구현

--- app/src/main/java/woowacourse/shopping/productlist/ProductListActivity.kt
+++ app/src/main/java/woowacourse/shopping/productlist/ProductListActivity.kt
@@ -1,11 +1,12 @@
-package woowacourse.shopping
+package woowacourse.shopping.productlist
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import woowacourse.shopping.R
 
-class MainActivity : AppCompatActivity() {
+class ProductListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_product_list)
     }
 }
