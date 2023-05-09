59b3760cad9a6a17ac15286a21fb38bd0bdc0857
changhwan <chang5hustle@gmail.com>
2023-05-09 21:19:14 +0900

    feat(ShoppingActivity): 인텐트 팩토리함수 작성

--- app/src/main/java/woowacourse/shopping/ui/productdetail/ProductDetailActivity.kt
+++ app/src/main/java/woowacourse/shopping/ui/productdetail/ProductDetailActivity.kt
@@ -1,10 +1,13 @@
 package woowacourse.shopping.ui.productdetail
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import woowacourse.shopping.R
 import woowacourse.shopping.databinding.ActivityProductDetailBinding
+import woowacourse.shopping.ui.model.UiProduct
 
 class ProductDetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityProductDetailBinding
@@ -12,5 +15,11 @@ class ProductDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_product_detail)
+    }
+
+    companion object {
+        private const val PRODUCT_KEY = "product"
+        fun getIntent(context: Context, product: UiProduct): Intent =
+            Intent(context, ProductDetailActivity::class.java).putExtra(PRODUCT_KEY, product)
     }
 }
