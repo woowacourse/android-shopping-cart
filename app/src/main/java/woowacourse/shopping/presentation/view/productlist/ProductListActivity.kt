456c52bb1cf258af73ec0876bd2cb8b02b273418
inseonyun <yuninseon@naver.com>
2023-05-09 15:31:50 +0900

    feat(ProductListActivity): 제품 목록 화면 작성

---
+++ app/src/main/java/woowacourse/shopping/presentation/view/productlist/ProductListActivity.kt
@@ -0,0 +1,12 @@
+package woowacourse.shopping.presentation.view.productlist
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import woowacourse.shopping.R
+
+class ProductListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_list)
+    }
+}
