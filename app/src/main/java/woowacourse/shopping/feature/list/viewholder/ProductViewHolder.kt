3774d76864a0e86aebdd1dafefa57cba61850aca
otter <a33397397@gmail.com>
2023-05-09 16:31:17 +0900

    feat(product): 상품 목록 이미지 출력기능 작성

--- app/src/main/java/woowacourse/shopping/feature/list/viewholder/ProductViewHolder.kt
+++ app/src/main/java/woowacourse/shopping/feature/list/viewholder/ProductViewHolder.kt
@@ -1,18 +1,25 @@
 package woowacourse.shopping.feature.list.viewholder
 
 import androidx.viewbinding.ViewBinding
+import com.bumptech.glide.Glide
+import woowacourse.shopping.R
 import woowacourse.shopping.databinding.ItemProductBinding
 import woowacourse.shopping.feature.list.item.ItemState
 import woowacourse.shopping.feature.list.item.ProductItem
 import java.text.DecimalFormat
 
 class ProductViewHolder(binding: ViewBinding) : ItemHolder(binding) {
-    val binding = binding as ItemProductBinding
+    private val binding = binding as ItemProductBinding
 
     override fun bind(itemState: ItemState) {
         val productItem = itemState as ProductItem
 
+        Glide.with(binding.root.context)
+            .load(productItem.imageUrl)
+            .error(R.drawable.ic_launcher_background)
+            .into(binding.productImage)
+
         binding.productName.text = productItem.name
-        binding.productPrice.text = DecimalFormat("#,###").format(productItem.price)
+        binding.productPrice.text = "${DecimalFormat("#,###").format(productItem.price)}원"
     }
 }
