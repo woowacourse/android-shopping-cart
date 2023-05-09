4f69db7674c914933a00a35bfb2268344781d600
chws0508 <chws0508@naver.com>
2023-05-09 14:36:09 +0900

    feat : Price model 생성

---
+++ domain/src/main/java/Price.kt
@@ -0,0 +1,9 @@
+class Price(val value: Int) {
+    init {
+        require(value >= 0) { PRICE_ERROR_MESSAGE }
+    }
+
+    companion object {
+        private const val PRICE_ERROR_MESSAGE = "가격은 0원 미만이 될 수 없다."
+    }
+}
