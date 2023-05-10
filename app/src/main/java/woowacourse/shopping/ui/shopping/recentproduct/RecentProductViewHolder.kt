f08fb31dbc0f0795c9f58080cbe73a87961fe9d4
changhwan <chang5hustle@gmail.com>
2023-05-10 14:57:09 +0900

    feat(ShoppingActivity): 멀티뷰 아이템 어댑터 생성

--- app/src/main/java/woowacourse/shopping/ui/shopping/recentproduct/RecentProductViewHolder.kt
+++ app/src/main/java/woowacourse/shopping/ui/shopping/recentproduct/RecentProductViewHolder.kt
@@ -1,11 +1,11 @@
-package woowacourse.shopping.ui.shopping
+package woowacourse.shopping.ui.shopping.recentproduct
 
 import androidx.recyclerview.widget.RecyclerView
-import woowacourse.shopping.databinding.ItemShoppingBinding
+import woowacourse.shopping.databinding.ItemRecentProductBinding
 import woowacourse.shopping.ui.model.UiProduct
 
-class ShoppingViewHolder(
-    private val binding: ItemShoppingBinding,
+class RecentProductViewHolder(
+    private val binding: ItemRecentProductBinding,
     onItemClick: (UiProduct) -> Unit
 ) : RecyclerView.ViewHolder(binding.root) {
     private lateinit var product: UiProduct
