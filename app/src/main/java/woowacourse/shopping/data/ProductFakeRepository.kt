deee066d4ddf258603fd42f19e41507e81d1470a
re4rk <37167652+re4rk@users.noreply.github.com>
2023-05-10 14:37:35 +0900

    refactor : 상품 더미데이터 클래스 이름 변경

--- app/src/main/java/woowacourse/shopping/data/ProductFakeRepository.kt
+++ app/src/main/java/woowacourse/shopping/data/ProductFakeRepository.kt
@@ -3,12 +3,12 @@ package woowacourse.shopping.data
 import com.example.domain.model.Product
 import com.example.domain.model.ProductRepository
 
-object ProductMockRepository : ProductRepository {
+object ProductFakeRepository : ProductRepository {
 
     private val products = List(100) {
         Product(
             it,
-            "[사미헌] 갈비탕${it}",
+            "[사미헌] 갈비탕$it",
             12000,
             "https://img-cf.kurly.com/cdn-cgi/image/quality=85,width=676/shop/data/goods/1648206780555l0.jpeg"
         )
