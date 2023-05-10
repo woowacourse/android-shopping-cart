1e0a4ef4004a75e57b1fb0dc9c0b2bb2f26d2d2c
Sujin Kim <sujinkim919@gmail.com>
2023-05-10 15:02:43 +0900

    feat: 상품 리사이클러뷰 내부 아이템 이미지 및 이름, 가격 보이도록 구현
    - glide 추가

--- app/src/main/java/woowacourse/shopping/ui/products/adapter/ProductListAdapter.kt
+++ app/src/main/java/woowacourse/shopping/ui/products/adapter/ProductListAdapter.kt
@@ -3,9 +3,11 @@ package woowacourse.shopping.ui.products.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import woowacourse.shopping.R
 import woowacourse.shopping.databinding.ItemProductBinding
 import woowacourse.shopping.ui.products.ProductUIState
+import java.text.DecimalFormat
 
 class ProductListAdapter(
     private val products: List<ProductUIState>,
@@ -39,8 +41,15 @@ class ProductListAdapter(
 
         fun bind(product: ProductUIState) {
             binding.product = product
-            // image glide
-            // string formatting #,### won
+            binding.tvProductPrice.text = itemView.context.getString(R.string.product_price)
+                .format(PRICE_FORMATTER.format(product.price))
+            Glide.with(itemView)
+                .load(product.imageUrl)
+                .into(binding.ivProduct)
+        }
+
+        companion object {
+            private val PRICE_FORMATTER = DecimalFormat("#,###")
         }
     }
 }
