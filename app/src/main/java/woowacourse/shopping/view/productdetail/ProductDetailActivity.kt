605c01ce850a51e6861638697dbbb5396c9f05ef
hyunji1203 <hj27720@gmail.com>
2023-05-10 16:22:56 +0900

    feat: 상품 상세 페이지 액션 바에 뒤로가기 버튼 추가

--- app/src/main/java/woowacourse/shopping/view/productdetail/ProductDetailActivity.kt
+++ app/src/main/java/woowacourse/shopping/view/productdetail/ProductDetailActivity.kt
@@ -3,6 +3,9 @@ package woowacourse.shopping.view.productdetail
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
@@ -37,6 +40,21 @@ class ProductDetailActivity : AppCompatActivity() {
             val intent = CartActivity.newIntent(this)
             startActivity(intent)
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_close, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.close -> {
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     companion object {
