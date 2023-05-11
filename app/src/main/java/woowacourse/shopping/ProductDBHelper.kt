74afc410cd80625f25ce6ae9453c9fd423671651
SangHyun Moon <no1msh1217@gmail.com>
2023-05-11 15:04:50 +0900

    feat: 상품 목록 데이터 베이스 구현

---
+++ app/src/main/java/woowacourse/shopping/ProductDBHelper.kt
@@ -0,0 +1,37 @@
+package woowacourse.shopping
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class ProductDBHelper(context: Context) : SQLiteOpenHelper(
+    context,
+    DB_NAME,
+    null,
+    DB_VERSION
+) {
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL(CREATE_TABLE_QUERY)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        db?.execSQL(DROP_TABLE_QUERY)
+    }
+
+    companion object {
+        const val DB_NAME = "ProductDB"
+        private const val DB_VERSION: Int = 1
+        const val TABLE_NAME = "products"
+        const val KEY_NAME = "name"
+        const val KEY_IMAGE = "image"
+        const val KEY_PRICE = "price"
+
+        private const val CREATE_TABLE_QUERY = "CREATE TABLE $TABLE_NAME (" +
+            "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+            "$KEY_NAME text," +
+            "$KEY_IMAGE text," +
+            "$KEY_PRICE text" +
+            ");"
+        private const val DROP_TABLE_QUERY = "DROP TABLE IF EXISTS $TABLE_NAME"
+    }
+}
---
+++ app/src/main/java/woowacourse/shopping/ProductDBRepository.kt
@@ -0,0 +1,45 @@
+package woowacourse.shopping
+
+import android.content.ContentValues
+import android.database.sqlite.SQLiteDatabase
+import woowacourse.shopping.ProductDBHelper.Companion.KEY_IMAGE
+import woowacourse.shopping.ProductDBHelper.Companion.KEY_NAME
+import woowacourse.shopping.ProductDBHelper.Companion.KEY_PRICE
+import woowacourse.shopping.ProductDBHelper.Companion.TABLE_NAME
+
+class ProductDBRepository(private val database: SQLiteDatabase) {
+    fun getAll(): List<ProductUIModel> {
+        val products = mutableListOf<ProductUIModel>()
+        database.rawQuery("SELECT * FROM $TABLE_NAME", null).use {
+            while (it.moveToNext()) {
+                val productUIModel = ProductUIModel(
+                    name = it.getString(it.getColumnIndexOrThrow(KEY_NAME)),
+                    url = it.getString(it.getColumnIndexOrThrow(KEY_IMAGE)),
+                    price = it.getInt(it.getColumnIndexOrThrow(KEY_PRICE)),
+                )
+                products.add(productUIModel)
+            }
+        }
+        return products
+    }
+
+    fun insert(productUIModel: ProductUIModel) {
+        val record = ContentValues().apply {
+            put(KEY_NAME, productUIModel.name)
+            put(KEY_IMAGE, productUIModel.url)
+            put(KEY_PRICE, productUIModel.price)
+        }
+        database.insert(TABLE_NAME, null, record)
+    }
+
+    fun clear() {
+        database.execSQL("DELETE FROM $TABLE_NAME")
+    }
+
+    fun isEmpty(): Boolean =
+        database.rawQuery("SELECT * FROM $TABLE_NAME", null).use { it.count == 0 }
+
+    fun close() {
+        database.close()
+    }
+}
