98c328346b2ea7866005173071d286a87a73f4fa
briandr97 <briandr@naver.com>
2023-05-11 17:45:01 +0900

    feat(ShoppingCart): ShoppingCartActivity 페이징 구현

--- app/src/main/java/woowacourse/shopping/presentation/ui/shoppingCart/presenter/ShoppingCartPresenter.kt
+++ app/src/main/java/woowacourse/shopping/presentation/ui/shoppingCart/presenter/ShoppingCartPresenter.kt
@@ -1,5 +1,6 @@
 package woowacourse.shopping.presentation.ui.shoppingCart.presenter
 
+import woowacourse.shopping.domain.PageNumber
 import woowacourse.shopping.domain.model.ProductInCart
 import woowacourse.shopping.domain.repository.ShoppingCartRepository
 
@@ -8,15 +9,48 @@ class ShoppingCartPresenter(
     private val shoppingCartRepository: ShoppingCartRepository,
 ) : ShoppingCartContract.Presenter {
     lateinit var shoppingCart: List<ProductInCart>
+    private var pageNumber = PageNumber()
 
     override fun getShoppingCart(page: Int) {
         val shoppingCart = shoppingCartRepository.getShoppingCart(SHOPPING_CART_ITEM_COUNT, page)
-
         view.setShoppingCart(shoppingCart)
     }
 
-    override fun getShoppingCartSize() {
-        shoppingCartRepository.getShoppingCartSize()
+    override fun setPageNumber() {
+        view.setPage(pageNumber.value)
+    }
+
+    private fun getPage() {
+        val shoppingCart =
+            shoppingCartRepository.getShoppingCart(SHOPPING_CART_ITEM_COUNT, pageNumber.value)
+        view.setShoppingCart(shoppingCart)
+    }
+
+    private fun goOtherPage() {
+        checkPageMovement()
+        setPageNumber()
+        getPage()
+    }
+
+    override fun goNextPage() {
+        pageNumber = pageNumber.nextPage()
+        goOtherPage()
+    }
+
+    override fun goPreviousPage() {
+        pageNumber = pageNumber.previousPage()
+        goOtherPage()
+    }
+
+    override fun checkPageMovement() {
+        val size = shoppingCartRepository.getShoppingCartSize()
+        val nextEnable = size > pageNumber.value * SHOPPING_CART_ITEM_COUNT
+        val previousEnable = pageNumber.value > 1
+        view.setPageButtonEnable(previousEnable, nextEnable)
+    }
+
+    override fun deleteProductInCart(productId: Long): Boolean {
+        return shoppingCartRepository.deleteProductInCart(productId)
     }
 
     companion object {
