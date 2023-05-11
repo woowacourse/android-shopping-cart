1b6decda7b8dd8e64bb17653babe471d133baf6e
SangHyun Moon <no1msh1217@gmail.com>
2023-05-11 16:24:11 +0900

    feat: 상품 상세 페이지 구현

---
+++ app/src/main/java/woowacourse/shopping/productdetail/ProductDetailActivity.kt
@@ -0,0 +1,63 @@
+package woowacourse.shopping.productdetail
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.Glide
+import woowacourse.shopping.BundleKeys
+import woowacourse.shopping.ProductDBHelper
+import woowacourse.shopping.ProductDBRepository
+import woowacourse.shopping.ProductUIModel
+import woowacourse.shopping.R
+import woowacourse.shopping.databinding.ActivityProductDetailBinding
+import woowacourse.shopping.getSerializableCompat
+
+class ProductDetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityProductDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_product_detail)
+
+        setSupportActionBar(binding.tbProductDetail)
+
+        val productData =
+            intent.getSerializableCompat(BundleKeys.KEY_PRODUCT) ?: ProductUIModel.dummy
+
+        val dbHelper = ProductDBHelper(this)
+        val db = dbHelper.writableDatabase
+        val repository = ProductDBRepository(db)
+        repository.insert(productData)
+
+        Glide.with(binding.root.context)
+            .load(productData.url)
+            .into(binding.ivProductImage)
+        binding.tvProductName.text = productData.name
+        binding.tvPrice.text = productData.price.toString()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.tool_bar_product_detail, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_cancel -> {
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    companion object {
+        fun intent(context: Context): Intent {
+            return Intent(context, ProductDetailActivity::class.java)
+        }
+    }
+}
