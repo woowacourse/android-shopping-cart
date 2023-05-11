f5072271aebcc95785ca6d6b74ce1681cabf1af2
s9hn <tnghqn@naver.com>
2023-05-11 15:12:56 +0900

    feat(ProductDetailPresenter) : 상세보기 프레젠터 추가

--- app/src/main/java/woowacourse/shopping/presentation/ui/productDetail/presenter/ProductDetailContract.kt
+++ app/src/main/java/woowacourse/shopping/presentation/ui/productDetail/presenter/ProductDetailContract.kt
@@ -1,15 +1,13 @@
 package woowacourse.shopping.presentation.ui.productDetail.presenter
 
-import woowacourse.shopping.domain.model.Product
-
 interface ProductDetailContract {
     interface View {
         val presenter: Presenter
     }
 
     interface Presenter {
-        val product: Product
         fun getProduct(id: Long)
-        fun putRecentlyViewed(id: Long)
+        fun addRecentlyViewedProduct(id: Long, unit: Int)
+        fun addProductInCart()
     }
 }
--- app/src/main/java/woowacourse/shopping/presentation/ui/productDetail/presenter/ProductDetailPresenter.kt
+++ app/src/main/java/woowacourse/shopping/presentation/ui/productDetail/presenter/ProductDetailPresenter.kt
@@ -1,19 +1,31 @@
 package woowacourse.shopping.presentation.ui.productDetail.presenter
 
+import android.util.Log
 import woowacourse.shopping.domain.model.Product
+import woowacourse.shopping.domain.model.ProductInCart
 import woowacourse.shopping.domain.repository.ProductRepository
+import woowacourse.shopping.domain.repository.ShoppingCartRepository
+import woowacourse.shopping.domain.util.WoowaResult.FAIL
+import woowacourse.shopping.domain.util.WoowaResult.SUCCESS
 
 class ProductDetailPresenter(
-    private val view: ProductDetailContract.View,
     private val productRepository: ProductRepository,
+    private val shoppingCartRepository: ShoppingCartRepository,
 ) : ProductDetailContract.Presenter {
-    override lateinit var product: Product
+    lateinit var selectedProduct: Product
 
     override fun getProduct(id: Long) {
-        // product = productRepository.getProduct(id)
+        when (val woowaResult = productRepository.getProduct(id)) {
+            is SUCCESS -> selectedProduct = woowaResult.data
+            is FAIL -> Log.d("ERROR", woowaResult.error.errorMessage)
+        }
     }
 
-    override fun putRecentlyViewed(id: Long) {
-        TODO("Not yet implemented")
+    override fun addRecentlyViewedProduct(id: Long, unit: Int) {
+        productRepository.addRecentlyViewedProduct(id, unit)
+    }
+
+    override fun addProductInCart() {
+        shoppingCartRepository.addProductInCart(ProductInCart(selectedProduct, 1))
     }
 }
