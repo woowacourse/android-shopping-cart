04c21fcef51abd16c9d22f961bb72f120b2725f3
chws0508 <chws0508@naver.com>
2023-05-11 17:43:26 +0900

    feat: 페이지 카운터 구현 및 Products 기능 추가

--- domain/src/main/java/woowacourse/shopping/Products.kt
+++ domain/src/main/java/woowacourse/shopping/Products.kt
@@ -6,7 +6,16 @@ class Products(products: List<Product> = listOf()) {
 
     val size get(): Int = _items.size
 
+    fun getProductsInRange(startIndex: Int, productSize: Int): Products {
+        if (startIndex + productSize > size) return Products(_items.subList(startIndex, size))
+        return Products(_items.subList(startIndex, startIndex + productSize))
+    }
+
     fun addProducts(products: List<Product>) {
         _items.addAll(products)
+    }
+
+    fun deleteProduct(productId: Int) {
+        _items.removeAll { it.id == productId }
     }
 }
