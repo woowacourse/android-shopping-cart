315a8cec8dbeeb6cdc2d54a2c8791e3c1e0d9d2a
hyemdooly <hyemdooly@gmail.com>
2023-05-11 16:08:51 +0900

    refactor: ProductDetailPresenter cartRepository 추상화 적용

--- app/src/main/java/woowacourse/shopping/view/productdetail/ProductDetailPresenter.kt
+++ app/src/main/java/woowacourse/shopping/view/productdetail/ProductDetailPresenter.kt
@@ -1,16 +1,16 @@
 package woowacourse.shopping.view.productdetail
 
-import woowacourse.shopping.data.CartDbRepository
+import woowacourse.shopping.domain.CartRepository
 import woowacourse.shopping.domain.RecentViewedRepository
 import woowacourse.shopping.model.ProductModel
 
 class ProductDetailPresenter(
     private val view: ProductDetailContract.View,
-    private val cartDbRepository: CartDbRepository,
+    private val cartRepository: CartRepository,
     private val recentViewedRepository: RecentViewedRepository,
 ) : ProductDetailContract.Presenter {
     override fun putInCart(product: ProductModel) {
-        cartDbRepository.add(product.id, 1)
+        cartRepository.add(product.id, 1)
         view.startCartActivity()
     }
 
