e8cb9c19d2f41ae7d814528f9317293247a2349e
mendel <rhthrhrl0@naver.com>
2023-05-12 15:40:36 +0900

    refactor(ImageViewBindingAdapter): 매개변수 타입을 url을 받기 위한 스트링으로 수정

--- app/src/main/java/woowacourse/shopping/feature/common/ImageViewBindingAdapter.kt
+++ app/src/main/java/woowacourse/shopping/feature/common/ImageViewBindingAdapter.kt
@@ -3,11 +3,12 @@ package woowacourse.shopping.feature.common
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
-import woowacourse.shopping.model.ProductUiModel
+import woowacourse.shopping.R
 
 @BindingAdapter("imgUrl")
-fun setImgUrl(imageView: ImageView, productUiModel: ProductUiModel) {
+fun setImgUrl(imageView: ImageView, url: String) {
     Glide.with(imageView.context)
-        .load(productUiModel.imgUrl)
+        .load(url)
+        .placeholder(R.drawable.ic_empty_image)
         .into(imageView)
 }
