f8ab3230aa53ce645fa5d63d8ef77be0ae773b9a
chws0508 <chws0508@naver.com>
2023-05-14 01:59:20 +0900

    feat: CartActivity DataBinding 적용

--- app/src/main/java/woowacourse/shopping/presentation/cart/viewholder/CartItemViewHolder.kt
+++ app/src/main/java/woowacourse/shopping/presentation/cart/viewholder/CartItemViewHolder.kt
@@ -1,8 +1,6 @@
 package woowacourse.shopping.presentation.cart.viewholder
 
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import woowacourse.shopping.R
 import woowacourse.shopping.databinding.ItemCartBinding
 import woowacourse.shopping.presentation.model.ProductModel
 
@@ -11,26 +9,14 @@ class CartItemViewHolder(
     deleteItem: (ProductModel) -> Unit,
 ) : RecyclerView.ViewHolder(binding.root) {
 
-    private var _productModel: ProductModel? = null
-    private val productModel get() = _productModel!!
+    private lateinit var productModel: ProductModel
 
     init {
         binding.imageCartDelete.setOnClickListener { deleteItem(productModel) }
     }
 
     fun bind(product: ProductModel) {
-        _productModel = product
-        binding.textCartProductName.text = product.name
-        binding.textCartProductPrice.text =
-            binding.textCartProductPrice.context.getString(R.string.price_format, product.price)
-        setImage(product.imageUrl)
-    }
-
-    private fun setImage(productUrl: String) {
-        Glide.with(binding.imageCartProductPoster.context)
-            .load(productUrl)
-            .error(R.drawable.default_image)
-            .centerCrop()
-            .into(binding.imageCartProductPoster)
+        productModel = product
+        binding.productModel = product
     }
 }
