96ff4325ee801db8cb072587127563a3efd74df1
vrexpert <vrexpert2302@gmail.com>
2023-05-14 10:04:00 +0900

    feat(CartPages): 쇼핑 Cart 페이지 기능 구현 및 테스트

---
+++ domain/src/main/java/woowacourse/shopping/CartPages.kt
@@ -0,0 +1,49 @@
+package woowacourse.shopping
+
+class CartPages(private val cartProducts: Products) {
+
+    var pageNumber = Counter(INITIAL_PAGE)
+        private set
+
+    fun getNextPageProducts(): Products {
+        pageNumber += PAGE_UNIT
+        return getCurrentProducts()
+    }
+
+    fun getPreviousPageProducts(): Products {
+        pageNumber -= PAGE_UNIT
+        return getCurrentProducts()
+    }
+
+    fun getDeletedProducts(productId: Int): Products {
+        cartProducts.deleteProduct(productId)
+        return getCurrentProducts()
+    }
+
+    private fun getCurrentProducts() = cartProducts.getProductsInRange(
+        (pageNumber.value - FIRST_PAGE) * PRODUCT_CART_SIZE,
+        PRODUCT_CART_SIZE,
+    )
+
+    fun isNextPageAble(): Boolean {
+        val lastPage = (cartProducts.size - FIRST_PAGE) / PRODUCT_CART_SIZE + FIRST_PAGE
+        if (pageNumber.value == lastPage) {
+            return false
+        }
+        return true
+    }
+
+    fun isPreviousPageAble(): Boolean {
+        if (pageNumber.value == FIRST_PAGE) {
+            return false
+        }
+        return true
+    }
+
+    companion object {
+        private const val INITIAL_PAGE = 0
+        private const val FIRST_PAGE = 1
+        private const val PAGE_UNIT = 1
+        private const val PRODUCT_CART_SIZE = 5
+    }
+}
