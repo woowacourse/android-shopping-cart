61c0dd1f62a4578e0065ad27054cad1d5b4ef4b4
EmilyCh0 <emily9803@ewhain.net>
2023-05-14 14:49:30 +0900

    refactor(CartContract): 사용하지 않는 코드 제거

--- app/src/main/java/woowacourse/shopping/data/sql/cart/CartContract.kt
+++ app/src/main/java/woowacourse/shopping/data/sql/cart/CartContract.kt
@@ -1,22 +1,16 @@
 package woowacourse.shopping.data.sql.cart
 
 object CartContract {
-    val TABLE_NAME = "cart_table"
+    const val TABLE_NAME = "cart_table"
     const val TABLE_COLUMN_CART_ID = "cart_id"
     const val TABLE_COLUMN_PRODUCT_ID = "product_id"
     const val TABLE_COLUMN_PRODUCT_COUNT = "product_count"
 
-    fun allColumn() = listOf(
-        TABLE_COLUMN_CART_ID,
-        TABLE_COLUMN_PRODUCT_ID,
-        TABLE_COLUMN_PRODUCT_COUNT
-    )
-
     fun createSQL(): String {
         return "CREATE TABLE $TABLE_NAME(" +
-                "  $TABLE_COLUMN_CART_ID INTEGER primary key autoincrement," +
-                "  $TABLE_COLUMN_PRODUCT_ID int," +
-                "  $TABLE_COLUMN_PRODUCT_COUNT int" +
-                ");"
+            "  $TABLE_COLUMN_CART_ID INTEGER primary key autoincrement," +
+            "  $TABLE_COLUMN_PRODUCT_ID int," +
+            "  $TABLE_COLUMN_PRODUCT_COUNT int" +
+            ");"
     }
 }
