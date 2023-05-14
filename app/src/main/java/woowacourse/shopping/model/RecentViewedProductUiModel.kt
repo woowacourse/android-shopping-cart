2efd7f896c2376790b6a57a2876d4fc42ce01d8f
boogieWoogie <kimjinuk1999@gmail.com>
2023-05-15 03:33:30 +0900

    refactor(model): 직렬화가 필요없는 클래스에 대해서 Serializable 제거

--- app/src/main/java/woowacourse/shopping/model/RecentViewedProductUiModel.kt
+++ app/src/main/java/woowacourse/shopping/model/RecentViewedProductUiModel.kt
@@ -1,9 +1,7 @@
 package woowacourse.shopping.model
 
-import java.io.Serializable
-
 data class RecentViewedProductUiModel(
     val id: Int,
     val name: String,
     val imageUrl: String,
-) : Serializable
+)
--- app/src/main/java/woowacourse/shopping/model/ShoppingCartProductUiModel.kt
+++ app/src/main/java/woowacourse/shopping/model/ShoppingCartProductUiModel.kt
@@ -1,11 +1,9 @@
 package woowacourse.shopping.model
 
-import java.io.Serializable
-
 data class ShoppingCartProductUiModel(
     val id: Int,
     val name: String,
     val imageUrl: String,
     val price: Int,
-    val count: Int
-) : Serializable
+    val count: Int,
+)
