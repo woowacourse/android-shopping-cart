6977e5f7f44d5378fd73d1ea1f39f4843d90a580
hyemdooly <hyemdooly@gmail.com>
2023-05-15 15:32:08 +0900

    refactor: test 코드 수정

--- app/src/test/java/woowacourse/shopping/ProductListPresenterTest.kt
+++ app/src/test/java/woowacourse/shopping/ProductListPresenterTest.kt
@@ -70,15 +70,38 @@ class ProductListPresenterTest {
         every { view.showProducts(capture(items)) } just runs
         presenter.fetchProducts()
 
-        val itemsExpected = ProductListViewItem.RecentViewedItem(
-            listOf(
-                0,
-                1,
-                2
-            ).map { id -> products.find { it.id == id }?.toUiModel() }
-                .sortedByDescending { it?.id } as List<ProductModel>
-        )
-        assertEquals(itemsExpected, items.captured[0])
+        val itemsExpected = listOf<ProductListViewItem>(
+            ProductListViewItem.RecentViewedItem(
+                listOf(
+                    ProductModel(2, "헛개차", "", 10000),
+                    ProductModel(1, "현미밥", "", 10000),
+                    ProductModel(0, "락토핏", "", 10000)
+                )
+            )
+        ) + listOf<ProductListViewItem>(
+            ProductListViewItem.ProductItem(ProductModel(0, "락토핏", "", 10000)),
+            ProductListViewItem.ProductItem(ProductModel(1, "현미밥", "", 10000)),
+            ProductListViewItem.ProductItem(ProductModel(2, "헛개차", "", 10000)),
+            ProductListViewItem.ProductItem(ProductModel(3, "키", "", 10000)),
+            ProductListViewItem.ProductItem(ProductModel(4, "닭가슴살", "", 10000)),
+            ProductListViewItem.ProductItem(ProductModel(5, "enffl", "", 10000)),
+            ProductListViewItem.ProductItem(ProductModel(6, "뽀또", "", 10000)),
+            ProductListViewItem.ProductItem(ProductModel(7, "둘리", "", 10000)),
+            ProductListViewItem.ProductItem(ProductModel(8, "안녕", "", 10000)),
+            ProductListViewItem.ProductItem(ProductModel(9, "9", "", 10000)),
+            ProductListViewItem.ProductItem(ProductModel(10, "10", "", 10000)),
+            ProductListViewItem.ProductItem(ProductModel(11, "11", "", 10000)),
+            ProductListViewItem.ProductItem(ProductModel(12, "12", "", 10000)),
+            ProductListViewItem.ProductItem(ProductModel(13, "13", "", 10000)),
+            ProductListViewItem.ProductItem(ProductModel(14, "14", "", 10000)),
+            ProductListViewItem.ProductItem(ProductModel(15, "15", "", 10000)),
+            ProductListViewItem.ProductItem(ProductModel(16, "16", "", 10000)),
+            ProductListViewItem.ProductItem(ProductModel(17, "17", "", 10000)),
+            ProductListViewItem.ProductItem(ProductModel(18, "18", "", 10000)),
+            ProductListViewItem.ProductItem(ProductModel(19, "19", "", 10000)),
+        ) + ProductListViewItem.ShowMoreItem()
+        assertEquals(itemsExpected.subList(0, 21), items.captured.subList(0, 21))
+        assertEquals(itemsExpected[itemsExpected.lastIndex].javaClass, items.captured[items.captured.lastIndex].javaClass)
     }
 
     @Test
@@ -94,350 +117,48 @@ class ProductListPresenterTest {
     }
 
     private val products = listOf(
-        Product(
-            0,
-            "락토핏",
-            "https://thumbnail6.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/6769030628798948-183ad194-f24c-44e6-b92f-1ed198b347cd.jpg",
-            Price(
-                10000,
-            ),
-        ),
-        Product(
-            1,
-            "현미밥",
-            "https://thumbnail7.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/1237954167000478-5b27108a-ee70-4e14-b605-181191a57bcb.jpg",
-            Price(
-                10000,
-            ),
-        ),
-        Product(
-            2,
-            "헛개차",
-            "https://thumbnail9.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/2418649993082166-2bfb64be-78dc-4c05-a2e3-1749f856fef8.jpg",
-            Price(
-                10000,
-            ),
-        ),
-        Product(
-            3,
-            "키",
-            "https://thumbnail6.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/1721669748108539-877f91ca-5964-4761-b3e0-bff7b970c31c.jpg",
-            Price(
-                10000,
-            ),
-        ),
-        Product(
-            4,
-            "닭가슴살",
-            "https://thumbnail7.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/2700754094560515-ebc4cbaa-4c4f-4750-8b41-2e6ae5ab26ed.jpg",
-            Price(
-                10000,
-            ),
-        ),
-        Product(
-            5,
-            "enffl",
-            "https://thumbnail6.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/6769030628798948-183ad194-f24c-44e6-b92f-1ed198b347cd.jpg",
-            Price(
-                10000,
-            ),
-        ),
-        Product(
-            6,
-            "뽀또",
-            "https://thumbnail7.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/1237954167000478-5b27108a-ee70-4e14-b605-181191a57bcb.jpg",
-            Price(
-                10000,
-            ),
-        ),
-        Product(
-            7,
-            "둘리",
-            "https://thumbnail9.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/2418649993082166-2bfb64be-78dc-4c05-a2e3-1749f856fef8.jpg",
-            Price(
-                10000,
-            ),
-        ),
-        Product(
-            8,
-            "안녕",
-            "https://thumbnail6.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/1721669748108539-877f91ca-5964-4761-b3e0-bff7b970c31c.jpg",
-            Price(
-                10000,
-            ),
-        ),
-        Product(
-            9,
-            "9",
-            "https://thumbnail7.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/2700754094560515-ebc4cbaa-4c4f-4750-8b41-2e6ae5ab26ed.jpg",
-            Price(
-                10000,
-            ),
-        ),
-        Product(
-            10,
-            "10",
-            "https://thumbnail6.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/6769030628798948-183ad194-f24c-44e6-b92f-1ed198b347cd.jpg",
-            Price(
-                10000,
-            ),
-        ),
-        Product(
-            11,
-            "11",
-            "https://thumbnail7.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/1237954167000478-5b27108a-ee70-4e14-b605-181191a57bcb.jpg",
-            Price(
-                10000,
-            ),
-        ),
-        Product(
-            12,
-            "12",
-            "https://thumbnail9.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/2418649993082166-2bfb64be-78dc-4c05-a2e3-1749f856fef8.jpg",
-            Price(
-                10000,
-            ),
-        ),
-        Product(
-            13,
-            "13",
-            "https://thumbnail6.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/1721669748108539-877f91ca-5964-4761-b3e0-bff7b970c31c.jpg",
-            Price(
-                10000,
-            ),
-        ),
-        Product(
-            14,
-            "14",
-            "https://thumbnail7.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/2700754094560515-ebc4cbaa-4c4f-4750-8b41-2e6ae5ab26ed.jpg",
-            Price(
-                10000,
-            ),
-        ),
-        Product(
-            15,
-            "15",
-            "https://thumbnail9.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/2418649993082166-2bfb64be-78dc-4c05-a2e3-1749f856fef8.jpg",
-            Price(
-                10000,
-            ),
-        ),
-        Product(
-            16,
-            "16",
-            "https://thumbnail6.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/1721669748108539-877f91ca-5964-4761-b3e0-bff7b970c31c.jpg",
-            Price(
-                10000,
-            ),
-        ),
-        Product(
-            17,
-            "17",
-            "https://thumbnail7.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/2700754094560515-ebc4cbaa-4c4f-4750-8b41-2e6ae5ab26ed.jpg",
-            Price(
-                10000,
-            ),
-        ),
-        Product(
-            18,
-            "18",
-            "https://thumbnail9.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/2418649993082166-2bfb64be-78dc-4c05-a2e3-1749f856fef8.jpg",
-            Price(
-                10000,
-            ),
-        ),
-        Product(
-            19,
-            "19",
-            "https://thumbnail6.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/1721669748108539-877f91ca-5964-4761-b3e0-bff7b970c31c.jpg",
-            Price(
-                10000,
-            ),
-        ),
-        Product(
-            20,
-            "20",
-            "https://thumbnail7.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/2700754094560515-ebc4cbaa-4c4f-4750-8b41-2e6ae5ab26ed.jpg",
-            Price(
-                10000,
-            ),
-        ),
-        Product(
-            21,
-            "21",
-            "https://thumbnail6.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/6769030628798948-183ad194-f24c-44e6-b92f-1ed198b347cd.jpg",
-            Price(
-                10000,
-            ),
-        ),
-        Product(
-            22,
-            "22",
-            "https://thumbnail7.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/1237954167000478-5b27108a-ee70-4e14-b605-181191a57bcb.jpg",
-            Price(
-                10000,
-            ),
-        ),
-        Product(
-            23,
-            "23",
-            "https://thumbnail9.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/2418649993082166-2bfb64be-78dc-4c05-a2e3-1749f856fef8.jpg",
-            Price(
-                10000,
-            ),
-        ),
-        Product(
-            24,
-            "24",
-            "https://thumbnail6.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/1721669748108539-877f91ca-5964-4761-b3e0-bff7b970c31c.jpg",
-            Price(
-                10000,
-            ),
-        ),
-        Product(
-            25,
-            "25",
-            "https://thumbnail7.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/2700754094560515-ebc4cbaa-4c4f-4750-8b41-2e6ae5ab26ed.jpg",
-            Price(
-                10000,
-            ),
-        ),
-        Product(
-            26,
-            "26",
-            "https://thumbnail9.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/2418649993082166-2bfb64be-78dc-4c05-a2e3-1749f856fef8.jpg",
-            Price(
-                10000,
-            ),
-        ),
-        Product(
-            27,
-            "27",
-            "https://thumbnail6.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/1721669748108539-877f91ca-5964-4761-b3e0-bff7b970c31c.jpg",
-            Price(
-                10000,
-            ),
-        ),
-        Product(
-            28,
-            "28",
-            "https://thumbnail7.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/2700754094560515-ebc4cbaa-4c4f-4750-8b41-2e6ae5ab26ed.jpg",
-            Price(
-                10000,
-            ),
-        ),
-        Product(
-            29,
-            "29",
-            "https://thumbnail9.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/2418649993082166-2bfb64be-78dc-4c05-a2e3-1749f856fef8.jpg",
-            Price(
-                10000,
-            ),
-        ),
-        Product(
-            30,
-            "30",
-            "https://thumbnail6.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/1721669748108539-877f91ca-5964-4761-b3e0-bff7b970c31c.jpg",
-            Price(
-                10000,
-            ),
-        ),
-        Product(
-            31,
-            "31",
-            "https://thumbnail7.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/2700754094560515-ebc4cbaa-4c4f-4750-8b41-2e6ae5ab26ed.jpg",
-            Price(
-                10000,
-            ),
-        ),
-        Product(
-            32,
-            "32",
-            "https://thumbnail6.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/6769030628798948-183ad194-f24c-44e6-b92f-1ed198b347cd.jpg",
-            Price(
-                10000,
-            ),
-        ),
-        Product(
-            33,
-            "33",
-            "https://thumbnail7.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/1237954167000478-5b27108a-ee70-4e14-b605-181191a57bcb.jpg",
-            Price(
-                10000,
-            ),
-        ),
-        Product(
-            34,
-            "34",
-            "https://thumbnail9.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/2418649993082166-2bfb64be-78dc-4c05-a2e3-1749f856fef8.jpg",
-            Price(
-                10000,
-            ),
-        ),
-        Product(
-            35,
-            "35",
-            "https://thumbnail6.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/1721669748108539-877f91ca-5964-4761-b3e0-bff7b970c31c.jpg",
-            Price(
-                10000,
-            ),
-        ),
-        Product(
-            36,
-            "36",
-            "https://thumbnail7.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/2700754094560515-ebc4cbaa-4c4f-4750-8b41-2e6ae5ab26ed.jpg",
-            Price(
-                10000,
-            ),
-        ),
-        Product(
-            37,
-            "37",
-            "https://thumbnail9.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/2418649993082166-2bfb64be-78dc-4c05-a2e3-1749f856fef8.jpg",
-            Price(
-                10000,
-            ),
-        ),
-        Product(
-            38,
-            "38",
-            "https://thumbnail6.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/1721669748108539-877f91ca-5964-4761-b3e0-bff7b970c31c.jpg",
-            Price(
-                10000,
-            ),
-        ),
-        Product(
-            39,
-            "39",
-            "https://thumbnail7.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/2700754094560515-ebc4cbaa-4c4f-4750-8b41-2e6ae5ab26ed.jpg",
-            Price(
-                10000,
-            ),
-        ),
-        Product(
-            40,
-            "40",
-            "https://thumbnail9.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/2418649993082166-2bfb64be-78dc-4c05-a2e3-1749f856fef8.jpg",
-            Price(
-                10000,
-            ),
-        ),
-        Product(
-            41,
-            "41",
-            "https://thumbnail6.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/1721669748108539-877f91ca-5964-4761-b3e0-bff7b970c31c.jpg",
-            Price(
-                10000,
-            ),
-        ),
-        Product(
-            42,
-            "42",
-            "https://thumbnail7.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/2700754094560515-ebc4cbaa-4c4f-4750-8b41-2e6ae5ab26ed.jpg",
-            Price(
-                10000,
-            ),
-        ),
-
+        Product(0, "락토핏", "", Price(10000)),
+        Product(1, "현미밥", "", Price(10000)),
+        Product(2, "헛개차", "", Price(10000)),
+        Product(3, "키", "", Price(10000)),
+        Product(4, "닭가슴살", "", Price(10000)),
+        Product(5, "enffl", "", Price(10000)),
+        Product(6, "뽀또", "", Price(10000)),
+        Product(7, "둘리", "", Price(10000)),
+        Product(8, "안녕", "", Price(10000)),
+        Product(9, "9", "", Price(10000)),
+        Product(10, "10", "", Price(10000)),
+        Product(11, "11", "", Price(10000)),
+        Product(12, "12", "", Price(10000)),
+        Product(13, "13", "", Price(10000)),
+        Product(14, "14", "", Price(10000)),
+        Product(15, "15", "", Price(10000)),
+        Product(16, "16", "", Price(10000)),
+        Product(17, "17", "", Price(10000)),
+        Product(18, "18", "", Price(10000)),
+        Product(19, "19", "", Price(10000)),
+        Product(20, "20", "", Price(10000)),
+        Product(21, "21", "", Price(10000)),
+        Product(22, "22", "", Price(10000)),
+        Product(23, "23", "", Price(10000)),
+        Product(24, "24", "", Price(10000)),
+        Product(25, "25", "", Price(10000)),
+        Product(26, "26", "", Price(10000)),
+        Product(27, "27", "", Price(10000)),
+        Product(28, "28", "", Price(10000)),
+        Product(29, "29", "", Price(10000)),
+        Product(30, "30", "", Price(10000)),
+        Product(31, "31", "", Price(10000)),
+        Product(32, "32", "", Price(10000)),
+        Product(33, "33", "", Price(10000)),
+        Product(34, "34", "", Price(10000)),
+        Product(35, "35", "", Price(10000)),
+        Product(36, "36", "", Price(10000)),
+        Product(37, "37", "", Price(10000)),
+        Product(38, "38", "", Price(10000)),
+        Product(39, "39", "", Price(10000)),
+        Product(40, "40", "", Price(10000)),
+        Product(41, "41", "", Price(10000)),
+        Product(42, "42", "", Price(10000))
     )
 }
