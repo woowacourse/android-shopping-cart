8f0658347f343d9f3698b4b1de9eada7817471e3
tmdgh1592 <tmdgh1592@naver.com>
2023-05-15 15:26:04 +0900

    test(BasketProductTest): 장바구니 도메인 모델 테스트 코드 작성

---
+++ domain/src/test/java/woowacourse/shopping/domain/BasketProductTest.kt
@@ -0,0 +1,24 @@
+package woowacourse.shopping.domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+
+internal class BasketProductTest {
+    @ParameterizedTest
+    @CsvSource("1, 1", "10, 5", "10, 10", "10, 11", "10, 100")
+    internal fun `최근_본_목록을_최대_개수_만큼만_추가할_수_있다`(maxCount: Int, addCount: Int) {
+        // given
+        var recentProducts = RecentProducts(maxCount = maxCount)
+
+        // when
+        (1..addCount).forEach { id ->
+            val item = RecentProduct(id, Product(id, "상품 $id", Price(1000), ""))
+            recentProducts = recentProducts.add(item)
+        }
+
+        // then
+        val actual = recentProducts.getItems().size
+        assertThat(actual).isEqualTo(addCount.coerceAtMost(maxCount))
+    }
+}
