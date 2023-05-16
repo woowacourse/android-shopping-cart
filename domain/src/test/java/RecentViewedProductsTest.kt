d86bb1595c0595a86a26b24a15de9005f2be9640
boogieWoogie <kimjinuk1999@gmail.com>
2023-05-16 14:47:44 +0900

    fix(RecentViewedProductsTest): 최근본 상품 목록에 대한 테스트 코드 수정

--- domain/src/test/java/RecentViewedProductsTest.kt
+++ domain/src/test/java/RecentViewedProductsTest.kt
@@ -8,15 +8,15 @@ class RecentViewedProductsTest {
     fun `최대 개수보다 많은 상품이 저장될 경우 상품이 삭제되고 삭제된 상품을 반환한다`() {
         // given
         val recentViewedProducts = RecentViewedProducts(
-            products = listOf(Product(name = "아메리카노")),
+            products = listOf(RecentViewedProduct(name = "아메리카노")),
             maxSize = 1
         )
 
         // when
-        val removedProducts = recentViewedProducts.add(Product(name = "카페라떼"))
+        val removedProducts = recentViewedProducts.add(RecentViewedProduct(name = "카페라떼"))
 
         // then
-        val expected = Product(name = "아메리카노")
+        val expected = RecentViewedProduct(name = "아메리카노")
         assertEquals(expected, removedProducts)
     }
 
@@ -25,20 +25,20 @@ class RecentViewedProductsTest {
         // given
         val recentViewedProducts = RecentViewedProducts(
             products = listOf(
-                Product(name = "아메리카노")
+                RecentViewedProduct(name = "아메리카노")
             ),
             maxSize = 5
         )
 
         // when
         val removedProducts = recentViewedProducts.add(
-            Product(name = "카페라떼")
+            RecentViewedProduct(name = "카페라떼")
         )
 
         // then
         val expectedValues = listOf(
-            Product(name = "아메리카노"),
-            Product(name = "카페라떼")
+            RecentViewedProduct(name = "아메리카노"),
+            RecentViewedProduct(name = "카페라떼")
         )
         val expected = null
         assertEquals(expected, removedProducts)
