39fa4fe6876457ed921b2995ce4299a8e7c53468
otter <a33397397@gmail.com>
2023-05-16 17:29:17 +0900

    refactor(product): 최근 상품 목록 databinding 적용

--- app/src/main/java/woowacourse/shopping/list/viewholder/RecentProductItemViewHolder.kt
+++ app/src/main/java/woowacourse/shopping/list/viewholder/RecentProductItemViewHolder.kt
@@ -1,23 +1,17 @@
 package woowacourse.shopping.list.viewholder
 
 import androidx.viewbinding.ViewBinding
-import com.bumptech.glide.Glide
-import woowacourse.shopping.R
 import woowacourse.shopping.databinding.ItemRecentBinding
 import woowacourse.shopping.list.item.ListItem
 import woowacourse.shopping.list.item.RecentProductListItem
+import woowacourse.shopping.model.mapper.toUi
 
-class RecentProductItemViewHolder(val binding: ViewBinding) : ItemHolder(binding) {
+class RecentProductItemViewHolder(binding: ViewBinding) : ItemHolder(binding) {
+    val binding = binding as ItemRecentBinding
 
     override fun bind(listItem: ListItem, onClick: (ListItem) -> Unit) {
-        binding as ItemRecentBinding
         listItem as RecentProductListItem
 
-        Glide.with(binding.root.context)
-            .load(listItem.productImageUrl)
-            .error(R.drawable.ic_launcher_background)
-            .into(binding.recentImageView)
-
-        binding.recentName.text = listItem.productName
+        binding.recentProduct = listItem.toUi()
     }
 }
