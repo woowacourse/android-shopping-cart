d81e482d6ef792efe5e70b94bde0a5f78de193da
chws0508 <chws0508@naver.com>
2023-05-16 17:43:17 +0900

    feat: Counter 객체 구현

---
+++ domain/src/main/java/woowacourse/shopping/Counter.kt
@@ -0,0 +1,15 @@
+package woowacourse.shopping
+
+class Counter(val value: Int = DEFAULT_COUNT) {
+    open operator fun plus(number: Int): Counter = Counter(value + number)
+
+    open operator fun minus(number: Int): Counter {
+        val result = value - number
+        if (result <= 0) return Counter(value)
+        return Counter(result)
+    }
+
+    companion object {
+        private const val DEFAULT_COUNT = 1
+    }
+}
