43a1a581e8757fba5ac44baf8e9caefb90bbb1a3
boogieWoogie <kimjinuk1999@gmail.com>
2023-05-16 14:38:13 +0900

    feat(Count): 상품의 개수를 관리할 Count 클래스 생성

---
+++ domain/src/main/java/model/Count.kt
@@ -0,0 +1,20 @@
+package model
+
+@JvmInline
+value class Count(val value: Int = 1) {
+
+    init {
+        require(value > MINIMUM_COUNT) {
+            MINIMUM_COUNT_ERROR
+        }
+    }
+
+    fun plus() = Count(value + 1)
+
+    fun minus() = Count(value - 1)
+
+    companion object {
+        private const val MINIMUM_COUNT_ERROR = "상품의 개수는 0보다 커야합니다."
+        private const val MINIMUM_COUNT = 0
+    }
+}
