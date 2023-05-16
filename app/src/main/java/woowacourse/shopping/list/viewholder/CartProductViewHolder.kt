f493af6e3c7c88a80b516beb35aaf788fba1bf21
otter <a33397397@gmail.com>
2023-05-16 17:21:20 +0900

    refactor(cart): 장바구니 제품 databinding 적용

--- app/src/main/java/woowacourse/shopping/list/viewholder/CartProductViewHolder.kt
+++ app/src/main/java/woowacourse/shopping/list/viewholder/CartProductViewHolder.kt
@@ -1,26 +1,19 @@
 package woowacourse.shopping.list.viewholder
 
 import androidx.viewbinding.ViewBinding
-import com.bumptech.glide.Glide
-import woowacourse.shopping.R
 import woowacourse.shopping.databinding.ItemCartProductBinding
 import woowacourse.shopping.list.item.CartProductListItem
 import woowacourse.shopping.list.item.ListItem
-import java.text.DecimalFormat
+import woowacourse.shopping.model.mapper.toUi
 
 class CartProductViewHolder(binding: ViewBinding) : ItemHolder(binding) {
     private val binding = binding as ItemCartProductBinding
 
     override fun bind(listItem: ListItem, onClick: (ListItem) -> Unit) {
-        val cartItem = listItem as CartProductListItem
+        listItem as CartProductListItem
 
-        Glide.with(binding.root.context)
-            .load(cartItem.productImageUrl)
-            .error(R.drawable.ic_launcher_background)
-            .into(binding.cartProductImageView)
+        binding.cartProduct = listItem.toUi()
 
-        binding.cartProductNameTv.text = cartItem.productName
-        binding.cartProductPriceTv.text = "${DecimalFormat("#,###").format(cartItem.productPrice)}원"
-        binding.cartClearImageView.setOnClickListener { onClick(cartItem) }
+        binding.cartClearImageView.setOnClickListener { onClick(listItem) }
     }
 }
