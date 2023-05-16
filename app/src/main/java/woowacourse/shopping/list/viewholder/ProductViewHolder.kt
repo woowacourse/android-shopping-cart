7414754084f0bbce916721b9ab181c04989edb38
otter <a33397397@gmail.com>
2023-05-16 17:25:47 +0900

    refactor(product): 상품 목록 databinding 적용

--- app/src/main/java/woowacourse/shopping/list/viewholder/ProductViewHolder.kt
+++ app/src/main/java/woowacourse/shopping/list/viewholder/ProductViewHolder.kt
@@ -1,26 +1,19 @@
 package woowacourse.shopping.list.viewholder
 
 import androidx.viewbinding.ViewBinding
-import com.bumptech.glide.Glide
-import woowacourse.shopping.R
 import woowacourse.shopping.databinding.ItemProductBinding
 import woowacourse.shopping.list.item.ListItem
 import woowacourse.shopping.list.item.ProductListItem
-import java.text.DecimalFormat
+import woowacourse.shopping.model.mapper.toUi
 
 class ProductViewHolder(binding: ViewBinding) : ItemHolder(binding) {
     private val binding = binding as ItemProductBinding
 
     override fun bind(listItem: ListItem, onClick: (ListItem) -> Unit) {
-        val productItem = listItem as ProductListItem
+        listItem as ProductListItem
 
-        Glide.with(binding.root.context)
-            .load(productItem.imageUrl)
-            .error(R.drawable.ic_launcher_background)
-            .into(binding.productImage)
+        binding.product = listItem.toUi()
 
-        binding.productName.text = productItem.name
-        binding.productPrice.text = "${DecimalFormat("#,###").format(productItem.price)}원"
-        binding.root.setOnClickListener { onClick(productItem) }
+        binding.root.setOnClickListener { onClick(listItem) }
     }
 }
