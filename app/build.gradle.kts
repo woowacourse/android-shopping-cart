667ee96445840a58b96653961eae3da2d2dc4391
Sujin Kim <sujinkim919@gmail.com>
2023-05-16 13:49:37 +0900

    test: db 및 테이블이 생성되었는지 테스트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation(project(mapOf("path" to ":domain")))
     implementation("com.github.bumptech.glide:glide:4.15.1")
     testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test:runner:1.5.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
