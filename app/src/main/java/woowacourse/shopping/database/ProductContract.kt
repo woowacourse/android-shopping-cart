44569a5b7179e286005134cd7e1b1e31b83b5d7b
ki960213 <ki960213@gmail.com>
2023-05-17 15:42:41 +0900

    feat(ProductContract): 테이블 스키마 변경

--- app/src/main/java/woowacourse/shopping/database/ProductContract.kt
+++ app/src/main/java/woowacourse/shopping/database/ProductContract.kt
@@ -9,10 +9,13 @@ object ProductContract {
     object CartEntry : BaseColumns {
         const val TABLE_NAME = "cart"
         const val COLUMN_NAME_PRODUCT_ID = "product_id"
+        const val COLUMN_NAME_ADDED_TIME = "added_time"
+        const val COLUMN_NAME_COUNT = "count"
     }
 
     object RecentlyViewedProductEntry : BaseColumns {
         const val TABLE_NAME = "recently_viewed_product"
         const val COLUMN_NAME_PRODUCT_ID = "product_id"
+        const val COLUMN_NAME_VIEWED_TIME = "viewed_time"
     }
 }
