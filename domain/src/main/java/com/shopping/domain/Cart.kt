8d39739c0894899fdb0a5a4932448a292ade8af6
SangHyun Moon <no1msh1217@gmail.com>
2023-05-17 21:06:34 +0900

    feat: 장바구니가 가지는 상품의 가격의 총합을 가져오는 기능 구현

--- domain/src/main/java/com/shopping/domain/Cart.kt
+++ domain/src/main/java/com/shopping/domain/Cart.kt
@@ -1,3 +1,11 @@
 package com.shopping.domain
 
-class Cart(val products: List<CartProduct>)
+class Cart(products: List<CartProduct>) {
+    val products: List<CartProduct>
+
+    init {
+        this.products = products.distinctBy { it.product.id }
+    }
+
+    fun getTotalPrice(): Int = products.sumOf { it.count * it.product.price.value }
+}
