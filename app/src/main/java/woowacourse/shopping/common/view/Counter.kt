d1b4d07d5dccfc6e3ccd6f5fad23cad180fd8dd9
Doyeop Kim <dygames34@gmail.com>
2023-05-17 17:13:44 +0900

    feat: Counter Compound Control 구현

---
+++ app/src/main/java/woowacourse/shopping/common/view/Counter.kt
@@ -0,0 +1,47 @@
+package woowacourse.shopping.common.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import androidx.databinding.BindingAdapter
+import androidx.databinding.DataBindingUtil
+import woowacourse.shopping.R
+import woowacourse.shopping.databinding.CompoundCounterBinding
+
+class Counter(context: Context, attrs: AttributeSet) : LinearLayout(context, attrs) {
+    private val binding: CompoundCounterBinding = DataBindingUtil.inflate(
+        LayoutInflater.from(context), R.layout.compound_counter, this, true
+    )
+
+    private var count: Int = 0
+
+    init {
+        setCounterText(count)
+    }
+
+    fun setCounterText(count: Int) {
+        this.count = count
+        binding.counterText.text = count.toString()
+    }
+
+    companion object {
+        @BindingAdapter("onMinusClick")
+        @JvmStatic
+        fun minusClick(counter: Counter, onClick: (Int) -> Unit) {
+            counter.binding.counterMinusButton.setOnClickListener {
+                counter.setCounterText(counter.count - 1)
+                onClick(counter.count)
+            }
+        }
+
+        @BindingAdapter("onPlusClick")
+        @JvmStatic
+        fun plusClick(counter: Counter, onClick: (Int) -> Unit) {
+            counter.binding.counterPlusButton.setOnClickListener {
+                counter.setCounterText(counter.count + 1)
+                onClick(counter.count)
+            }
+        }
+    }
+}
