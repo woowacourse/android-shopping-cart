3d033817ce3d46adc00f30ed445a8dc0e251e1f5
SangHyun Moon <no1msh1217@gmail.com>
2023-05-18 14:37:28 +0900

    test: ProductDetailPresenter 테스트

--- app/src/test/java/woowacourse/shopping/ProductDetailPresenterTest.kt
+++ app/src/test/java/woowacourse/shopping/ProductDetailPresenterTest.kt
@@ -1,26 +1,27 @@
 package woowacourse.shopping
 
+import com.shopping.domain.CartProduct
+import com.shopping.domain.Product
+import com.shopping.domain.RecentProduct
 import io.mockk.every
 import io.mockk.just
 import io.mockk.mockk
 import io.mockk.runs
-import io.mockk.slot
 import io.mockk.verify
-import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 import woowacourse.shopping.datas.CartRepository
 import woowacourse.shopping.datas.RecentRepository
+import woowacourse.shopping.mapper.toDomain
+import woowacourse.shopping.mapper.toUIModel
 import woowacourse.shopping.productdetail.ProductDetailContract
 import woowacourse.shopping.productdetail.ProductDetailPresenter
-import woowacourse.shopping.uimodel.CartProductUIModel
 import woowacourse.shopping.uimodel.ProductUIModel
-import woowacourse.shopping.uimodel.RecentProductUIModel
 
 class ProductDetailPresenterTest {
 
     private lateinit var view: ProductDetailContract.View
-    private lateinit var productUIModel: ProductUIModel
+    private lateinit var product: Product
     private lateinit var recentRepository: RecentRepository
     private lateinit var cartRepository: CartRepository
     private lateinit var presenter: ProductDetailContract.Presenter
@@ -28,24 +29,11 @@ class ProductDetailPresenterTest {
     @Before
     fun setUp() {
         view = mockk(relaxed = true)
-        productUIModel = ProductUIModel.dummy
+        product = ProductUIModel.dummy.toDomain()
         recentRepository = mockk()
         cartRepository = mockk()
-        presenter = ProductDetailPresenter(view, productUIModel, recentRepository, cartRepository)
-    }
-
-    @Test
-    fun `초기화면을 그릴 때 필요한 데이터를 넘겨준다`() {
-        // given
-        val slot = slot<ProductUIModel>()
-        every { view.setViews(productUIModel) } just runs
-
-        // when
-        presenter.initPage()
-
-        // then
-        verify { view.setViews(capture(slot)) }
-        assertEquals(productUIModel, slot.captured)
+        presenter =
+            ProductDetailPresenter(view, product.toUIModel(), recentRepository, cartRepository)
     }
 
     @Test
@@ -53,40 +41,26 @@ class ProductDetailPresenterTest {
         val currentTime = 1000L
 
         // given
-        every {
-            recentRepository.insert(
-                RecentProductUIModel(
-                    currentTime,
-                    productUIModel
-                )
-            )
-        } just runs
+        every { recentRepository.insert(RecentProduct(currentTime, product)) } just runs
 
         // when
         presenter.insertRecentRepository(currentTime)
 
         // then
-        verify {
-            recentRepository.insert(
-                RecentProductUIModel(
-                    currentTime,
-                    productUIModel
-                )
-            )
-        }
+        verify { recentRepository.insert(RecentProduct(currentTime, product)) }
     }
 
     @Test
     fun `장바구니에 추가하는 버튼이 클릭 될 때 데이터를 레포지토리에 저장하고 장바구니 페이지를 보여준다`() {
         // given
-        every { cartRepository.insert(CartProductUIModel(1, productUIModel)) } just runs
+        every { cartRepository.insert(CartProduct(1, product)) } just runs
         every { view.showCartPage() } just runs
 
         // when
-        presenter.onClickAddToCart()
+        presenter.addToCart()
 
         // then
-        verify { cartRepository.insert(CartProductUIModel(1, productUIModel)) }
+        verify { cartRepository.insert(CartProduct(1, product)) }
         verify { view.showCartPage() }
     }
 }
