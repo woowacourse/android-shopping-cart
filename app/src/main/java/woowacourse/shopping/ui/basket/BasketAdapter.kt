0d6f87a4adbcc9654a39e1768dacc62d8aa4b6ae
changhwan <chang5hustle@gmail.com>
2023-05-18 15:36:24 +0900

    refactor(BaksetAdapter): 함수명 변경

--- app/src/main/java/woowacourse/shopping/ui/basket/BasketAdapter.kt
+++ app/src/main/java/woowacourse/shopping/ui/basket/BasketAdapter.kt
@@ -7,13 +7,13 @@ import androidx.recyclerview.widget.ListAdapter
 import woowacourse.shopping.databinding.ItemBasketBinding
 import woowacourse.shopping.ui.model.UiBasketProduct
 
-class BasketAdapter(private val onItemClick: (UiBasketProduct, List<UiBasketProduct>) -> Unit) :
+class BasketAdapter(private val onRemoveItemClick: (UiBasketProduct, List<UiBasketProduct>) -> Unit) :
     ListAdapter<UiBasketProduct, BasketViewHolder>(basketDiffUtil) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BasketViewHolder {
         val binding =
             ItemBasketBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return BasketViewHolder(binding) { onItemClick(it, currentList) }
+        return BasketViewHolder(binding) { onRemoveItemClick(it, currentList) }
     }
 
     override fun onBindViewHolder(holder: BasketViewHolder, position: Int) {
