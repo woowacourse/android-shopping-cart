ce9c5c3f257f2b0d7e24504b5179e4edb7e44dd1
mendel <rhthrhrl0@naver.com>
2023-05-19 00:11:48 +0900

    test(MockRemoteProductRepositoryImplTest): 테스트 코드 추가

---
+++ domain/src/test/java/com/example/domain/remote/MockRemoteProductRepositoryTest.kt
@@ -0,0 +1,50 @@
+package com.example.domain.remote
+
+import com.example.domain.cache.ProductLocalCache
+import com.example.domain.datasource.productsDatasource
+import com.example.domain.repository.ProductRepository
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+internal class MockRemoteProductRepositoryTest {
+    private lateinit var mockRemoteProductRepository: ProductRepository
+    private val mockProductRemoteService: MockProductRemoteService =
+        MockProductRemoteService()
+
+    @Before
+    fun init() {
+        mockRemoteProductRepository =
+            MockRemoteProductRepositoryImpl(mockProductRemoteService, ProductLocalCache)
+        ProductLocalCache.clear()
+    }
+
+    @After
+    fun after() {
+//        mockProductRemoteService.
+    }
+
+    @Test
+    fun `처음 20개의 상품을 불러온다`() {
+        val products = mockRemoteProductRepository.getFirstProducts()
+
+        val expected = productsDatasource.subList(0, 20)
+        assert(products == expected)
+    }
+
+    @Test
+    fun `20번 아이디 상품까지 받은 상태에서 추가적으로 더 불러온다`() {
+        val nextProducts = mockRemoteProductRepository.getNextProducts(20L)
+
+        val expected = productsDatasource.subList(20, 40)
+        assert(nextProducts == expected)
+    }
+
+    @Test
+    fun `40번 아이디 상품까지 받은 상태에서 추가적으로 더 불러온다`() {
+        val nextProducts = mockRemoteProductRepository.getNextProducts(40L)
+
+        val expected = productsDatasource.subList(40, 41)
+        assert(nextProducts == expected)
+    }
+}
