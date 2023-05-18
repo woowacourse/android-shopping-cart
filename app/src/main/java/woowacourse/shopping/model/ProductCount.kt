e878428602bf7438cf4201b58b0df05c8c1cfbdc
tmdgh1592 <tmdgh1592@naver.com>
2023-05-18 21:35:46 +0900

    fix(ProductCount): 장바구니 개수가 99개가 넘으면 99개만 보여주도록 변경

--- app/src/main/java/woowacourse/shopping/model/ProductCount.kt
+++ app/src/main/java/woowacourse/shopping/model/ProductCount.kt
@@ -7,8 +7,5 @@ typealias UiProductCount = ProductCount
 
 @Parcelize
 data class ProductCount(val value: Int) : Parcelable {
-    fun toText(): String {
-        if (value > 99) return "99+"
-        return value.toString()
-    }
+    fun toText(): String = if (value > 99) "99" else value.toString()
 }
