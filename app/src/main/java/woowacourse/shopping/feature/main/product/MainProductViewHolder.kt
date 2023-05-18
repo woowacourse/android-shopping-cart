e2a82040342b148b91ca9f1f56260b9ad16c31a5
EmilyCh0 <emily9803@ewhain.net>
2023-05-18 16:42:05 +0900

    feat(MainProductViewHolder): 상품 개수에 따라 visibility 처리

--- app/src/main/java/woowacourse/shopping/feature/main/product/MainProductViewHolder.kt
+++ app/src/main/java/woowacourse/shopping/feature/main/product/MainProductViewHolder.kt
@@ -1,5 +1,6 @@
 package woowacourse.shopping.feature.main.product
 
+import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import woowacourse.shopping.databinding.ItemMainProductBinding
 import woowacourse.shopping.model.ProductUiModel
@@ -9,16 +10,39 @@ class MainProductViewHolder(
     private val mainProductClickListener: MainProductClickListener
 ) : RecyclerView.ViewHolder(binding.root) {
     fun bind(product: ProductUiModel) {
-        binding.product = product
-        binding.countView.count = product.count
-        binding.mainProductLayout.setOnClickListener {
-            mainProductClickListener.onProductClick(product)
+        binding.apply {
+            this.product = product
+            countView.count = product.count
+            setVisibility(product.count != 0)
+
+            mainProductLayout.setOnClickListener {
+                mainProductClickListener.onProductClick(product)
+            }
+            plusFab.setOnClickListener {
+                setVisibility(true)
+                mainProductClickListener.onPlusClick(product, countView.count)
+                countView.count = 1
+            }
+            countView.plusClickListener = {
+                mainProductClickListener.onPlusClick(product, countView.count)
+            }
+            countView.minusClickListener = {
+                val currentCount = countView.count
+                if (currentCount == 1) {
+                    setVisibility(false)
+                }
+                mainProductClickListener.onMinusClick(product, currentCount)
+            }
         }
-        binding.countView.plusClickListener = {
-            mainProductClickListener.onPlusClick(product, binding.countView.count)
-        }
-        binding.countView.minusClickListener = {
-            mainProductClickListener.onMinusClick(product, binding.countView.count)
+    }
+
+    private fun setVisibility(isCountOn: Boolean) {
+        if (!isCountOn) {
+            binding.countView.visibility = View.GONE
+            binding.plusFab.visibility = View.VISIBLE
+        } else {
+            binding.countView.visibility = View.VISIBLE
+            binding.plusFab.visibility = View.GONE
         }
     }
 }
