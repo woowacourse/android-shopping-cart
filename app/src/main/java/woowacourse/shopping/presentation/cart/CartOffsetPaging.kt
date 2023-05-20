53641fc28e814c33c0c599312a706545165ba8ae
chws0508 <chws0508@naver.com>
2023-05-21 02:06:29 +0900

    refactor: 페이징 객체 수정

--- app/src/main/java/woowacourse/shopping/presentation/cart/CartOffsetPaging.kt
+++ app/src/main/java/woowacourse/shopping/presentation/cart/CartOffsetPaging.kt
@@ -6,27 +6,34 @@ import woowacourse.shopping.repository.CartRepository
 import woowacourse.shopping.util.OffsetPaging
 
 class CartOffsetPaging(
+    override val limit: Int = LIMIT,
+    private val startPage: Int = START_PAGE,
     private val cartRepository: CartRepository,
-) : OffsetPaging<CartProductInfo>() {
-
-    override val limit: Int = LIMIT
-    override fun plusPage(): Page {
-        currentPage += PAGE_STEP
-        return currentPage
+) : OffsetPaging<CartProductInfo>(startPage) {
+    fun plusPage() {
+        setPage(currentPage.value.plus(PAGE_STEP))
     }
 
-    override fun minusPage(): Page {
-        currentPage -= PAGE_STEP
-        return currentPage
+    fun minusPage() {
+        setPage(currentPage.value.minus(PAGE_STEP))
     }
 
-    override fun getPageItems(page: Page): List<CartProductInfo> {
+    override fun loadPageItems(page: Page): List<CartProductInfo> {
         return cartRepository.getCartProductsInfo(limit, page.getOffset(limit)).items
     }
 
-    override fun isPlusPageAble(): Boolean = getPageItems(currentPage + 1).isNotEmpty()
+    override fun isPlusPageAble(): Boolean {
+        val page = currentPage.value.plus(PAGE_STEP)
+        return loadPageItems(page).isNotEmpty()
+    }
+
+    override fun isMinusPageAble(): Boolean {
+        val page = currentPage.value
+        return page.value != startPage
+    }
 
     companion object {
+        private const val START_PAGE = 1
         private const val PAGE_STEP = 1
         private const val LIMIT = 5
     }
