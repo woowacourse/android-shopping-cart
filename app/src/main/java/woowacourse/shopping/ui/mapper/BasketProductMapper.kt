1a99c0ab61388a0d8c9e4510a5458b1fd43beeae
changhwan <chang5hustle@gmail.com>
2023-05-21 03:30:29 +0900

    feat(BasketProduct): checked속성 domain 으로 이동

--- app/src/main/java/woowacourse/shopping/ui/mapper/BasketProductMapper.kt
+++ app/src/main/java/woowacourse/shopping/ui/mapper/BasketProductMapper.kt
@@ -4,7 +4,12 @@ import woowacourse.shopping.domain.BasketProduct
 import woowacourse.shopping.ui.model.UiBasketProduct
 
 fun UiBasketProduct.toDomain(): BasketProduct =
-    BasketProduct(id = id, count = count.toDomain(), product = product.toDomain())
+    BasketProduct(
+        id = id,
+        count = count.toDomain(),
+        product = product.toDomain(),
+        checked = checked
+    )
 
 fun BasketProduct.toUi(): UiBasketProduct =
-    UiBasketProduct(id = id, count = count.toUi(), product = product.toUi())
+    UiBasketProduct(id = id, count = count.toUi(), product = product.toUi(), checked = checked)
