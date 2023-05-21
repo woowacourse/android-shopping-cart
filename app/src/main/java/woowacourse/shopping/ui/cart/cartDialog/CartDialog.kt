f33fde9129f31d0279bea0e128581de8d158d660
re4rk <37167652+re4rk@users.noreply.github.com>
2023-05-22 04:18:35 +0900

    refactor: 상세페이지에서 물건을 담을 때 xml로 최소 개수 1개로 설정

--- app/src/main/java/woowacourse/shopping/ui/cart/cartDialog/CartDialog.kt
+++ app/src/main/java/woowacourse/shopping/ui/cart/cartDialog/CartDialog.kt
@@ -1,6 +1,5 @@
 package woowacourse.shopping.ui.cart.cartDialog
 
-import android.annotation.SuppressLint
 import android.app.Dialog
 import android.content.Context
 import android.view.LayoutInflater
@@ -8,7 +7,6 @@ import androidx.databinding.DataBindingUtil
 import woowacourse.shopping.R
 import woowacourse.shopping.databinding.LayoutCartDialogBinding
 
-@SuppressLint("SetTextI18n")
 class CartDialog(context: Context, title: String, price: Int, addToCart: (Int) -> Unit) :
     Dialog(context) {
     private val binding: LayoutCartDialogBinding = DataBindingUtil
@@ -19,7 +17,6 @@ class CartDialog(context: Context, title: String, price: Int, addToCart: (Int) -
 
         binding.productName = title
         binding.productPrice = price
-        binding.cvProductCounter.minCount = 1
         binding.addToCartListener = {
             dismiss()
             addToCart(binding.cvProductCounter.count)
