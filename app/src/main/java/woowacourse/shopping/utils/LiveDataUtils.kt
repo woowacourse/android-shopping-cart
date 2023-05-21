38efa0bb39b80ab8ca138762f994b57dd13368db
re4rk <37167652+re4rk@users.noreply.github.com>
2023-05-22 03:43:27 +0900

    refactor: 라이브 데이터 사용하지 않는 메소드 제거

--- app/src/main/java/woowacourse/shopping/utils/LiveDataUtils.kt
+++ app/src/main/java/woowacourse/shopping/utils/LiveDataUtils.kt
@@ -23,8 +23,4 @@ class NonNullMutableLiveData<T : Any>(value: T) : NonNullLiveData<T>(value) {
     public override fun setValue(value: T) {
         super.setValue(value)
     }
-
-    public override fun postValue(value: T) {
-        super.postValue(value)
-    }
 }
