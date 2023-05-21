f2f189b2f5098fa54e14b14bcbdf44ac67ccca71
changhwan <chang5hustle@gmail.com>
2023-05-21 18:54:49 +0900

    test(count): 테스트 추가작성

--- domain/src/test/java/woowacourse/shopping/domain/CountTest.kt
+++ domain/src/test/java/woowacourse/shopping/domain/CountTest.kt
@@ -1,5 +1,6 @@
 package woowacourse.shopping.domain
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
@@ -7,5 +8,10 @@ class CountTest {
     @Test
     fun `상품 갯수는 음수일 수 없다`() {
         assertThrows<IllegalArgumentException> { Count(-3) }
+    }
+
+    @Test
+    fun `0인지 판별한다`() {
+        assertThat(Count(0).isZero()).isEqualTo(true)
     }
 }
