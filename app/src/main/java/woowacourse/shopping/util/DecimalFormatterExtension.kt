7a18a14927df36debc05d82889b810b3b2049a5e
mendel <rhthrhrl0@naver.com>
2023-05-22 01:45:45 +0900

    remove(DecimalFormatterExtension): 안쓰는 파일 제거

--- app/src/main/java/woowacourse/shopping/util/DecimalFormatterExtension.kt
+++
@@ -1,7 +0,0 @@
-package woowacourse.shopping.util
-
-import java.text.DecimalFormat
-
-private val DECIMAL_FORMATTER = DecimalFormat("#,###")
-
-fun convertToMoneyFormat(money: Int): String = DECIMAL_FORMATTER.format(money)
