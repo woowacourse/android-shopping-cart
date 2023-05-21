dd343e3421ffd676100806506ae2b9bace048283
SangHyun Moon <no1msh1217@gmail.com>
2023-05-21 13:11:38 +0900

    feat: 장바구니 추가 기능 구현
    - 다른 장바구니를 한번에 모두 추가할 수 있다
    - 다른 상품들을 한번에 추가할 수 있다
    - 장바구니의 상품을 제거할 수 있다
    - 장바구니의 상품에서 다른 장바구니 상품들을 한번에 제거할 수 있다

--- domain/src/main/java/com/shopping/domain/Cart.kt
+++ domain/src/main/java/com/shopping/domain/Cart.kt
@@ -1,11 +1,19 @@
 package com.shopping.domain
 
-class Cart(products: List<CartProduct>) {
+class Cart(products: List<CartProduct> = emptyList()) {
     val products: List<CartProduct>
 
     init {
         this.products = products.distinctBy { it.product.id }
     }
+
+    fun addAll(anotherCart: Cart): Cart = Cart(this.products + anotherCart.products)
+
+    fun addAll(cartProducts: List<CartProduct>): Cart = Cart(this.products + cartProducts)
+
+    fun remove(product: CartProduct): Cart = Cart(this.products - product)
+
+    fun removeAll(anotherCart: Cart): Cart = Cart(this.products - anotherCart.products)
 
     fun getPickedProductsTotalPrice(): Int = products
         .filter { it.isPicked }
@@ -23,4 +31,9 @@ class Cart(products: List<CartProduct>) {
     }
 
     fun getTotalPickedProductsCount(): Int = products.filter { it.isPicked }.sumOf { it.count }
+
+    fun isAllPicked(): Boolean = products.count() == products.count { it.isPicked }
+
+    fun setIsPickAllProduct(isPick: Boolean): Cart =
+        Cart(products.map { CartProduct(isPick, it.count, it.product) })
 }
