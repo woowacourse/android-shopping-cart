34f6fe7b7efa7f593ab1ec9e4ef20419bb8012f8
ki960213 <ki960213@gmail.com>
2023-05-21 22:49:49 +0900

    refactor(Counter): 필요없는 코드 제거

--- app/src/main/java/woowacourse/shopping/utils/customview/Counter.kt
+++ app/src/main/java/woowacourse/shopping/utils/customview/Counter.kt
@@ -5,8 +5,6 @@ import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.LinearLayout
 import woowacourse.shopping.databinding.LayoutCounterBinding
-import kotlin.properties.ObservableProperty
-import kotlin.reflect.KProperty
 
 class Counter(
     context: Context,
@@ -14,25 +12,12 @@ class Counter(
 ) : LinearLayout(context, attrs) {
     val binding = LayoutCounterBinding.inflate(LayoutInflater.from(context), this, true)
 
-    var minCount = 1
-    var maxCount = 100
-
-    var count: Int by object : ObservableProperty<Int>(1) {
-        override fun beforeChange(property: KProperty<*>, oldValue: Int, newValue: Int): Boolean {
-            return count in minCount..maxCount
-        }
-        override fun getValue(thisRef: Any?, property: KProperty<*>): Int {
-            return binding.tvCount.text.toString().toInt()
-        }
-        override fun setValue(thisRef: Any?, property: KProperty<*>, value: Int) {
+    var count: Int
+        get() = binding.tvCount.text.toString().toInt()
+        set(value) {
             binding.tvCount.text = value.toString()
         }
-    }
 
     val tvPlus = binding.tvPlus
     val tvMinus = binding.tvMinus
-
-    init {
-        binding.tvCount.text = "1"
-    }
 }
