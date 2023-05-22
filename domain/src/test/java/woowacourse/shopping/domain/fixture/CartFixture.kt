944a27ba4d98fa225a7c33ab476ac58629090566
tmdgh1592 <tmdgh1592@naver.com>
2023-05-22 16:46:57 +0900

    test(PaginationTest): 테스트 코드 작성

--- domain/src/test/java/woowacourse/shopping/domain/fixture/CartFixture.kt
+++ domain/src/test/java/woowacourse/shopping/domain/fixture/CartFixture.kt
@@ -6,7 +6,7 @@ import woowacourse.shopping.domain.model.Price
 import woowacourse.shopping.domain.model.Product
 import woowacourse.shopping.domain.model.ProductCount
 
-internal val UNCHECKED_CARTS: Cart = Cart(
+internal val ALL_UNCHECKED_CARTS: Cart = Cart(
     items = List(100) { id ->
         CartProduct(
             id = id,
@@ -17,13 +17,24 @@ internal val UNCHECKED_CARTS: Cart = Cart(
     }
 )
 
-internal val CHECKED_CARTS: Cart = Cart(
+internal val ALL_CHECKED_CARTS: Cart = Cart(
     items = List(100) { id ->
         CartProduct(
             id = id,
             Product(id = id, name = "상품$id", price = Price(1000), imageUrl = ""),
             selectedCount = ProductCount(1),
             isChecked = true
+        )
+    }
+)
+
+internal fun getCart(size: Int): Cart = Cart(
+    items = List(size) { id ->
+        CartProduct(
+            id = id,
+            Product(id = id, name = "상품$id", price = Price(1000), imageUrl = ""),
+            selectedCount = ProductCount(1),
+            isChecked = false
         )
     }
 )
