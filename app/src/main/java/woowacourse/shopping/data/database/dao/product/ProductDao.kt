e054ba904e3ee62f132f0db21aa26ff0dcfe86e8
tmdgh1592 <tmdgh1592@naver.com>
2023-05-22 22:35:00 +0900

    refactor(ProductDao): 더 이상 사용되지 않으므로 제거

--- app/src/main/java/woowacourse/shopping/data/database/dao/product/ProductDao.kt
+++
@@ -1,7 +0,0 @@
-package woowacourse.shopping.data.database.dao.product
-
-import woowacourse.shopping.data.model.Product
-
-interface ProductDao {
-    fun getPartially(size: Int, lastId: Int): List<Product>
-}
