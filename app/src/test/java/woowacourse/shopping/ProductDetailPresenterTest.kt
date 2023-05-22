3ed791f3d136374fe3643ae403668862dd993ee3
SangHyun Moon <no1msh1217@gmail.com>
2023-05-22 17:30:24 +0900

    test(ProductDetailPresenter): 가장 최근 본 상품을 가져오는 기능 테스트

--- app/src/test/java/woowacourse/shopping/ProductDetailPresenterTest.kt
+++ app/src/test/java/woowacourse/shopping/ProductDetailPresenterTest.kt
@@ -51,16 +51,29 @@ class ProductDetailPresenterTest {
     }
 
     @Test
+    fun `가장 최근 본 상품을 가져온다`() {
+        // given
+        val dummyRecentProduct = RecentProduct(10000L, product)
+        every { recentRepository.getLatestProduct() } returns dummyRecentProduct
+
+        // when
+        presenter.getMostRecentProduct()
+
+        // then
+        verify(exactly = 1) { view.showRecentProduct(any()) }
+    }
+
+    @Test
     fun `장바구니에 추가하는 버튼이 클릭 될 때 데이터를 레포지토리에 저장하고 장바구니 페이지를 보여준다`() {
         // given
-        every { cartRepository.insert(CartProduct(1, product)) } just runs
+        every { cartRepository.insert(CartProduct(true, 1, product)) } just runs
         every { view.showCartPage() } just runs
 
         // when
         presenter.addToCart()
 
         // then
-        verify { cartRepository.insert(CartProduct(1, product)) }
+        verify { cartRepository.insert(CartProduct(true, 1, product)) }
         verify { view.showCartPage() }
     }
 }
