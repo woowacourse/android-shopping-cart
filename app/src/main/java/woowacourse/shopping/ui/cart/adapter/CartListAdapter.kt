04e4bb992ecd38be9909cb38a65a6758a71f70d6
ki960213 <ki960213@gmail.com>
2023-05-25 11:42:16 +0900

    feat(CartListViewHolder): 장바구니 아이템을 보여주는 리사이클러 뷰를 위한 뷰 홀더를 다른 파일로 추출하고 모든 리스너 등록을 뷰 홀더에 데이터를 바인드 할 때가 아니라 뷰 홀더가 생성되었을 때 등록하도록 변경

--- app/src/main/java/woowacourse/shopping/ui/cart/adapter/CartListAdapter.kt
+++ app/src/main/java/woowacourse/shopping/ui/cart/adapter/CartListAdapter.kt
@@ -1,14 +1,9 @@
 package woowacourse.shopping.ui.cart.adapter
 
 import android.annotation.SuppressLint
-import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import woowacourse.shopping.R
-import woowacourse.shopping.databinding.ItemCartBinding
 import woowacourse.shopping.ui.cart.uistate.CartItemUIState
-import woowacourse.shopping.utils.PRICE_FORMAT
 
 class CartListAdapter(
     private val onClickCloseButton: (Long) -> Unit,
@@ -16,7 +11,7 @@ class CartListAdapter(
     private val onClickPlus: (Long) -> Unit,
     private val onClickMinus: (Long) -> Unit,
     private val cartItems: MutableList<CartItemUIState> = mutableListOf()
-) : RecyclerView.Adapter<CartListAdapter.CartListViewHolder>() {
+) : RecyclerView.Adapter<CartListViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CartListViewHolder {
         return CartListViewHolder.create(
@@ -39,55 +34,5 @@ class CartListAdapter(
         this.cartItems.clear()
         this.cartItems.addAll(cartItems)
         notifyDataSetChanged()
-    }
-
-    class CartListViewHolder private constructor(
-        private val binding: ItemCartBinding,
-        private val onClickCloseButton: (Long) -> Unit,
-        private val onClickCheckBox: (Long, Boolean) -> Unit,
-        private val onClickPlus: (Long) -> Unit,
-        private val onClickMinus: (Long) -> Unit
-    ) : RecyclerView.ViewHolder(binding.root) {
-
-        fun bind(cartItem: CartItemUIState) {
-            binding.tvCartName.text = cartItem.name
-            binding.tvCartPrice.text = itemView.context.getString(R.string.product_price).format(
-                PRICE_FORMAT.format(cartItem.price),
-            )
-            Glide.with(itemView)
-                .load(cartItem.imageUrl)
-                .into(binding.ivCart)
-            binding.cbCartItemSelected.setOnCheckedChangeListener { _, isChecked ->
-                onClickCheckBox(cartItem.id, isChecked)
-            }
-            binding.cbCartItemSelected.isChecked = cartItem.isSelected
-            binding.btnCartClose.setOnClickListener {
-                onClickCloseButton(cartItem.id)
-            }
-            binding.counter.tvPlus.setOnClickListener { onClickPlus(cartItem.id) }
-            binding.counter.tvMinus.setOnClickListener { onClickMinus(cartItem.id) }
-            binding.counter.count = cartItem.count
-        }
-
-        companion object {
-            fun create(
-                parent: ViewGroup,
-                onClickCloseButton: (Long) -> Unit,
-                onClickCheckBox: (Long, Boolean) -> Unit,
-                onClickPlus: (Long) -> Unit,
-                onClickMinus: (Long) -> Unit
-            ): CartListViewHolder {
-                val view = LayoutInflater.from(parent.context)
-                    .inflate(R.layout.item_cart, parent, false)
-                val binding = ItemCartBinding.bind(view)
-                return CartListViewHolder(
-                    binding,
-                    onClickCloseButton,
-                    onClickCheckBox,
-                    onClickPlus,
-                    onClickMinus
-                )
-            }
-        }
     }
 }
