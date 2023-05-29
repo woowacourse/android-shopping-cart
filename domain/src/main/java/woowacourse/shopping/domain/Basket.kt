52ba7d36771b6fbf7796692eeae70f90e5881b0f
changhwan <chang5hustle@gmail.com>
2023-05-29 17:34:18 +0900

    refactor(Basket): add,delete 함수 분리

--- domain/src/main/java/woowacourse/shopping/domain/Basket.kt
+++ domain/src/main/java/woowacourse/shopping/domain/Basket.kt
@@ -3,27 +3,42 @@ package woowacourse.shopping.domain
 data class Basket(val products: List<BasketProduct>) {
     fun add(basketProduct: BasketProduct): Basket =
         if (basketProduct in products) Basket(
-            products.map {
-                if (it == basketProduct) BasketProduct(
-                    id = it.id,
-                    count = it.count + basketProduct.count,
-                    product = it.product
-                ) else it
-            }
+            products.map { plusCountTargetBasketProduct(it, basketProduct) }
         )
         else Basket(products + basketProduct)
 
+    private fun plusCountTargetBasketProduct(
+        existingBasketProduct: BasketProduct,
+        target: BasketProduct
+    ): BasketProduct {
+        val processedCount = existingBasketProduct.count + target.count
+        return processTargetBasketProduct(existingBasketProduct, target, processedCount)
+    }
+
     fun delete(basketProduct: BasketProduct): Basket =
         if (basketProduct in products) Basket(
-            products.map {
-                if (it == basketProduct) BasketProduct(
-                    id = it.id,
-                    count = it.count - basketProduct.count,
-                    product = it.product
-                ) else it
-            }.filter { !it.count.isZero() }
+            products.map { minusCountTargetBasketProduct(it, basketProduct) }
+                .filter { !it.count.isZero() }
         )
         else Basket((products - basketProduct).filter { !it.count.isZero() })
+
+    private fun minusCountTargetBasketProduct(
+        existingBasketProduct: BasketProduct,
+        target: BasketProduct
+    ): BasketProduct {
+        val processedCount = existingBasketProduct.count - target.count
+        return processTargetBasketProduct(existingBasketProduct, target, processedCount)
+    }
+
+    private fun processTargetBasketProduct(
+        existingBasketProduct: BasketProduct,
+        target: BasketProduct,
+        processedCount: Count
+    ): BasketProduct = if (existingBasketProduct == target) BasketProduct(
+        id = existingBasketProduct.id,
+        count = processedCount,
+        product = existingBasketProduct.product
+    ) else existingBasketProduct
 
     fun remove(basketProduct: BasketProduct): Basket =
         Basket(products.toMutableList().apply { remove(basketProduct) })
@@ -44,7 +59,8 @@ data class Basket(val products: List<BasketProduct>) {
     }
 
     fun updateCheck(basketProduct: BasketProduct) {
-        products.find { it.product.id == basketProduct.product.id }?.isChecked = basketProduct.isChecked
+        products.find { it.product.id == basketProduct.product.id }?.isChecked =
+            basketProduct.isChecked
     }
 
     fun getCheckedProductsCount(): Int =
