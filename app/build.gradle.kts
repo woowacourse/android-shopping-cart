e723437697cbe9933c8cc5764d4c9484fea4056e
krrong <khk9664@naver.com>
2023-05-30 22:04:11 +0900

    feat(ProductWebServer): MockWebServer를 사용하여 테스트 환경 구성

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,5 +49,10 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("com.github.bumptech.glide:glide:4.15.1")
+
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+    implementation("com.squareup.okhttp3:mockwebserver:4.11.0")
+
+    testImplementation("org.json:json:20140107")
     implementation(project(":domain"))
 }
