9613ecfdcbdf5e11aecd283fa1df0f5a34b4ca91
Yerin <myheaven_b@naver.com>
2023-05-31 02:04:54 +0900

    test: ShoppingCart 프레젠터 테스트 작성

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,6 @@ dependencies {
 
     implementation("com.squareup.okhttp3:okhttp:4.10.0")
     implementation("com.squareup.okhttp3:mockwebserver:4.11.0")
+
+    testImplementation("io.mockk:mockk-android:1.13.5")
 }
