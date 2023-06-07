7631d520cd9ed24ec1ae9baead80dfed7c5a5b7f
krrong <khk9664@naver.com>
2023-06-07 18:57:15 +0900

    refactor(CartProductItem): count를 생성자로 넣어 발생하는 문제를 해결하기 위해 equals, hashCode 함수 재정의

--- app/src/main/java/woowacourse/shopping/feature/list/item/ProductView.kt
+++ app/src/main/java/woowacourse/shopping/feature/list/item/ProductView.kt
@@ -19,6 +19,28 @@ sealed interface ProductView {
             return this
         }
 
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as CartProductItem
+
+            if (id != other.id) return false
+            if (imageUrl != other.imageUrl) return false
+            if (name != other.name) return false
+            if (price != other.price) return false
+
+            return true
+        }
+
+        override fun hashCode(): Int {
+            var result = id
+            result = 31 * result + imageUrl.hashCode()
+            result = 31 * result + name.hashCode()
+            result = 31 * result + price
+            return result
+        }
+
         companion object {
             private const val DEFAULT_COUNT = 1
         }
