6298ef2ca83f4c87fe189ae27cb887e8c0c59ac1
김진우 <jinwoo3661@naver.com>
2024-05-14 22:35:47 +0900

    feat: CartItem repository 적용
    Co-authored-by: JoYehyun99 <jo990714@naver.com>

--- app/src/main/java/woowacourse/shopping/data/repository/ProductRepositoryImpl.kt
+++ app/src/main/java/woowacourse/shopping/data/repository/ProductRepositoryImpl.kt
@@ -1,19 +1,35 @@
 package woowacourse.shopping.data.repository
 
+import android.content.Context
+import woowacourse.shopping.data.db.cartItem.CartItemDatabase
 import woowacourse.shopping.data.db.product.ProductDao
+import woowacourse.shopping.domain.model.CartItem
 import woowacourse.shopping.domain.model.Product
 import woowacourse.shopping.domain.repository.ProductRepository
 import woowacourse.shopping.utils.NoSuchDataException
 
-class ProductRepositoryImpl : ProductRepository {
+class ProductRepositoryImpl(context: Context) : ProductRepository {
     private val productDao = ProductDao()
+    private val cartItemDao = CartItemDatabase.getInstance(context).cartItemDao()
 
-    override fun loadProduct(): List<Product> {
+    override fun loadProducts(): List<Product> {
         return productDao.findAll()
     }
 
     override fun getProduct(productId: Long): Product {
         val product = productDao.findProductById(productId)
         return product ?: throw NoSuchDataException()
+    }
+
+    override fun addCartItem(cartItem: CartItem) {
+        cartItemDao.saveCartItem(cartItem.toCartItemEntity())
+    }
+
+    override fun loadCartItems(): List<CartItem> {
+        return cartItemDao.findAll().map { it.toCartItem() }
+    }
+
+    override fun deleteCartItem(itemId: Long) {
+        cartItemDao.deleteCartItemById(itemId)
     }
 }
--- app/src/main/java/woowacourse/shopping/domain/repository/ProductRepository.kt
+++ app/src/main/java/woowacourse/shopping/domain/repository/ProductRepository.kt
@@ -1,9 +1,16 @@
 package woowacourse.shopping.domain.repository
 
+import woowacourse.shopping.domain.model.CartItem
 import woowacourse.shopping.domain.model.Product
 
 interface ProductRepository {
-    fun loadProduct(): List<Product>
+    fun loadProducts(): List<Product>
 
     fun getProduct(productId: Long) : Product
+
+    fun addCartItem(cartItem: CartItem)
+
+    fun loadCartItems(): List<CartItem>
+
+    fun deleteCartItem(itemId: Long)
 }
