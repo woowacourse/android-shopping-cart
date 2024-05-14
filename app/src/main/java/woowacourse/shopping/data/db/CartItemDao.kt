945d3b7506917c6766c68a2a2cd610e216d49ed3
김진우 <jinwoo3661@naver.com>
2024-05-14 15:46:23 +0900

    feat: CartItem Dao 구현
    Co-authored-by: JoYehyun99 <jo990714@naver.com>

---
+++ app/src/main/java/woowacourse/shopping/data/db/CartItemDao.kt
@@ -0,0 +1,23 @@
+package woowacourse.shopping.data.db
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import woowacourse.shopping.data.db.CartItemDatabase.Companion.CART_ITEMS_DB_NAME
+import woowacourse.shopping.data.model.CartItemEntity
+
+@Dao
+interface CartItemDao {
+    @Insert
+    fun saveCartItem(cartItemEntity: CartItemEntity): Long
+
+    @Query("SELECT * FROM $CART_ITEMS_DB_NAME")
+    fun findAll(): List<CartItemEntity>
+
+    @Query("SELECT * FROM $CART_ITEMS_DB_NAME WHERE id = :itemId")
+    fun findCartItemById(itemId: Long): CartItemEntity?
+
+    @Query("DELETE FROM $CART_ITEMS_DB_NAME WHERE id = :itemId")
+    fun deleteCartItemById(itemId: Long)
+
+}
--- app/src/main/java/woowacourse/shopping/data/model/CartItemEntity.kt
+++ app/src/main/java/woowacourse/shopping/data/model/CartItemEntity.kt
@@ -1,5 +1,8 @@
 package woowacourse.shopping.data.model
 
+import androidx.room.Entity
+import woowacourse.shopping.data.db.CartItemDatabase.Companion.CART_ITEMS_DB_NAME
 import woowacourse.shopping.domain.Product
 
+@Entity(tableName = CART_ITEMS_DB_NAME)
 data class CartItemEntity(val id: Long, val product: Product)
