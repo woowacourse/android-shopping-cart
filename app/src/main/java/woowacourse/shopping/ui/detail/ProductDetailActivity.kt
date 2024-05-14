ac1d4e3979a051d7af95c101f8c9323286bc9e8b
Namyunsuk <yunsuk0328@naver.com>
2024-05-14 17:32:54 +0900

    feat: 상품 상세 정보를 보여주는 기능 구현

--- app/src/main/java/woowacourse/shopping/ui/detail/ProductDetailActivity.kt
+++ app/src/main/java/woowacourse/shopping/ui/detail/ProductDetailActivity.kt
@@ -1,12 +1,45 @@
 package woowacourse.shopping.ui.detail
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.Glide
 import woowacourse.shopping.R
+import woowacourse.shopping.databinding.ActivityProductDetailBinding
+import woowacourse.shopping.model.ProductsImpl
 
 class ProductDetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityProductDetailBinding
+    private val productKey by lazy {
+        intent.getLongExtra(
+            ProductDetailKey.EXTRA_PRODUCT_KEY,
+            EXTRA_DEFAULT_VALUE,
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_detail)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_product_detail)
+        val product = ProductsImpl.find(productKey)
+        binding.product = product
+        Glide.with(this)
+            .load(product.imageUrl)
+            .into(binding.ivProductImage)
+    }
+
+    companion object {
+        private const val EXTRA_DEFAULT_VALUE = -1L
+
+        fun startActivity(
+            context: Context,
+            productId: Long,
+        ) = Intent(context, ProductDetailActivity::class.java).run {
+            putExtra(ProductDetailKey.EXTRA_PRODUCT_KEY, productId)
+            context.startActivity(this)
+        }
     }
 }
