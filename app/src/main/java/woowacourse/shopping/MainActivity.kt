cfec2ae60fe5c0a44f929661419c12d4b0518fe4
somin <salth0601@gmail.com>
2024-05-14 14:59:36 +0900

    feat: AppBar 추가

--- app/src/main/java/woowacourse/shopping/MainActivity.kt
+++ app/src/main/java/woowacourse/shopping/MainActivity.kt
@@ -1,11 +1,24 @@
 package woowacourse.shopping
 
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.viewModels
+import woowacourse.shopping.databinding.ActivityMainBinding
+import woowacourse.shopping.presentation.base.BaseActivity
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class MainActivity : BaseActivity<ActivityMainBinding>() {
+    override val layoutResourceId: Int
+        get() = R.layout.activity_main
+
+    override fun initStartView() {
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return true
     }
 }
