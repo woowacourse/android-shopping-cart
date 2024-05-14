7bf44f6aca0e45796b37d7f8adaeee370d0ca505
ii2001 <iivks2001@gmail.com>
2024-05-14 17:50:31 +0900

    :fire: 불필요한 프로퍼티 삭제

--- app/src/main/java/woowacourse/shopping/productList/ProductListViewModel.kt
+++ app/src/main/java/woowacourse/shopping/productList/ProductListViewModel.kt
@@ -1,14 +1,10 @@
 package woowacourse.shopping.productList
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import woowacourse.shopping.db.Product
 import woowacourse.shopping.repository.DummyProductStore
 
 class ProductListViewModel : ViewModel() {
-    val productImage = MutableLiveData<String>()
-    val productName = MutableLiveData<String>()
-    val productPrice = MutableLiveData<Int>()
     private val dummyProductStore by lazy { DummyProductStore() }
     private var currentIndex = 0
     fun loadProducts(): List<Product> {
