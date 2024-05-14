7815cd3da5df2ba7e427da9faced8ab1f4cc075a
Namyunsuk <yunsuk0328@naver.com>
2024-05-14 21:06:37 +0900

    feat: 장바구니에서 뒤로가기 버튼 구현

--- app/src/main/java/woowacourse/shopping/ui/cart/CartActivity.kt
+++ app/src/main/java/woowacourse/shopping/ui/cart/CartActivity.kt
@@ -3,6 +3,7 @@ package woowacourse.shopping.ui.cart
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import woowacourse.shopping.R
@@ -24,6 +25,15 @@ class CartActivity : AppCompatActivity() {
                 adapter.removeItem(CartsImpl.findAll())
             }
         binding.rvCart.adapter = adapter
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     companion object {
