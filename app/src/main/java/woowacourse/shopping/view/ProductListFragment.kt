6d658c4323051bc4065c630ff580bc2a0d605958
김진우 <jinwoo3661@naver.com>
2024-05-14 18:06:02 +0900

    feat: 상품 목록 ui 구현
    Co-authored-by: JoYehyun99 <jo990714@naver.com>

---
+++ app/src/main/java/woowacourse/shopping/view/ProductListFragment.kt
@@ -0,0 +1,32 @@
+package woowacourse.shopping.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import woowacourse.shopping.R
+import woowacourse.shopping.databinding.FragmentProductListBinding
+
+class ProductListFragment : Fragment() {
+    private var _binding: FragmentProductListBinding? = null
+    val binding: FragmentProductListBinding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentProductListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
