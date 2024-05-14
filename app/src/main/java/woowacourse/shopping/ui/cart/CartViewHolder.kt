b22a99ce4bd759500d586ab51670b2e6da350017
Namyunsuk <yunsuk0328@naver.com>
2024-05-14 20:24:51 +0900

    feat: 장바구니의 상품의 이미지가 보이는 기능 구현

--- app/src/main/java/woowacourse/shopping/ui/cart/CartViewHolder.kt
+++ app/src/main/java/woowacourse/shopping/ui/cart/CartViewHolder.kt
@@ -1,6 +1,7 @@
 package woowacourse.shopping.ui.cart
 
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import woowacourse.shopping.databinding.ItemCartBinding
 import woowacourse.shopping.model.Product
 
@@ -8,5 +9,9 @@ class CartViewHolder(private val binding: ItemCartBinding) :
     RecyclerView.ViewHolder(binding.root) {
     fun bind(product: Product) {
         binding.product = product
+
+        Glide.with(binding.root)
+            .load(product.imageUrl)
+            .into(binding.ivProductImage)
     }
 }
