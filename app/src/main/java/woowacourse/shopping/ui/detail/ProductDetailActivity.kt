d2b79979a4cddeffbde552bde42b29f61f888821
Namyunsuk <yunsuk0328@naver.com>
2024-05-14 16:59:46 +0900

    feat: 상품 상세 ui구현

---
+++ app/src/main/java/woowacourse/shopping/ui/detail/ProductDetailActivity.kt
@@ -0,0 +1,12 @@
+package woowacourse.shopping.ui.detail
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import woowacourse.shopping.R
+
+class ProductDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_detail)
+    }
+}
