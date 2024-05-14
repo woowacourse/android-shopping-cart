e04544548ec56f549b90dc13dfc812220e4a1e2b
김진우 <jinwoo3661@naver.com>
2024-05-14 22:01:52 +0900

    feat: viewModelFactory 적용
    Co-authored-by: JoYehyun99 <jo990714@naver.com>

--- app/src/main/java/woowacourse/shopping/view/MainActivity.kt
+++ app/src/main/java/woowacourse/shopping/view/MainActivity.kt
@@ -3,7 +3,10 @@ package woowacourse.shopping.view
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.viewModelFactory
 import woowacourse.shopping.R
+import woowacourse.shopping.data.repository.ProductRepositoryImpl
 import woowacourse.shopping.databinding.ActivityMainBinding
 import woowacourse.shopping.view.products.ProductListFragment
 
@@ -11,7 +14,8 @@ class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
-    val viewModel: MainViewModel by viewModels()
+    private val viewModelFactory = MainViewModelFactory(ProductRepositoryImpl())
+    val viewModel: MainViewModel = viewModelFactory.create(MainViewModel::class.java)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/woowacourse/shopping/view/MainViewModel.kt
+++ app/src/main/java/woowacourse/shopping/view/MainViewModel.kt
@@ -3,17 +3,20 @@ package woowacourse.shopping.view
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import woowacourse.shopping.data.db.product.ProductDao
 import woowacourse.shopping.domain.model.Product
+import woowacourse.shopping.domain.repository.ProductRepository
 
 class MainViewModel(
-    private val productDao: ProductDao = ProductDao(),
+    private val repository: ProductRepository,
 ): ViewModel() {
     private val _products: MutableLiveData<List<Product>> = MutableLiveData(emptyList())
     val products: LiveData<List<Product>> get() = _products
 
 
     init {
-        _products.value = productDao.findAll()
+        _products.value = repository.loadProduct()
     }
 }
+
