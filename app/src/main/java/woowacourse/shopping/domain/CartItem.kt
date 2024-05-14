5e1d9f3c2adce6d676a3ccea7d04cdc28e033141
Gimun Kim <kmkim7575@gmail.com>
2024-05-14 16:52:57 +0900

    feat: 카트 아이템의 아이디 유효성 검증 로직 추가
    Co-authored-by: haeum808 <haeumueah@gmail.com>

--- app/src/main/java/woowacourse/shopping/domain/CartItem.kt
+++ app/src/main/java/woowacourse/shopping/domain/CartItem.kt
@@ -1,16 +1,19 @@
 package woowacourse.shopping.domain
 
 data class CartItem(
-    val id: Long,
+    val id: Long = 1,
     val productId: Long,
     val quantity: Int = 1,
 ) {
     init {
-        require(quantity >= MINIMUM_QUANTITY) { EXCEPTION_QUANTITY }
+        require(id >= MINIMUM_ID) { EXCEPTION_ILLEGAL_ID }
+        require(quantity >= MINIMUM_QUANTITY) { EXCEPTION_ILLEGAL_QUANTITY }
     }
 
     companion object {
         private const val MINIMUM_QUANTITY = 1
-        private const val EXCEPTION_QUANTITY = "카트 아이템 수량은 최소 ${MINIMUM_QUANTITY}개 이상이어야 한다."
+        private const val MINIMUM_ID = 1
+        private const val EXCEPTION_ILLEGAL_QUANTITY = "카트 아이템 수량은 최소 ${MINIMUM_QUANTITY}개 이상이어야 한다."
+        private const val EXCEPTION_ILLEGAL_ID = "카트 아이템 아이디는 최소 ${MINIMUM_ID}이상이어야 한다."
     }
 }
--- app/src/test/java/woowacourse/shopping/CartItemTest.kt
+++ app/src/test/java/woowacourse/shopping/CartItemTest.kt
@@ -15,4 +15,14 @@ class CartItemTest {
         // then
         assertThat(actualMessage).isEqualTo("카트 아이템 수량은 최소 1개 이상이어야 한다.")
     }
+
+    @Test
+    fun `장바구니로 담은 상품의 아이디가 유효하지 않는 아이디라면 예외를 발생시킨다`() {
+        // given
+        // when
+        val actualMessage = assertThrows<IllegalArgumentException> { CartItem(0, 1, 1) }.message
+
+        // then
+        assertThat(actualMessage).isEqualTo("카트 아이템 아이디는 최소 1이상이어야 한다.")
+    }
 }
