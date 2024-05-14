6a0b1a0abcdc1b4fb0b11ea8105c8224218dc3a7
Namyunsuk <yunsuk0328@naver.com>
2024-05-14 14:58:58 +0900

    feat: 상품 목록 화면 ui구현

--- app/src/main/java/woowacourse/shopping/ProductContentsActivity.kt
+++ app/src/main/java/woowacourse/shopping/ProductContentsActivity.kt
@@ -3,9 +3,9 @@ package woowacourse.shopping
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class ProductContentsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_product_contents)
     }
 }
