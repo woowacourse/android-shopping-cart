bfaa747d1c89f81f89739f9dcfdaded8324649ec
Junyoung-WON <wjyo60@gmail.com>
2024-05-15 17:15:46 +0900

    feat: 장바구니 화면 파일 추가 및 임시 작성

---
+++ app/src/main/java/woowacourse/shopping/shoppingcart/ShoppingCartActivity.kt
@@ -0,0 +1,31 @@
+package woowacourse.shopping.shoppingcart
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import woowacourse.shopping.databinding.ActivityShoppingCartBinding
+
+class ShoppingCartActivity: AppCompatActivity() {
+    private lateinit var binding: ActivityShoppingCartBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityShoppingCartBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    companion object {
+        private const val EXTRA_PRODUCT_ID = "productId"
+
+        fun newInstance(
+            context: Context,
+            productId: Long,
+        ) = Intent(context, ShoppingCartActivity::class.java).apply {
+            this.putExtra(
+                EXTRA_PRODUCT_ID,
+                productId,
+            )
+        }
+    }
+}
