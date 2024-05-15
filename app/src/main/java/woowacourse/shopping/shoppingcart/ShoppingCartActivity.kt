d62ab955243e796b9079ef53712d775a68f33fa7
gaeun <geun5744@gmail.com>
2024-05-15 21:09:18 +0900

    feat: ShoppingCartActivity 데이터 로딩 기능 구현

--- app/src/main/java/woowacourse/shopping/shoppingcart/ShoppingCartActivity.kt
+++ app/src/main/java/woowacourse/shopping/shoppingcart/ShoppingCartActivity.kt
@@ -3,29 +3,67 @@ package woowacourse.shopping.shoppingcart
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import woowacourse.shopping.databinding.ActivityShoppingCartBinding
+import woowacourse.shopping.util.ViewModelFactory
 
-class ShoppingCartActivity : AppCompatActivity() {
+class ShoppingCartActivity : AppCompatActivity(), ShoppingCartClickAction {
     private lateinit var binding: ActivityShoppingCartBinding
+    private lateinit var adapter: ShoppingCartAdapter
+
+    private val viewModel: ShoppingCartViewModel by viewModels { ViewModelFactory() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityShoppingCartBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.lifecycleOwner = this
+        binding.vm = viewModel
+
+        initShoppingCart()
+
+        adapter = ShoppingCartAdapter(this)
+        binding.rcvShoppingCart.adapter = adapter
+
+        updateView()
+    }
+
+    private fun initShoppingCart() {
+        viewModel.loadCartItems(DEFAULT_CURRENT_PAGE)
+        viewModel.updatePageSize()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = "Cart"
+    }
+
+    private fun updateView() {
+        viewModel.cartItems.observe(this) { cartItems ->
+            adapter.submitList(cartItems)
+        }
+
+        viewModel.currentPage.observe(this) { currentPage ->
+            binding.btnShoppingCartLeft.isEnabled = !currentPage.equals(DEFAULT_CURRENT_PAGE)
+            binding.btnShoppingCartRight.isEnabled = !currentPage.equals(viewModel.totalSize.value)
+            viewModel.loadCartItems(currentPage)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onItemRemoveBtnClicked(id: Long) {
+        viewModel.deleteCartItem(id)
+        viewModel.updatePageSize()
     }
 
     companion object {
-        private const val EXTRA_PRODUCT_ID = "productId"
+        private const val DEFAULT_CURRENT_PAGE = 1
 
-        fun newInstance(
-            context: Context,
-            productId: Long,
-        ) = Intent(context, ShoppingCartActivity::class.java).apply {
-            this.putExtra(
-                EXTRA_PRODUCT_ID,
-                productId,
-            )
-        }
+        fun newInstance(context: Context) = Intent(context, ShoppingCartActivity::class.java)
     }
 }
