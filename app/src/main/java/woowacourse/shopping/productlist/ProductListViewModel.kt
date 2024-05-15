0b8a6e50ff975610475d1318b76dd92b5c6b9b02
Junyoung-WON <wjyo60@gmail.com>
2024-05-15 17:12:28 +0900

    feat: 상품 목록 화면의 ViewModel 및 Factory 생성

---
+++ app/src/main/java/woowacourse/shopping/productlist/ProductListViewModel.kt
@@ -0,0 +1,25 @@
+package woowacourse.shopping.productlist
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import woowacourse.shopping.ShoppingRepository
+import woowacourse.shopping.domain.Product
+
+class ProductListViewModel(
+    private val repository: ShoppingRepository
+): ViewModel() {
+    private val _products: MutableLiveData<List<Product>> = MutableLiveData()
+    val products: LiveData<List<Product>> get() = _products
+
+    fun loadProducts() {
+        runCatching {
+            repository.products()
+        }.onSuccess {
+            _products.value = it
+        }.onFailure {
+            Log.d(this::class.java.simpleName, "$it")
+        }
+    }
+}
--- app/src/main/java/woowacourse/shopping/util/ViewModelFactory.kt
+++ app/src/main/java/woowacourse/shopping/util/ViewModelFactory.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import woowacourse.shopping.DummyShoppingRepository
 import woowacourse.shopping.productdetail.ProductDetailViewModel
+import woowacourse.shopping.productlist.ProductListViewModel
 
 class ViewModelFactory : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
@@ -12,6 +13,12 @@ class ViewModelFactory : ViewModelProvider.Factory {
                 val repository = DummyShoppingRepository
                 ProductDetailViewModel(repository) as T
             }
+
+            modelClass.isAssignableFrom(ProductListViewModel::class.java) -> {
+                val repository = DummyShoppingRepository
+                ProductListViewModel(repository) as T
+            }
+
             else -> error("Failed to create ViewModel : ${modelClass.name}")
         }
     }
