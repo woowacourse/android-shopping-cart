534513d2b5e6c05e84b42f96551f442606df618f
JunJangE <wnswkd486@gmail.com>
2024-05-15 21:38:03 +0900

    feat: BindingActivity 메시지 전달 메서드 구현

--- app/src/main/java/woowacourse/shopping/presentation/base/BindingActivity.kt
+++ app/src/main/java/woowacourse/shopping/presentation/base/BindingActivity.kt
@@ -1,14 +1,19 @@
 package woowacourse.shopping.presentation.base
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
+import com.google.android.material.snackbar.Snackbar
 
 abstract class BindingActivity<T : ViewDataBinding> : AppCompatActivity() {
     abstract val layoutResourceId: Int
     private var _binding: T? = null
     val binding get() = requireNotNull(_binding)
+
+    private var toast: Toast? = null
+    private var snackbar: Snackbar? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,8 +23,25 @@ abstract class BindingActivity<T : ViewDataBinding> : AppCompatActivity() {
 
     abstract fun initStartView()
 
+    fun showToastMessage(message: String) {
+        toast?.cancel()
+        toast = Toast.makeText(this, message, Toast.LENGTH_SHORT)
+        toast?.show()
+    }
+
+    fun showSnackbar(
+        message: String,
+        action: Snackbar.() -> Snackbar = { this },
+    ) {
+        snackbar?.dismiss()
+        snackbar = Snackbar.make(binding.root, message, Snackbar.LENGTH_SHORT).action()
+        snackbar?.show()
+    }
+
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+        toast = null
+        snackbar = null
     }
 }
