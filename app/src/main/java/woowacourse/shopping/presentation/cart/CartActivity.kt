31d45a35553c82145ca4d68eb3bf02ff9a8ea21a
Gimun Kim <kmkim7575@gmail.com>
2024-05-15 17:04:35 +0900

    feat: 장바구니 화면에서 이전 화면으로 이동하는 기능 구현
    Co-authored-by: haeum808 <haeumueah@gmail.com>

--- app/src/main/java/woowacourse/shopping/presentation/cart/CartActivity.kt
+++ app/src/main/java/woowacourse/shopping/presentation/cart/CartActivity.kt
@@ -4,12 +4,21 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import woowacourse.shopping.R
+import woowacourse.shopping.databinding.ActivityCartBinding
 
 class CartActivity : AppCompatActivity() {
+    private val binding: ActivityCartBinding by lazy {
+        DataBindingUtil.setContentView(this, R.layout.activity_cart)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cart)
+        setSupportActionBar(binding.toolbarCart)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_back)
     }
 
     companion object {
