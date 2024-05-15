8e6f1c2de909c8fed246bc94f02c381781ae138c
gaeun <geun5744@gmail.com>
2024-05-15 11:39:04 +0900

    style: ktlint 적용

--- app/src/main/java/woowacourse/shopping/domain/Product.kt
+++ app/src/main/java/woowacourse/shopping/domain/Product.kt
@@ -2,7 +2,7 @@ package woowacourse.shopping.domain
 
 data class Product(
     val id: Long,
-    val imageUrl: ImageUrl,
     val name: String,
     val price: Price,
+    val imageUrl: ImageUrl,
 )
