b3983919a470b77eecef461c417593ba09510da1
JunJangE <wnswkd486@gmail.com>
2024-05-15 22:08:26 +0900

    chore: StateViewModel 필요없는 코드 제거

--- app/src/main/java/woowacourse/shopping/presentation/base/StateViewModel.kt
+++ app/src/main/java/woowacourse/shopping/presentation/base/StateViewModel.kt
@@ -5,19 +5,8 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 abstract class StateViewModel : ViewModel() {
-    private val _loading: MutableLiveData<Event<Boolean>> = MutableLiveData()
-    val loading: LiveData<Event<Boolean>> get() = _loading
-
     private val _message: MutableLiveData<Event<MessageType>> = MutableLiveData()
     val message: LiveData<Event<MessageType>> get() = _message
-
-    fun showLoading() {
-        _loading.emit(false)
-    }
-
-    fun dismissLoading() {
-        _loading.emit(true)
-    }
 
     fun showMessage(messageType: MessageType) {
         _message.emit(messageType)
