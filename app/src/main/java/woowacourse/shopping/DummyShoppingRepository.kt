7fc4eb6ed8ac5772ea88894f687991038f91c3cb
Junyoung-WON <wjyo60@gmail.com>
2024-05-15 17:11:27 +0900

    feat: 임시 상품 데이터 추가

--- app/src/main/java/woowacourse/shopping/DummyShoppingRepository.kt
+++ app/src/main/java/woowacourse/shopping/DummyShoppingRepository.kt
@@ -52,6 +52,42 @@ object DummyShoppingRepository : ShoppingRepository {
                 Price(12000),
                 ImageUrl("https://github.com/woowacourse/android-movie-theater/assets/92314556/0e4959a4-08a7-40e4-a876-a832495233be"),
             ),
+            Product(
+                6,
+                "PET보틀-납작(260ml)",
+                Price(61800),
+                ImageUrl("https://github.com/woowacourse/android-movie-theater/assets/92314556/fed0b624-737a-4e4a-be3e-2e8a8cd953fc"),
+            ),
+            Product(
+                7,
+                "PET보틀-원형(200ml)",
+                Price(88800),
+                ImageUrl("https://github.com/woowacourse/android-movie-theater/assets/92314556/015018f9-f668-4e71-94c8-cb214f034321"),
+            ),
+            Product(
+                8,
+                "PET보틀-정사각(370ml)",
+                Price(41000),
+                ImageUrl("https://github.com/woowacourse/android-movie-theater/assets/92314556/0d14a4d3-312d-4688-8699-f2c269c70ce5"),
+            ),
+            Product(
+                9,
+                "PET보틀-밀크티(250ml)",
+                Price(12000),
+                ImageUrl("https://github.com/woowacourse/android-movie-theater/assets/92314556/11533147-d5b7-4d41-9668-03d8b5dcbb7b"),
+            ),
+            Product(
+                10,
+                "PET보틀-단지(100ml)",
+                Price(10000),
+                ImageUrl("https://github.com/woowacourse/android-movie-theater/assets/92314556/0277eaa6-f0a4-4be6-8b9d-7b732cd411f4"),
+            ),
+            Product(
+                11,
+                "PET보틀-단지(150ml)",
+                Price(12000),
+                ImageUrl("https://github.com/woowacourse/android-movie-theater/assets/92314556/0e4959a4-08a7-40e4-a876-a832495233be"),
+            )
         )
 
     override fun products(): List<Product> = products
