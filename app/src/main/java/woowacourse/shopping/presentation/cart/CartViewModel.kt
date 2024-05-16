a602304a8b2c9da4da7f5b55e9d0f825a54ad75d
Hogu59 <dbstjdguswkd@naver.com>
2024-05-16 12:16:38 +0900

    refactor(CartViewModel): 상수화, 네이밍 변경

--- app/src/main/java/woowacourse/shopping/presentation/cart/CartViewModel.kt
+++ app/src/main/java/woowacourse/shopping/presentation/cart/CartViewModel.kt
@@ -7,57 +7,65 @@ import woowacourse.shopping.domain.model.CartItem
 import woowacourse.shopping.domain.repository.CartRepository
 
 class CartViewModel(private val repository: CartRepository) : ViewModel() {
-    private val pageSize = 5
-    
+    private val pageSize = PAGE_SIZE
+
     private val _shoppingCart = MutableLiveData<List<CartItem>>()
     val shoppingCart: LiveData<List<CartItem>> = _shoppingCart
-    
+
     private val _currentPage = MutableLiveData(0)
     val currentPage: LiveData<Int> = _currentPage
-    
+
     private val _isFirstPage = MutableLiveData(true)
     val isFirstPage: LiveData<Boolean> = _isFirstPage
-    
+
     private val _isLastPage = MutableLiveData(false)
     val isLastPage: LiveData<Boolean> = _isLastPage
-    
+
     private val _isPageControlVisible = MutableLiveData<Boolean>()
     val isPageControlVisible: LiveData<Boolean> = _isPageControlVisible
-    
+
+    private var lastPage: Int = DEFAULT_PAGE
+
     init {
-        loadPage(_currentPage.value ?: 0)
+        loadPage(_currentPage.value ?: DEFAULT_PAGE)
     }
-    
-    private fun updatePageControlVisibility() {
-        val totalItems = repository.findAll().items.size
+
+    private fun updatePageControlVisibility(totalItems: Int) {
         _isPageControlVisible.postValue(totalItems > pageSize)
     }
-    
+
     private fun loadPage(page: Int) {
+        // TODO: item의 size를 가져오는 방법 변경
         val totalItems = repository.findAll().items.size
-        val maxPage = totalItems / pageSize
-        
-        _currentPage.value = page.coerceIn(0, maxPage)
-        _isFirstPage.value = _currentPage.value == 0
-        _isLastPage.value = _currentPage.value == maxPage
-        
-        val shoppingCart = repository.findAllPagedItems(_currentPage.value ?: 0, pageSize)
+        lastPage = (totalItems - PAGE_STEP) / pageSize
+
+        _currentPage.value = page.coerceIn(0, lastPage)
+        _isFirstPage.value = _currentPage.value == DEFAULT_PAGE
+        _isLastPage.value = _currentPage.value == lastPage
+
+        val shoppingCart = repository.findAllPagedItems(_currentPage.value ?: DEFAULT_PAGE, pageSize)
         _shoppingCart.postValue(shoppingCart.items)
-        updatePageControlVisibility()
+        updatePageControlVisibility(totalItems)
     }
-    
+
     fun loadNextPage() {
-        val nextPage = (_currentPage.value ?: 0) + 1
+        val nextPage = (_currentPage.value ?: DEFAULT_PAGE) + PAGE_STEP
         loadPage(nextPage)
     }
-    
+
     fun loadPreviousPage() {
-        val prevPage = (_currentPage.value ?: 0) - 1
+        val prevPage = (_currentPage.value ?: DEFAULT_PAGE) - PAGE_STEP
         loadPage(prevPage)
     }
-    
+
     fun deleteItem(itemId: Long) {
         repository.delete(itemId)
-        loadPage(_currentPage.value ?: 0)
+        loadPage(_currentPage.value ?: DEFAULT_PAGE)
+    }
+
+    companion object {
+        private const val PAGE_SIZE = 5
+        private const val DEFAULT_PAGE = 0
+        private const val PAGE_STEP = 1
     }
 }
