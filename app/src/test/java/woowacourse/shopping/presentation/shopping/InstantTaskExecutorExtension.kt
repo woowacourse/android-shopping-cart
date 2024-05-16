690e9bfe4ceba8155694596b483c93d03a9982a8
Hogu59 <dbstjdguswkd@naver.com>
2024-05-16 15:25:21 +0900

    test: 뷰모델 데스트 환경 세팅

--- app/src/test/java/woowacourse/shopping/presentation/shopping/InstantTaskExecutorExtension.kt
+++ app/src/test/java/woowacourse/shopping/presentation/shopping/InstantTaskExecutorExtension.kt
@@ -1,4 +1,4 @@
-package woowacourse.shopping
+package woowacourse.shopping.presentation.shopping
 
 import androidx.arch.core.executor.ArchTaskExecutor
 import androidx.arch.core.executor.TaskExecutor
--- app/src/test/java/woowacourse/shopping/presentation/shopping/getOrAwaitValue.kt
+++ app/src/test/java/woowacourse/shopping/presentation/shopping/getOrAwaitValue.kt
@@ -1,4 +1,4 @@
-package woowacourse.shopping
+package woowacourse.shopping.presentation.shopping
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
