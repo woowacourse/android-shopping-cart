94588c9e493c04ab9832923673afcd44276185d3
김진우 <jinwoo3661@naver.com>
2024-05-16 17:13:00 +0900

    test: ShoppingCartFragment 테스트
    Co-authored-by: JoYehyun99 <jo990714@naver.com>

--- app/src/androidTest/java/woowacourse/shopping/TestFixture.kt
+++ app/src/androidTest/java/woowacourse/shopping/TestFixture.kt
@@ -1,9 +1,22 @@
 package woowacourse.shopping
 
 import woowacourse.shopping.data.db.cartItem.CartItemDatabase
+import woowacourse.shopping.data.model.CartItemEntity
+import woowacourse.shopping.domain.model.Product
 
 object TestFixture {
     fun CartItemDatabase.deleteAll() {
         this.openHelper.writableDatabase.execSQL("DELETE FROM ${CartItemDatabase.CART_ITEMS_DB_NAME}")
+    }
+
+    fun makeCartItemEntity(): CartItemEntity{
+        return CartItemEntity(
+            product = Product(
+                imageUrl = "",
+                price = 10000,
+                name = "아메리카노",
+                id = 0L,
+            )
+        )
     }
 }
