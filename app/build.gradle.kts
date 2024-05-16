055b2b661a4f41bb2a7ee4dba4152d456f259ed3
murjune <sis9221302@gmail.com>
2024-05-16 17:00:48 +0900

    test(ProductListFragmentTest): 상품 더보기 테스트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,7 @@ dependencies {
     testImplementation("org.assertj:assertj-core:3.25.3")
     testImplementation("io.kotest:kotest-runner-junit5:5.8.0")
     // instrument test
+    debugImplementation("androidx.fragment:fragment-testing:1.6.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation("androidx.test.espresso:espresso-contrib:3.5.1")
