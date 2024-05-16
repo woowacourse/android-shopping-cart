240f715446bb329b78f829ac6dba753623a2cdca
김진우 <jinwoo3661@naver.com>
2024-05-16 16:57:17 +0900

    test: ProductListFragment 테스트
    Co-authored-by: JoYehyun99 <jo990714@naver.com>

--- app/src/androidTest/java/woowacourse/shopping/data/view/ProductListFragmentTest.kt
+++ app/src/androidTest/java/woowacourse/shopping/data/view/ProductListFragmentTest.kt
@@ -2,42 +2,44 @@ package woowacourse.shopping.data.view
 
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.CoreMatchers.not
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import woowacourse.shopping.R
 import woowacourse.shopping.view.MainActivity
-import woowacourse.shopping.view.detail.ProductDetailFragment
+import woowacourse.shopping.view.products.ProductsListFragment
 
 @RunWith(AndroidJUnit4::class)
-class ProductDetailFragmentTest {
+class ProductListFragmentTest {
     @get:Rule
     val activityRule = ActivityScenarioRule(MainActivity::class.java)
 
     @Before
     fun setUp() {
-        val productDetailFragment = ProductDetailFragment().apply {
-            arguments = ProductDetailFragment.createBundle(0L)
-        }
         activityRule.scenario.onActivity { activity ->
             activity.supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, productDetailFragment)
+                .replace(R.id.fragment_container, ProductsListFragment())
                 .commitNow()
         }
     }
 
     @Test
-    fun `선택한_상품의_이름이_보여져야_한다`() {
-        onView(withId(R.id.tv_detail_product_name)).check(matches(withText("PET보틀-단지(400ml) 레몬청")))
+    fun `상품_목록을_보여준다`() {
+        onView(withId(R.id.rv_products))
+            .check(matches(isDisplayed()))
     }
 
     @Test
-    fun `선택한_상품의_가격이_보여져야_한다`() {
-        onView(withId(R.id.tv_detail_product_price)).check(matches(withText("10,000원")))
+    fun `데이터가_모두_로드되지_않은_경우_더보기_버튼은_보여지지_않아야_한다`() {
+        onView(withId(R.id.btn_more_product))
+            .check(matches(not(isDisplayed())))
     }
+
 }
+
