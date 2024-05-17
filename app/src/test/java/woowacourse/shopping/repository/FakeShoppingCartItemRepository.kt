9f06a66f09cf94c0e3fb4224cd314b08bdce129c
sh1mj1 <7wlgns@gmail.com>
2024-05-17 18:01:58 +0900

    :sparkles: feat(FakeShoppingCartItemRepository): 테스트에 사용하는 가짜 객체

---
+++ app/src/test/java/woowacourse/shopping/repository/FakeShoppingCartItemRepository.kt
@@ -0,0 +1,31 @@
+package woowacourse.shopping.repository
+
+import woowacourse.shopping.FiveCartItemPagingStrategy
+import woowacourse.shopping.PagingStrategy
+import woowacourse.shopping.db.Product
+
+class FakeShoppingCartItemRepository(
+    private val cartItems: MutableList<Product> = mutableListOf(),
+    private val pagingStrategy: PagingStrategy<Product> = FiveCartItemPagingStrategy(),
+) : ShoppingCartItemRepository {
+    override fun addCartItem(product: Product): Int {
+        val addedProduct = product.copy(id = cartItems.size + 1)
+        cartItems.add(addedProduct)
+        return addedProduct.id
+    }
+
+    override fun findById(id: Int): Product =
+        cartItems.find { it.id == id } ?: throw NoSuchElementException("there is no product with id: $id")
+
+    override fun loadPagedCartItems(page: Int): List<Product> = pagingStrategy.loadPagedData(page, cartItems)
+
+    override fun removeCartItem(productId: Int): Product {
+        val product = cartItems.find { it.id == productId } ?: throw NoSuchElementException()
+        cartItems.remove(product)
+        return product
+    }
+
+    override fun clearAllCartItems() {
+        cartItems.clear()
+    }
+}
