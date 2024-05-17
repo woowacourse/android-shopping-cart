398a5e7208096036a4e970061c981cb8958bab2d
김진우 <jinwoo3661@naver.com>
2024-05-17 10:59:57 +0900

    refactor: ViewModelFactory 재구성

--- app/src/main/java/woowacourse/shopping/view/MainActivity.kt
+++ app/src/main/java/woowacourse/shopping/view/MainActivity.kt
@@ -7,14 +7,13 @@ import woowacourse.shopping.data.repository.ProductRepositoryImpl
 import woowacourse.shopping.databinding.ActivityMainBinding
 import woowacourse.shopping.view.products.ProductsListFragment
 import woowacourse.shopping.view.viewmodel.MainViewModel
-import woowacourse.shopping.view.viewmodel.MainViewModelFactory
 
 class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
     val viewModel: MainViewModel by lazy {
-        val viewModelFactory = MainViewModelFactory(ProductRepositoryImpl(context = this@MainActivity))
+        val viewModelFactory = ViewModelFactory{MainViewModel(ProductRepositoryImpl(context = this@MainActivity))}
         viewModelFactory.create(MainViewModel::class.java)
     }
 
