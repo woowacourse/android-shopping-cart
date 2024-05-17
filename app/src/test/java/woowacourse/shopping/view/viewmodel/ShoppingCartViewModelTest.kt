4b37d19b214961e2756dd56ec71d6b8d480ec8f2
김진우 <jinwoo3661@naver.com>
2024-05-17 15:28:45 +0900

    test: ShoppingCartViewModel 테스트 구현

--- app/src/test/java/woowacourse/shopping/view/viewmodel/ShoppingCartViewModelTest.kt
+++ app/src/test/java/woowacourse/shopping/view/viewmodel/ShoppingCartViewModelTest.kt
@@ -1,48 +1,49 @@
 package woowacourse.shopping.view.viewmodel
 
-import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import woowacourse.shopping.InstantTaskExecutorExtension
 import woowacourse.shopping.MockProductRepository
+import woowacourse.shopping.MockShoppingCartRepository
 import woowacourse.shopping.TestFixture.getOrAwaitValue
-import woowacourse.shopping.domain.model.CartItem
-import woowacourse.shopping.domain.model.Product
 import woowacourse.shopping.domain.repository.ProductRepository
+import woowacourse.shopping.domain.repository.ShoppingCartRepository
+import woowacourse.shopping.view.cart.ShoppingCartViewModel
 
 @ExtendWith(InstantTaskExecutorExtension::class)
-class MainViewModelTest {
-    private lateinit var repository: ProductRepository
-    private lateinit var viewModel: MainViewModel
+class ShoppingCartViewModelTest {
+    private lateinit var shoppingCartRepository: ShoppingCartRepository
+    private lateinit var viewModel: ShoppingCartViewModel
 
     @BeforeEach
     fun setUp() {
-        repository = MockProductRepository()
-        viewModel = MainViewModel(repository)
+        shoppingCartRepository = MockShoppingCartRepository()
+        viewModel = ShoppingCartViewModel(shoppingCartRepository)
     }
 
 
     @Test
     fun `offset을_기준으로_장바구니_리스트를_요청하면_장바구니_정해진_개수만큼_반환해야_한다`() {
         val before = viewModel.shoppingCart.cartItems.getOrAwaitValue()
-        assertThat(before.size).isEqualTo(0)
+        Assertions.assertThat(before.size).isEqualTo(0)
 
         viewModel.loadPagingCartItem(3)
 
         val result = viewModel.shoppingCart.cartItems.getOrAwaitValue()
-        assertThat(result.size).isEqualTo(3)
+        Assertions.assertThat(result.size).isEqualTo(3)
     }
 
     @Test
     fun `장바구니_id로_장바구니_목록을_삭제하면_전체_상품에서_해당_id와_일치하는_아이템이_삭제되어야_한다`() {
         viewModel.loadPagingCartItem(3)
         val before = viewModel.shoppingCart.cartItems.getOrAwaitValue()
-        assertThat(before.size).isEqualTo(3)
+        Assertions.assertThat(before.size).isEqualTo(3)
 
         viewModel.deleteShoppingCartItem(0)
 
         val result = viewModel.shoppingCart.cartItems.getOrAwaitValue()
-        assertThat(result.size).isEqualTo(2)
+        Assertions.assertThat(result.size).isEqualTo(2)
     }
 }
