7321e57e0fc8d1c5523b39262a2c9eb07ed32ea7
hevton <hevton22@gmail.com>
2024-05-17 15:17:44 +0900

    test(ProductDetailActivityTest): 장바구니 담기 테스트 구현

--- app/src/androidTest/java/woowacourse/shopping/presentation/ui/detail/ProductDetailActivityTest.kt
+++ app/src/androidTest/java/woowacourse/shopping/presentation/ui/detail/ProductDetailActivityTest.kt
@@ -3,16 +3,21 @@ package woowacourse.shopping.presentation.ui.detail
 import android.content.Intent
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import io.kotest.matchers.shouldBe
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import woowacourse.shopping.R
+import woowacourse.shopping.data.remote.DummyCartRepository
 import woowacourse.shopping.presentation.ui.detail.ProductDetailActivity.Companion.EXTRA_PRODUCT_ID
+import woowacourse.shopping.presentation.ui.dummyProduct
+import java.lang.IllegalStateException
 
 @RunWith(AndroidJUnit4::class)
 class ProductDetailActivityTest {
@@ -44,5 +49,15 @@ class ProductDetailActivityTest {
         onView(withId(R.id.tv_price_value)).check(
             matches(isDisplayed()),
         )
+    }
+
+    @Test
+    fun `장바구니에_담으면_상품이_데이터에_추가된다`() {
+        onView(withId(R.id.tv_add_cart)).perform(click())
+        DummyCartRepository.load(0, 1).onSuccess {
+            it.size shouldBe 1
+        }.onFailure {
+            throw IllegalStateException()
+        }
     }
 }
