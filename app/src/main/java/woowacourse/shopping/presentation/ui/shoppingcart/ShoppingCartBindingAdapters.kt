2c353d8017b5c4445e7e34e93829e3d6fc82f01e
JunJangE <wnswkd486@gmail.com>
2024-05-18 12:24:46 +0900

    refactor: 바인딩 어댑터로 구현한 로직을 데이터 바인딩 표현식으로 구현

--- app/src/main/java/woowacourse/shopping/presentation/ui/shoppingcart/ShoppingCartBindingAdapters.kt
+++ app/src/main/java/woowacourse/shopping/presentation/ui/shoppingcart/ShoppingCartBindingAdapters.kt
@@ -3,30 +3,6 @@ package woowacourse.shopping.presentation.ui.shoppingcart
 import android.view.View
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
-import androidx.recyclerview.widget.RecyclerView
-import woowacourse.shopping.domain.model.Order
-
-@BindingAdapter("recyclerViewVisible")
-fun RecyclerView.bindRecyclerViewVisible(orderList: List<Order>?) {
-    orderList?.let { items ->
-        if (items.isEmpty()) {
-            this.visibility = View.GONE
-        } else {
-            this.visibility = View.VISIBLE
-        }
-    }
-}
-
-@BindingAdapter("emptyTextVisible")
-fun TextView.binEmptyTextVisible(orderList: List<Order>?) {
-    orderList?.let { items ->
-        if (items.isEmpty()) {
-            this.visibility = View.VISIBLE
-        } else {
-            this.visibility = View.GONE
-        }
-    }
-}
 
 @BindingAdapter("currentPage", "last")
 fun TextView.bindPageNavigationTextVisible(
