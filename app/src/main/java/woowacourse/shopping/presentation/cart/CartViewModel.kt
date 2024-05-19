f3bf7a934f164146427b2dac9078fef568728c1c
kkosang <eiwoqp77@gmail.com>
2024-05-19 23:32:58 +0900

    refactor: 페이지네이션 관리 로직 수정

--- app/src/main/java/woowacourse/shopping/presentation/cart/CartViewModel.kt
+++ app/src/main/java/woowacourse/shopping/presentation/cart/CartViewModel.kt
@@ -4,60 +4,42 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.map
 import woowacourse.shopping.data.cart.CartRepository
 import woowacourse.shopping.presentation.base.BaseViewModelFactory
 
 class CartViewModel(
     private val cartRepository: CartRepository,
 ) : ViewModel() {
-    private val _products = MutableLiveData<List<CartProductUi>>(emptyList())
-    val products: LiveData<List<CartProductUi>> get() = _products
 
     private val _currentPage = MutableLiveData(START_PAGE)
     val currentPage: LiveData<Int> get() = _currentPage
-
-    private val _canLoadPrevPage: MutableLiveData<Boolean> = MutableLiveData(false)
-    val canLoadPrevPage: LiveData<Boolean> get() = _canLoadPrevPage
-
-    private val _canLoadNextPage: MutableLiveData<Boolean> = MutableLiveData(false)
-    val canLoadNextPage: LiveData<Boolean> get() = _canLoadNextPage
-
-    init {
-        _products.value = cartRepository.cartProducts(1).map { it.toUiModel() }
-        updateEnabledPage()
+    val products: LiveData<List<CartProductUi>> = _currentPage.map { page ->
+        cartRepository.cartProducts(page).map { it.toUiModel() }
     }
+    val canLoadPrevPage: LiveData<Boolean> = _currentPage.map { page ->
+        page > 1
+    }
+    val canLoadNextPage: LiveData<Boolean> =
+        _currentPage.map { page -> cartRepository.canLoadMoreCartProducts(page) }
 
     fun deleteProduct(position: Int) {
-        val deletedItem = _products.value?.get(position)
-        if (deletedItem != null) {
-            _products.value = _products.value?.minus(deletedItem)
-            cartRepository.deleteCartProduct(deletedItem.product.id)
-        }
+        val currentProducts = products.value.orEmpty()
+        val deletedItem = currentProducts.getOrNull(position) ?: return
+        _currentPage.value = _currentPage.value
+        cartRepository.deleteCartProduct(deletedItem.product.id)
     }
 
     fun plusPage() {
         val currentPage = _currentPage.value ?: return
         if (cartRepository.canLoadMoreCartProducts(currentPage)) {
-            _products.value =
-                cartRepository.cartProducts(currentPage + INCREMENT_AMOUNT).map { it.toUiModel() }
-            _currentPage.value = _currentPage.value?.plus(INCREMENT_AMOUNT)
+            _currentPage.value = currentPage + INCREMENT_AMOUNT
         }
-        updateEnabledPage()
     }
 
     fun minusPage() {
         val currentPage = _currentPage.value ?: return
-
-        _products.value =
-            cartRepository.cartProducts(currentPage - INCREMENT_AMOUNT).map { it.toUiModel() }
-        _currentPage.value = _currentPage.value?.minus(INCREMENT_AMOUNT)
-        updateEnabledPage()
-    }
-
-    private fun updateEnabledPage() {
-        val currentPage = _currentPage.value ?: return
-        _canLoadPrevPage.value = currentPage > 1
-        _canLoadNextPage.value = cartRepository.canLoadMoreCartProducts(currentPage)
+        _currentPage.value = currentPage - INCREMENT_AMOUNT
     }
 
     companion object {
