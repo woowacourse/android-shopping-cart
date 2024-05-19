50098beabe8a3707c450eb38d9256aeea63329fc
sh1mj1 <7wlgns@gmail.com>
2024-05-20 03:12:49 +0900

    :sparkles: feat(ProductListViewModel2): 다음 페이지를 로드하고 현재 페이지를 계산 마지막 페이지인지를 판단한다

--- app/src/main/java/woowacourse/shopping/productList/ProductListViewModel.kt
+++ app/src/main/java/woowacourse/shopping/productList/ProductListViewModel.kt
@@ -1,10 +1,12 @@
 package woowacourse.shopping.productList
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import woowacourse.shopping.db.Product
 import woowacourse.shopping.repository.DummyProductStore
+import woowacourse.shopping.repository.ShoppingProductsRepository
 
 class ProductListViewModel : ViewModel() {
     private val dummyProductStore by lazy { DummyProductStore() }
@@ -30,5 +32,47 @@ class ProductListViewModel : ViewModel() {
 
     companion object {
         private const val COUNT_EACH_LOADING = 20
+    }
+}
+
+class ProductListViewModel2(
+    private val productsRepository: ShoppingProductsRepository,
+    private var _currentPage: MutableLiveData<Int> = MutableLiveData(FIRST_PAGE),
+) : ViewModel() {
+    val currentPage: LiveData<Int> get() = _currentPage
+
+    private val _loadedProducts: MutableLiveData<List<Product>> =
+        MutableLiveData(
+            productsRepository.loadPagedItems(currentPage.value ?: FIRST_PAGE),
+        )
+
+    val loadedProducts: LiveData<List<Product>>
+        get() = _loadedProducts
+
+    private var _isLastPage: MutableLiveData<Boolean> =
+        MutableLiveData(
+            productsRepository.isFinalPage(currentPage.value ?: FIRST_PAGE),
+        )
+    val isLastPage: LiveData<Boolean> get() = _isLastPage
+
+    fun loadNextPageProducts() {
+        if (isLastPage.value == true) return
+        _currentPage.value = _currentPage.value?.plus(PAGE_MOVE_COUNT)
+        _isLastPage.value = productsRepository.isFinalPage(currentPage.value ?: FIRST_PAGE)
+
+        val result = productsRepository.loadPagedItems(currentPage.value ?: FIRST_PAGE)
+        Log.d(TAG, "loadProducts: $result")
+        Log.d(TAG, "loadProducts: ${_loadedProducts.value}")
+
+        _loadedProducts.value =
+            _loadedProducts.value?.toMutableList()?.apply {
+                addAll(result)
+            }
+    }
+
+    companion object {
+        private const val FIRST_PAGE = 1
+        private const val PAGE_MOVE_COUNT = 1
+        private const val TAG = "ProductListViewModel2"
     }
 }
---
+++ app/src/test/java/woowacourse/shopping/productList/ProductListViewModelTest.kt
@@ -0,0 +1,148 @@
+package woowacourse.shopping.productList
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.mockkStatic
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import woowacourse.shopping.InstantTaskExecutorExtension
+import woowacourse.shopping.getOrAwaitValue
+import woowacourse.shopping.productsTestFixture
+import woowacourse.shopping.repository.FakeShoppingProductsRepository
+
+@ExtendWith(InstantTaskExecutorExtension::class)
+class ProductListViewModelTest {
+    private lateinit var viewModel: ProductListViewModel2
+
+    @BeforeEach
+    fun setUp() {
+        mockkStatic(Log::class)
+        every { Log.d(any(), any()) } returns mockk(relaxed = true)
+    }
+
+    @Test
+    fun `데이터 20개 로드`() {
+        // given
+        val repo =
+            FakeShoppingProductsRepository(
+                productsTestFixture(20),
+            )
+        viewModel =
+            ProductListViewModel2(
+                productsRepository = repo,
+                _currentPage = MutableLiveData(1),
+            )
+
+        // then
+        assertThat(viewModel.loadedProducts.getOrAwaitValue()).isEqualTo(productsTestFixture(20))
+    }
+
+    @Test
+    fun `데이터 40개 로드`() {
+        // given
+        viewModel =
+            ProductListViewModel2(
+                productsRepository =
+                    FakeShoppingProductsRepository(
+                        productsTestFixture(40),
+                    ),
+                _currentPage = MutableLiveData(1),
+            )
+
+        // when
+        viewModel.loadNextPageProducts()
+
+        // then
+        assertThat(viewModel.loadedProducts.getOrAwaitValue()).isEqualTo(productsTestFixture(40))
+    }
+
+    @Test
+    fun `총 데이터가 15 개일 때 현재 페이지는 1페이지이다`() {
+        // givne
+        viewModel =
+            ProductListViewModel2(
+                productsRepository =
+                    FakeShoppingProductsRepository(
+                        productsTestFixture(15),
+                    ),
+                _currentPage = MutableLiveData(1),
+            )
+
+        // then
+        assertThat(viewModel.currentPage.getOrAwaitValue()).isEqualTo(1)
+    }
+
+    @Test
+    fun `총 데이터가 21 개일 때 데이터를 두번 로드하면 2페이지이다`() {
+        // given
+        viewModel =
+            ProductListViewModel2(
+                productsRepository =
+                    FakeShoppingProductsRepository(
+                        productsTestFixture(21),
+                    ),
+                _currentPage = MutableLiveData(1),
+            )
+
+        // when
+        viewModel.loadNextPageProducts()
+
+        // then
+        assertThat(viewModel.currentPage.getOrAwaitValue()).isEqualTo(2)
+    }
+
+    @Test
+    fun `총 데이터가 20 개 일때 첫 페이지가 마지막 페이지이다`() {
+        // given
+        viewModel =
+            ProductListViewModel2(
+                productsRepository =
+                    FakeShoppingProductsRepository(
+                        productsTestFixture(20),
+                    ),
+                _currentPage = MutableLiveData(1),
+            )
+
+        // then
+        assertThat(viewModel.isLastPage.getOrAwaitValue()).isTrue
+    }
+
+    @Test
+    fun `총 데이터가 21 개 일때 첫 페이지가 마지막 페이지가 아니다`() {
+        // given
+        viewModel =
+            ProductListViewModel2(
+                productsRepository =
+                    FakeShoppingProductsRepository(
+                        productsTestFixture(21),
+                    ),
+                _currentPage = MutableLiveData(1),
+            )
+
+        // then
+        assertThat(viewModel.isLastPage.getOrAwaitValue()).isFalse
+    }
+
+    @Test
+    fun `총 데이터가 21 개 일때 두번째 페이지가 마지막 페이지이다`() {
+        // given
+        viewModel =
+            ProductListViewModel2(
+                productsRepository =
+                    FakeShoppingProductsRepository(
+                        productsTestFixture(21),
+                    ),
+                _currentPage = MutableLiveData(1),
+            )
+
+        // when
+        viewModel.loadNextPageProducts()
+
+        // then
+        assertThat(viewModel.isLastPage.getOrAwaitValue()).isTrue
+    }
+}
