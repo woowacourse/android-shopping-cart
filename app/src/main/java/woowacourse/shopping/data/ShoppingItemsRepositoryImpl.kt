93e0e0efde91ae8e04d54c59a34a6fe6f9b4bce0
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2024-05-19 18:16:55 +0900

    refactor(ShoppingRepository): 사용하지 않는 함수 삭제

--- app/src/main/java/woowacourse/shopping/data/ShoppingItemsRepositoryImpl.kt
+++ app/src/main/java/woowacourse/shopping/data/ShoppingItemsRepositoryImpl.kt
@@ -6,10 +6,6 @@ import woowacourse.shopping.domain.repository.ShoppingItemsRepository
 class ShoppingItemsRepositoryImpl() : ShoppingItemsRepository {
     private val items: List<Product> = DummyShoppingItems.items
 
-    override fun getAllProducts(): List<Product> {
-        return items
-    }
-
     override fun findProductItem(id: Long): Product? {
         return items.firstOrNull { it.id == id }
     }
--- app/src/main/java/woowacourse/shopping/domain/repository/ShoppingItemsRepository.kt
+++ app/src/main/java/woowacourse/shopping/domain/repository/ShoppingItemsRepository.kt
@@ -3,8 +3,6 @@ package woowacourse.shopping.domain.repository
 import woowacourse.shopping.domain.model.Product
 
 interface ShoppingItemsRepository {
-    fun getAllProducts(): List<Product>
-
     fun findProductItem(id: Long): Product?
 
     fun findProductsByPage(
