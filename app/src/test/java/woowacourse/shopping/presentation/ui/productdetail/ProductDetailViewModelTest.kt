9f327702dc5b41ac7f78c595d61b9601686be665
JunJangE <wnswkd486@gmail.com>
2024-05-19 22:17:44 +0900

    test: ProductDetailViewModelTest

--- app/src/test/java/woowacourse/shopping/presentation/ui/productdetail/ProductDetailViewModelTest.kt
+++ app/src/test/java/woowacourse/shopping/presentation/ui/productdetail/ProductDetailViewModelTest.kt
@@ -1,22 +1,24 @@
 package woowacourse.shopping.presentation.ui.productdetail
 
 import androidx.lifecycle.SavedStateHandle
+import io.mockk.CapturingSlot
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
 import io.mockk.just
 import io.mockk.runs
-import io.mockk.verify
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import woowacourse.shopping.InstantTaskExecutorExtension
 import woowacourse.shopping.data.repsoitory.DummyData.STUB_PRODUCT_A
+import woowacourse.shopping.domain.model.Product
 import woowacourse.shopping.domain.repository.ProductRepository
 import woowacourse.shopping.domain.repository.ShoppingCartRepository
 import woowacourse.shopping.getOrAwaitValue
 
-@ExtendWith(InstantTaskExecutorExtension::class)
-@ExtendWith(MockKExtension::class)
+@ExtendWith(InstantTaskExecutorExtension::class, MockKExtension::class)
 class ProductDetailViewModelTest {
     private lateinit var viewModel: ProductDetailViewModel
 
@@ -30,8 +32,11 @@ class ProductDetailViewModelTest {
 
     private val productId = 1
 
-    private fun initViewModel() {
-        val initialState = mutableMapOf(ProductDetailActivity.PUT_EXTRA_PRODUCT_ID to productId)
+    @BeforeEach
+    fun setUp() {
+        every { productRepository.findProductById(productId) } returns Result.success(STUB_PRODUCT_A)
+
+        val initialState = mapOf(ProductDetailActivity.PUT_EXTRA_PRODUCT_ID to productId)
         savedStateHandle = SavedStateHandle(initialState)
         viewModel =
             ProductDetailViewModel(savedStateHandle, productRepository, shoppingCartRepository)
@@ -39,26 +44,21 @@ class ProductDetailViewModelTest {
 
     @Test
     fun `선택한 상품의 상세 정보를 불러온다`() {
-        // given & when
-        every { productRepository.findProductById(productId) } returns Result.success(STUB_PRODUCT_A)
-        initViewModel()
-
         // then
         val actual = viewModel.product.getOrAwaitValue()
-        assert(actual == STUB_PRODUCT_A)
+        assertThat(actual).isEqualTo(STUB_PRODUCT_A)
     }
 
     @Test
     fun `선택한 상품을 장바구니에 추가한다`() {
         // given
-        every { productRepository.findProductById(productId) } returns Result.success(STUB_PRODUCT_A)
-        every { shoppingCartRepository.addOrder(STUB_PRODUCT_A) } just runs
-        initViewModel()
+        val productSlot = CapturingSlot<Product>()
+        every { shoppingCartRepository.addOrder(capture(productSlot)) } just runs
 
         // when
         viewModel.addToCart()
 
         // then
-        verify { shoppingCartRepository.addOrder(STUB_PRODUCT_A) }
+        assertThat(productSlot.captured).isEqualTo(STUB_PRODUCT_A)
     }
 }
