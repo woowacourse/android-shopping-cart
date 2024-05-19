c72d6ee8a869808f5fba55a75068b040876d2e55
sh1mj1 <7wlgns@gmail.com>
2024-05-20 04:43:44 +0900

    :fire: remove: 사용하지 않는 클래스 제거
    - ProductStore, DummyProductStore, ShoppingCart 제거

--- app/src/main/java/woowacourse/shopping/repository/ProductStore.kt
+++
@@ -1,9 +0,0 @@
-package woowacourse.shopping.repository
-
-import woowacourse.shopping.db.Product
-
-interface ProductStore {
-    fun load20Data(currentIndex: Int): List<Product>
-
-    fun findById(findId: Int): Product?
-}
