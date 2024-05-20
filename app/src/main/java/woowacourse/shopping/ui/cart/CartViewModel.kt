42738910077fcbed2b593d091e24677ab9342051
songpink <jejeje04@naver.com>
2024-05-20 16:04:12 +0900

    refactor: 카트 뷰모델이 카트 페이지를 라이브데이터로 갖도록 수정

--- app/src/main/java/woowacourse/shopping/ui/cart/CartViewModel.kt
+++ app/src/main/java/woowacourse/shopping/ui/cart/CartViewModel.kt
@@ -3,59 +3,44 @@ package woowacourse.shopping.ui.cart
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import woowacourse.shopping.model.CartPageManager
+import woowacourse.shopping.model.CartPage
 import woowacourse.shopping.model.Product
 import woowacourse.shopping.model.data.CartDao
 import kotlin.math.min
 
 class CartViewModel(private val cartDao: CartDao) : ViewModel() {
-    private val cartPageManager by lazy { CartPageManager() }
-    private val _pageNumber: MutableLiveData<Int> = MutableLiveData()
-
-    private val _canMoveNextPage: MutableLiveData<Boolean> = MutableLiveData()
-    val canMoveNextPage: LiveData<Boolean> get() = _canMoveNextPage
-
-    private val _canMovePreviousPage: MutableLiveData<Boolean> = MutableLiveData()
-    val canMovePreviousPage: LiveData<Boolean> get() = _canMovePreviousPage
-
-    val pageNumber: LiveData<Int> get() = _pageNumber
+    private val _cartPage: MutableLiveData<CartPage> = MutableLiveData()
+    val cartPage: LiveData<CartPage> get() = _cartPage
 
     private val _cart: MutableLiveData<List<Product>> = MutableLiveData()
     val cart: LiveData<List<Product>> get() = _cart
 
-    private val cartItems get() = cartDao.findAll()
+    val cartItems get() = cartDao.findAll()
 
     fun loadCartItems() {
-        _pageNumber.value = cartPageManager.pageNum
+        _cartPage.value = CartPage()
         _cart.value = getProducts()
-        _canMovePreviousPage.value = cartPageManager.canMovePreviousPage()
-        _canMoveNextPage.value = cartPageManager.canMoveNextPage(cartItems.size)
     }
 
     fun removeCartItem(productId: Long) {
         cartDao.delete(productId)
-        _canMoveNextPage.value = cartPageManager.canMoveNextPage(cartItems.size)
         _cart.value = getProducts()
+        val currentPage = _cartPage.value?.number
+        _cartPage.value = CartPage(currentPage ?: -1)
     }
 
     fun plusPageNum() {
-        cartPageManager.plusPageNum()
-        _pageNumber.value = cartPageManager.pageNum
-        _canMovePreviousPage.value = cartPageManager.canMovePreviousPage()
-        _canMoveNextPage.value = cartPageManager.canMoveNextPage(cartItems.size)
+        _cartPage.value = _cartPage.value?.plus()
         _cart.value = getProducts()
     }
 
     fun minusPageNum() {
-        cartPageManager.minusPageNum()
-        _pageNumber.value = cartPageManager.pageNum
-        _canMovePreviousPage.value = cartPageManager.canMovePreviousPage()
-        _canMoveNextPage.value = cartPageManager.canMoveNextPage(cartItems.size)
+        _cartPage.value = _cartPage.value?.minus()
         _cart.value = getProducts()
     }
 
     private fun getProducts(): List<Product> {
-        val fromIndex = (cartPageManager.pageNum - OFFSET) * PAGE_SIZE
+        val fromIndex = (cartPage.value!!.number - OFFSET) * PAGE_SIZE
         val toIndex = min(fromIndex + PAGE_SIZE, cartItems.size)
         return cartItems.toList().subList(fromIndex, toIndex)
     }
