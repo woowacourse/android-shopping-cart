6af54b6a8e22570343da3670860d786611f70b22
김진우 <jinwoo3661@naver.com>
2024-05-21 22:15:38 +0900

    feat: 이벤트를 보낼 메인 뷰모델 생성

---
+++ app/src/main/java/woowacourse/shopping/view/MainViewModel.kt
@@ -0,0 +1,15 @@
+package woowacourse.shopping.view
+
+import androidx.lifecycle.ViewModel
+import woowacourse.shopping.utils.MutableSingleLiveData
+import woowacourse.shopping.utils.SingleLiveData
+
+class MainViewModel : ViewModel() {
+    private val _updateProductEvent: MutableSingleLiveData<Map<Long, Int>> = MutableSingleLiveData()
+    val updateProductEvent: SingleLiveData<Map<Long, Int>> get() = _updateProductEvent
+
+    fun saveUpdate(updateList: Map<Long, Int>) {
+        _updateProductEvent.postValue(updateList)
+    }
+
+}
