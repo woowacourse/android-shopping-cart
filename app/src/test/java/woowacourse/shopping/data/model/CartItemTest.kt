b3f06edb466c413568450702e8d7ec6d39952351
haeum808 <haeum808@gmail.com>
2024-05-21 17:18:24 +0900

    fix: CartItem 생성자 파라미터 위치 수정으로 인한 버그 수정

--- app/src/test/java/woowacourse/shopping/data/model/CartItemTest.kt
+++ app/src/test/java/woowacourse/shopping/data/model/CartItemTest.kt
@@ -9,7 +9,7 @@ class CartItemTest {
     fun `장바구니로 담은 상품의 수량이 최소 개수보다 적으면 예외를 발생시킨다`() {
         // given
         // when
-        val actualMessage = assertThrows<IllegalArgumentException> { CartItem(1, 1, 0) }.message
+        val actualMessage = assertThrows<IllegalArgumentException> { CartItem(1, 0, 1) }.message
 
         // then
         assertThat(actualMessage).isEqualTo("카트 아이템 수량은 최소 1개 이상이어야 한다.")
