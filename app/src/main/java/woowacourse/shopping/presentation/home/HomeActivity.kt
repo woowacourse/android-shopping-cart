c3e9f1d3664c4627b9b9b65ee3fb5dbf6443ec34
haeum808 <haeum808@gmail.com>
2024-05-21 22:01:48 +0900

    feat: 홈화면에서 장바구니 갯수를 보여주는 기능 구현

--- app/src/main/java/woowacourse/shopping/presentation/home/HomeActivity.kt
+++ app/src/main/java/woowacourse/shopping/presentation/home/HomeActivity.kt
@@ -7,7 +7,9 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import woowacourse.shopping.R
+import woowacourse.shopping.data.datasource.DefaultCart
 import woowacourse.shopping.data.datasource.DefaultProducts
+import woowacourse.shopping.data.repository.CartRepositoryImpl
 import woowacourse.shopping.data.repository.ProductRepositoryImpl
 import woowacourse.shopping.databinding.ActivityHomeBinding
 import woowacourse.shopping.presentation.cart.CartActivity
@@ -19,7 +21,7 @@ import woowacourse.shopping.presentation.home.adapter.ProductsGridLayoutManager
 import woowacourse.shopping.presentation.home.viewmodel.HomeViewModel
 import woowacourse.shopping.presentation.home.viewmodel.HomeViewModelFactory
 
-class HomeActivity : AppCompatActivity(), ProductItemClickListener, LoadClickListener {
+class HomeActivity : AppCompatActivity(), ProductItemClickListener, LoadClickListener, HomeClickListener {
     private val binding: ActivityHomeBinding by lazy {
         ActivityHomeBinding.inflate(layoutInflater)
     }
@@ -27,11 +29,19 @@ class HomeActivity : AppCompatActivity(), ProductItemClickListener, LoadClickLis
     private val viewModel: HomeViewModel by lazy {
         ViewModelProvider(
             this,
-            HomeViewModelFactory(ProductRepositoryImpl(DefaultProducts)),
+            HomeViewModelFactory(
+                ProductRepositoryImpl(DefaultProducts),
+                CartRepositoryImpl(DefaultCart)
+            ),
         )[HomeViewModel::class.java]
     }
     private val adapter: ProductAdapter by lazy {
         ProductAdapter(this, this)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        viewModel.loadTotalCartCount()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,7 +57,9 @@ class HomeActivity : AppCompatActivity(), ProductItemClickListener, LoadClickLis
         binding.productAdapter = adapter
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
+        binding.homeClickListener = this
 
+        viewModel.loadTotalCartCount()
         viewModel.loadProducts()
         viewModel.products.observe(this) {
             adapter.addProducts(it)
@@ -60,25 +72,15 @@ class HomeActivity : AppCompatActivity(), ProductItemClickListener, LoadClickLis
         supportActionBar?.setDisplayShowTitleEnabled(false)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_home, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.menu_shopping_cart -> {
-                startActivity(CartActivity.newIntent(this))
-            }
-        }
-        return super.onOptionsItemSelected(item)
-    }
-
     override fun onProductItemClick(id: Long) {
         startActivity(DetailActivity.newIntent(this, id))
     }
 
     override fun onLoadClick() {
         viewModel.loadProducts()
+    }
+
+    override fun moveToCart() {
+        startActivity(CartActivity.newIntent(this))
     }
 }
