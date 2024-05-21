3402debfa8b7b2e52be7d79e36a803652b68e824
김진우 <jinwoo3661@naver.com>
2024-05-21 11:25:40 +0900

    feat: CartItemCounter 도메인 구현

---
+++ app/src/main/java/woowacourse/shopping/domain/model/CartItemCounter.kt
@@ -0,0 +1,36 @@
+package woowacourse.shopping.domain.model
+
+import woowacourse.shopping.view.cartcounter.ChangeCartItemResultState
+
+class CartItemCounter(
+    count: Int = DEFAULT_ITEM_COUNT,
+    isSelected: Boolean =  DEFAULT_ITEM_SELECTED,
+) {
+    var itemCount: Int = count
+        private set
+    var isSelected: Boolean = isSelected
+        private set
+
+    fun increase(): ChangeCartItemResultState {
+        itemCount++
+        return ChangeCartItemResultState.Success
+    }
+
+    fun decrease(): ChangeCartItemResultState {
+        return if (itemCount > DEFAULT_ITEM_COUNT) {
+            itemCount--
+            ChangeCartItemResultState.Success
+        } else{
+            ChangeCartItemResultState.Fail
+        }
+    }
+
+    fun selectItem(){
+        isSelected = true
+    }
+
+    companion object {
+        const val DEFAULT_ITEM_COUNT = 0
+        const val DEFAULT_ITEM_SELECTED = false
+    }
+}
