a80910006bdc59732d8b21c6cec1d5cdff01621e
Namyunsuk <yunsuk0328@naver.com>
2024-05-24 00:41:59 +0900

    build: Room 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,12 +50,14 @@ android {
 }
 
 dependencies {
+    kapt("androidx.room:room-compiler:2.6.1")
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.activity:activity-ktx:1.8.2")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation("androidx.room:room-runtime:2.6.1")
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
     testImplementation("org.assertj:assertj-core:3.25.3")
     testImplementation("io.kotest:kotest-runner-junit5:5.8.0")
