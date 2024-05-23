a6f09c3d52c178f06a67f4901dc613cd985d1210
JunJangE <wnswkd486@gmail.com>
2024-05-23 15:15:10 +0900

    chore: gradle okhttp 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,4 +76,12 @@ dependencies {
     implementation("androidx.room:room-runtime:2.6.1")
     annotationProcessor("androidx.room:room-compiler:2.6.1")
     kapt("androidx.room:room-compiler:2.6.1")
+
+    // okhttp
+    implementation("com.squareup.okhttp3:mockwebserver:4.12.0")
+    testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
+    testImplementation("io.projectreactor:reactor-test")
+
+    // gson
+    implementation("com.google.code.gson:gson:2.10.1")
 }
