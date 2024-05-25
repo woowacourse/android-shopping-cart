179728032d01c0a91693818a8da727985547494d
Namyunsuk <yunsuk0328@naver.com>
2024-05-25 21:39:54 +0900

    chore: 테스트를 위한 코드 제거
    - 앱 실행 마다 모든 데이터베이스의 데이터 제거하는 코드 제거

--- app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
+++ app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
@@ -3,11 +3,9 @@ package woowacourse.shopping
 import android.app.Application
 import woowacourse.shopping.data.api.ProductMockWebServer
 import woowacourse.shopping.data.cart.CartDatabase
-import woowacourse.shopping.data.cart.CartRepositoryImpl
 import woowacourse.shopping.data.product.ProductRepository
 import woowacourse.shopping.data.product.ProductRepositoryImpl
 import woowacourse.shopping.data.recentproduct.RecentProductDatabase
-import woowacourse.shopping.data.recentproduct.RecentProductRepositoryImpl
 
 class ShoppingApplication : Application() {
     val productRepository: ProductRepository by lazy {
@@ -19,21 +17,10 @@ class ShoppingApplication : Application() {
         RecentProductDatabase.initialize(this)
         CartDatabase.initialize(this)
         productRepository.start()
-        forTest()
     }
 
     override fun onTerminate() {
         super.onTerminate()
         productRepository.shutdown()
-    }
-
-    private fun forTest() {
-        Thread {
-            RecentProductRepositoryImpl.get(RecentProductDatabase.database().recentProductDao())
-                .deleteAll()
-
-            CartRepositoryImpl.get(CartDatabase.database().cartDao())
-                .deleteAll()
-        }.start()
     }
 }
