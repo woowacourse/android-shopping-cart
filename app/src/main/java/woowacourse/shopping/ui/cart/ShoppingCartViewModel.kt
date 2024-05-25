66456d79179c27b03904dd9ab155b0d561d7698f
sh1mj1 <7wlgns@gmail.com>
2024-05-26 00:54:12 +0900

    :recycle: refactor(ShoppingCartViewModel): 생성자 변경

--- app/src/main/java/woowacourse/shopping/ui/cart/ShoppingCartViewModel.kt
+++ app/src/main/java/woowacourse/shopping/ui/cart/ShoppingCartViewModel.kt
@@ -6,65 +6,54 @@ import androidx.lifecycle.ViewModel
 import woowacourse.shopping.MutableSingleLiveData
 import woowacourse.shopping.SingleLiveData
 import woowacourse.shopping.currentPageIsNullException
-import woowacourse.shopping.data.model.ProductData
-import woowacourse.shopping.domain.repository.ShoppingCartItemRepository
+import woowacourse.shopping.domain.model.Product
+import woowacourse.shopping.domain.repository.ShoppingProductsRepository
 
 class ShoppingCartViewModel(
-    private val shoppingCartItemRepository: ShoppingCartItemRepository,
+    private val shoppingProductsRepository: ShoppingProductsRepository,
     private var _currentPage: MutableLiveData<Int> = MutableLiveData(FIRST_PAGE),
 ) : ViewModel(), CartItemRecyclerViewAdapter.OnProductItemClickListener {
     val currentPage: LiveData<Int> get() = _currentPage
 
     private var _itemsInCurrentPage =
-        MutableLiveData<List<ProductData>>(
-            shoppingCartItemRepository.loadPagedCartItems(currentPage.value ?: currentPageIsNullException()),
-        )
-    val itemsInCurrentPage: LiveData<List<ProductData>> get() = _itemsInCurrentPage
+        MutableLiveData<List<Product>>()
+    val itemsInCurrentPage: LiveData<List<Product>> get() = _itemsInCurrentPage
 
-    private var _isLastPage: MutableLiveData<Boolean> =
-        MutableLiveData(
-            shoppingCartItemRepository.isFinalPage(currentPage.value ?: currentPageIsNullException()),
-        )
+    private var _isLastPage: MutableLiveData<Boolean> = MutableLiveData()
     val isLastPage: LiveData<Boolean> get() = _isLastPage
 
     private var _deletedItemId: MutableSingleLiveData<Int> = MutableSingleLiveData()
     val deletedItemId: SingleLiveData<Int> get() = _deletedItemId
 
+    fun loadAll() {
+        _itemsInCurrentPage.value = shoppingProductsRepository.loadProductsInCart(page = currentPage.value ?: currentPageIsNullException())
+        _isLastPage.value = shoppingProductsRepository.isCartFinalPage(currentPage.value ?: currentPageIsNullException())
+    }
+
     fun nextPage() {
         if (isLastPage.value == true) return
 
         _currentPage.value = _currentPage.value?.plus(PAGE_MOVE_COUNT)
-        _isLastPage.value = shoppingCartItemRepository.isFinalPage(currentPage.value ?: currentPageIsNullException())
+        _isLastPage.value = shoppingProductsRepository.isCartFinalPage(currentPage.value ?: currentPageIsNullException())
 
-        _itemsInCurrentPage.value =
-            shoppingCartItemRepository.loadPagedCartItems(
-                currentPage.value ?: currentPageIsNullException(),
-            )
+        _itemsInCurrentPage.value = shoppingProductsRepository.loadProductsInCart(page = currentPage.value ?: currentPageIsNullException())
     }
 
     fun previousPage() {
         if (currentPage.value == FIRST_PAGE) return
 
         _currentPage.value = _currentPage.value?.minus(PAGE_MOVE_COUNT)
-        _isLastPage.value = shoppingCartItemRepository.isFinalPage(currentPage.value ?: currentPageIsNullException())
+        _isLastPage.value = shoppingProductsRepository.isCartFinalPage(currentPage.value ?: currentPageIsNullException())
 
-        _itemsInCurrentPage.postValue(
-            shoppingCartItemRepository.loadPagedCartItems(
-                currentPage.value ?: currentPageIsNullException(),
-            ),
-        )
+        _itemsInCurrentPage.value = shoppingProductsRepository.loadProductsInCart(page = currentPage.value ?: currentPageIsNullException())
     }
 
     fun deleteItem(cartItemId: Int) {
-        shoppingCartItemRepository.removeCartItem(cartItemId)
+        shoppingProductsRepository.removeShoppingCartProduct(cartItemId)
 
-        _itemsInCurrentPage.postValue(
-            shoppingCartItemRepository.loadPagedCartItems(
-                currentPage.value ?: currentPageIsNullException(),
-            ),
-        )
+        _itemsInCurrentPage.value = shoppingProductsRepository.loadProductsInCart(page = currentPage.value ?: currentPageIsNullException())
 
-        _isLastPage.value = shoppingCartItemRepository.isFinalPage(currentPage.value ?: currentPageIsNullException())
+        _isLastPage.value = shoppingProductsRepository.isCartFinalPage(currentPage.value ?: currentPageIsNullException())
     }
 
     override fun onClick(productId: Int) {
@@ -74,5 +63,6 @@ class ShoppingCartViewModel(
     companion object {
         private const val FIRST_PAGE = 1
         private const val PAGE_MOVE_COUNT = 1
+        private const val TAG = "ShoppingCartViewModel"
     }
 }
