f6d60c9244fd0786ba978c0fd76a4f227ef7e532
sh1mj1 <7wlgns@gmail.com>
2024-05-27 03:48:04 +0900

    :sparkles: feat HistoryProduct: db, dao, entity, test

--- app/src/main/java/woowacourse/shopping/local/cart/ShoppingCartDatabase.kt
+++ app/src/main/java/woowacourse/shopping/local/cart/ShoppingCartDatabase.kt
@@ -22,23 +22,3 @@ abstract class ShoppingCartDatabase : RoomDatabase() {
             }
     }
 }
-
-/*
-@Database(entities = [ReservationTicket::class], version = 1)
-@TypeConverters(ReservationTicketConverters::class)
-abstract class ReservationTicketDatabase : RoomDatabase() {
-    abstract fun reservationDao(): ReservationTicketRoomDao
-
-    companion object {
-        @Volatile
-        private var instance: ReservationTicketDatabase? = null
-
-        fun getDatabase(context: Context): ReservationTicketDatabase =
-            instance ?: synchronized(this) {
-                Room.databaseBuilder(context, ReservationTicketDatabase::class.java, "reservation_database")
-                    .build().also { instance = it }
-            }
-    }
-}
-
- */
