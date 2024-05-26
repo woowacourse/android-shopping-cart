b9cedc410947533a8bf8fcb360f9710182d81772
sh1mj1 <7wlgns@gmail.com>
2024-05-26 18:26:32 +0900

    :lipstick: feat fragment_product_detail.xml: 마지막으로 본 상품

--- app/src/main/java/woowacourse/shopping/domain/model/Product.kt
+++ app/src/main/java/woowacourse/shopping/domain/model/Product.kt
@@ -10,16 +10,16 @@ data class Product(
     val quantity: Int,
 ) {
     companion object {
-        val NULL = Product(
-            id = -1,
-            imgUrl = "0",
-            name = "상품이 없습니다.",
-            price = 0,
-            quantity = 0
-        )
+        val NULL =
+            Product(
+                id = -1,
+                imgUrl = "0",
+                name = "상품이 없습니다.",
+                price = 0,
+                quantity = 0,
+            )
     }
 }
-
 
 fun Product.toData(): ProductData =
     ProductData(
