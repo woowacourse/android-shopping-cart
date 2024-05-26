a0c8bb8594aeb4418e44e36bff7e9c61f9a8702f
aprilgom <aprilgom@gmail.com>
2024-05-26 17:17:20 +0900

    feat: RemoteProductDataSource 구현

--- app/src/test/java/woowacourse/shopping/data/datasource/RemoteProductDataSourceTest.kt
+++ app/src/test/java/woowacourse/shopping/data/datasource/RemoteProductDataSourceTest.kt
@@ -13,23 +13,25 @@ import org.junit.jupiter.api.Test
 import woowacourse.shopping.data.DummyData.STUB_PRODUCT_A
 import woowacourse.shopping.data.DummyData.STUB_PRODUCT_B
 import woowacourse.shopping.data.DummyData.STUB_PRODUCT_C
+import woowacourse.shopping.data.datasource.product.RemoteProductDataSource
 import woowacourse.shopping.data.okhttp.api.DefaultProductApi
 import woowacourse.shopping.data.okhttp.api.ProductApi
 import woowacourse.shopping.domain.model.Product
 
 class RemoteProductDataSourceTest {
     private val api: ProductApi = DefaultProductApi(OkHttpClient())
+    private val remoteProductDataSource: RemoteProductDataSource = RemoteProductDataSource(api)
 
     @Test
     fun `하나의 product 읽기`() {
-        val actual = api.findById(1).getOrThrow()
+        val actual = remoteProductDataSource.findProductById(1).getOrThrow()
         val expected = STUB_PRODUCT_A
         assertThat(actual).isEqualTo(expected)
     }
 
     @Test
     fun `offset과_size로_product_list_찾기`() {
-        val actual = api.findByOffsetAndSize(1, 3).getOrThrow()
+        val actual = remoteProductDataSource.getOffsetRanged(1, 3).getOrThrow()
         val expected = listOf(STUB_PRODUCT_A, STUB_PRODUCT_B, STUB_PRODUCT_C)
         assertThat(actual).isEqualTo(expected)
     }
@@ -63,12 +65,14 @@ class RemoteProductDataSourceTest {
                                     .setResponseCode(200)
                                     .setBody(productsJson)
                             }
+
                             "/products/1" -> {
                                 MockResponse()
                                     .setHeader("Content-Type", "application/json")
                                     .setResponseCode(200)
                                     .setBody(productJson)
                             }
+
                             else -> {
                                 MockResponse().setResponseCode(404)
                             }
