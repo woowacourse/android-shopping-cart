c0e3febf1364a6f158cec2e74f642f7d363768a4
aprilgom <aprilgom@gmail.com>
2024-05-26 17:49:33 +0900

    fix: 최근 본 상품 목록이 비었을 때 크래시나는 버그 수정

--- app/src/main/java/woowacourse/shopping/presentation/ui/productdetail/bindingadapter/ProductDetailBindingAdapters.kt
+++ app/src/main/java/woowacourse/shopping/presentation/ui/productdetail/bindingadapter/ProductDetailBindingAdapters.kt
@@ -7,12 +7,16 @@ import woowacourse.shopping.domain.model.Product
 
 @BindingAdapter(value = ["hideIfRecentHistory", "hideIfRecentProduct"], requireAll = true)
 fun View.hideIfCountLessThanZero(
-    hideIfRecentHistory: History,
+    hideIfRecentHistory: History?,
     hideIfRecentProduct: Product,
 ) {
-    if (hideIfRecentHistory.product == hideIfRecentProduct) {
-        visibility = View.GONE
-    } else {
-        visibility = View.VISIBLE
-    }
+    visibility =
+        if (
+            hideIfRecentHistory == null ||
+            hideIfRecentHistory.product == hideIfRecentProduct
+        ) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
 }
