fed490c125746a1e3dfc31490a52df66d6dccf66
sh1mj1 <7wlgns@gmail.com>
2024-05-26 23:34:07 +0900

    :construction_worker: build: room kapt 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,4 +76,6 @@ dependencies {
     androidTestImplementation("de.mannodermaus.junit5:android-test-core:1.3.0")
     androidTestRuntimeOnly("de.mannodermaus.junit5:android-test-runner:1.3.0")
     testImplementation("io.mockk:mockk:1.13.8")
+    implementation("androidx.room:room-ktx:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
 }
