529086109ce0ba190db49d784c468458fac8a8cd
kimhyemin <kimhm0728@naver.com>
2024-05-26 14:12:51 +0900

    test: Quantity 테스트 수정

--- app/src/main/java/woowacourse/shopping/model/Quantity.kt
+++ app/src/main/java/woowacourse/shopping/model/Quantity.kt
@@ -1,6 +1,10 @@
 package woowacourse.shopping.model
 
 data class Quantity(val count: Int = DEFAULT_VALUE) {
+    init {
+        require(count >= MIN_VALUE) { INVALID_MIN_VALUE_MESSAGE }
+    }
+
     operator fun inc(): Quantity {
         if (count == MAX_VALUE) return this
         return Quantity(count + 1)
@@ -19,5 +23,7 @@ data class Quantity(val count: Int = DEFAULT_VALUE) {
         private const val DEFAULT_VALUE = 0
         private const val MAX_VALUE = 99
         private const val MIN_VALUE = 0
+
+        private const val INVALID_MIN_VALUE_MESSAGE = "개수는 $MIN_VALUE 보다 작을 수 없습니다."
     }
 }
--- app/src/test/java/woowacourse/shopping/model/QuantityTest.kt
+++ app/src/test/java/woowacourse/shopping/model/QuantityTest.kt
@@ -2,12 +2,29 @@ package woowacourse.shopping.model
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+import java.lang.IllegalArgumentException
 
 class QuantityTest {
     @Test
-    fun `상품 개수의 초기값은 1이다`() {
+    fun `상품 개수의 초기값은 0이다`() {
         val quantity = Quantity()
-        assertThat(quantity.count).isEqualTo(1)
+        assertThat(quantity.count).isEqualTo(0)
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [-1, -10, -1000])
+    fun `초기값보다 작은 상품 개수는 예외가 발생한다`(count: Int) {
+        assertThrows<IllegalArgumentException> { Quantity(count) }
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [0, 5, 10, 1000])
+    fun `초기값보다 같거나 큰 상품 개수는 예외가 발생하지 않는다`(count: Int) {
+        assertDoesNotThrow { Quantity(count) }
     }
 
     @Test
@@ -32,15 +49,15 @@ class QuantityTest {
     }
 
     @Test
-    fun `상품 개수가 1개일 때 감소시켜도 변화가 없다`() {
-        var quantity = Quantity(1)
+    fun `상품 개수가 0개일 때 감소시켜도 변화가 없다`() {
+        var quantity = Quantity(0)
         quantity--
-        assertThat(quantity.count).isEqualTo(1)
+        assertThat(quantity.count).isEqualTo(0)
     }
 
     @Test
-    fun `상품 개수가 1개면 상품 개수의 최소값이다`() {
-        val quantity = Quantity(1)
+    fun `상품 개수가 0개면 상품 개수의 최소값이다`() {
+        val quantity = Quantity(0)
         val actual = quantity.isMin()
         assertThat(actual).isTrue
     }
@@ -49,6 +66,20 @@ class QuantityTest {
     fun `상품 개수가 5개면 상품 개수의 최소값이 아니다`() {
         val quantity = Quantity(5)
         val actual = quantity.isMin()
+        assertThat(actual).isFalse
+    }
+
+    @Test
+    fun `상품 개수가 5개면 상품 개수의 최소값보다 크다`() {
+        val quantity = Quantity(5)
+        val actual = quantity.isGreaterThanMin()
+        assertThat(actual).isTrue
+    }
+
+    @Test
+    fun `상품 개수가 0개면 상품 개수의 최소값보다 크지 않다`() {
+        val quantity = Quantity(0)
+        val actual = quantity.isGreaterThanMin()
         assertThat(actual).isFalse
     }
 }
