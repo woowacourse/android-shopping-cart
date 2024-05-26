f7ab201e48d3e965cf02305a0a8c73c1f909df7f
kimhyemin <kimhm0728@naver.com>
2024-05-26 13:04:59 +0900

    test: ProductDetailViewModel 테스트 수정

--- app/src/test/java/woowacourse/shopping/ui/detail/ProductDetailViewModelTest.kt
+++ app/src/test/java/woowacourse/shopping/ui/detail/ProductDetailViewModelTest.kt
@@ -5,66 +5,137 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import woowacourse.shopping.InstantTaskExecutorExtension
+import woowacourse.shopping.FakeCartRepository
+import woowacourse.shopping.FakeProductRepository
+import woowacourse.shopping.FakeRecentProductRepository
 import woowacourse.shopping.data.cart.CartRepository
-import woowacourse.shopping.data.cart.FakeCartRepository
-import woowacourse.shopping.data.product.FakeProductRepository
 import woowacourse.shopping.data.product.ProductRepository
+import woowacourse.shopping.data.recent.RecentProductRepository
+import woowacourse.shopping.data.recent.entity.RecentProduct
 import woowacourse.shopping.getOrAwaitValue
-import woowacourse.shopping.imageUrl
-import woowacourse.shopping.model.Product
-import woowacourse.shopping.price
-import woowacourse.shopping.title
+import woowacourse.shopping.products
+import java.time.LocalDateTime
 
 @ExtendWith(InstantTaskExecutorExtension::class)
 class ProductDetailViewModelTest {
     private lateinit var viewModel: ProductDetailViewModel
     private lateinit var productRepository: ProductRepository
+    private lateinit var recentProductRepository: RecentProductRepository
     private val cartRepository: CartRepository = FakeCartRepository()
 
     @BeforeEach
     fun setUp() {
-        productRepository = FakeProductRepository(listOf(product(0L)))
+        productRepository = FakeProductRepository(products(2))
+        recentProductRepository = FakeRecentProductRepository()
     }
 
     @Test
     fun `상품 id에 맞는 상품을 불러온다`() {
-        viewModel = ProductDetailViewModel(0L, productRepository, cartRepository)
+        viewModel = ProductDetailViewModel(
+            0L,
+            productRepository,
+            recentProductRepository,
+            cartRepository,
+            false,
+        )
 
         val actual = viewModel.productUiModel.getOrAwaitValue()
-        actual.assertThat(imageUrl, title, price)
+        assertThat(actual.productId).isEqualTo(0L)
     }
 
     @Test
     fun `상품 id에 해당하는 상품이 없는 경우 에러가 발생한다`() {
-        viewModel = ProductDetailViewModel(-1L, productRepository, cartRepository)
-
-        assertThat(viewModel.productLoadError.getOrAwaitValue().peekContent()).isTrue
+        viewModel = ProductDetailViewModel(
+            -1L,
+            productRepository,
+            recentProductRepository,
+            cartRepository,
+            false,
+        )
+        assertThat(viewModel.productLoadError.getOrAwaitValue().peekContent()).isNotNull
     }
 
     @Test
     fun `상품을 장바구니에 담는다`() {
         // given
-        viewModel = ProductDetailViewModel(0L, productRepository, cartRepository)
+        viewModel = ProductDetailViewModel(
+            0L,
+            productRepository,
+            recentProductRepository,
+            cartRepository,
+            false,
+        )
 
         // when
         viewModel.addCartProduct()
 
         // then
-        val actual = cartRepository.findRange(0, 5).first()
-        actual.product.assertThat(imageUrl, title, price)
-        assertThat(actual.quantity.count).isEqualTo(1)
+        val actual = cartRepository.find(0L)
+        assertThat(actual.productId).isEqualTo(0L)
         assertThat(viewModel.isSuccessAddCart.getOrAwaitValue().peekContent()).isTrue
     }
 
-    private fun Product.assertThat(
-        expectedImageUrl: String,
-        expectedTitle: String,
-        expectedPrice: Int,
-    ) {
-        assertThat(imageUrl).isEqualTo(expectedImageUrl)
-        assertThat(title).isEqualTo(expectedTitle)
-        assertThat(price).isEqualTo(expectedPrice)
+    @Test
+    fun `마지막으로 본 상품을 불러온다`() {
+        setUpRecentRepository(productId = 1L)
+        viewModel = ProductDetailViewModel(
+            0L,
+            productRepository,
+            recentProductRepository,
+            cartRepository,
+            false,
+        )
+
+        val actual = viewModel.lastRecentProduct.getOrAwaitValue()
+        assertThat(actual.productId).isEqualTo(1L)
     }
 
-    private fun product(id: Long) = Product(id, imageUrl, title, price)
+    @Test
+    fun `마지막으로 본 상품이 없는 경우 불러올 수 없다`() {
+        viewModel = ProductDetailViewModel(
+            0L,
+            productRepository,
+            recentProductRepository,
+            cartRepository,
+            false,
+        )
+
+        val actual = viewModel.lastRecentProduct.isInitialized
+        assertThat(actual).isFalse
+    }
+
+    @Test
+    fun `마지막으로 본 상품이 현재 상품과 동일힌 경우 마지막으로 본 상품이 보이지 않는다`() {
+        setUpRecentRepository(productId = 0L)
+        viewModel = ProductDetailViewModel(
+            0L,
+            productRepository,
+            recentProductRepository,
+            cartRepository,
+            false,
+        )
+
+        val actual = viewModel.isVisibleLastRecentProduct.getOrAwaitValue()
+        assertThat(actual).isFalse
+    }
+
+    @Test
+    fun `마지막으로 본 상품을 타고 들어온 경우 마지막으로 본 상품이 보이지 않는다`() {
+        setUpRecentRepository(productId = 1L)
+        viewModel = ProductDetailViewModel(
+            0L,
+            productRepository,
+            recentProductRepository,
+            cartRepository,
+            true,
+        )
+
+        val actual = viewModel.isVisibleLastRecentProduct.getOrAwaitValue()
+        assertThat(actual).isFalse
+    }
+
+    private fun setUpRecentRepository(productId: Long) {
+        recentProductRepository =
+            FakeRecentProductRepository(listOf(RecentProduct(0L, productId, LocalDateTime.now())))
+    }
 }
