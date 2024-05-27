1e2fadfd9872caad19f4f2eb2770291eae54ccc4
somin <salth0601@gmail.com>
2024-05-27 14:01:27 +0900

    fix: 마지막으로 본 상품을 장바구니 담을 때 메인 액티비티가 두 개 쌓이는 버그 수정

--- app/src/main/java/woowacourse/shopping/presentation/ui/detail/ProductDetailActivity.kt
+++ app/src/main/java/woowacourse/shopping/presentation/ui/detail/ProductDetailActivity.kt
@@ -86,6 +86,7 @@ class ProductDetailActivity : BindingActivity<ActivityProductDetailBinding>(), D
             this,
             EventObserver {
                 Intent(applicationContext, ShoppingActivity::class.java).apply {
+                    addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
                     putExtra(EXTRA_PRODUCT_ID, id)
                     putExtra(EXTRA_NEW_PRODUCT_QUANTITY, it)
                     setResult(RESULT_OK, this)
