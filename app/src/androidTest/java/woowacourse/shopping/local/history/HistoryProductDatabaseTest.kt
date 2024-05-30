131f5bf3939e747d07e39276a62c4b51a6bad797
sh1mj1 <7wlgns@gmail.com>
2024-05-30 22:24:25 +0900

    :white_check_mark: test HistoryProductDatabaseTest 의 DisplayName 추가, test fixture 추가

--- app/src/androidTest/java/woowacourse/shopping/local/history/HistoryProductDatabaseTest.kt
+++ app/src/androidTest/java/woowacourse/shopping/local/history/HistoryProductDatabaseTest.kt
@@ -10,6 +10,7 @@ import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 import org.junit.runner.RunWith
 import woowacourse.shopping.data.model.HistoryProduct
@@ -45,18 +46,17 @@ class HistoryProductDatabaseTest {
     @DisplayName("이미 존재하는 HistoryProduct를 추가하면? 예외 던진다.")
     fun testInsert2() {
         // given
-        dao.insert(HistoryProduct(1))
-        dao.insert(HistoryProduct(2))
+        insertHistories(1, 2)
 
         // when & then
         assertThrows<SQLiteConstraintException> { dao.insert(HistoryProduct(1)) }
     }
 
     @Test
+    @DisplayName("상품 내역에 존재하는 상품 내역을 제거한다")
     fun testDelete() {
         // given
-        dao.insert(HistoryProduct(1))
-        dao.insert(HistoryProduct(2))
+        insertHistories(1, 2)
 
         // when
         dao.delete(HistoryProduct(1))
@@ -66,6 +66,7 @@ class HistoryProductDatabaseTest {
     }
 
     @Test
+    @DisplayName("상품 내역에 존재하는 상품 id 를 찾는다")
     fun testFindById() {
         // given
         dao.insert(HistoryProduct(1L))
@@ -79,11 +80,10 @@ class HistoryProductDatabaseTest {
     }
 
     @Test
+    @DisplayName("상품 내역에 존재하는 가장 최근 상품을 찾는다")
     fun testFindLatest() {
         // given
-        dao.insert(HistoryProduct(1))
-        dao.insert(HistoryProduct(3))
-        dao.insert(HistoryProduct(2))
+        insertHistories(1, 3, 2)
 
         // when
         val latest = dao.findLatest()
@@ -93,19 +93,33 @@ class HistoryProductDatabaseTest {
     }
 
     @Test
+    @DisplayName("상품 내역에 존재하는 모든 상품을 찾는다")
     fun testFindAll() {
         // given
-        dao.insert(HistoryProduct(1))
-        dao.insert(HistoryProduct(7))
-        dao.insert(HistoryProduct(4))
-        dao.insert(HistoryProduct(3))
-        dao.insert(HistoryProduct(2))
-        dao.insert(HistoryProduct(8))
+        insertHistories(1, 7, 4, 3, 2, 8)
 
         // when
         val all = dao.findAll()
 
         // then
         assertEquals(6, all.size)
+        assertDoesNotThrow {
+            dao.findById(1)
+            dao.findById(7)
+            dao.findById(4)
+            dao.findById(3)
+            dao.findById(2)
+            dao.findById(8)
+        }
+    }
+
+    /**
+     * 상품 내역에 id 값들을 넣는다
+     * @param ids 상품 내역에 저장할 id 값들
+     */
+    private fun insertHistories(vararg ids: Long) {
+        ids.forEach { id ->
+            dao.insert(HistoryProduct(id = id))
+        }
     }
 }
