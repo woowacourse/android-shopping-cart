ffaf8e827c864c69d2915bca084999d43f9d0c12
Gimun Kim <kmkim7575@gmail.com>
2024-06-01 17:40:48 +0900

    chore: 사용하지 않는 클래스 코드 제거

--- app/src/main/java/woowacourse/shopping/presentation/home/HomeUiState.kt
+++ app/src/main/java/woowacourse/shopping/presentation/home/HomeUiState.kt
@@ -9,13 +9,3 @@ data class HomeUiState(
     val totalQuantity: Int = 0,
     val productHistory: List<RecentProduct> = emptyList(),
 )
-
-//sealed class HomeUiState {
-//    data object Loading : HomeUiState()
-//    data class Error(val message: String) : HomeUiState()
-//    data class Success(
-//        val products: List<CartableProduct> = emptyList(),
-//        val totalQuantity: Int = 0,
-//        val productHistory: List<RecentProduct> = emptyList(),
-//    ) : HomeUiState()
-//}
