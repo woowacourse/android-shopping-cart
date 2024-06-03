d6936364e3461ad6f31061bf07542862e767e1cb
sh1mj1 <7wlgns@gmail.com>
2024-06-03 15:08:44 +0900

    :sparkles: OnItemQuantityChangeListener add 추가

--- app/src/main/java/woowacourse/shopping/ui/OnItemQuantityChangeListener.kt
+++ app/src/main/java/woowacourse/shopping/ui/OnItemQuantityChangeListener.kt
@@ -1,6 +1,10 @@
 package woowacourse.shopping.ui
 
 interface OnItemQuantityChangeListener {
+    fun onAdd(productId: Long){
+        // TODO: 상품을 추가하는 기능을 구현합니다.
+    }
+
     fun onIncrease(productId: Long)
 
     fun onDecrease(productId: Long)
