82bfa49251a2a4e989cb7dd6728bfc36e7b333c4
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-05-13 14:52:46 +0900

    feat: 상품 목록 화면 구현

--- app/src/main/java/woowacourse/shopping/ProductActivity.kt
+++ app/src/main/java/woowacourse/shopping/ProductActivity.kt
@@ -5,16 +5,25 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.commit
+import woowacourse.shopping.databinding.ActivityProductBinding
 
-class MainActivity : AppCompatActivity() {
+class ProductActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityProductBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_product)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.cl_product)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        supportFragmentManager.commit {
+            replace(R.id.fcv_products, ProductsFragment())
         }
     }
 }
