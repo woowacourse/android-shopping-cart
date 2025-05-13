077d1da4351801db9f37cd31062bb8d19c1ecac8
yrsel <xxknu88@gmail.com>
2025-05-13 19:43:45 +0900

    feat: Money 일급 객체 구현

--- app/src/main/java/woowacourse/shopping/domain/product/Product.kt
+++ app/src/main/java/woowacourse/shopping/domain/product/Product.kt
@@ -3,13 +3,7 @@ package woowacourse.shopping.domain.product
 data class Product(
     val imageUrl: String,
     val name: String,
-    val price: Int
+    private val _price: Money,
 ) {
-    init {
-        require(price >= 0) { ERROR_INVALID_PRICE }
-    }
-
-    companion object {
-        private const val ERROR_INVALID_PRICE = "금액은 0 이상 이어야 합니다."
-    }
+    val price: Int get() = _price.amount
 }
