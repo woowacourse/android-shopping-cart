6df90c16fe5860a233fca8ed3596b5a4f888f979
hwannow <vnfdlvgid061@naver.com>
2025-05-13 20:08:46 +0900

    feat: 장바구니 RecyclerView Adapter 구현

---
+++ app/src/main/java/woowacourse/shopping/presentation/shoppingcart/ShoppingCartAdapter.kt
@@ -0,0 +1,33 @@
+package woowacourse.shopping.presentation.shoppingcart
+
+import android.annotation.SuppressLint
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import woowacourse.shopping.domain.model.Goods
+
+class ShoppingCartAdapter(
+    private var items: List<Goods>,
+    private val clickListener: ShoppingCartClickListener,
+) : RecyclerView.Adapter<ShoppingCartViewHolder>() {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int,
+    ): ShoppingCartViewHolder {
+        return ShoppingCartViewHolder(parent, clickListener)
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    override fun onBindViewHolder(
+        holder: ShoppingCartViewHolder,
+        position: Int,
+    ) {
+        holder.bind(items[position])
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun updateItems(goods: List<Goods>) {
+        items = goods
+        notifyDataSetChanged()
+    }
+}
