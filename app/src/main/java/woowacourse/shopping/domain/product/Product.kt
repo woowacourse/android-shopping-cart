c5888298d7c4ca810d1f68ae02a7f1c1fe551cb9
지원 <cheerfulboys@naver.com>
2025-05-13 20:59:02 +0900

    feat: Product serialize 직렬화 적용

--- app/src/main/java/woowacourse/shopping/domain/product/Product.kt
+++ app/src/main/java/woowacourse/shopping/domain/product/Product.kt
@@ -1,9 +1,11 @@
 package woowacourse.shopping.domain.product
 
+import java.io.Serializable
+
 data class Product(
     val imageUrl: String,
     val name: String,
     private val _price: Money,
-) {
+): Serializable {
     val price: Int get() = _price.amount
 }
