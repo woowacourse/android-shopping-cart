40f15f9200f28e634cfe9b321ff17991e6542af5
DoableTuple <doabletuple@gmail.com>
2025-05-13 15:12:16 +0900

    refactor: 상품 상세 화면 툴바 수정

---
+++ app/src/main/java/woowacourse/shopping/ProductDetailActivity.kt
@@ -0,0 +1,20 @@
+package woowacourse.shopping
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ProductDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_product_detail)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.product_detail)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
