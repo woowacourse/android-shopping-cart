cd87c786864d312f8c3c9bec22b7d23e2e27bd2a
gahyunkim <mapledt001@naver.com>
2025-05-13 20:49:53 +0900

    chore: CartProduct 데이터베이스 삭제

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,9 +49,6 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.room.runtime)
-    kapt(libs.androidx.room.compiler)
-
     implementation(libs.androidx.activity.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.constraintlayout)
