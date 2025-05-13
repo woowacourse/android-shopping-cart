524c9737eac5872712defef60d7ba1835ec261db
moondev03 <moondev03@gmail.com>
2025-05-13 15:20:57 +0900

    feat: 상품 목록 더미 추가
    - 나이키 신발

---
+++ app/src/main/java/woowacourse/shopping/data/ProductStorage.kt
@@ -0,0 +1,68 @@
+package woowacourse.shopping.data
+
+import woowacourse.shopping.domain.model.Price
+import woowacourse.shopping.domain.model.Product
+
+val dummyProducts =
+    listOf(
+        Product(
+            1,
+            "나이키 에어 포스 1 '07 LX",
+            "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/afbbdaaf-1169-449e-98ff-745eba632fd9/WMNS+AIR+FORCE+1+%2707+LX.png",
+            Price(149_000),
+        ),
+        Product(
+            2,
+            "나이키 줌 보메로 5",
+            "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/1471109b-d228-4e18-8885-99c8d2d01fb1/W+NIKE+ZOOM+VOMERO+5.png",
+            Price(189_000),
+        ),
+        Product(
+            3,
+            "나이키 에어 테라 휴마라 SP",
+            "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/2fc25634-1149-4f4d-bfcc-96d38015a8af/NIKE+AIR+TERRA+HUMARA+SP.png",
+            Price(169_000),
+        ),
+        Product(
+            4,
+            "나이키 빅토리 원",
+            "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/48af7369-52b8-4692-921e-8e2a258874ed/NIKE+VICTORI+ONE+SHOWER+SLIDE.png",
+            Price(29_000),
+        ),
+        Product(
+            5,
+            "나이키 에어 포스 1 '07",
+            "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/4f37fca8-6bce-43e7-ad07-f57ae3c13142/AIR+FORCE+1+%2707.png",
+            Price(139_000),
+        ),
+        Product(
+            6,
+            "나이키 P-6000",
+            "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/48555f40-ae26-43e7-b27b-22699a1b892f/NIKE+P-6000.png",
+            Price(139_000),
+        ),
+        Product(
+            7,
+            "나이키 코트 버로우 미드 2",
+            "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/c4dd7c2e-789a-4a5c-bb5e-e63fc68e4f49/COURT+BOROUGH+MID+2+%28GS%29.png",
+            Price(89_000),
+        ),
+        Product(
+            8,
+            "나이키 스우시 1",
+            "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/3aa32913-dc45-474b-add2-8febcf3912fc/NIKE+SWOOSH+1+%28TD%29.png",
+            Price(69_000),
+        ),
+        Product(
+            9,
+            "나이키 코르테즈 빈티지 스웨이드",
+            "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/2ddaa009-c1c7-4d41-933f-e2741d907046/W+NIKE+CORTEZ+VNTG.png",
+            Price(129_000),
+        ),
+        Product(
+            10,
+            "나이키 킬샷 2",
+            "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/56968dea-097b-429e-b22f-65475c721f56/KILLSHOT+2.png",
+            Price(119_000),
+        ),
+    )
