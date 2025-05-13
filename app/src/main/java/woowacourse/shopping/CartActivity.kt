89f319c55c5325a01b14ae82a9c5bb996a819921
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-05-13 17:22:21 +0900

    feat: 장바구니 페이지 구현

---
+++ app/src/main/java/woowacourse/shopping/CartActivity.kt
@@ -0,0 +1,31 @@
+package woowacourse.shopping
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import woowacourse.shopping.databinding.ActivityCartBinding
+
+class CartActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCartBinding
+    private val cartProductAdapter: CartProductAdapter by lazy {
+        CartProductAdapter(
+            products,
+            this,
+        )
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_cart)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.cl_cart)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.rvCartProduct.adapter = cartProductAdapter
+    }
+}
---
+++ app/src/main/java/woowacourse/shopping/CartProductAdapter.kt
@@ -0,0 +1,44 @@
+package woowacourse.shopping
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import woowacourse.shopping.databinding.ItemCartProductBinding
+
+class CartProductAdapter(
+    val products: List<Product>,
+    val context: Context,
+) : RecyclerView.Adapter<CartProductAdapter.CartProductViewHolder>() {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): CartProductViewHolder {
+        val view = ItemCartProductBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return CartProductViewHolder(view)
+    }
+
+    override fun onBindViewHolder(
+        holder: CartProductViewHolder,
+        position: Int
+    ) {
+        val cartProduct = products[position]
+        holder.binding.apply {
+            tvCartProductName.text = cartProduct.name
+            tvCartProductPrice.text = cartProduct.price.toString()
+            Glide.with(context)
+                .load(cartProduct.imageUrl)
+                .placeholder(R.drawable.maxim_arabica)
+                .fallback(R.drawable.maxim_arabica)
+                .error(R.drawable.maxim_arabica)
+                .into(ivCartProduct)
+        }
+    }
+
+    override fun getItemCount(): Int = products.size
+
+    class CartProductViewHolder(
+        val binding: ItemCartProductBinding
+    ) : RecyclerView.ViewHolder(binding.root)
+}
