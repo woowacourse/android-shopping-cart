c567b0a3353e8d844bda125666f945f7fd99f80b
DoableTuple <doabletuple@gmail.com>
2025-05-13 16:08:31 +0900

    feat: 상품 더미 데이터 생성

--- app/src/main/java/woowacourse/shopping/domain/Product.kt
+++ app/src/main/java/woowacourse/shopping/domain/Product.kt
@@ -1,11 +1,7 @@
 package woowacourse.shopping.domain
 
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
-data class Product(
+class Product(
     val name: String,
     val price: Int,
     val imageUrl: String,
-) : Parcelable
+)
