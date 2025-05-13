6695d8d9e241aa2e9c04c659b68a158a73d167fe
DongjooSEO <readyjoo24@gmail.com>
2025-05-13 17:59:26 +0900

    feat: parcelable extension 구현

--- app/src/main/java/woowacourse/shopping/presentation/model/ProductUiModel.kt
+++ app/src/main/java/woowacourse/shopping/presentation/model/ProductUiModel.kt
@@ -1,13 +1,16 @@
 package woowacourse.shopping.presentation.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import woowacourse.shopping.domain.model.Product
 
+@Parcelize
 data class ProductUiModel(
     val id: Long,
     val name: String,
     val imageUrl: String,
     val price: Int,
-)
+) : Parcelable
 
 fun Product.toUiModel() =
     ProductUiModel(
