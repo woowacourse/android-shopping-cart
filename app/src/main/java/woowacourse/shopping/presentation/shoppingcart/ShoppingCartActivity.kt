a2507115a3e797637324f9de7814485ba4c57ec1
hwannow <vnfdlvgid061@naver.com>
2025-05-13 20:08:59 +0900

    feat: 장바구니 목록 삭제 기능 구현

--- app/src/main/java/woowacourse/shopping/presentation/shoppingcart/ShoppingCartActivity.kt
+++ app/src/main/java/woowacourse/shopping/presentation/shoppingcart/ShoppingCartActivity.kt
@@ -1,26 +1,56 @@
 package woowacourse.shopping.presentation.shoppingcart
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
 import woowacourse.shopping.R
+import woowacourse.shopping.databinding.ActivityShoppingCartBinding
 
 class ShoppingCartActivity : AppCompatActivity() {
+    private val binding: ActivityShoppingCartBinding by lazy {
+        DataBindingUtil.setContentView(this, R.layout.activity_shopping_cart)
+    }
+    private val viewModel: ShoppingCartViewModel by viewModels()
+
+    @SuppressLint("NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setUpScreen()
+        setupAppBar()
+
+        val adapter =
+            ShoppingCartAdapter(viewModel.goods.value ?: listOf()) { goods ->
+                viewModel.deleteGoods(goods)
+            }
+        binding.rvSelectedGoodsList.apply {
+            this.adapter = adapter
+            layoutManager = LinearLayoutManager(this@ShoppingCartActivity)
+        }
+
+        viewModel.goods.observe(this) { newList ->
+            adapter.updateItems(newList)
+        }
+    }
+
+    private fun setUpScreen() {
         enableEdgeToEdge()
-        setContentView(R.layout.activity_shopping_cart)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
 
+    private fun setupAppBar() {
         supportActionBar?.apply {
             setDisplayHomeAsUpEnabled(true)
             title = getString(R.string.label_shopping_cart_title)
