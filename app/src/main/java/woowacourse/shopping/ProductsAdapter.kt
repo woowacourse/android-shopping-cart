8f56e05031b17a5e981bbd85b3ee3bf2ae725d15
oungsi2000 <oungsi1000@gmail.com>
2025-05-13 16:49:25 +0900

    feat: 리사이클러 뷰 생성

---
+++ app/src/main/java/woowacourse/shopping/ProductsAdapter.kt
@@ -0,0 +1,27 @@
+package woowacourse.shopping
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import woowacourse.shopping.databinding.ItemProductBinding
+import woowacourse.shopping.domain.Product
+
+class ProductsAdapter : RecyclerView.Adapter<ViewHolder>() {
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+}
+
+class ViewHolder(binding: ItemProductBinding) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(item: Product) {
+
+    }
+
+}
