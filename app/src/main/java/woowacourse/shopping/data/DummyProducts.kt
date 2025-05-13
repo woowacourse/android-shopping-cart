58229a4c9d676ca0cce0c6495bc44d647e4447b9
Ham BeomJoon <hbj0802@naver.com>
2025-05-13 19:50:55 +0900

    feat: dummyProducts 분리

---
+++ app/src/main/java/woowacourse/shopping/data/DummyProducts.kt
@@ -0,0 +1,39 @@
+package woowacourse.shopping.data
+
+import woowacourse.shopping.domain.Product
+
+object DummyProducts {
+    val values: List<Product> =
+        listOf(
+            Product(
+                "맥심 모카골드 마일드",
+                12000,
+                "https://sitem.ssgcdn.com/64/93/82/item/0000006829364_i1_464.jpg",
+            ),
+            Product(
+                "맥심 모카골드 마일드",
+                8000,
+                "https://sitem.ssgcdn.com/64/93/82/item/0000006829364_i1_464.jpg",
+            ),
+            Product(
+                "맥심 모카골드 마일드",
+                7000,
+                "https://sitem.ssgcdn.com/64/93/82/item/0000006829364_i1_464.jpg",
+            ),
+            Product(
+                "맥심 모카골드 마일드",
+                6000,
+                "https://sitem.ssgcdn.com/64/93/82/item/0000006829364_i1_464.jpg",
+            ),
+            Product(
+                "맥심 모카골드 마일드",
+                5000,
+                "https://sitem.ssgcdn.com/64/93/82/item/0000006829364_i1_463.jpg",
+            ),
+            Product(
+                "맥심 모카골드 마일드",
+                3000,
+                "https://sitem.ssgcdn.com/64/93/82/item/0000006829364_i1_464.jpg",
+            ),
+        )
+}
--- app/src/main/java/woowacourse/shopping/domain/Product.kt
+++ app/src/main/java/woowacourse/shopping/domain/Product.kt
@@ -7,37 +7,3 @@ data class Product(
     val price: Int,
     val imageUrl: String,
 ) : Serializable
-
-val products: List<Product> =
-    listOf(
-        Product(
-            "맥심 모카골드 마일드",
-            9999,
-            "https://sitem.ssgcdn.com/64/93/82/item/0000006829364_i1_464.jpg",
-        ),
-        Product(
-            "맥심 모카골드 마일드",
-            9000,
-            "https://sitem.ssgcdn.com/64/93/82/item/0000006829364_i1_464.jpg",
-        ),
-        Product(
-            "맥심 모카골드 마일드",
-            9000,
-            "https://sitem.ssgcdn.com/64/93/82/item/0000006829364_i1_464.jpg",
-        ),
-        Product(
-            "맥심 모카골드 마일드",
-            9000,
-            "https://sitem.ssgcdn.com/64/93/82/item/0000006829364_i1_464.jpg",
-        ),
-        Product(
-            "맥심 모카골드 마일드",
-            9000,
-            "https://sitem.ssgcdn.com/64/93/82/item/0000006829364_i1_463.jpg",
-        ),
-        Product(
-            "맥심 모카골드 마일드",
-            9000,
-            "https://sitem.ssgcdn.com/64/93/82/item/0000006829364_i1_464.jpg",
-        ),
-    )
