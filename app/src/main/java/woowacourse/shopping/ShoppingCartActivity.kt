b0e2295c8a2f0a50bef69f95b2ed91f5999f1e08
oungsi2000 <oungsi1000@gmail.com>
2025-05-13 20:51:17 +0900

    fix: 장바구니 화면의 뒤로가기 액션바 수정

--- app/src/main/java/woowacourse/shopping/ShoppingCartActivity.kt
+++ app/src/main/java/woowacourse/shopping/ShoppingCartActivity.kt
@@ -3,6 +3,7 @@ package woowacourse.shopping
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import woowacourse.shopping.base.BaseActivity
 import woowacourse.shopping.databinding.ActivityShoppingCartBinding
 import woowacourse.shopping.domain.Product
@@ -10,6 +11,14 @@ import woowacourse.shopping.domain.Product
 class ShoppingCartActivity : BaseActivity<ActivityShoppingCartBinding>(R.layout.activity_shopping_cart) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val menuBar = binding.toolbar as Toolbar
+        menuBar.menu.findItem(R.id.menu_item_shopping_cart).isVisible = false
+        setSupportActionBar(menuBar)
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+
+            title = "Cart"
+        }
     }
 
     companion object {
