29aa2e10372e605d0856aba40e6c7f56d26a2f38
hwannow <vnfdlvgid061@naver.com>
2025-05-13 16:11:38 +0900

    feat: DataBinding 및 ViewModel 적용

--- app/src/main/java/woowacourse/shopping/presentation/goods/detail/GoodsDetailActivity.kt
+++ app/src/main/java/woowacourse/shopping/presentation/goods/detail/GoodsDetailActivity.kt
@@ -4,22 +4,33 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
 import woowacourse.shopping.R
+import woowacourse.shopping.databinding.ActivityGoodsDetailBinding
 import woowacourse.shopping.domain.model.Goods
+import woowacourse.shopping.util.getSerializableCompat
 
 class GoodsDetailActivity : AppCompatActivity() {
+    private val binding: ActivityGoodsDetailBinding by lazy {
+        DataBindingUtil.setContentView(this, R.layout.activity_goods_detail)
+    }
+    private val viewModel: GoodsDetailViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_goods_detail)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        viewModel.setGoods(intent.getSerializableCompat<Goods>(EXTRA_GOODS))
+        binding.goods = viewModel.goods.value
     }
 
     companion object {
