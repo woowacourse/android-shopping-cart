b335c8c9466007503e1245edcfecdce5d0ac15d9
gahyunkim <mapledt001@naver.com>
2025-05-13 14:47:24 +0900

    chore: kapt dependency 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.android.junit5)
     alias(libs.plugins.kotlin.android)
+    id("kotlin-kapt")
 }
 
 android {
@@ -40,6 +41,9 @@ android {
             excludes += "META-INF/**"
             excludes += "win32-x86*/**"
         }
+    }
+    buildFeatures {
+        dataBinding = true
     }
 }
 
