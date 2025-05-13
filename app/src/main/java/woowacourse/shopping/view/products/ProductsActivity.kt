4b8f118edf73c0bce561c145f5327fdf39630195
박지민 <pjm5489@naver.com>
2025-05-13 20:51:02 +0900

    refactor: 상품목록 뷰 개선

--- app/src/main/java/woowacourse/shopping/view/products/ProductsActivity.kt
+++ app/src/main/java/woowacourse/shopping/view/products/ProductsActivity.kt
@@ -1,13 +1,16 @@
 package woowacourse.shopping.view.products
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
+import woowacourse.shopping.CartActivity
 import woowacourse.shopping.R
 import woowacourse.shopping.databinding.ActivityProductsBinding
+import woowacourse.shopping.model.CartNavigationListener
 import woowacourse.shopping.model.Products
 
 class ProductsActivity : AppCompatActivity() {
@@ -19,7 +22,13 @@ class ProductsActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_products)
         adapter =
             ProductsAdapter(Products().value)
+        binding.rvProducts.addItemDecoration(GridSpacingItemDecoration(SPAN_COUNT, SPACING_DP))
         binding.rvProducts.adapter = adapter
+
+        binding.navigationListener =
+            CartNavigationListener {
+                startActivity(Intent(this, CartActivity::class.java))
+            }
 
         enableEdgeToEdge()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -27,5 +36,10 @@ class ProductsActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    companion object {
+        private const val SPAN_COUNT = 2
+        private const val SPACING_DP = 12f
     }
 }
