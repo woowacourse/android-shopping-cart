a5284f7fec3cf43ef80aba8c2b1faeecb6a98be7
DoableTuple <doabletuple@gmail.com>
2025-05-13 18:14:19 +0900

    refactor: ProductDetailActivity 인텐트 생성 메서드 구현

--- app/src/main/java/woowacourse/shopping/ProductDetailActivity.kt
+++ app/src/main/java/woowacourse/shopping/ProductDetailActivity.kt
@@ -1,10 +1,13 @@
 package woowacourse.shopping
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import woowacourse.shopping.domain.Product
 
 class ProductDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +18,19 @@ class ProductDetailActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+
+    companion object {
+        private const val KEY_PRODUCT = "product"
+
+        fun newIntent(
+            context: Context,
+            product: Product,
+        ): Intent {
+            return Intent(context, ProductDetailActivity::class.java).apply {
+                putExtra(KEY_PRODUCT, product)
+            }
         }
     }
 }
