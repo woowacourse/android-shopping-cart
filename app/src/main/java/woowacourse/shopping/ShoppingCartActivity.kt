53b2e2dddb136fcbe92eaf5144ec634a879859f7
oungsi2000 <oungsi1000@gmail.com>
2025-05-13 20:57:42 +0900

    feat: product 가져오는 로직 추가

--- app/src/main/java/woowacourse/shopping/ShoppingCartActivity.kt
+++ app/src/main/java/woowacourse/shopping/ShoppingCartActivity.kt
@@ -11,18 +11,25 @@ import woowacourse.shopping.domain.Product
 class ShoppingCartActivity : BaseActivity<ActivityShoppingCartBinding>(R.layout.activity_shopping_cart) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initMenuBar()
+        val product = intent.extras?.getParcelableCompat<Product>(KEY_PRODUCT)
+            ?:throw IllegalArgumentException(ERR_PRODUCT_IS_NULL)
+
+    }
+
+    private fun initMenuBar() {
         val menuBar = binding.toolbar as Toolbar
         menuBar.menu.findItem(R.id.menu_item_shopping_cart).isVisible = false
         setSupportActionBar(menuBar)
         supportActionBar?.apply {
             setDisplayHomeAsUpEnabled(true)
-
             title = "Cart"
         }
     }
 
     companion object {
         private const val KEY_PRODUCT = "product"
+        private const val ERR_PRODUCT_IS_NULL = "상품 정보가 로드되지 못했습니다"
 
         fun newIntent(
             context: Context,
