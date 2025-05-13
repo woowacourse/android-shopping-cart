31da3c96a5cd822a91b3c2e8e01dee9c80ab8bb7
hwannow <vnfdlvgid061@naver.com>
2025-05-13 15:37:05 +0900

    feat: 상품 상세 화면 추가

---
+++ app/src/main/java/woowacourse/shopping/presentation/goods/detail/GoodsDetailActivity.kt
@@ -0,0 +1,36 @@
+package woowacourse.shopping.presentation.goods.detail
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import woowacourse.shopping.R
+import woowacourse.shopping.domain.model.Goods
+
+class GoodsDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_goods_detail)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    companion object {
+        private const val EXTRA_GOODS = "goods"
+
+        fun newIntent(
+            context: Context,
+            goods: Goods,
+        ): Intent =
+            Intent(context, GoodsDetailActivity::class.java).apply {
+                putExtra(EXTRA_GOODS, goods)
+            }
+    }
+}
