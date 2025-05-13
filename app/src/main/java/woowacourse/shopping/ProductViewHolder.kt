c1bebdfefb056862cf7f20c3138e876ffdc07806
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-05-13 15:06:41 +0900

    feat: 상품 목록 화면에서 행당 2개의 상품을 출력

---
+++ app/src/main/java/woowacourse/shopping/ProductViewHolder.kt
@@ -0,0 +1,22 @@
+package woowacourse.shopping
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import woowacourse.shopping.databinding.ItemProductBinding
+
+class ProductViewHolder(
+    private val binding: ItemProductBinding,
+) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(item: Product) {
+        binding.product = item
+    }
+
+    companion object {
+        fun from(parent: ViewGroup): ProductViewHolder {
+            val layoutInflater = LayoutInflater.from(parent.context)
+            val binding = ItemProductBinding.inflate(layoutInflater, parent, false)
+            return ProductViewHolder(binding)
+        }
+    }
+}
--- app/src/main/java/woowacourse/shopping/ProductsActivity.kt
+++ app/src/main/java/woowacourse/shopping/ProductsActivity.kt
@@ -5,16 +5,25 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import woowacourse.shopping.databinding.ActivityProductsBinding
 
-class MainActivity : AppCompatActivity() {
+class ProductsActivity : AppCompatActivity() {
+    private val binding: ActivityProductsBinding by lazy {
+        ActivityProductsBinding.inflate(
+            layoutInflater,
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.productRoot)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.adapter = ProductAdapter(Product.dummies)
     }
 }
