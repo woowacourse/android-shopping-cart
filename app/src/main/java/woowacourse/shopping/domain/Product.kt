f18a2fa6d07c9bf7308bacc3b864b13e3039a769
jerry8282 <ka06196@naver.com>
2025-05-13 14:41:44 +0900

    feat: 상품의 가격은 0보다 크거나 같은 기능 구현

---
+++ app/src/main/java/woowacourse/shopping/domain/Product.kt
@@ -0,0 +1,16 @@
+package woowacourse.shopping.domain
+
+data class Product(
+    val productId: Long,
+    val name: String,
+    val price: Int,
+) {
+    init {
+        require(price >= MINIMUM_PRODUCT_PRICE) { INVALID_PRICE_MESSAGE }
+    }
+
+    companion object {
+        private const val INVALID_PRICE_MESSAGE = "가격은 0이상 이어야 합니다."
+        private const val MINIMUM_PRODUCT_PRICE = 0
+    }
+}
