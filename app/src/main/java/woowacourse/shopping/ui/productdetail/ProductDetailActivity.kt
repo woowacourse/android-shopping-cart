0d548180e3876c89e87d82c50cd0ea1284f7729b
지원 <cheerfulboys@naver.com>
2025-05-13 20:58:12 +0900

    feat: 상품 상세 페이지 구현

---
+++ app/src/main/java/woowacourse/shopping/ui/productdetail/ProductDetailActivity.kt
@@ -0,0 +1,61 @@
+package woowacourse.shopping.ui.productdetail
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import woowacourse.shopping.R
+import woowacourse.shopping.databinding.ActivityProductDetailBinding
+import woowacourse.shopping.domain.product.Product
+import woowacourse.shopping.utils.PriceFormatter
+import woowacourse.shopping.utils.intentSerializable
+
+class ProductDetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityProductDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_product_detail)
+        applyWindowInsets()
+        val product = intent?.intentSerializable(EXTRA_PRODUCT, Product::class.java) ?: throw IllegalArgumentException("알 수 없는 값입니다.")
+        binding.product = product
+        binding.detailClickListener = object : DetailClickListener {
+            override fun onAddToCartClick(product: Product) {
+                // 데이터 베이스에 저장한다
+                Toast.makeText(this@ProductDetailActivity, R.string.message_add_cart, Toast.LENGTH_SHORT).show()
+            }
+
+        }
+        binding.priceFormatter = PriceFormatter
+    }
+
+    private fun applyWindowInsets() {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    companion object {
+        private const val EXTRA_PRODUCT = "product"
+        fun newIntent(
+            context: Context,
+            product: Product
+        ): Intent {
+            return Intent(context, ProductDetailActivity::class.java).apply {
+                putExtra(
+                    EXTRA_PRODUCT,
+                    product,
+                )
+            }
+        }
+    }
+}
