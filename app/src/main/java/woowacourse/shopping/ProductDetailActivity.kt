5278e88e2c579398eec973e20853453ae4d563b7
junseo511 <prime54@g.skku.edu>
2025-05-13 15:28:59 +0900

    feat: 상품 상세 레이아웃 구현

---
+++ app/src/main/java/woowacourse/shopping/ProductDetailActivity.kt
@@ -0,0 +1,7 @@
+package woowacourse.shopping
+
+import woowacourse.shopping.databinding.ActivityProductDetailBinding
+
+class ProductDetailActivity : BaseActivity<ActivityProductDetailBinding>(R.layout.activity_product_detail) {
+
+}
