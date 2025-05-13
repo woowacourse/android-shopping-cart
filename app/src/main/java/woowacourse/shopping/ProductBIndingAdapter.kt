f920d5305c16985dae756c2abeea4ec8f335be78
oungsi2000 <oungsi1000@gmail.com>
2025-05-13 16:49:07 +0900

    feat: 바인딩어댑터 생성
    glide, 가격 포맷팅 적용

---
+++ app/src/main/java/woowacourse/shopping/ProductBIndingAdapter.kt
@@ -0,0 +1,19 @@
+package woowacourse.shopping
+
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+
+@BindingAdapter("android:price")
+fun setPrice(view: TextView, price:Int) {
+    view.text = view.context.getString(R.string.template_price, price)
+}
+
+@BindingAdapter("android:image")
+fun setImage(view: ImageView, url:String) {
+    Glide.with(view.context)
+        .load(url)
+        .placeholder(R.drawable.placeholder_product)
+        .into(view)
+}
