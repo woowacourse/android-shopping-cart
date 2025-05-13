0f5c14304c3839101ff4b56877454ec3d6f64c9a
DongjooSEO <readyjoo24@gmail.com>
2025-05-13 18:01:20 +0900

    feat: 상품 상세 화면 구현

--- app/src/main/java/woowacourse/shopping/presentation/view/detail/DetailFragment.kt
+++ app/src/main/java/woowacourse/shopping/presentation/view/detail/DetailFragment.kt
@@ -1,7 +1,31 @@
 package woowacourse.shopping.presentation.view.detail
 
+import android.os.Bundle
+import android.view.View
+import androidx.core.os.bundleOf
 import woowacourse.shopping.R
 import woowacourse.shopping.databinding.FragmentDetailBinding
 import woowacourse.shopping.presentation.base.BaseFragment
+import woowacourse.shopping.presentation.extension.getParcelableCompat
+import woowacourse.shopping.presentation.model.ProductUiModel
 
-class DetailFragment : BaseFragment<FragmentDetailBinding>(R.layout.fragment_detail)
+class DetailFragment : BaseFragment<FragmentDetailBinding>(R.layout.fragment_detail) {
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val product = arguments.getParcelableCompat<ProductUiModel>(EXTRA_PRODUCT)
+        binding.product = product
+    }
+
+    companion object {
+        private const val EXTRA_PRODUCT = "product"
+
+        fun newBundle(product: ProductUiModel) =
+            bundleOf(
+                EXTRA_PRODUCT to product,
+            )
+    }
+}
