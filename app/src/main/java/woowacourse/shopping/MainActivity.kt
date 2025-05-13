37c6c4027ba6d358370be32f41f94b275998b20e
oungsi2000 <oungsi1000@gmail.com>
2025-05-13 16:48:13 +0900

    refactor: BaseActivity 생성

---
+++ app/src/main/java/woowacourse/shopping/MainActivity.kt
@@ -0,0 +1,16 @@
+package woowacourse.shopping
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import woowacourse.shopping.base.BaseActivity
+import woowacourse.shopping.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding.rvProductList.adapter = ProductsAdapter()
+    }
+}
---
+++ app/src/main/java/woowacourse/shopping/base/BaseActivity.kt
@@ -0,0 +1,32 @@
+package woowacourse.shopping.base
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<T : ViewDataBinding>(
+    @LayoutRes private val layoutResId: Int,
+) : AppCompatActivity(layoutResId) {
+    protected lateinit var binding: T
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, layoutResId)
+        enableEdgeToEdge()
+        setWindowInsets()
+    }
+
+    private fun setWindowInsets() {
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
