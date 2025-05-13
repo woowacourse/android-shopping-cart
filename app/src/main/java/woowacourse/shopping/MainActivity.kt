9bc73323cd3f61a028e7a41417946dfadb1fa862
윤동현 <82762769+donghyun81@users.noreply.github.com>
2025-05-13 15:44:59 +0900

    feat: 상품 목록 화면 Ui구현

--- app/src/main/java/woowacourse/shopping/MainActivity.kt
+++ app/src/main/java/woowacourse/shopping/MainActivity.kt
@@ -1,6 +1,7 @@
 package woowacourse.shopping
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,10 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.action_bar_menu, menu)
+        return true
     }
 }
