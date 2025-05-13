bb848f090d35a0e95d910358912e847e5280dcfb
Ham BeomJoon <hbj0802@naver.com>
2025-05-13 15:01:45 +0900

    refactor: 상품 리사이클러뷰 패딩 수정

--- app/src/main/java/woowacourse/shopping/Product.kt
+++ app/src/main/java/woowacourse/shopping/Product.kt
@@ -7,10 +7,10 @@ data class Product(
 )
 
 val products: List<Product> = listOf(
-    Product("맥심 모카 골드 마일드 커피믹스", 4240, "https://sitem.ssgcdn.com/64/93/82/item/0000006829364_i1_464.jpg"),
-    Product("맥심 모카 골드 마일드 커피믹스", 4240, "https://sitem.ssgcdn.com/64/93/82/item/0000006829364_i1_464.jpg"),
-    Product("맥심 모카 골드 마일드 커피믹스", 4240, "https://sitem.ssgcdn.com/64/93/82/item/0000006829364_i1_464.jpg"),
-    Product("맥심 모카 골드 마일드 커피믹스", 4240, "https://sitem.ssgcdn.com/64/93/82/item/0000006829364_i1_464.jpg"),
-    Product("맥심 모카 골드 마일드 커피믹스", 4240, "https://sitem.ssgcdn.com/64/93/82/item/0000006829364_i1_464.jpg"),
-    Product("맥심 모카 골드 마일드 커피믹스", 4240, "https://sitem.ssgcdn.com/64/93/82/item/0000006829364_i1_464.jpg"),
+    Product("맥심 아라비카 100 리필 150g", 4240, "https://sitem.ssgcdn.com/64/93/82/item/0000006829364_i1_464.jpg"),
+    Product("맥심 아라비카 100 리필 150g", 4240, "https://sitem.ssgcdn.com/64/93/82/item/0000006829364_i1_464.jpg"),
+    Product("맥심 아라비카 100 리필 150g", 4240, "https://sitem.ssgcdn.com/64/93/82/item/0000006829364_i1_464.jpg"),
+    Product("맥심 아라비카 100 리필 150g", 4240, "https://sitem.ssgcdn.com/64/93/82/item/0000006829364_i1_464.jpg"),
+    Product("맥심 아라비카 100 리필 150g", 4240, "https://sitem.ssgcdn.com/64/93/82/item/0000006829364_i1_464.jpg"),
+    Product("맥심 아라비카 100 리필 150g", 4240, "https://sitem.ssgcdn.com/64/93/82/item/0000006829364_i1_464.jpg"),
 )
