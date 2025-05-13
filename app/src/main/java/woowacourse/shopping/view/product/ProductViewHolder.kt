83bdcd1659371add98beb61fe1242832ded0148c
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-05-13 16:08:07 +0900

    feat: 이미지 바인딩 어댑터 사용

--- app/src/main/java/woowacourse/shopping/view/product/ProductViewHolder.kt
+++ app/src/main/java/woowacourse/shopping/view/product/ProductViewHolder.kt
@@ -3,14 +3,16 @@ package woowacourse.shopping.view.product
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import woowacourse.shopping.domain.product.Product
+import woowacourse.shopping.data.ProductUrls.url
 import woowacourse.shopping.databinding.ItemProductBinding
+import woowacourse.shopping.domain.product.Product
 
 class ProductViewHolder(
     private val binding: ItemProductBinding,
 ) : RecyclerView.ViewHolder(binding.root) {
     fun bind(item: Product) {
         binding.product = item
+        binding.imageUrl = item.url
     }
 
     companion object {
