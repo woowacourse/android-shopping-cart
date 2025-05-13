ad3f31d749cca20887a98037224613af6d1c8619
윤동현 <82762769+donghyun81@users.noreply.github.com>
2025-05-13 15:42:31 +0900

    feat: 상품 정보 변경

--- app/src/main/java/woowacourse/shopping/data/ProductStorage.kt
+++ app/src/main/java/woowacourse/shopping/data/ProductStorage.kt
@@ -10,49 +10,55 @@ class ProductStorage {
         initialize()
     }
 
-    fun initialize() {
+    private fun initialize() {
         listOf(
             Product(
-                id = 1L,
-                name = "PET보틀-정사각(370ml)",
-                poster = "pet_bottle_square_370ml",
-                price = Price(10000),
+                1L,
+                "마리오 그린올리브 300g",
+                Price(3980),
+                "https://images.emarteveryday.co.kr/images/app/webapps/evd_web2/share/SKU/mall/27/41/8412707034127_1.png",
             ),
             Product(
-                id = 2L,
-                name = "PET보틀-정사각(500ml)",
-                poster = "pet_bottle_square_500ml",
-                price = Price(10000),
+                2L,
+                "비비고 통새우 만두 200g",
+                Price(81980),
+                "https://images.emarteveryday.co.kr/images/product/8801392067167/vSYMPCA3qqbLJjhv.png",
             ),
             Product(
-                id = 3L,
-                name = "PET보틀-밀크티(4500ml)",
-                poster = "pet_bottle_milktea",
-                price = Price(12000),
+                3L,
+                "스테비아 방울토마토 500g",
+                Price(89860),
+                "https://images.emarteveryday.co.kr/images/app/webapps/evd_web2/share/SKU/mall/97/12/2500000351297_1.png",
             ),
             Product(
-                id = 4L,
-                name = "PET보틀-납작(270ml)",
-                poster = "pet_bottle_flat",
-                price = Price(12000),
+                4L,
+                "디벨라 스파게티면 500g",
+                Price(1980),
+                "https://images.emarteveryday.co.kr/images/app/webapps/evd_web2/share/SKU/mall/85/00/8005121000085_1.png",
             ),
             Product(
-                id = 5L,
-                name = "PET보틀-단지(270ml)",
-                poster = "pet_bottle_jar",
-                price = Price(10000),
+                5L,
+                "생훈제연어 150g",
+                Price(89810),
+                "https://images.emarteveryday.co.kr/images/app/webapps/evd_web2/share/SKU/mall/00/29/8809433792900_1.png",
             ),
             Product(
-                id = 6L,
-                name = "PET보틀-단지-레몬(270ml)",
-                poster = "pet_bottle_jar_lemon",
-                price = Price(10000),
+                6L,
+                "CJ 고메 소바바치킨 소이허니윙 300g",
+                Price(89820),
+                "https://images.emarteveryday.co.kr/images/app/webapps/evd_web2/share/SKU/mall/32/30/8801392033032_1.png",
             ),
             Product(
-                id = 7L,
-                name = "PET보틀-원형(500ml)",
-                poster = "pet_bottle_round_500ml",
-                price = Price(12000),
+                7L,
+                "아리기 바질패스토 190g",
+                Price(8980),
+                "https://images.emarteveryday.co.kr/images/app/webapps/evd_web2/share/SKU/mall/65/07/8003740660765_1.jpg",
+            ),
+            Product(
+                8L,
+                "피코크 초마짬뽕 1240g",
+                Price(9980),
+                "https://images.emarteveryday.co.kr/images/app/webapps/evd_web2/share/SKU/mall/77/16/8809269671677_1.png",
             ),
         ).forEach {
             products[it.id] = it
