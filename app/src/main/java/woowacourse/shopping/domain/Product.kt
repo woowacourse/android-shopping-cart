fe1c4d4a93920b795f53b29e9b3efdaebf8217ce
DoableTuple <doabletuple@gmail.com>
2025-05-13 19:53:19 +0900

    refactor: Product를 data class로 변경

--- app/src/main/java/woowacourse/shopping/domain/Product.kt
+++ app/src/main/java/woowacourse/shopping/domain/Product.kt
@@ -4,7 +4,7 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-class Product(
+data class Product(
     val name: String,
     val price: Int,
     val imageUrl: String,
