1ead2127a12187b1f2e6d51a0864dec322427ca0
hwannow <vnfdlvgid061@naver.com>
2025-05-13 15:38:52 +0900

    feat: 직렬화 추가
    - Goods 객체를 직렬화합니다

--- app/src/main/java/woowacourse/shopping/domain/model/Goods.kt
+++ app/src/main/java/woowacourse/shopping/domain/model/Goods.kt
@@ -1,7 +1,9 @@
 package woowacourse.shopping.domain.model
 
+import java.io.Serializable
+
 data class Goods(
     val name: String,
     val price: Int,
     val imageRes: Int,
-)
+) : Serializable
