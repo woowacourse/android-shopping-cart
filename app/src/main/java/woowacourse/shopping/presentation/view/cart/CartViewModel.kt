d0721b65edc043cb25d9f53a4ffc4db8f09fb8ac
DongjooSEO <readyjoo24@gmail.com>
2025-05-14 16:53:22 +0900

    feat: 장바구니 조회 구현

--- app/src/main/java/woowacourse/shopping/presentation/view/cart/CartViewModel.kt
+++ app/src/main/java/woowacourse/shopping/presentation/view/cart/CartViewModel.kt
@@ -5,12 +5,13 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
-import woowacourse.shopping.domain.model.Price
-import woowacourse.shopping.domain.model.Product
+import woowacourse.shopping.RepositoryProvider
+import woowacourse.shopping.domain.repository.CartRepository
 import woowacourse.shopping.presentation.model.ProductUiModel
+import woowacourse.shopping.presentation.model.toUiModel
 
 class CartViewModel(
-    private val shoppingCart: List<Product>,
+    private val cartRepository: CartRepository,
 ) : ViewModel() {
     private val _products = MutableLiveData<List<ProductUiModel>>()
     val products: LiveData<List<ProductUiModel>> = _products
@@ -20,15 +21,9 @@ class CartViewModel(
     }
 
     private fun fetchShoppingCart() {
-        _products.value =
-            shoppingCart.map {
-                ProductUiModel(
-                    id = it.id,
-                    name = it.name,
-                    imageUrl = it.imageUrl,
-                    price = it.price.value,
-                )
-            }
+        cartRepository.getCartItems {
+            _products.postValue(it.map { it.toUiModel() })
+        }
     }
 
     fun deleteProduct(product: ProductUiModel) {
@@ -41,23 +36,10 @@ class CartViewModel(
                 override fun <T : ViewModel> create(
                     modelClass: Class<T>,
                     extras: CreationExtras,
-                ): T =
-                    CartViewModel(
-                        listOf(
-                            Product(
-                                1,
-                                "나이키 에어 포스 1 '07 LX",
-                                "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/afbbdaaf-1169-449e-98ff-745eba632fd9/WMNS+AIR+FORCE+1+%2707+LX.png",
-                                Price(149_000),
-                            ),
-                            Product(
-                                2,
-                                "나이키 줌 보메로 5",
-                                "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/1471109b-d228-4e18-8885-99c8d2d01fb1/W+NIKE+ZOOM+VOMERO+5.png",
-                                Price(189_000),
-                            ),
-                        ),
-                    ) as T
+                ): T {
+                    val repository = RepositoryProvider.cartRepository
+                    return CartViewModel(repository) as T
+                }
             }
     }
 }
