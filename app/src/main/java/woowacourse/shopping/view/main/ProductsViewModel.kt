c5fa8c1c8f28ebb0dc0e7ee96dbb06706b3963cb
oungsi2000 <oungsi1000@gmail.com>
2025-05-14 16:45:56 +0900

    feat: 상품 목록에서 더보기 버튼을 눌러 추가 로드하는 기능 추가

--- app/src/main/java/woowacourse/shopping/view/main/ProductsViewModel.kt
+++ app/src/main/java/woowacourse/shopping/view/main/ProductsViewModel.kt
@@ -4,24 +4,26 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import woowacourse.shopping.data.DummyProducts
+import woowacourse.shopping.data.DummyShoppingCart
 import woowacourse.shopping.domain.Product
-import woowacourse.shopping.view.page.Page
+import kotlin.math.min
 
 class ProductsViewModel : ViewModel() {
     private var allProducts: Set<Product> = DummyProducts.products.toSet()
-    private val _productsLiveData: MutableLiveData<Page<Product>> = MutableLiveData()
+    private val _productsLiveData: MutableLiveData<List<Product>> = MutableLiveData()
+    private val _pageLiveData: MutableLiveData<Int> = MutableLiveData()
 
     val totalSize: Int get() = allProducts.size
-    val productsLiveData: LiveData<Page<Product>> get() = _productsLiveData
+    val productsLiveData: LiveData<List<Product>> get() = _productsLiveData
+    val pageLiveData: LiveData<Int> get() = _pageLiveData
 
-    fun requestProductsPage(requestPage: Int) {
-        val page =
-            Page.from(
-                allProducts.toList(),
-                requestPage,
-                PAGE_SIZE,
-            )
-        _productsLiveData.value = page
+    val products: List<Product> get() = _productsLiveData.value ?: requestProductsPage(1)
+
+    fun requestProductsPage(page: Int): List<Product> {
+        val until = min(page*PAGE_SIZE, allProducts.size)
+        _productsLiveData.value = allProducts.toList().subList(0, until)
+        _pageLiveData.value = page
+        return _productsLiveData.value ?: emptyList()
     }
 
     companion object {
