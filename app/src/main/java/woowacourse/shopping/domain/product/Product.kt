79c3c6a3e9451d2be818382c7cb40383a12dacee
m6z1 <sonmyungj1zz@gmail.com>
2025-05-14 11:16:37 +0900

    feat: Product 직렬화

--- app/src/main/java/woowacourse/shopping/domain/product/Product.kt
+++ app/src/main/java/woowacourse/shopping/domain/product/Product.kt
@@ -1,10 +1,12 @@
 package woowacourse.shopping.domain.product
 
+import java.io.Serializable
+
 data class Product(
     val id: Long,
     val name: String,
     val price: Int,
-) {
+) : Serializable {
     companion object {
         val dummies: List<Product>
             get() =
