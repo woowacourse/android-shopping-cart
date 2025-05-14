6a29b859a2c00fcb2591024dad8fa08102a84c53
Ham BeomJoon <hbj0802@naver.com>
2025-05-14 10:43:07 +0900

    chore: 상품 더미 데이터 추가

--- app/src/main/java/woowacourse/shopping/data/DummyProducts.kt
+++ app/src/main/java/woowacourse/shopping/data/DummyProducts.kt
@@ -1,39 +1,106 @@
 package woowacourse.shopping.data
 
+import woowacourse.shopping.domain.Price
 import woowacourse.shopping.domain.Product
 
 object DummyProducts {
     val values: List<Product> =
         listOf(
             Product(
+                0,
                 "맥심 모카골드 마일드",
-                12000,
+                Price(12000),
                 "https://sitem.ssgcdn.com/64/93/82/item/0000006829364_i1_464.jpg",
             ),
             Product(
-                "맥심 모카골드 마일드",
-                8000,
-                "https://sitem.ssgcdn.com/64/93/82/item/0000006829364_i1_464.jpg",
+                1,
+                "맥심 슈프림골드 커피믹스",
+                Price(8000),
+                "https://sitem.ssgcdn.com/03/58/88/item/1000172885803_i1_464.jpg",
             ),
             Product(
-                "맥심 모카골드 마일드",
-                7000,
-                "https://sitem.ssgcdn.com/64/93/82/item/0000006829364_i1_464.jpg",
+                2,
+                "맥심 모카밀크 믹스커피",
+                Price(7000),
+                "https://sitem.ssgcdn.com/50/49/52/item/1000260524950_i1_464.jpg",
             ),
             Product(
-                "맥심 모카골드 마일드",
-                6000,
-                "https://sitem.ssgcdn.com/64/93/82/item/0000006829364_i1_464.jpg",
+                3,
+                "노브랜드 까페 N 블랙",
+                Price(6000),
+                "https://sitem.ssgcdn.com/43/58/17/item/1000555175843_i1_464.jpg",
             ),
             Product(
-                "맥심 모카골드 마일드",
-                5000,
-                "https://sitem.ssgcdn.com/64/93/82/item/0000006829364_i1_463.jpg",
+                4,
+                "노브랜드 까페 N 헤이즐넛향",
+                Price(5000),
+                "https://sitem.ssgcdn.com/44/58/17/item/1000555175844_i1_464.jpg",
             ),
             Product(
-                "맥심 모카골드 마일드",
-                3000,
-                "https://sitem.ssgcdn.com/64/93/82/item/0000006829364_i1_464.jpg",
+                5,
+                "노브랜드 코코넛 커피",
+                Price(3000),
+                "https://sitem.ssgcdn.com/24/94/44/item/1000595449424_i1_464.jpg",
+            ),
+            Product(
+                6,
+                "스타벅스 미디엄 로스트 아메리카노",
+                Price(12300),
+                "https://sitem.ssgcdn.com/10/22/20/item/1000602202210_i1_464.jpg",
+            ),
+            Product(
+                7,
+                "맥심 아라비카 100",
+                Price(3000),
+                "https://sitem.ssgcdn.com/82/57/69/item/0000007695782_i1_464.jpg",
+            ),
+            Product(
+                8,
+                "콜드브루 미니컵 커피",
+                Price(9000),
+                "https://sitem.ssgcdn.com/23/32/44/item/1000641443223_i1_464.jpg",
+            ),
+            Product(
+                9,
+                "아라비카 시그니처 블렌디드",
+                Price(17000),
+                "https://sitem.ssgcdn.com/45/77/23/item/1000646237745_i1_464.jpg",
+            ),
+            Product(
+                10,
+                "카누 에스프레스 쇼콜라 라떼",
+                Price(11000),
+                "https://sitem.ssgcdn.com/66/68/49/item/1000632496866_i1_464.jpg",
+            ),
+            Product(
+                11,
+                "이디야 트리플 아메리카노",
+                Price(13200),
+                "https://sitem.ssgcdn.com/14/47/79/item/1000626794714_i1_464.jpg",
+            ),
+            Product(
+                12,
+                "테이스터스 초이 오리지날",
+                Price(8800),
+                "https://sitem.ssgcdn.com/30/03/11/item/1000563110330_i1_464.jpg",
+            ),
+            Product(
+                13,
+                "카누 에스프레스 말차 라떼",
+                Price(9100),
+                "https://sitem.ssgcdn.com/04/25/36/item/1000567362504_i1_464.jpg",
+            ),
+            Product(
+                14,
+                "맥심 카누 돌체라떼 8입",
+                Price(4550),
+                "https://sitem.ssgcdn.com/64/88/40/item/1000105408864_i1_464.jpg",
+            ),
+            Product(
+                15,
+                "노브랜드 헤이즐넛향 드립백 원두커피",
+                Price(12500),
+                "https://sitem.ssgcdn.com/58/17/14/item/1000428141758_i1_464.jpg",
             ),
         )
 }
