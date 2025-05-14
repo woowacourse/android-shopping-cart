54434fc01a514bb9363876e77debbdfb6123a7a3
jerry8282 <ka06196@naver.com>
2025-05-14 22:25:46 +0900

    feat: dummyData 추가

--- app/src/main/java/woowacourse/shopping/data/dummy/ProductData.kt
+++ app/src/main/java/woowacourse/shopping/data/dummy/ProductData.kt
@@ -4,7 +4,7 @@ import woowacourse.shopping.domain.Product
 
 @Suppress("ktlint:standard:max-line-length")
 object ProductData {
-    val products: List<Product> =
+    private val items: List<Product> =
         listOf(
             Product(
                 1L,
@@ -42,5 +42,34 @@ object ProductData {
                 name = "에스프레소",
                 price = 2_000,
             ),
+            Product(
+                7L,
+                imageUrl = "https://naverbooking-phinf.pstatic.net/20240513_26/1715596375297jOjfM_JPEG/%BE%C6%C0%CC%BD%BA%BE%C6%B8%DE%B8%AE%C4%AB%B3%EB24.jpg",
+                name = "아메리카노",
+                price = 2_500,
+            ),
+            Product(
+                8L,
+                imageUrl = "https://naverbooking-phinf.pstatic.net/20240513_157/1715596288161xxA8S_JPEG/%B7%D5%BA%ED%B7%A2.JPG",
+                name = "롱블랙",
+                price = 2_300,
+            ),
+            Product(
+                9L,
+                imageUrl = "https://naverbooking-phinf.pstatic.net/20240513_109/1715596308956Qe7Ct_JPEG/%B7%D5%BA%ED%B7%A2_%B6%F3%B6%BC.JPG",
+                name = "롱블랙 라떼",
+                price = 3_000,
+            ),
+            Product(
+                10L,
+                imageUrl = "https://naverbooking-phinf.pstatic.net/20240513_50/1715596488074wl7IL_JPEG/%B6%F4%BF%C2%C5%A9%B8%B2%B6%F3%B6%BC.JPG",
+                name = "크림 라떼",
+                price = 4_200,
+            ),
         )
+
+    val products: List<Product> =
+        (0..5).flatMap { i ->
+            items.map { it.copy(id = it.id + i * 10) }
+        }
 }
