2b9e20612eb7cc0c98a0943fa04fc79066288165
chanho0908 <uni070@naver.com>
2025-05-14 09:05:31 +0900

    feat : 이미지 바인딩 어댑터 Null 처리 추가

--- app/src/main/java/woowacourse/shopping/view/util/bindingadapter/ImageBindingAdapter.kt
+++ app/src/main/java/woowacourse/shopping/view/util/bindingadapter/ImageBindingAdapter.kt
@@ -6,10 +6,14 @@ import com.bumptech.glide.Glide
 import woowacourse.shopping.R
 
 @BindingAdapter("setImage")
-fun ImageView.imageBindingAdapter(url: String) {
-    Glide.with(this)
-        .load(url)
-        .placeholder(R.drawable.landscape_placeholder)
-        .error(R.drawable.no_image)
-        .into(this)
+fun ImageView.imageBindingAdapter(url: String?) {
+    if (!url.isNullOrEmpty()) {
+        Glide.with(this)
+            .load(url)
+            .placeholder(R.drawable.landscape_placeholder)
+            .error(R.drawable.no_image)
+            .into(this)
+    } else {
+        setImageResource(R.drawable.landscape_placeholder)
+    }
 }
