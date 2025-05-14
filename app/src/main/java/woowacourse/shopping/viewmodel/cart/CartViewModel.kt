fa46ac49bb77320e878998c03db1702b8b45ee58
박지민 <pjm5489@naver.com>
2025-05-14 15:31:24 +0900

    feat: CartViewModel 생성

---
+++ app/src/main/java/woowacourse/shopping/viewmodel/cart/CartViewModel.kt
@@ -0,0 +1,43 @@
+package woowacourse.shopping.viewmodel.cart
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import woowacourse.shopping.model.products.Product
+import woowacourse.shopping.view.cart.Cart
+
+class CartViewModel(
+    private val cart: Cart,
+) : ViewModel() {
+    private val _productsInCart = MutableLiveData(cart.productsInCart)
+    val productsInCart = _productsInCart
+
+    fun addToCart(product: Product) {
+        cart.add(product)
+    }
+
+    fun removeToCart(product: Product) {
+        cart.remove(product)
+    }
+
+    companion object {
+        val Factory: ViewModelProvider.Factory =
+            object : ViewModelProvider.Factory {
+                @Suppress("UNCHECKED_CAST")
+                override fun <T : ViewModel> create(
+                    modelClass: Class<T>,
+                    extras: CreationExtras,
+                ): T {
+                    checkNotNull(extras[APPLICATION_KEY])
+                    extras.createSavedStateHandle()
+
+                    return CartViewModel(
+                        Cart,
+                    ) as T
+                }
+            }
+    }
+}
