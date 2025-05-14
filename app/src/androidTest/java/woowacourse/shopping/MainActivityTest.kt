ecfe24df174ed6b350fb8f8f9fc82b699fb5329a
DoableTuple <doabletuple@gmail.com>
2025-05-14 20:47:55 +0900

    test: 상품 목록 화면 테스트 코드 작성

--- app/src/androidTest/java/woowacourse/shopping/MainActivityTest.kt
+++ app/src/androidTest/java/woowacourse/shopping/MainActivityTest.kt
@@ -6,14 +6,7 @@ import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import org.junit.Rule
 import org.junit.Test
-import woowacourse.shopping.matcher.RecyclerViewMatcher.Companion.withRecyclerView
-import woowacourse.shopping.matcher.isDisplayed
-import woowacourse.shopping.matcher.isEllipsized
-import woowacourse.shopping.matcher.matchSize
-import woowacourse.shopping.matcher.matchText
-import woowacourse.shopping.matcher.performClick
-import woowacourse.shopping.matcher.scrollToPosition
-import woowacourse.shopping.matcher.sizeGreaterThan
+import woowacourse.shopping.RecyclerViewMatcher.Companion.withRecyclerView
 import woowacourse.shopping.view.main.MainActivity
 
 @Suppress("FunctionName")
@@ -24,19 +17,19 @@ class MainActivityTest {
     @Test
     fun 상품의_목록이_표시된다() {
         onView(
-            withRecyclerView(R.id.product_list).atPositionOnView(
+            withRecyclerView(R.id.rv_product_list).atPositionOnView(
                 0,
                 R.id.tv_product_name,
             ),
         ).matchText("[병천아우내] 모듬순대")
         onView(
-            withRecyclerView(R.id.product_list).atPositionOnView(
+            withRecyclerView(R.id.rv_product_list).atPositionOnView(
                 0,
                 R.id.tv_product_price,
             ),
         ).matchText("11,900원")
         onView(
-            withRecyclerView(R.id.product_list).atPositionOnView(
+            withRecyclerView(R.id.rv_product_list).atPositionOnView(
                 0,
                 R.id.iv_product_image,
             ),
@@ -45,21 +38,21 @@ class MainActivityTest {
 
     @Test
     fun 상품의_목록은_20개_단위로_표시된다() {
-        onView(withId(R.id.product_list)).check(matchSize(20))
+        onView(withId(R.id.rv_product_list)).check(matchSize(20))
     }
 
     @Test
     fun 더보기_버튼을_눌러서_상품을_추가_로드할_수_있다() {
-        onView(withId(R.id.product_list)).perform(scrollToPosition(19))
+        onView(withId(R.id.rv_product_list)).perform(scrollToPosition(19))
         Thread.sleep(1000)
         onView(withId(R.id.btn_load_more_products)).performClick()
-        onView(withId(R.id.product_list)).check(sizeGreaterThan(20))
+        onView(withId(R.id.rv_product_list)).check(sizeGreaterThan(20))
     }
 
     @Test
     fun 상품의_이름이_너무_길_경우_말줄임표로_표시된다() {
         onView(
-            withRecyclerView(R.id.product_list).atPositionOnView(
+            withRecyclerView(R.id.rv_product_list).atPositionOnView(
                 1,
                 R.id.tv_product_name,
             ),
@@ -69,7 +62,7 @@ class MainActivityTest {
     @Test
     fun 상품을_클릭하면_상품_상세_화면으로_이동된다() {
         onView(
-            withRecyclerView(R.id.product_list).atPositionOnView(
+            withRecyclerView(R.id.rv_product_list).atPositionOnView(
                 0,
                 R.id.tv_product_name,
             ),
@@ -80,6 +73,6 @@ class MainActivityTest {
     @Test
     fun 장바구니_아이콘을_클릭하면_장바구니_화면으로_이동된다() {
         onView(withId(R.id.menu_item_shopping_cart)).performClick()
-        onView(withId(R.id.shopping_cart_list)).isDisplayed()
+        onView(withId(R.id.rv_shopping_cart_list)).isDisplayed()
     }
 }
