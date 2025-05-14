484d6c9aa8a0d2f737c5139e7d204e145521429f
oungsi2000 <oungsi1000@gmail.com>
2025-05-14 13:37:57 +0900

    feat: 장바구니 더미데이터 추가

--- app/src/main/java/woowacourse/shopping/view/shoppingcart/ShoppingCartViewModel.kt
+++ app/src/main/java/woowacourse/shopping/view/shoppingcart/ShoppingCartViewModel.kt
@@ -3,10 +3,13 @@ package woowacourse.shopping.view.shoppingcart
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import woowacourse.shopping.data.DummyShoppingCart
 import woowacourse.shopping.domain.Product
 
 class ShoppingCartViewModel : ViewModel() {
-    private val _productsLiveData: MutableLiveData<List<Product>> = MutableLiveData(listOf())
+    private val _productsLiveData: MutableLiveData<List<Product>> = MutableLiveData(
+        DummyShoppingCart.products
+    )
     val productsLiveData: LiveData<List<Product>> get() = _productsLiveData
     val products: List<Product> get() = _productsLiveData.value ?: emptyList()
 
