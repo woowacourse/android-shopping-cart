af0ec2856f53bdddb6b1fd9d24c89ab5c1d1c57d
oungsi2000 <oungsi1000@gmail.com>
2025-05-14 17:58:55 +0900

    refactor: Page 객체 생성

--- app/src/main/java/woowacourse/shopping/view/page/Page.kt
+++ app/src/main/java/woowacourse/shopping/view/page/Page.kt
@@ -2,25 +2,30 @@ package woowacourse.shopping.view.page
 
 import kotlin.math.min
 
-data class Page<T>(
+data class Page <T> (
     val items: List<T>,
     val hasPrevious: Boolean,
     val hasNext: Boolean,
-    val currentPage: Int,
+    val currentPage: Int
 ) {
+    operator fun plus(page: Page<T>?): Page<T> {
+        page?: return this
+        return Page(
+            items = this.items + page.items,
+            hasPrevious = this.hasPrevious,
+            hasNext = page.hasNext,
+            currentPage = page.currentPage
+        )
+    }
     companion object {
-        fun <T> from(
-            allItems: List<T>,
-            requestPage: Int,
-            pageSize: Int,
-        ): Page<T> {
+        fun <T> from(allItems: List<T>, requestPage: Int, pageSize: Int):Page<T> {
             val from = requestPage * pageSize
             val until = min(from + pageSize, allItems.size)
             return Page(
                 items = allItems.subList(from, until),
                 hasPrevious = requestPage > 0,
                 hasNext = until < allItems.size,
-                requestPage,
+                requestPage
             )
         }
     }
