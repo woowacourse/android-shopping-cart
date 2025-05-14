c16b1d12c10c2702966c8bd48a0aad3af0828051
DoableTuple <doabletuple@gmail.com>
2025-05-14 16:16:52 +0900

    feat: 상품 목록의 맨 밑까지 스크롤하면 더보기 버튼이 표시된다

--- app/src/main/java/woowacourse/shopping/view/main/MainActivity.kt
+++ app/src/main/java/woowacourse/shopping/view/main/MainActivity.kt
@@ -1,7 +1,9 @@
 package woowacourse.shopping.view.main
 
 import android.os.Bundle
+import android.view.View
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import woowacourse.shopping.R
 import woowacourse.shopping.data.DummyProducts
 import woowacourse.shopping.databinding.ActivityMainBinding
@@ -16,6 +18,24 @@ class MainActivity :
         super.onCreate(savedInstanceState)
         binding.rvProductList.adapter = ProductsAdapter(DummyProducts.products, this)
         binding.rvProductList.layoutManager = GridLayoutManager(this, 2)
+        binding.rvProductList.addOnScrollListener(
+            object : RecyclerView.OnScrollListener() {
+                override fun onScrolled(
+                    recyclerView: RecyclerView,
+                    dx: Int,
+                    dy: Int,
+                ) {
+                    val layoutManager = recyclerView.layoutManager as GridLayoutManager
+                    val lastVisibleItemPosition = layoutManager.findLastVisibleItemPosition()
+                    binding.btnLoadMoreProducts.visibility =
+                        if (lastVisibleItemPosition == layoutManager.itemCount - 1) {
+                            View.VISIBLE
+                        } else {
+                            View.GONE
+                        }
+                }
+            },
+        )
     }
 
     override fun onProductSelected(product: Product) {
