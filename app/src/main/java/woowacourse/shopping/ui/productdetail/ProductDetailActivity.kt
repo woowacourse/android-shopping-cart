316e7455c06556534da2c251d13b6a31e347e4d0
지원 <cheerfulboys@naver.com>
2025-05-14 17:01:59 +0900

    feat: 상품 상세 페이지 툴바 구현

--- app/src/main/java/woowacourse/shopping/ui/productdetail/ProductDetailActivity.kt
+++ app/src/main/java/woowacourse/shopping/ui/productdetail/ProductDetailActivity.kt
@@ -3,6 +3,8 @@ package woowacourse.shopping.ui.productdetail
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -15,6 +17,8 @@ import woowacourse.shopping.data.product.CartRepositoryImpl
 import woowacourse.shopping.databinding.ActivityProductDetailBinding
 import woowacourse.shopping.domain.cart.CartRepository
 import woowacourse.shopping.domain.product.Product
+import woowacourse.shopping.ui.cart.CartActivity
+import woowacourse.shopping.ui.productlist.ProductListActivity
 import woowacourse.shopping.utils.intentSerializable
 
 class ProductDetailActivity : AppCompatActivity() {
@@ -27,6 +31,8 @@ class ProductDetailActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_product_detail)
         applyWindowInsets()
         val product = intent?.intentSerializable(EXTRA_PRODUCT, Product::class.java) ?: throw IllegalArgumentException("알 수 없는 값입니다.")
+        setSupportActionBar(binding.toolbarProductDetail)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
         binding.product = product
         binding.detailClickListener =
             object : DetailClickListener {
@@ -35,6 +41,21 @@ class ProductDetailActivity : AppCompatActivity() {
                     Toast.makeText(this@ProductDetailActivity, R.string.message_add_cart, Toast.LENGTH_SHORT).show()
                 }
             }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_product_detail, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_close -> {
+                startActivity(ProductListActivity.newIntent(this))
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 
     private fun applyWindowInsets() {
