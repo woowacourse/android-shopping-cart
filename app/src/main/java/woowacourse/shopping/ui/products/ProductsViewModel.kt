9abbcf2d1f725eae598e33649d4aa433c69770e2
leeyerin0210 <lyl8661@gmail.com>
2025-05-15 14:20:07 +0900

    refactor: ProductsViewModel 생성자에 DummyRepository 추가

--- app/src/main/java/woowacourse/shopping/ui/products/ProductsViewModel.kt
+++ app/src/main/java/woowacourse/shopping/ui/products/ProductsViewModel.kt
@@ -6,21 +6,27 @@ import androidx.lifecycle.ViewModel
 import woowacourse.shopping.data.repository.ProductDummyRepositoryImpl
 import woowacourse.shopping.domain.model.Product
 
-class ProductsViewModel : ViewModel() {
+class ProductsViewModel(
+    private val productsDummyRepository: ProductDummyRepositoryImpl = ProductDummyRepositoryImpl,
+) : ViewModel() {
     private val _products: MutableLiveData<List<Product>> = MutableLiveData(emptyList<Product>())
     val products: LiveData<List<Product>> get() = _products
 
     private val _isLoadable: MutableLiveData<Boolean> = MutableLiveData(false)
     val isLoadable: LiveData<Boolean> get() = _isLoadable
 
-    val productsDummyRepository = ProductDummyRepositoryImpl
-
-    fun updateProducts(count: Int) {
-        val newProducts = productsDummyRepository.fetchProducts(count, products.value?.lastOrNull()?.id ?: 0)
+    fun updateProducts(count: Int = SHOWN_PRODUCTS_COUNT) {
+        val newProducts =
+            productsDummyRepository.fetchProducts(count, products.value?.lastOrNull()?.id ?: 0)
         _products.value = products.value?.plus(newProducts)
     }
 
     fun updateIsLoadable() {
-        _isLoadable.value = productsDummyRepository.fetchIsProductsLoadable(products.value?.lastOrNull()?.id ?: 0)
+        _isLoadable.value =
+            productsDummyRepository.fetchIsProductsLoadable(products.value?.lastOrNull()?.id ?: 0)
+    }
+
+    companion object {
+        private const val SHOWN_PRODUCTS_COUNT: Int = 20
     }
 }
