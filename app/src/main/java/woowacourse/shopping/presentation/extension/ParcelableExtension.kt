00e162a44167783d2c5913d847d686d852238acc
moondev03 <moondev03@gmail.com>
2025-05-15 23:57:55 +0900

    refactor: Parcelable Extension 개선
    - 의미가 불분명한 메서드 제거
    - getParcelableCompat 개선

--- app/src/main/java/woowacourse/shopping/presentation/extension/ParcelableExtension.kt
+++ app/src/main/java/woowacourse/shopping/presentation/extension/ParcelableExtension.kt
@@ -4,20 +4,16 @@ import android.os.Build
 import android.os.Bundle
 import android.os.Parcelable
 
-inline fun <reified T : Parcelable> Bundle?.getParcelableCompat(key: String): T =
-    this.getParcelableCompatInternal(key) { k ->
+inline fun <reified T : Parcelable> Bundle?.getParcelableCompat(key: String): T {
+    requireNotNull(this) { "Bundle이 null입니다." }
+
+    val value =
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            this?.getParcelable(k, T::class.java)
+            this.getParcelable(key, T::class.java)
         } else {
             @Suppress("DEPRECATION")
-            this?.getParcelable(k) as? T
+            this.getParcelable(key) as? T
         }
-    }
 
-inline fun <T : Parcelable> Any?.getParcelableCompatInternal(
-    key: String,
-    getter: (String) -> T?,
-): T {
-    requireNotNull(this) { "Intent 또는 Bundle이 null입니다." }
-    return requireNotNull(getter(key)) { "$key 키에 해당하는 Parcelable 값이 존재하지 않습니다." }
+    return requireNotNull(value) { "$key 키에 해당하는 ${T::class.java.simpleName} 값이 존재하지 않습니다." }
 }
