fc3a8eed7dca20224fa83e2e4bd7fc0ad2b365d5
moondev03 <moondev03@gmail.com>
2025-05-17 11:43:05 +0900

    test: DetailViewModel 테스트 코드 작성

---
+++ app/src/test/java/woowacourse/shopping/fixture/FakeCartRepository.kt
@@ -0,0 +1,39 @@
+package woowacourse.shopping.fixture
+
+import woowacourse.shopping.domain.model.CartItem
+import woowacourse.shopping.domain.model.Product
+import woowacourse.shopping.domain.repository.CartRepository
+
+val fakeCartRepository =
+    object : CartRepository {
+        private val cart = mutableListOf<CartItem>()
+        private var nextId = 1L
+
+        override fun getCartItems(
+            limit: Int,
+            offset: Int,
+            callback: (List<CartItem>, Boolean) -> Unit,
+        ) {
+            val endIndex = (offset + limit).coerceAtMost(cart.size)
+            val cartItems = cart.subList(offset, endIndex)
+            val hasMore = endIndex < cart.size
+            callback(cartItems, hasMore)
+        }
+
+        override fun deleteCartItem(
+            id: Long,
+            callback: (Long) -> Unit,
+        ) {
+            val removed = cart.removeIf { it.id == id }
+            if (removed) callback(id)
+        }
+
+        override fun addCartItem(
+            product: Product,
+            callback: () -> Unit,
+        ) {
+            val cartItem = CartItem(nextId++, product.id, 1)
+            cart.add(cartItem)
+            callback()
+        }
+    }
---
+++ app/src/test/java/woowacourse/shopping/presentation/view/DetailViewModelTest.kt
@@ -0,0 +1,44 @@
+package woowacourse.shopping.presentation.view
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import woowacourse.shopping.fixture.dummyProductsFixture
+import woowacourse.shopping.fixture.fakeCartRepository
+import woowacourse.shopping.fixture.fakeProductRepository
+import woowacourse.shopping.presentation.model.toProduct
+import woowacourse.shopping.presentation.view.detail.DetailViewModel
+import woowacourse.shopping.presentation.view.util.InstantTaskExecutorExtension
+import woowacourse.shopping.presentation.view.util.getOrAwaitValue
+
+@ExtendWith(InstantTaskExecutorExtension::class)
+class DetailViewModelTest {
+    private lateinit var viewModel: DetailViewModel
+
+    @BeforeEach
+    fun setUp() {
+        viewModel = DetailViewModel(fakeCartRepository, fakeProductRepository)
+    }
+
+    @Test
+    fun `상품 ID에 해당하는 상품을 조회한다`() {
+        viewModel.fetchProduct(1)
+
+        val result = viewModel.product.getOrAwaitValue().toProduct()
+        val expected = dummyProductsFixture.find { it.id == 1L }
+
+        assertThat(result).isEqualTo(expected)
+    }
+
+    @Test
+    fun `상품을 장바구니에 추가하면 저장 상태가 변경된다`() {
+        viewModel.fetchProduct(1)
+
+        viewModel.addProduct()
+
+        val result = viewModel.saveState.getValue()
+
+        assertThat(result).isNotNull
+    }
+}
