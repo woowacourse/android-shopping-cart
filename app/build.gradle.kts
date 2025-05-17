3aafa66e3d1a941cb040bb98b08d7f94d6d23ab2
moondev03 <moondev03@gmail.com>
2025-05-17 10:41:54 +0900

    test: disable animations for tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,9 @@ android {
             excludes += "win32-x86*/**"
         }
     }
+    testOptions {
+        animationsDisabled = true
+    }
     buildFeatures {
         dataBinding = true
     }
