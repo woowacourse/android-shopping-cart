04a11189ce31f3cfbbddf931dfe780f4dd34bebb
이지윤 <02leejiyun@naver.com>
2025-05-19 02:49:16 +0900

    test: 상품 목록 ViewModel 테스트 수정

--- app/src/test/java/woowacourse/shopping/viewmodel/product/ProductCatalogViewModelTest.kt
+++ app/src/test/java/woowacourse/shopping/viewmodel/product/ProductCatalogViewModelTest.kt
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.extension.ExtendWith
 import woowacourse.shopping.data.product.ProductRepository
 import woowacourse.shopping.fixture.FakeProductRepository
 import woowacourse.shopping.view.product.catalog.ProductCatalogViewModel
+import woowacourse.shopping.view.product.catalog.adapter.ProductCatalogItem
 import woowacourse.shopping.viewmodel.InstantTaskExecutorExtension
 
 @ExtendWith(InstantTaskExecutorExtension::class)
@@ -21,51 +22,54 @@ class ProductCatalogViewModelTest {
     }
 
     @Test
-    fun `초기 로드시 첫 페이지의 상품 목록을 로드한다`() {
+    fun `초기 로드 시 첫 페이지의 상품과 더보기 버튼이 포함된다`() {
         // when
-        val products = viewModel.products.value
+        val items = viewModel.productItems.value.orEmpty()
 
         // then
-        assertEquals(20, products?.size)
-        assertEquals("Product 0", products?.first()?.name)
-        assertEquals(true, viewModel.hasNext)
+        assertEquals(21, items.size) // 20 ProductItem + 1 LoadMoreItem
+        assert(items.first() is ProductCatalogItem.ProductItem)
+        assert(items.last() is ProductCatalogItem.LoadMoreItem)
     }
 
     @Test
-    fun `더보기 버튼 클릭 시 다음 페이지의 상품이 추가된다`() {
-        // when
-        viewModel.onMoreClick()
-        val products = viewModel.products.value
-
-        // then
-        assertEquals(40, products?.size)
-        assertEquals(true, viewModel.hasNext)
-    }
-
-    @Test
-    fun `이전 페이지의 상품이 다음 페이지 로드 후에도 유지된다`() {
+    fun `더보기 버튼 클릭 시 다음 페이지의 상품이 추가되고 더보기 버튼이 포함된다`() {
         // when
         viewModel.onMoreClick()
+        val items = viewModel.productItems.value.orEmpty()
 
         // then
-        val products = viewModel.products.value!!
-        assertEquals("Product 0", products[0].name)
-        assertEquals("Product 21", products[21].name)
+        assertEquals(41, items.size) // 40 ProductItem + 1 LoadMoreItem
+        assert(items.last() is ProductCatalogItem.LoadMoreItem)
     }
 
     @Test
-    fun `마지막 페이지까지 로딩 후에는 hasNext가 false여야 한다`() {
+    fun `마지막 페이지 이후에는 더보기 버튼이 없다`() {
         // when
         repeat(4) { viewModel.onMoreClick() }
+        val items = viewModel.productItems.value.orEmpty()
 
         // then
-        val products = viewModel.products.value
-        assertEquals(100, products?.size)
-        assertEquals(false, viewModel.hasNext)
+        assertEquals(100, items.size)
+        assert(items.none { it is ProductCatalogItem.LoadMoreItem })
     }
 
     @Test
-    fun `상품 클릭 시 selectedProduct에 반영된다`() {
+    fun `더보기 후에도 이전 페이지 상품이 유지된다`() {
+        viewModel.onMoreClick()
+
+        val items =
+            viewModel.productItems.value
+                .orEmpty()
+                .filterIsInstance<ProductCatalogItem.ProductItem>()
+                .map { it.product }
+
+        assertEquals("Product 0", items.first().name)
+        assertEquals("Product 39", items.last().name)
+    }
+
+    @Test
+    fun `상품 클릭 시 선택된 상품이 selectedProduct에 반영된다`() {
         // given
         val product = repository.getAll().first()
 
