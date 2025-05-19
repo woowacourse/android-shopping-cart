4f0d8fb1cd50ffc73d34daec4cc2cab61566b7ea
oungsi2000 <oungsi1000@gmail.com>
2025-05-19 17:53:05 +0900

    fix: 안드로이드 환경에서 glide 로드 안되는 문제 해결

--- app/src/main/java/woowacourse/shopping/view/mapper/ProductBindingAdapter.kt
+++ app/src/main/java/woowacourse/shopping/view/mapper/ProductBindingAdapter.kt
@@ -1,9 +1,16 @@
 package woowacourse.shopping.view.mapper
 
+import android.graphics.drawable.Drawable
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.SingleRequest
+import com.bumptech.glide.request.target.Target
 import woowacourse.shopping.R
 
 @BindingAdapter("android:price")
@@ -22,6 +29,6 @@ fun setImage(
     Glide.with(view.context)
         .load(url)
         .placeholder(R.drawable.placeholder_product)
-        .override(154, 154)
         .into(view)
+
 }
