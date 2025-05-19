671035571ba24e700a6a3226560e317a2ae33ea6
oungsi2000 <oungsi1000@gmail.com>
2025-05-19 20:46:01 +0900

    refactor: 바인딩어댑터 접두사 변경

--- app/src/main/java/woowacourse/shopping/view/mapper/ProductBindingAdapter.kt
+++ app/src/main/java/woowacourse/shopping/view/mapper/ProductBindingAdapter.kt
@@ -1,16 +1,9 @@
 package woowacourse.shopping.view.mapper
 
-import android.graphics.drawable.Drawable
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.DataSource
-import com.bumptech.glide.load.engine.GlideException
-import com.bumptech.glide.request.RequestListener
-import com.bumptech.glide.request.RequestOptions
-import com.bumptech.glide.request.SingleRequest
-import com.bumptech.glide.request.target.Target
 import woowacourse.shopping.R
 
 @BindingAdapter("android:price")
@@ -21,7 +14,7 @@ fun setPrice(
     view.text = view.context.getString(R.string.template_price, price)
 }
 
-@BindingAdapter("android:image")
+@BindingAdapter("shoppingCart:image")
 fun setImage(
     view: ImageView,
     url: String,
@@ -30,5 +23,4 @@ fun setImage(
         .load(url)
         .placeholder(R.drawable.placeholder_product)
         .into(view)
-
 }
