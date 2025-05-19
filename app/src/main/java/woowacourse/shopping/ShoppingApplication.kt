234f7e8c43468227475422821bb0918b1c646dec
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-05-19 15:30:42 +0900

    refactor(ProductRespositoryImpl): 데이터를 외부에서 받아 올 수 있도록 수정

--- app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
+++ app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
@@ -3,11 +3,16 @@ package woowacourse.shopping
 import android.app.Application
 import woowacourse.shopping.data.cartRepository.CartRepository
 import woowacourse.shopping.data.cartRepository.CartRepositoryImpl
+import woowacourse.shopping.data.productsRepository.DummyProducts
 import woowacourse.shopping.data.productsRepository.ProductRepository
 import woowacourse.shopping.data.productsRepository.ProductRepositoryImpl
 
 class ShoppingApplication : Application() {
-    val productRepository: ProductRepository by lazy { ProductRepositoryImpl.initialize() }
+    val productRepository: ProductRepository by lazy {
+        ProductRepositoryImpl.initialize(
+            DummyProducts.value,
+        )
+    }
     val cartRepository: CartRepository by lazy { CartRepositoryImpl.initialize(this) }
 
     override fun onCreate() {
--- app/src/main/java/woowacourse/shopping/data/productsRepository/ProductRepositoryImpl.kt
+++ app/src/main/java/woowacourse/shopping/data/productsRepository/ProductRepositoryImpl.kt
@@ -2,13 +2,15 @@ package woowacourse.shopping.data.productsRepository
 
 import woowacourse.shopping.domain.Product
 
-class ProductRepositoryImpl private constructor() : ProductRepository {
+class ProductRepositoryImpl private constructor(
+    private val data: List<Product>,
+) : ProductRepository {
     private var currentIndex = 0
 
-    override fun getProducts(): List<Product> = DummyProducts.value
+    override fun getProducts(): List<Product> = data
 
     override fun getProducts(limit: Int): List<Product> {
-        val products = DummyProducts.value.drop(currentIndex).take(limit)
+        val products = data.drop(currentIndex).take(limit)
         currentIndex += limit
         return products
     }
@@ -16,6 +18,6 @@ class ProductRepositoryImpl private constructor() : ProductRepository {
     companion object {
         private var instance: ProductRepositoryImpl? = null
 
-        fun initialize(): ProductRepositoryImpl = instance ?: ProductRepositoryImpl().also { instance = it }
+        fun initialize(data: List<Product>): ProductRepositoryImpl = instance ?: ProductRepositoryImpl(data).also { instance = it }
     }
 }
