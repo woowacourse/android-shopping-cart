588bd2c1a3a6964aff593e643a68b3fdb1e786dd
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-05-19 15:27:08 +0900

    refactor(ShoppingApplication): ProductRepositoryImpl, CartRepositoryImpl -> ProductRepository, CartRepository 타입 변경

--- app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
+++ app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
@@ -1,12 +1,14 @@
 package woowacourse.shopping
 
 import android.app.Application
+import woowacourse.shopping.data.cartRepository.CartRepository
 import woowacourse.shopping.data.cartRepository.CartRepositoryImpl
+import woowacourse.shopping.data.productsRepository.ProductRepository
 import woowacourse.shopping.data.productsRepository.ProductRepositoryImpl
 
 class ShoppingApplication : Application() {
-    val productRepository: ProductRepositoryImpl by lazy { ProductRepositoryImpl.initialize() }
-    val cartRepository: CartRepositoryImpl by lazy { CartRepositoryImpl.initialize(this) }
+    val productRepository: ProductRepository by lazy { ProductRepositoryImpl.initialize() }
+    val cartRepository: CartRepository by lazy { CartRepositoryImpl.initialize(this) }
 
     override fun onCreate() {
         super.onCreate()
