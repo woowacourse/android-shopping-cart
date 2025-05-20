e73ebb566475f6d4dfb8eec926bbdfd628efde97
m6z1 <sonmyungj1zz@gmail.com>
2025-05-20 14:55:56 +0900

    test: ProductsViewModel 테스트 추가

--- app/src/test/java/woowacourse/shopping/fixture/ProductsItemFixtures.kt
+++ app/src/test/java/woowacourse/shopping/fixture/ProductsItemFixtures.kt
@@ -3,4 +3,5 @@ package woowacourse.shopping.fixture
 import woowacourse.shopping.view.product.ProductsItem
 import woowacourse.shopping.view.product.ProductsItem.LoadItem
 
-val PRODUCT_ITEMS = PRODUCTS.map { ProductsItem.ProductItem(it) } + LoadItem(true)
+val PRODUCT_ITEMS_20_MORE = PRODUCTS_COUNT_20.map { ProductsItem.ProductItem(it) } + LoadItem
+val PRODUCT_ITEMS_20 = PRODUCTS_COUNT_20.map { ProductsItem.ProductItem(it) }
---
+++ app/src/test/java/woowacourse/shopping/view/product/ProductsViewModelTest.kt
@@ -0,0 +1,105 @@
+package woowacourse.shopping.view.product
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import io.mockk.clearAllMocks
+import io.mockk.every
+import io.mockk.invoke
+import io.mockk.mockk
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Rule
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
+import org.junit.jupiter.api.extension.ExtendWith
+import woowacourse.shopping.data.product.repository.ProductsRepository
+import woowacourse.shopping.domain.product.Product
+import woowacourse.shopping.fixture.PRODUCTS_COUNT_20
+import woowacourse.shopping.fixture.PRODUCTS_COUNT_21
+import woowacourse.shopping.fixture.PRODUCT_ITEMS_20
+import woowacourse.shopping.fixture.PRODUCT_ITEMS_20_MORE
+import woowacourse.shopping.view.common.InstantTaskExecutorExtension
+import woowacourse.shopping.view.common.getOrAwaitValue
+import woowacourse.shopping.view.product.ProductsItem.LoadItem
+import woowacourse.shopping.view.product.ProductsItem.ProductItem
+
+@ExtendWith(InstantTaskExecutorExtension::class)
+class ProductsViewModelTest {
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var productViewModel: ProductsViewModel
+    private lateinit var productsRepository: ProductsRepository
+
+    @BeforeEach
+    fun setUp() {
+        productsRepository = mockk()
+        productViewModel = ProductsViewModel(productsRepository)
+    }
+
+    @Test
+    fun `상품 목록 업데이트 성공 시 결과값 개수가 20개 초과일 경우 ProductItem 과 LoadItem 추가된 products 값이 된다`() {
+        // given:
+        every {
+            productsRepository.load(any(), any(), captureLambda())
+        } answers {
+            lambda<(Result<List<Product>>) -> Unit>().invoke(Result.success(PRODUCTS_COUNT_21))
+        }
+
+        // when:
+        productViewModel.updateProducts()
+
+        // then:
+        val result = productViewModel.products.getOrAwaitValue()
+        assertAll({
+            assertThat(result.count { it is ProductItem }).isEqualTo(20)
+            assertThat(result.last()).isInstanceOf(LoadItem::class.java)
+            assertThat(result).isEqualTo(PRODUCT_ITEMS_20_MORE)
+        })
+    }
+
+    @Test
+    fun `상품 목록 업데이트 성공 시 결과값 개수가 20개 이하일 경우 ProductItem 만 추가된 products 값이 된다`() {
+        // given:
+        every {
+            productsRepository.load(any(), any(), captureLambda())
+        } answers {
+            lambda<(Result<List<Product>>) -> Unit>().invoke(Result.success(PRODUCTS_COUNT_20))
+        }
+
+        // when:
+        productViewModel.updateProducts()
+
+        // then:
+        val result = productViewModel.products.getOrAwaitValue()
+        assertAll({
+            assertThat(result.count { it is ProductItem }).isEqualTo(20)
+            assertThat(result).isEqualTo(PRODUCT_ITEMS_20)
+        })
+    }
+
+    @Test
+    fun `상품 목록 업데이트 성공 시 결과값이 비어있을 경우 products 는 빈 값 이다`() {
+        // given:
+        every {
+            productsRepository.load(any(), any(), captureLambda())
+        } answers {
+            lambda<(Result<List<Product>>) -> Unit>().invoke(Result.success(emptyList()))
+        }
+
+        // when:
+        productViewModel.updateProducts()
+
+        // then:
+        val result = productViewModel.products.getOrAwaitValue()
+        assertAll({
+            assertThat(result.count { it is ProductItem }).isEqualTo(0)
+            assertThat(result).isEqualTo(emptyList<ProductsItem>())
+        })
+    }
+
+    @AfterEach
+    fun tearDown() {
+        clearAllMocks()
+    }
+}
