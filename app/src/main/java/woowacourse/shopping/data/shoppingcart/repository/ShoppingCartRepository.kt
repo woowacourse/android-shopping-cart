7691b17ab0db3e2aea7ea1454d13dd056e4d49b9
cucumber99 <dlsguq1@gmail.com>
2025-05-20 18:43:36 +0900

    feat: ShoppingCartRepository 선언

---
+++ app/src/main/java/woowacourse/shopping/data/shoppingcart/repository/ShoppingCartRepository.kt
@@ -0,0 +1,11 @@
+package woowacourse.shopping.data.shoppingcart.repository
+
+import woowacourse.shopping.domain.model.Goods
+
+interface ShoppingCartRepository {
+    fun addGoods(goods: Goods)
+
+    fun removeGoods(goods: Goods)
+
+    fun getGoods(page: Int, count: Int): List<Goods>
+}
