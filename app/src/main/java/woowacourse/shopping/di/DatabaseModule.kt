bc08560015cbec6dae15689a0bb9f8d524f7b579
junseo511 <prime54@g.skku.edu>
2025-05-20 13:47:15 +0900

    refactor: 불필요한 모듈 제거

--- app/src/main/java/woowacourse/shopping/di/DatabaseModule.kt
+++
@@ -1,8 +0,0 @@
-package woowacourse.shopping.di
-
-import android.content.Context
-import woowacourse.shopping.data.database.ShoppingDatabase
-
-object DatabaseModule {
-    fun provideDatabase(context: Context): ShoppingDatabase = ShoppingDatabase.getInstance(context)
-}
