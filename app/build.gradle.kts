2777627a11c29babd6c4aa63257271780cbf5f13
junseo511 <prime54@g.skku.edu>
2025-05-20 15:36:16 +0900

    refactor: 리사이클러뷰 스크롤 영역 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,4 +69,8 @@ dependencies {
     implementation(libs.glide)
     implementation(libs.androidx.room.runtime)
     ksp(libs.androidx.room.compiler)
+    implementation(platform("com.squareup.okhttp3:okhttp-bom:4.12.0"))
+    implementation("com.squareup.okhttp3:okhttp")
+    implementation("com.squareup.okhttp3:logging-interceptor")
+    testImplementation("com.squareup.okhttp3:mockwebserver")
 }
