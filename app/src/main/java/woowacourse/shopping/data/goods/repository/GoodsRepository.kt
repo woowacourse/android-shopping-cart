cb0740c433441444a65c5cf6a84a5a14b48432c4
cucumber99 <dlsguq1@gmail.com>
2025-05-20 17:10:52 +0900

    feat: GoodsRepository 선언

---
+++ app/src/main/java/woowacourse/shopping/data/goods/repository/GoodsRepository.kt
@@ -0,0 +1,7 @@
+package woowacourse.shopping.data.goods.repository
+
+import woowacourse.shopping.domain.model.Goods
+
+interface GoodsRepository {
+    fun getPagedGoods(page: Int, count: Int): List<Goods>
+}
