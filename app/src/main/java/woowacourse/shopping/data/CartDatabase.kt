6bd138349d8b4609a3dde3b5835ea6672fd5464a
gahyunkim <mapledt001@naver.com>
2025-05-20 14:32:45 +0900

    refactor: CartDatabase 백킹 프로퍼티 수정

--- app/src/main/java/woowacourse/shopping/data/CartDatabase.kt
+++ app/src/main/java/woowacourse/shopping/data/CartDatabase.kt
@@ -4,14 +4,13 @@ import woowacourse.shopping.product.catalog.ProductUiModel
 import woowacourse.shopping.product.detail.CartUiState
 
 object CartDatabase : CartProductDataSource {
-    private val _cartProducts: MutableList<ProductUiModel> = mutableListOf()
-    val cartProducts: List<ProductUiModel> get() = _cartProducts.toList()
+    private val cartProducts: MutableList<ProductUiModel> = mutableListOf()
 
     override fun insertCartProduct(cartProduct: ProductUiModel): CartUiState {
-        val beforeInsertSize = _cartProducts.size
-        _cartProducts.add(cartProduct)
+        val beforeInsertSize = cartProducts.size
+        cartProducts.add(cartProduct)
 
-        return if (beforeInsertSize < _cartProducts.size) {
+        return if (beforeInsertSize < cartProducts.size) {
             CartUiState.SUCCESS
         } else {
             CartUiState.FAIL
@@ -19,8 +18,8 @@ object CartDatabase : CartProductDataSource {
     }
 
     override fun deleteCartProduct(cartProduct: ProductUiModel) {
-        _cartProducts.remove(cartProduct)
+        cartProducts.remove(cartProduct)
     }
 
-    override fun cartProducts(): List<ProductUiModel> = cartProducts
+    override fun cartProducts(): List<ProductUiModel> = cartProducts.toList()
 }
