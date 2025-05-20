07b67648857a3e6f0b8c2bb2e88e1f9f4d4f6065
cucumber99 <dlsguq1@gmail.com>
2025-05-20 18:43:46 +0900

    feat: ShoppingCartRepository 구현체 추가

---
+++ app/src/main/java/woowacourse/shopping/data/shoppingcart/repository/ShoppingCartRepositoryImpl.kt
@@ -0,0 +1,21 @@
+package woowacourse.shopping.data.shoppingcart.repository
+
+import woowacourse.shopping.data.shoppingcart.ShoppingDataBase
+import woowacourse.shopping.domain.model.Goods
+
+class ShoppingCartRepositoryImpl(
+    private val database: ShoppingDataBase = ShoppingDataBase
+) : ShoppingCartRepository {
+    override fun addGoods(goods: Goods) {
+        database.addItem(goods)
+    }
+
+    override fun removeGoods(goods: Goods) {
+        database.removeItem(goods)
+    }
+
+    override fun getGoods(page: Int, count: Int): List<Goods> {
+        return database.getPagedGoods(page, count)
+    }
+
+}
