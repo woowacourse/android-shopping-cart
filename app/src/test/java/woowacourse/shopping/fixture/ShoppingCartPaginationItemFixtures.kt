c1f043ce5ee170169dced44f6304e840ab3ae954
m6z1 <sonmyungj1zz@gmail.com>
2025-05-20 15:25:18 +0900

    test: ShoppingCartViewModel 테스트 추가

--- app/src/test/java/woowacourse/shopping/fixture/ShoppingCartPaginationItemFixtures.kt
+++ app/src/test/java/woowacourse/shopping/fixture/ShoppingCartPaginationItemFixtures.kt
@@ -1,8 +1,26 @@
 package woowacourse.shopping.fixture
 
-import woowacourse.shopping.view.product.ProductsItem.LoadItem
 import woowacourse.shopping.view.shoppingCart.ShoppingCartItem
 
-val SHOPPING_CART_PRODUCT_ITEMS_5_MORE =
-    getProducts(5).map { ShoppingCartItem.ProductItem(it) } + LoadItem
-val SHOPPING_CART_PRODUCT_ITEMS_4 = getProducts(4).map { ShoppingCartItem.ProductItem(it) }
+val SHOPPING_CART_PRODUCT_ITEMS_5_PAGE_1MORE =
+    getProducts(5).map { ShoppingCartItem.ProductItem(it) } +
+        ShoppingCartItem.PaginationItem(
+            1,
+            true,
+            false,
+        )
+val SHOPPING_CART_PRODUCT_ITEMS_4_PAGE_1 =
+    getProducts(4).map { ShoppingCartItem.ProductItem(it) } +
+        ShoppingCartItem.PaginationItem(
+            1,
+            false,
+            false,
+        )
+
+val SHOPPING_CART_PRODUCT_ITEMS_4_PAGE_2 =
+    getProducts(4).map { ShoppingCartItem.ProductItem(it) } +
+            ShoppingCartItem.PaginationItem(
+                2,
+                false,
+                true,
+            )
