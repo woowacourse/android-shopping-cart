75ffaa511fded2341254177dab5f0033cc099eb6
moondev03 <moondev03@gmail.com>
2025-05-21 09:55:43 +0900

    refactor: Mapper.kt 파일 삭제

--- app/src/main/java/woowacourse/shopping/data/mapper/Mapper.kt
+++
@@ -1,6 +0,0 @@
-package woowacourse.shopping.data.mapper
-
-import woowacourse.shopping.data.db.CartEntity
-import woowacourse.shopping.domain.model.Product
-
-fun Product.toProductEntity() = CartEntity(productId = id)
