2d5739f138d523187461b680ac73a7a2916e64b1
oungsi2000 <oungsi1000@gmail.com>
2025-05-22 08:42:50 +0900

    refactor: ProductDetailViewModel.kt 저장 로직 변경
    product 저장 -> shoppingCartItem 저장

--- app/src/main/java/woowacourse/shopping/view/detail/ProductDetailViewModel.kt
+++ app/src/main/java/woowacourse/shopping/view/detail/ProductDetailViewModel.kt
@@ -3,19 +3,42 @@ package woowacourse.shopping.view.detail
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import woowacourse.shopping.data.DummyShoppingCart
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import woowacourse.shopping.ShoppingCartApplication
+import woowacourse.shopping.data.repository.ShoppingCartRepository
 import woowacourse.shopping.domain.Product
+import woowacourse.shopping.domain.ShoppingCartItem
 
-class ProductDetailViewModel : ViewModel() {
+class ProductDetailViewModel(
+    private val shoppingCartRepository: ShoppingCartRepository,
+) : ViewModel() {
     private val _productLiveData: MutableLiveData<Product> = MutableLiveData()
     val productLiveData: LiveData<Product> get() = _productLiveData
     val quantityLiveData: MutableLiveData<Int> = MutableLiveData(1)
 
     fun addProduct(product: Product) {
-        DummyShoppingCart.products.add(0, product)
+        shoppingCartRepository.save(
+            ShoppingCartItem(
+                product = product,
+                quantity = quantityLiveData.value!!,
+            ),
+        )
     }
 
     fun setProduct(product: Product) {
         _productLiveData.value = product
+    }
+
+    companion object {
+        val Factory: ViewModelProvider.Factory =
+            viewModelFactory {
+                initializer {
+                    val application = (this[APPLICATION_KEY] as ShoppingCartApplication)
+                    ProductDetailViewModel(application.shoppingCartRepository)
+                }
+            }
     }
 }
