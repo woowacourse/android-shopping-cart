e982514214bfa055ea61f81500315ea743d0adcd
jiyunlee <02leejiyun@naver.com>
2025-05-21 14:28:58 +0900

    feat: 장바구니 상품에 기본 수량을 1로 설정

--- app/src/main/java/woowacourse/shopping/domain/CartProduct.kt
+++ app/src/main/java/woowacourse/shopping/domain/CartProduct.kt
@@ -3,4 +3,9 @@ package woowacourse.shopping.domain
 data class CartProduct(
     val id: Long,
     val product: Product,
-)
+    val quantity: Int = MINIMUM_PRODUCT_QUANTITY,
+) {
+    companion object {
+        private const val MINIMUM_PRODUCT_QUANTITY = 1
+    }
+}
