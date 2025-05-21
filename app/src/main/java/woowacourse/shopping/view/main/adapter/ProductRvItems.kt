56ab1316e493ac589f5806857a9be5073096503c
chanho0908 <uni070@naver.com>
2025-05-21 09:56:19 +0900

    refactor : ProductRvItems.ProductItem가 ProductState를 직접 참조하도록 수정
    - ProductRvItems.ProductItem 내부의 item, quantity, quantityVisible 속성을 ProductState로 대체
    - 이에 따라 ProductViewHolder, ProductAdapter, item_product.xml, item_quantity_changer.xml 수정
    - 불필요해진 ProductState.toProductRvItems() 확장 함수 제거
    - ProductState에 cartQuantityValue 속성 추가

--- app/src/main/java/woowacourse/shopping/view/main/adapter/ProductRvItems.kt
+++ app/src/main/java/woowacourse/shopping/view/main/adapter/ProductRvItems.kt
@@ -1,13 +1,10 @@
 package woowacourse.shopping.view.main.adapter
 
-import woowacourse.shopping.domain.product.Product
 import woowacourse.shopping.view.main.vm.state.ProductState
 
 sealed class ProductRvItems(val viewType: ViewType) {
     data class ProductItem(
-        val item: Product,
-        val quantity: Int,
-        val quantityVisible: Boolean,
+        val data: ProductState,
     ) : ProductRvItems(ViewType.VIEW_TYPE_PRODUCT)
 
     object LoadItem : ProductRvItems(ViewType.VIEW_TYPE_LOAD)
@@ -16,12 +13,4 @@ sealed class ProductRvItems(val viewType: ViewType) {
         VIEW_TYPE_PRODUCT,
         VIEW_TYPE_LOAD,
     }
-}
-
-fun ProductState.toProductRvItems(): ProductRvItems.ProductItem {
-    return ProductRvItems.ProductItem(
-        item = item,
-        quantity = cartQuantity.value,
-        quantityVisible = quantityVisible,
-    )
 }
