819097af9e3f660f13544ad181ff3507f43c4f0e
hwannow <vnfdlvgid061@naver.com>
2025-05-22 08:46:32 +0900

    test: GoodsDetailViewModel 테스트 작성

--- app/src/test/java/woowacourse/shopping/presentation/goods/detail/GoodsDetailViewModelTest.kt
+++ app/src/test/java/woowacourse/shopping/presentation/goods/detail/GoodsDetailViewModelTest.kt
@@ -1,13 +1,18 @@
 package woowacourse.shopping.presentation.goods.detail
 
 import io.kotest.matchers.shouldBe
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.just
 import io.mockk.mockk
+import io.mockk.slot
+import io.mockk.verify
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import woowacourse.shopping.InstantTaskExecutorExtension
 import woowacourse.shopping.domain.repository.ShoppingRepository
-import woowacourse.shopping.fixture.SUNDAE
+import woowacourse.shopping.fixture.GOODS_SUNDAE
 import woowacourse.shopping.getOrAwaitValue
 import woowacourse.shopping.presentation.model.toUiModel
 
@@ -15,19 +20,55 @@ import woowacourse.shopping.presentation.model.toUiModel
 class GoodsDetailViewModelTest {
     private lateinit var goodsDetailViewModel: GoodsDetailViewModel
     private val shoppingRepository: ShoppingRepository = mockk(relaxed = true)
-    private val goods = SUNDAE
+    private val goods = GOODS_SUNDAE
 
     @BeforeEach
     fun setUp() {
         goodsDetailViewModel = GoodsDetailViewModel(shoppingRepository)
+        goodsDetailViewModel.setGoods(goods.toUiModel())
     }
 
     @Test
     fun `장바구니에 항목을 추가할 수 있다`() {
+        // given
+        val id = slot<Int>()
+        val count = slot<Int>()
+        every { shoppingRepository.increaseItemQuantity(capture(id), capture(count)) } just Runs
+
         // when
-        goodsDetailViewModel.setGoods(goods.toUiModel())
+        goodsDetailViewModel.addToShoppingCart()
 
         // then
-        goodsDetailViewModel.goods.getOrAwaitValue() shouldBe goods.toUiModel()
+        verify { shoppingRepository.increaseItemQuantity(any(), any()) }
+
+        id.captured shouldBe goods.id
+        count.captured shouldBe 1
+    }
+
+    @Test
+    fun `장바구니에 추가할 상품 수량의 기본값은 1이다`() {
+        // then
+        goodsDetailViewModel.count.getOrAwaitValue() shouldBe 1
+    }
+
+    @Test
+    fun `장바구니에 추가할 상품 수량을 증가할 수 있다`() {
+        // when
+        goodsDetailViewModel.increaseCount()
+
+        // then
+        goodsDetailViewModel.count.getOrAwaitValue() shouldBe 2
+    }
+
+    @Test
+    fun `장바구니에 추가할 상품 수량을 감소할 수 있다`() {
+        // given
+        goodsDetailViewModel.increaseCount()
+
+        // when
+        goodsDetailViewModel.decreaseCount()
+
+        // then
+        goodsDetailViewModel.count.getOrAwaitValue() shouldBe 1
     }
 }
