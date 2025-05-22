5936435d17aae8d7f11a02ac366f06ce05cb33f0
Ham BeomJoon <hbj0802@naver.com>
2025-05-23 08:28:58 +0900

    refactor: 툴바, 장바구니 뱃지 색 변경

--- app/src/main/java/woowacourse/shopping/presentation/bindingadapter/ButtonBindingAdapter.kt
+++ app/src/main/java/woowacourse/shopping/presentation/bindingadapter/ButtonBindingAdapter.kt
@@ -11,6 +11,6 @@ fun setCyanOrGrayTint(
     view: View,
     isEnabled: Boolean,
 ) {
-    val colorRes = if (isEnabled) R.color.cyan else R.color.gray1
+    val colorRes = if (isEnabled) R.color.baemin else R.color.gray1
     view.backgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(view.context, colorRes))
 }
