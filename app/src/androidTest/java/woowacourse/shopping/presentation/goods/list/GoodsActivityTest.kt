3d136ebb642148a9f62db5f787c1f99823fff628
hwannow <vnfdlvgid061@naver.com>
2025-05-22 11:26:06 +0900

    test: 목록 화면에서 상품 수량 조절 버튼 관련 UI 테스트 추가

--- app/src/androidTest/java/woowacourse/shopping/presentation/goods/list/GoodsActivityTest.kt
+++ app/src/androidTest/java/woowacourse/shopping/presentation/goods/list/GoodsActivityTest.kt
@@ -1,6 +1,8 @@
 package woowacourse.shopping.presentation.goods.list
 
 import android.content.Intent
+import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
@@ -12,11 +14,16 @@ import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.Intents.intended
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import org.awaitility.kotlin.await
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
 import org.hamcrest.Matchers.not
+import org.hamcrest.TypeSafeMatcher
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -42,39 +49,48 @@ class GoodsActivityTest {
 
     @Test
     fun `상품의_이름이_보인다`() {
-        onView(withId(R.id.rv_goods_list))
-            .perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(0))
+        // given
+        onView(withId(R.id.rv_goods_list)).perform(
+            RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
+                0,
+            ),
+        )
 
-        onView(withText("[병천아우내] 모듬순대"))
-            .check(matches(isDisplayed()))
+        // then
+        onView(withText("[병천아우내] 모듬순대")).check(matches(isDisplayed()))
     }
 
     @Test
     fun `상품의_가격이_보인다`() {
-        onView(withId(R.id.rv_goods_list))
-            .perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(0))
+        // given
+        onView(withId(R.id.rv_goods_list)).perform(
+            RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
+                0,
+            ),
+        )
 
-        onView(withText("11,900원"))
-            .check(matches(isDisplayed()))
+        // then
+        onView(withText("11,900원")).check(matches(isDisplayed()))
     }
 
     @Test
     fun `장바구니_버튼을_클릭하면_장바구니_목록_화면으로_이동한다`() {
-        onView(withId(R.id.action_cart))
-            .perform(click())
+        // when
+        onView(withId(R.id.action_cart)).perform(click())
 
+        // then
         intended(hasComponent(ShoppingCartActivity::class.java.name))
     }
 
     @Test
     fun `페이지_끝에_도달하면_더보기_버튼이_나타난다`() {
-        onView(withId(R.id.rv_goods_list))
-            .perform(swipeUp())
+        // when
+        onView(withId(R.id.rv_goods_list)).perform(swipeUp())
 
+        // then
         await.atMost(1, TimeUnit.SECONDS).until {
             try {
-                onView(withId(R.id.btn_load_more))
-                    .check(matches(isDisplayed()))
+                onView(withId(R.id.btn_load_more)).check(matches(isDisplayed()))
                 true
             } catch (e: Throwable) {
                 false
@@ -84,19 +100,104 @@ class GoodsActivityTest {
 
     @Test
     fun `더보기_버튼을_클릭하면_사라진다`() {
-        onView(withId(R.id.rv_goods_list))
-            .perform(swipeUp())
+        // when
+        onView(withId(R.id.rv_goods_list)).perform(swipeUp())
 
+        // then
         await.atMost(1, TimeUnit.SECONDS).until {
             try {
-                onView(withId(R.id.btn_load_more))
-                    .perform(click())
+                onView(withId(R.id.btn_load_more)).perform(click())
 
-                onView(withId(R.id.btn_load_more))
-                    .check(matches(not(isDisplayed())))
+                onView(withId(R.id.btn_load_more)).check(matches(not(isDisplayed())))
                 true
             } catch (e: Throwable) {
                 false
+            }
+        }
+    }
+
+    @Test
+    fun `선택되지_않은_상품의_수량_조절_버튼은_보이지_않는다`() {
+        onView(
+            allOf(
+                withId(R.id.cl_goods_quantity),
+                isDescendantOfA(nthChildOf(withId(R.id.rv_goods_list), 0)),
+            ),
+        ).check(matches(not(isDisplayed())))
+    }
+
+    @Test
+    fun `더하기_버튼을_누르면_수량_조절_버튼이_보인다`() {
+        // when
+        onView(
+            allOf(
+                withId(R.id.btn_plus),
+                isDescendantOfA(nthChildOf(withId(R.id.rv_goods_list), 0)),
+            ),
+        ).perform(click())
+
+        // then
+        await.atMost(1, TimeUnit.SECONDS).until {
+            try {
+                onView(
+                    allOf(
+                        withId(R.id.cl_goods_quantity),
+                        isDescendantOfA(nthChildOf(withId(R.id.rv_goods_list), 0)),
+                    ),
+                ).check(matches(isDisplayed()))
+                true
+            } catch (e: Throwable) {
+                false
+            }
+        }
+    }
+
+    @Test
+    fun `선택된_개수가_0이_되면_수량_조절_버튼이_사라진다`() {
+        // given
+        onView(
+            allOf(
+                withId(R.id.btn_plus),
+                isDescendantOfA(nthChildOf(withId(R.id.rv_goods_list), 0)),
+            ),
+        ).perform(click())
+
+        // when
+        onView(
+            allOf(
+                withId(R.id.tv_minus),
+                isDescendantOfA(
+                    allOf(
+                        withId(R.id.cl_goods_quantity),
+                        isDescendantOfA(nthChildOf(withId(R.id.rv_goods_list), 0)),
+                    ),
+                ),
+            ),
+        ).perform(click())
+
+        // then
+        onView(
+            allOf(
+                withId(R.id.cl_goods_quantity),
+                isDescendantOfA(nthChildOf(withId(R.id.rv_goods_list), 0)),
+            ),
+        ).check(matches(not(isDisplayed())))
+    }
+
+    private fun nthChildOf(
+        parentMatcher: Matcher<View>,
+        childPosition: Int,
+    ): Matcher<View> {
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Nth child of parent matcher")
+            }
+
+            override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent) && parent.getChildAt(
+                    childPosition,
+                ) == view
             }
         }
     }
