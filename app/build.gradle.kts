e7b3d2ac13ae623e203fc96429e5433f13b5f48f
chanho0908 <uni070@naver.com>
2025-05-22 21:33:51 +0900

    build : 네트워크 라이브러리 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,4 +71,6 @@ dependencies {
     androidTestImplementation(libs.kotest.runner.junit5)
     androidTestImplementation(libs.mannodermaus.junit5.core)
     androidTestRuntimeOnly(libs.mannodermaus.junit5.runner)
+    implementation(libs.okhttp)
+    implementation(libs.mock.web.server)
 }
