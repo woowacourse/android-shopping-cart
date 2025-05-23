e97da5850c0ca9e1db6c6eb7010b27058875a296
cucumber99 <dlsguq1@gmail.com>
2025-05-23 15:37:18 +0900

    feat: 상품 도메인에 id 필드 추가

--- app/src/main/java/woowacourse/shopping/domain/model/Goods.kt
+++ app/src/main/java/woowacourse/shopping/domain/model/Goods.kt
@@ -1,17 +1,20 @@
 package woowacourse.shopping.domain.model
 
 data class Goods(
+    val id: Long,
     val name: Name,
     val price: Price,
     val imageUrl: String,
 ) {
     companion object {
         fun of(
+            id: Long,
             name: String,
             price: Int,
             imageUrl: String,
         ): Goods =
             Goods(
+                id,
                 Name(name),
                 Price(price),
                 imageUrl,
