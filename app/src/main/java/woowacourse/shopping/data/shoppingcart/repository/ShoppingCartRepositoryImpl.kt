a1079028f1b7c6a8cd8497f1e1d5a0e1db1b3f19
cucumber99 <dlsguq1@gmail.com>
2025-05-23 15:24:15 +0900

    feat: 장바구니에 아이템 추가, 삭제, 페이지 조회, 전체 개수 조회 기능 구현

--- app/src/main/java/woowacourse/shopping/data/shoppingcart/repository/ShoppingCartRepositoryImpl.kt
+++ app/src/main/java/woowacourse/shopping/data/shoppingcart/repository/ShoppingCartRepositoryImpl.kt
@@ -1,24 +1,82 @@
 package woowacourse.shopping.data.shoppingcart.repository
 
-import woowacourse.shopping.data.shoppingcart.ShoppingDataBase
-import woowacourse.shopping.domain.model.Goods
+import woowacourse.shopping.data.shoppingcart.database.ShoppingCartDao
+import woowacourse.shopping.data.shoppingcart.util.toDomain
+import woowacourse.shopping.data.shoppingcart.util.toEntity
+import woowacourse.shopping.domain.model.ShoppingCartItem
 import woowacourse.shopping.domain.repository.ShoppingCartRepository
+import kotlin.concurrent.thread
 
 class ShoppingCartRepositoryImpl(
-    private val database: ShoppingDataBase = ShoppingDataBase,
+    private val dao: ShoppingCartDao,
 ) : ShoppingCartRepository {
-    override fun addGoods(goods: Goods) {
-        database.addItem(goods)
+    override fun addItem(
+        item: ShoppingCartItem,
+        onResult: (Result<Unit>) -> Unit
+    ) {
+        doAsyncCatching(
+            block = {
+                dao.upsertItem(item.toEntity())
+            },
+            onResult = onResult,
+        )
     }
 
-    override fun removeGoods(goods: Goods) {
-        database.removeItem(goods)
+    override fun addItems(
+        items: List<ShoppingCartItem>,
+        onResult: (Result<Unit>) -> Unit
+    ) {
+        doAsyncCatching(
+            block = {
+                val entities = items.map { it.toEntity() }
+                dao.upsertItems(entities)
+            },
+            onResult = onResult,
+        )
     }
 
-    override fun getGoods(
+    override fun removeItem(
+        shoppingCartItem: ShoppingCartItem,
+        onResult: (Result<Unit>) -> Unit
+    ) {
+        doAsyncCatching(
+            block = {
+                dao.delete(shoppingCartItem.toEntity())
+            },
+            onResult = onResult,
+        )
+    }
+
+    override fun getPagedItems(
         page: Int,
         count: Int,
-    ): List<Goods> {
-        return database.getPagedGoods(page, count)
+        onResult: (Result<List<ShoppingCartItem>>) -> Unit
+    ) {
+        doAsyncCatching(
+            block = {
+                val offset = page * count
+                dao.getPage(offset, count).map { it.toDomain() }
+            },
+            onResult = onResult,
+        )
+    }
+
+    override fun getTotalQuantity(onResult: (Result<Int>) -> Unit) {
+        doAsyncCatching(
+            block = {
+                dao.getAll().count()
+            },
+            onResult = onResult,
+        )
+    }
+
+    private inline fun <T> doAsyncCatching(
+        crossinline block: () -> T,
+        crossinline onResult: (Result<T>) -> Unit,
+    ) {
+        thread {
+            val result = runCatching { block() }
+            onResult(result)
+        }
     }
 }
