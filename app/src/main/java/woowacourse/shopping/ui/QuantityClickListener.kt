a86dfc2f884193d443ef40819f3a79d213a1d4a2
지원 <cheerfulboys@naver.com>
2025-05-23 14:17:41 +0900

    feat: 수량 변경하는 클릭 리스너 구현

---
+++ app/src/main/java/woowacourse/shopping/ui/QuantityClickListener.kt
@@ -0,0 +1,8 @@
+package woowacourse.shopping.ui
+
+import woowacourse.shopping.domain.product.CartItem
+
+interface QuantityClickListener {
+    fun onIncreaseClick(cartItem: CartItem)
+    fun onDecreaseClick(cartItem: CartItem)
+}
