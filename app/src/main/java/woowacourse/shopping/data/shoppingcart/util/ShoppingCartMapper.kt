7766b772635a8c105067c7611247befc326b8bc5
cucumber99 <dlsguq1@gmail.com>
2025-05-23 15:20:31 +0900

    feat: 장바구니 관련 Mapper 추가

---
+++ app/src/main/java/woowacourse/shopping/data/shoppingcart/util/ShoppingCartMapper.kt
@@ -0,0 +1,28 @@
+package woowacourse.shopping.data.shoppingcart.util
+
+import woowacourse.shopping.data.shoppingcart.database.ShoppingCartEntity
+import woowacourse.shopping.domain.model.Goods
+import woowacourse.shopping.domain.model.ShoppingCartItem
+
+fun ShoppingCartEntity.toDomain(): ShoppingCartItem {
+    return ShoppingCartItem(
+        goods = Goods.of(
+            id = this.id,
+            name = this.name,
+            price = this.price,
+            imageUrl = this.imageUrl
+        ),
+        quantity = this.quantity,
+    )
+}
+
+fun ShoppingCartItem.toEntity(): ShoppingCartEntity {
+    val goods = this.goods
+    return ShoppingCartEntity(
+        id = goods.id,
+        name = goods.name.value,
+        price = goods.price.value,
+        imageUrl = goods.imageUrl,
+        quantity = this.quantity,
+    )
+}
