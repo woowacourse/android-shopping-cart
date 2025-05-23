9245aedbcb79fa83cfb35a9beb6ba9e19b4029c3
Ham BeomJoon <hbj0802@naver.com>
2025-05-23 09:09:55 +0900

    refactor: 상품 본 시간 localDateTime -> System.currentTimeMillis로 변경

--- app/src/main/java/woowacourse/shopping/data/db/RecentlyViewedProduct.kt
+++ app/src/main/java/woowacourse/shopping/data/db/RecentlyViewedProduct.kt
@@ -2,10 +2,9 @@ package woowacourse.shopping.data.db
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import java.time.LocalDateTime
 
 @Entity(tableName = "recentlyViewedProduct")
 data class RecentlyViewedProduct(
     @PrimaryKey val productId: Long,
-    val viewedAt: LocalDateTime,
+    val viewedAt: Long = System.currentTimeMillis(),
 )
