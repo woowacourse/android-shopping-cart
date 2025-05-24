05d7ffe6f1d85a0b0f8b30050142abaafc62bebe
devfeijoa <devfeijoa@gmail.com>
2025-05-25 02:42:49 +0900

    feat: id 추가

--- app/src/main/java/woowacourse/shopping/model/products/Product.kt
+++ app/src/main/java/woowacourse/shopping/model/products/Product.kt
@@ -3,6 +3,7 @@ package woowacourse.shopping.model.products
 import java.io.Serializable
 
 data class Product(
+    val id: String,
     val title: String,
     val imageUrl: String,
     val price: Int,
