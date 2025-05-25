9e946e7ce3f764e62d61a8d176caa7164a15ed4e
oungsi2000 <oungsi1000@gmail.com>
2025-05-25 09:48:27 +0900

    test: productVieModel 테스트 추가

--- app/src/androidTest/java/woowacourse/shopping/view/main/ProductsViewModelTest.kt
+++ app/src/androidTest/java/woowacourse/shopping/view/main/ProductsViewModelTest.kt
@@ -1,27 +1,34 @@
 package woowacourse.shopping.view.main
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Rule
 import org.junit.Test
 import woowacourse.shopping.data.page.Page
+import woowacourse.shopping.domain.ShoppingCartItem
 import woowacourse.shopping.fixture.FakeProductsRepository
 import woowacourse.shopping.fixture.FakeRecentProductRepository
 import woowacourse.shopping.fixture.FakeShoppingCartRepository
 import woowacourse.shopping.fixture.TestProducts
 import woowacourse.shopping.getOrAwaitValue
 import woowacourse.shopping.mapper.toProductUiModel
+import woowacourse.shopping.view.uimodel.ProductUiModel
+import woowacourse.shopping.view.uimodel.QuantityInfo
 
 @Suppress("FunctionName")
 class ProductsViewModelTest {
     @get:Rule
     val instantExecutorRule = InstantTaskExecutorRule()
 
+    val productsRepository = FakeProductsRepository()
+    val shoppingCartRepository = FakeShoppingCartRepository()
+    val recentProductRepository = FakeRecentProductRepository()
     val viewModel =
         ProductsViewModel(
-            FakeProductsRepository(),
-            FakeShoppingCartRepository(),
-            FakeRecentProductRepository(),
+            productsRepository,
+            shoppingCartRepository,
+            recentProductRepository,
         )
 
     @Test
@@ -42,5 +49,43 @@ class ProductsViewModelTest {
     @Test
     fun 상품의_총_개수를_반환한다() {
         assertThat(viewModel.totalSize.getOrAwaitValue()).isEqualTo(TestProducts.productUiModels.size)
+    }
+
+    @Test
+    fun 현재_상품들의_수량을_장바구니에_저장할_수_있다() {
+        val quantityInfo =
+            QuantityInfo(
+                TestProducts.productUiModels.subList(0, 1)
+                    .map { it.toProductUiModel() }
+                    .associateWith { MutableLiveData(2) },
+            )
+        viewModel.saveCurrentShoppingCart(quantityInfo)
+        Thread.sleep(500)
+
+        assertThat(
+            shoppingCartRepository.findAll(),
+        ).contains(
+            ShoppingCartItem(
+                id = 1,
+                product = TestProducts.productUiModels[0],
+                quantity = 2,
+            ),
+        )
+    }
+
+    @Test
+    fun 최근_본_상품을_가져올_수_있다() {
+        viewModel.requestRecentProducts()
+        assertThat(viewModel.recentProductsLiveData.getOrAwaitValue())
+            .isEqualTo(
+                recentProductRepository.findAll().map {
+                    ProductUiModel(
+                        id = it.product.id,
+                        name = it.product.name,
+                        price = it.product.price,
+                        imageUrl = it.product.imageUrl,
+                    )
+                },
+            )
     }
 }
