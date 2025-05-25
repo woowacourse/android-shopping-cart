34ba0ab517770025a7b4b6592e6214ebbe0e7960
rosemin928 <rosemin928@naver.com>
2025-05-25 16:49:42 +0900

    feat: mockWebServer 구현

--- app/src/main/java/woowacourse/shopping/data/cart/repository/CartRepositoryImpl.kt
+++ app/src/main/java/woowacourse/shopping/data/cart/repository/CartRepositoryImpl.kt
@@ -2,7 +2,7 @@ package woowacourse.shopping.data.cart.repository
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.map
-import woowacourse.shopping.data.cart.CartDatabase
+import woowacourse.shopping.data.cart.CartDao
 import woowacourse.shopping.data.toDomain
 import woowacourse.shopping.data.toEntity
 import woowacourse.shopping.domain.model.Cart
@@ -10,11 +10,11 @@ import woowacourse.shopping.domain.model.Carts
 import kotlin.concurrent.thread
 
 class CartRepositoryImpl(
-    private val cartDatabase: CartDatabase,
+    private val dao: CartDao,
 ) : CartRepository {
     override fun getAll(callback: (Carts) -> Unit) {
         thread {
-            val cartList = cartDatabase.cartDao().getAll().map { it.toDomain() }
+            val cartList = dao.getAll().map { it.toDomain() }
             val totalQuantity = cartList.sumOf { it.quantity }
             callback(Carts(carts = cartList, totalQuantity = totalQuantity))
         }
@@ -22,7 +22,6 @@ class CartRepositoryImpl(
 
     override fun insert(cart: Cart) {
         thread {
-            val dao = cartDatabase.cartDao()
             val existing = dao.findById(cart.goods.id)
             if (existing != null) {
                 val updated =
@@ -38,14 +37,12 @@ class CartRepositoryImpl(
 
     override fun insertAll(cart: Cart) {
         thread {
-            val dao = cartDatabase.cartDao()
             dao.updateQuantity(cart.toEntity())
         }
     }
 
     override fun delete(cart: Cart) {
         thread {
-            val dao = cartDatabase.cartDao()
             val existing = dao.findById(cart.goods.id)
             if (existing != null) {
                 if (existing.quantity > 1) {
@@ -65,17 +62,17 @@ class CartRepositoryImpl(
         limit: Int,
         offset: Int,
     ): LiveData<Carts> =
-        cartDatabase.cartDao().getPage(limit, offset).map { entities ->
+        dao.getPage(limit, offset).map { entities ->
             val cartList = entities.map { it.toDomain() }
             val totalQuantity = cartList.sumOf { it.quantity }
             Carts(carts = cartList, totalQuantity = totalQuantity)
         }
 
-    override fun getAllItemsSize(): LiveData<Int> = cartDatabase.cartDao().getAllItemsSize()
+    override fun getAllItemsSize(): LiveData<Int> = dao.getAllItemsSize()
 
     override fun getTotalQuantity(callback: (Int) -> Unit) {
         thread {
-            val totalQuantity = cartDatabase.cartDao().getTotalQuantity()
+            val totalQuantity = dao.getTotalQuantity()
             callback(totalQuantity)
         }
     }
