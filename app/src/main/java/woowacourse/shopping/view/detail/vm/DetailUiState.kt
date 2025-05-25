edf78e6609c95960d4eb6f1a4dd324ae8e60fb30
chanho0908 <uni070@naver.com>
2025-05-26 01:09:13 +0900

    feat: DetailUiState에 productPrice와 hasLastSeenProduct 필드 추가

--- app/src/main/java/woowacourse/shopping/view/detail/vm/DetailUiState.kt
+++ app/src/main/java/woowacourse/shopping/view/detail/vm/DetailUiState.kt
@@ -6,4 +6,10 @@ import woowacourse.shopping.view.main.state.ProductState
 data class DetailUiState(
     val product: ProductState,
     val lastSeenProduct: Product?,
-)
+) {
+    val productPrice: Int
+        get() = product.item.priceValue * product.cartQuantity.value
+
+    val hasLastSeenProduct: Boolean
+        get() = lastSeenProduct != null
+}
