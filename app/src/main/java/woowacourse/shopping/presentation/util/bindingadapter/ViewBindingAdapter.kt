d3fe45518e50df85fac9bfafda477fd61cad0f47
hwannow <vnfdlvgid061@naver.com>
2025-05-25 20:36:06 +0900

    refactor: isVisible 바인딩 어댑터 추가 및 적용

---
+++ app/src/main/java/woowacourse/shopping/presentation/util/bindingadapter/ViewBindingAdapter.kt
@@ -0,0 +1,10 @@
+package woowacourse.shopping.presentation.util.bindingadapter
+
+import android.view.View
+import androidx.core.view.isVisible
+import androidx.databinding.BindingAdapter
+
+@BindingAdapter("isVisible")
+fun View.bindIsVisible(isVisible: Boolean) {
+    this.isVisible = isVisible
+}
