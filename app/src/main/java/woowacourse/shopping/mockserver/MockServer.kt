ac550c84339e06af29833c5496302838f5426f24
moondev03 <moondev03@gmail.com>
2025-05-26 17:39:01 +0900

    refactor: MockServer 초기화 시 스레드 사용 제거

--- app/src/main/java/woowacourse/shopping/mockserver/MockServer.kt
+++ app/src/main/java/woowacourse/shopping/mockserver/MockServer.kt
@@ -2,7 +2,6 @@ package woowacourse.shopping.mockserver
 
 import okhttp3.HttpUrl
 import okhttp3.mockwebserver.MockWebServer
-import kotlin.concurrent.thread
 
 class MockServer {
     private lateinit var server: MockWebServer
@@ -14,9 +13,7 @@ class MockServer {
     fun createUrlWithBase(requestUrl: String): HttpUrl = server.url(requestUrl)
 
     private fun startMockServer() {
-        thread {
-            server = MockWebServer()
-            server.dispatcher = MockProductDispatcher(dummyProducts)
-        }
+        server = MockWebServer()
+        server.dispatcher = MockProductDispatcher(dummyProducts)
     }
 }
