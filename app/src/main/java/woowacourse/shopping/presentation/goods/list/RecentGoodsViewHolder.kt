8a105cf65532d15b2a957d97f2fa352db4eb01aa
cucumber99 <dlsguq1@gmail.com>
2025-05-26 14:28:37 +0900

    feat: RecentGoodsViewHolder 구현

---
+++ app/src/main/java/woowacourse/shopping/presentation/goods/list/RecentGoodsViewHolder.kt
@@ -0,0 +1,25 @@
+package woowacourse.shopping.presentation.goods.list
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import woowacourse.shopping.R
+import woowacourse.shopping.databinding.ItemRecentGoodsBinding
+import woowacourse.shopping.domain.model.Goods
+
+class RecentGoodsViewHolder(
+    parent: ViewGroup,
+    clickListener: GoodsClickListener,
+) : RecyclerView.ViewHolder(
+    LayoutInflater.from(parent.context).inflate(R.layout.item_recent_goods, parent, false)
+) {
+    private val binding = ItemRecentGoodsBinding.bind(itemView)
+
+    init {
+        binding.clickListener = clickListener
+    }
+
+    fun bind(goods: Goods) {
+        binding.goods = goods
+    }
+}
