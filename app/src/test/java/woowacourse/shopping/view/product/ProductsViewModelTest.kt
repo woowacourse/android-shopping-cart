612f085807c885699c164d20f6e86d17f7ed56c4
m6z1 <sonmyungj1zz@gmail.com>
2025-05-26 16:35:51 +0900

    test: 변경된 프로덕션 코드에 테스트 성공하도록 수정

--- app/src/test/java/woowacourse/shopping/view/product/ProductsViewModelTest.kt
+++ app/src/test/java/woowacourse/shopping/view/product/ProductsViewModelTest.kt
@@ -13,9 +13,13 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertAll
 import org.junit.jupiter.api.extension.ExtendWith
 import woowacourse.shopping.data.product.repository.ProductsRepository
+import woowacourse.shopping.data.shoppingCart.repository.ShoppingCartRepository
 import woowacourse.shopping.domain.product.Product
+import woowacourse.shopping.domain.shoppingCart.ShoppingCartProduct
 import woowacourse.shopping.fixture.PRODUCT_ITEMS_20
 import woowacourse.shopping.fixture.PRODUCT_ITEMS_20_MORE
+import woowacourse.shopping.fixture.PRODUCT_ITEMS_20_MORE_AND_RECENT
+import woowacourse.shopping.fixture.RECENT_PRODUCTS
 import woowacourse.shopping.fixture.getProducts
 import woowacourse.shopping.view.common.InstantTaskExecutorExtension
 import woowacourse.shopping.view.common.getOrAwaitValue
@@ -29,26 +33,45 @@ class ProductsViewModelTest {
 
     private lateinit var productViewModel: ProductsViewModel
     private lateinit var productsRepository: ProductsRepository
+    private lateinit var shoppingCartRepository: ShoppingCartRepository
 
     @BeforeEach
     fun setUp() {
-        productsRepository = mockk()
-        productViewModel = ProductsViewModel(productsRepository)
+        productsRepository = mockk(relaxed = true)
+        shoppingCartRepository = mockk(relaxed = true)
+
+        productViewModel =
+            ProductsViewModel(
+                productsRepository = productsRepository,
+                shoppingCartRepository = shoppingCartRepository,
+            )
     }
 
     @Test
     fun `상품 목록 업데이트 성공 시 결과값 개수가 20개 초과일 경우 ProductItem 과 LoadItem 추가된 products 값이 된다`() {
-        // given:
+        // given
         every {
-            productsRepository.load(any(), any(), captureLambda())
+            productsRepository.load(null, 21, captureLambda())
         } answers {
             lambda<(Result<List<Product>>) -> Unit>().invoke(Result.success(getProducts(21)))
         }
 
-        // when:
+        every {
+            productsRepository.getRecentWatchingProducts(any(), captureLambda())
+        } answers {
+            lambda<(Result<List<Product>>) -> Unit>().invoke(Result.success(emptyList()))
+        }
+
+        every {
+            shoppingCartRepository.fetchSelectedQuantity(any<List<Product>>(), captureLambda())
+        } answers {
+            lambda<(Result<List<ShoppingCartProduct>>) -> Unit>().invoke(Result.success(emptyList()))
+        }
+
+        // when
         productViewModel.updateProducts()
 
-        // then:
+        // then
         val result = productViewModel.products.getOrAwaitValue()
         assertAll({
             assertThat(result.count { it is ProductItem }).isEqualTo(20)
@@ -58,12 +81,53 @@ class ProductsViewModelTest {
     }
 
     @Test
+    fun `상품 목록 업데이트 성공 시 결과값 개수가 20개 초과이면서 최근 본 상품이 있을 경우 최근 본 상품, ProductItem 과 LoadItem 추가된 products 값이 된다`() {
+        // given
+        every {
+            productsRepository.load(null, 21, captureLambda())
+        } answers {
+            lambda<(Result<List<Product>>) -> Unit>().invoke(Result.success(getProducts(21)))
+        }
+
+        every {
+            productsRepository.getRecentWatchingProducts(any(), captureLambda())
+        } answers {
+            lambda<(Result<List<Product>>) -> Unit>().invoke(Result.success(RECENT_PRODUCTS))
+        }
+
+        every {
+            shoppingCartRepository.fetchSelectedQuantity(any<List<Product>>(), captureLambda())
+        } answers {
+            lambda<(Result<List<ShoppingCartProduct>>) -> Unit>().invoke(Result.success(emptyList()))
+        }
+
+        // when
+        productViewModel.updateProducts()
+
+        // then
+        val result = productViewModel.products.getOrAwaitValue()
+        assertThat(result).isEqualTo(PRODUCT_ITEMS_20_MORE_AND_RECENT)
+    }
+
+    @Test
     fun `상품 목록 업데이트 성공 시 결과값 개수가 20개 이하일 경우 ProductItem 만 추가된 products 값이 된다`() {
         // given:
         every {
             productsRepository.load(any(), any(), captureLambda())
         } answers {
             lambda<(Result<List<Product>>) -> Unit>().invoke(Result.success(getProducts(20)))
+        }
+
+        every {
+            productsRepository.getRecentWatchingProducts(any(), captureLambda())
+        } answers {
+            lambda<(Result<List<Product>>) -> Unit>().invoke(Result.success(emptyList()))
+        }
+
+        every {
+            shoppingCartRepository.fetchSelectedQuantity(any<List<Product>>(), captureLambda())
+        } answers {
+            lambda<(Result<List<ShoppingCartProduct>>) -> Unit>().invoke(Result.success(emptyList()))
         }
 
         // when:
@@ -84,6 +148,18 @@ class ProductsViewModelTest {
             productsRepository.load(any(), any(), captureLambda())
         } answers {
             lambda<(Result<List<Product>>) -> Unit>().invoke(Result.success(emptyList()))
+        }
+
+        every {
+            productsRepository.getRecentWatchingProducts(any(), captureLambda())
+        } answers {
+            lambda<(Result<List<Product>>) -> Unit>().invoke(Result.success(emptyList()))
+        }
+
+        every {
+            shoppingCartRepository.fetchSelectedQuantity(any<List<Product>>(), captureLambda())
+        } answers {
+            lambda<(Result<List<ShoppingCartProduct>>) -> Unit>().invoke(Result.success(emptyList()))
         }
 
         // when:
--- app/src/test/java/woowacourse/shopping/view/productDetail/ProductDetailViewModelTest.kt
+++ app/src/test/java/woowacourse/shopping/view/productDetail/ProductDetailViewModelTest.kt
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import woowacourse.shopping.data.product.repository.ProductsRepository
 import woowacourse.shopping.data.shoppingCart.repository.ShoppingCartRepository
 import woowacourse.shopping.fixture.PRODUCT_LUCKY
 import woowacourse.shopping.view.common.InstantTaskExecutorExtension
@@ -23,18 +24,20 @@ class ProductDetailViewModelTest {
 
     private lateinit var productDetailViewModel: ProductDetailViewModel
     private lateinit var shoppingCartRepository: ShoppingCartRepository
+    private lateinit var productsRepository: ProductsRepository
 
     @BeforeEach
     fun setUp() {
-        shoppingCartRepository = mockk()
-        productDetailViewModel = ProductDetailViewModel(shoppingCartRepository)
+        productsRepository = mockk(relaxed = true)
+        shoppingCartRepository = mockk(relaxed = true)
+        productDetailViewModel = ProductDetailViewModel(shoppingCartRepository, productsRepository)
     }
 
     @Test
     fun `상품을 업데이트하면 product의 값이 해당 상품이 된다`() {
         // given:
         // when:
-        productDetailViewModel.updateProduct(PRODUCT_LUCKY)
+        productDetailViewModel.updateProduct(PRODUCT_LUCKY, false)
 
         // then:
         val result = productDetailViewModel.product.getOrAwaitValue()
@@ -44,8 +47,8 @@ class ProductDetailViewModelTest {
     @Test
     fun `쇼핑카트에 상품을 추가했을 때 성공 시 휘발성 이벤트 ADD_SHOPPING_CART_SUCCESS 값을 가진다`() {
         // given:
-        productDetailViewModel.updateProduct(PRODUCT_LUCKY)
-        every { shoppingCartRepository.add(PRODUCT_LUCKY, captureLambda()) } answers {
+        productDetailViewModel.updateProduct(PRODUCT_LUCKY, false)
+        every { shoppingCartRepository.add(PRODUCT_LUCKY, any(), captureLambda()) } answers {
             lambda<(Result<Unit>) -> Unit>().invoke(Result.success(Unit))
         }
 
@@ -60,8 +63,8 @@ class ProductDetailViewModelTest {
     @Test
     fun `쇼핑카트에 상품을 추가했을 때 실패 시 휘발성 이벤트 ADD_SHOPPING_CART_FAILURE 값을 가진다`() {
         // given:
-        productDetailViewModel.updateProduct(PRODUCT_LUCKY)
-        every { shoppingCartRepository.add(PRODUCT_LUCKY, captureLambda()) } answers {
+        productDetailViewModel.updateProduct(PRODUCT_LUCKY, false)
+        every { shoppingCartRepository.add(PRODUCT_LUCKY, any(), captureLambda()) } answers {
             lambda<(Result<Unit>) -> Unit>().invoke(Result.failure(RuntimeException()))
         }
 
