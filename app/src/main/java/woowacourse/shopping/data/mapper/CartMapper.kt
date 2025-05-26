8b975bbe654c5543301c1044e209c01edf04d278
junseo511 <prime54@g.skku.edu>
2025-05-26 15:35:26 +0900

    refactor: 사용하지 않는 함수 제거

--- app/src/main/java/woowacourse/shopping/data/mapper/CartMapper.kt
+++ app/src/main/java/woowacourse/shopping/data/mapper/CartMapper.kt
@@ -1,7 +1,6 @@
 package woowacourse.shopping.data.mapper
 
 import woowacourse.shopping.data.dto.CartProductDetailDto
-import woowacourse.shopping.data.entity.CartProductEntity
 import woowacourse.shopping.domain.model.CartProduct
 import woowacourse.shopping.domain.model.Product.Companion.EMPTY_PRODUCT
 
@@ -9,10 +8,4 @@ fun CartProductDetailDto.toDomain(): CartProduct =
     CartProduct(
         product = product?.toDomain() ?: EMPTY_PRODUCT,
         quantity = cartProduct.quantity,
-    )
-
-fun CartProduct.toData(): CartProductEntity =
-    CartProductEntity(
-        productId = product.id,
-        quantity = quantity,
     )
