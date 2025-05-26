01e84f89dcb764eee7585b772ece9d19e2453006
cucumber99 <dlsguq1@gmail.com>
2025-05-26 16:27:51 +0900

    feat: RecentGoodsRepository 초기화

--- app/src/main/java/woowacourse/shopping/RepositoryProvider.kt
+++ app/src/main/java/woowacourse/shopping/RepositoryProvider.kt
@@ -2,18 +2,24 @@ package woowacourse.shopping
 
 import android.app.Application
 import woowacourse.shopping.data.goods.repository.GoodsRepositoryImpl
+import woowacourse.shopping.data.recentgoods.database.RecentGoodsDatabase
+import woowacourse.shopping.data.recentgoods.repository.RecentGoodsRepositoryImpl
 import woowacourse.shopping.data.shoppingcart.database.ShoppingCartDatabase
 import woowacourse.shopping.data.shoppingcart.repository.ShoppingCartRepositoryImpl
 import woowacourse.shopping.domain.repository.GoodsRepository
+import woowacourse.shopping.domain.repository.RecentGoodsRepository
 import woowacourse.shopping.domain.repository.ShoppingCartRepository
 
 object RepositoryProvider {
     lateinit var goodsRepository: GoodsRepository
     lateinit var shoppingCartRepository: ShoppingCartRepository
+    lateinit var recentGoodsRepository: RecentGoodsRepository
 
     fun init(application: Application) {
-        val database = ShoppingCartDatabase.getDatabase(application)
+        val shoppingCartDatabase = ShoppingCartDatabase.getDatabase(application)
+        val recentGoodsDatabase = RecentGoodsDatabase.getDatabase(application)
         goodsRepository = GoodsRepositoryImpl()
-        shoppingCartRepository = ShoppingCartRepositoryImpl(database.shoppingCartDao())
+        shoppingCartRepository = ShoppingCartRepositoryImpl(shoppingCartDatabase.shoppingCartDao())
+        recentGoodsRepository = RecentGoodsRepositoryImpl(recentGoodsDatabase.recentGoodsDao())
     }
 }
