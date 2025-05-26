c543a26a91cf6409f0eb43b4b60bd14ab86d7855
devfeijoa <devfeijoa@gmail.com>
2025-05-26 15:27:53 +0900

    refactor: 사용하지 않는 메서드 제거

--- app/src/main/java/woowacourse/shopping/model/products/CartState.kt
+++ app/src/main/java/woowacourse/shopping/model/products/CartState.kt
@@ -3,11 +3,6 @@ package woowacourse.shopping.model.products
 data class CartState(
     val items: Map<String, ShoppingCartItem> = emptyMap(),
 ) {
-    val totalItems: Int get() = items.values.sumOf { it.quantity }
-    val totalPrice: Int get() = items.values.sumOf { it.product.price * it.quantity }
-
-    val isEmpty: Boolean get() = items.isEmpty()
-
     fun getQuantity(productId: String): Int = items[productId]?.quantity ?: 0
 
     fun getAllShoppingCartItem(): List<ShoppingCartItem> = items.values.toList()
