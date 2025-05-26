2ce0deee5764b350a038376c10cdb3a29cad86ac
윤동현 <donghyunyoon81@gmail.com>
2025-05-26 15:11:00 +0900

    feat:상품 목록의 HTTP Client를 구현한다

--- app/src/main/java/woowacourse/shopping/data/datasource/ProductDataSourceImpl.kt
+++ app/src/main/java/woowacourse/shopping/data/datasource/ProductDataSourceImpl.kt
@@ -1,21 +1,34 @@
 package woowacourse.shopping.data.datasource
 
-import woowacourse.shopping.data.database.DummyProduct
+import com.google.gson.Gson
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import woowacourse.shopping.data.server.ProductServer
 import woowacourse.shopping.domain.Product
+import kotlin.concurrent.thread
 
-@Suppress("ktlint:standard:max-line-length")
-class ProductDataSourceImpl : ProductDataSource {
-    private val productDatabase = DummyProduct
+class ProductDataSourceImpl(
+    private val productServer: ProductServer,
+    private val client: OkHttpClient,
+    private val gson: Gson,
+) : ProductDataSource {
+    private lateinit var products: List<Product>
 
-    override fun get(id: Long): Product = productDatabase.products[id] ?: throw IllegalArgumentException()
+    init {
+        thread {
+            loadProducts()
+        }.join()
+    }
+
+    override fun get(id: Long): Product = products.find { id == it.id } ?: throw IllegalArgumentException()
 
     override fun getProducts(
         page: Int,
         pageSize: Int,
     ): List<Product> {
         val fromIndex = (page - 1) * pageSize
-        val toIndex = minOf(fromIndex + pageSize, productDatabase.size)
-        return productDatabase.productsValues.subList(fromIndex, toIndex)
+        val toIndex = minOf(fromIndex + pageSize, products.size)
+        return products.subList(fromIndex, toIndex)
     }
 
     override fun notHasMoreProduct(
@@ -23,6 +36,19 @@ class ProductDataSourceImpl : ProductDataSource {
         pageSize: Int,
     ): Boolean {
         val fromIndex = page * pageSize
-        return fromIndex >= productDatabase.size
+        return fromIndex >= products.size
+    }
+
+    private fun loadProducts() {
+        val baseUrl = productServer.mockWebServer.url("/")
+
+        val request =
+            Request.Builder()
+                .url(baseUrl.resolve("/products")!!)
+                .build()
+
+        val response = client.newCall(request).execute()
+        val responseBody = response.body?.string() ?: return
+        products = gson.fromJson(responseBody, Array<Product>::class.java).toList()
     }
 }
--- app/src/main/java/woowacourse/shopping/di/AppContainer.kt
+++ app/src/main/java/woowacourse/shopping/di/AppContainer.kt
@@ -1,6 +1,8 @@
 package woowacourse.shopping.di
 
 import android.content.Context
+import com.google.gson.Gson
+import okhttp3.OkHttpClient
 import woowacourse.shopping.data.database.ShoppingDatabase.Companion.getDatabase
 import woowacourse.shopping.data.datasource.CartLocalDataSource
 import woowacourse.shopping.data.datasource.CartLocalDataSourceImpl
@@ -11,13 +13,23 @@ import woowacourse.shopping.data.datasource.RecentProductLocalDataSourceImpl
 import woowacourse.shopping.data.repository.CartRepositoryImpl
 import woowacourse.shopping.data.repository.ProductRepositoryImpl
 import woowacourse.shopping.data.repository.RecentProductRepositoryImpl
+import woowacourse.shopping.data.server.ProductServer
 import woowacourse.shopping.domain.repository.CartRepository
 import woowacourse.shopping.domain.repository.ProductRepository
 import woowacourse.shopping.domain.repository.RecentProductRepository
 
 class AppContainer(applicationContext: Context) {
+    private val client = OkHttpClient()
+    private val gson = Gson()
     private val database = getDatabase(applicationContext)
-    private val productDataSource: ProductDataSource by lazy { ProductDataSourceImpl() }
+    private val productServer = ProductServer(gson)
+    private val productDataSource: ProductDataSource by lazy {
+        ProductDataSourceImpl(
+            productServer,
+            client,
+            gson,
+        )
+    }
     private val cartLocalDataSource: CartLocalDataSource by lazy { CartLocalDataSourceImpl(database.cartDao()) }
     private val recentProductLocalDataSource: RecentProductLocalDataSource by lazy {
         RecentProductLocalDataSourceImpl(
