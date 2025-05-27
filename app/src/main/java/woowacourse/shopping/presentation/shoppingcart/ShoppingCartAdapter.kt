bef960ce3fc47e8897dfe744f6d9e5e60669e669
hwannow <vnfdlvgid061@naver.com>
2025-05-28 08:50:08 +0900

    refactor: ShoppingCartAdapter 아이템 업데이트 로직 개선

--- app/src/main/java/woowacourse/shopping/presentation/shoppingcart/ShoppingCartAdapter.kt
+++ app/src/main/java/woowacourse/shopping/presentation/shoppingcart/ShoppingCartAdapter.kt
@@ -1,6 +1,5 @@
 package woowacourse.shopping.presentation.shoppingcart
 
-import android.annotation.SuppressLint
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import woowacourse.shopping.domain.model.Goods
@@ -10,7 +9,7 @@ class ShoppingCartAdapter(
     private val shoppingCartClickListener: ShoppingCartClickListener,
     private val quantitySelectorListener: QuantitySelectorListener,
 ) : RecyclerView.Adapter<ShoppingCartViewHolder>() {
-    private var items: List<Goods> = emptyList()
+    private var items: MutableList<Goods> = mutableListOf()
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
@@ -28,9 +27,37 @@ class ShoppingCartAdapter(
         holder.bind(items[position])
     }
 
-    @SuppressLint("NotifyDataSetChanged")
-    fun updateItems(goods: List<Goods>) {
-        items = goods
-        notifyDataSetChanged()
+    fun updateItems(newItems: List<Goods>) {
+        val oldItems = items.toList()
+
+        items.clear()
+        items.addAll(newItems)
+
+        updateChangedItems(oldItems, newItems)
+        notifyItemCountChanged(oldItems.size, newItems.size)
+    }
+
+    private fun updateChangedItems(
+        oldItems: List<Goods>,
+        newItems: List<Goods>,
+    ) {
+        val minSize = minOf(oldItems.size, newItems.size)
+
+        for (index in 0 until minSize) {
+            if (oldItems[index] != newItems[index]) {
+                notifyItemChanged(index)
+            }
+        }
+    }
+
+    private fun notifyItemCountChanged(
+        oldSize: Int,
+        newSize: Int,
+    ) {
+        if (newSize > oldSize) {
+            notifyItemRangeInserted(oldSize, newSize - oldSize)
+        } else if (oldSize > newSize) {
+            notifyItemRangeRemoved(newSize, oldSize - newSize)
+        }
     }
 }
