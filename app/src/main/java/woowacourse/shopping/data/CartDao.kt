832d12abffa354215f479dc35ff4b3559f585d2c
devfeijoa <devfeijoa@gmail.com>
2025-05-29 21:29:34 +0900

    feat: CartDao 구현

--- app/src/main/java/woowacourse/shopping/data/CartDao.kt
+++ app/src/main/java/woowacourse/shopping/data/CartDao.kt
@@ -1,25 +1,17 @@
-// package woowacourse.shopping.data
-//
-// import androidx.lifecycle.LiveData
-// import androidx.room.Dao
-// import androidx.room.Insert
-// import androidx.room.OnConflictStrategy
-// import androidx.room.Query
-//
-// @Dao
-// interface CartDao {
-//    @Query("SELECT * FROM shopping_cart_items")
-//    fun getAllItemsLiveData(): LiveData<List<ShoppingCartItemEntity>>
-//
-//    @Query("SELECT * FROM shopping_cart_items")
-//    suspend fun getAllItemsList(): List<ShoppingCartItemEntity>
-//
-//    @Query("SELECT * FROM shopping_cart_items WHERE productId = :productId")
-//    suspend fun getItemById(productId: Int): ShoppingCartItemEntity?
-//
-//    @Insert(onConflict = OnConflictStrategy.REPLACE)
-//    suspend fun insertItem(item: ShoppingCartItemEntity)
-//
-//    @Query("DELETE FROM shopping_cart_items WHERE productId = :productId")
-//    suspend fun deleteItemById(productId: Int)
-// }
+package woowacourse.shopping.data
+
+import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Upsert
+
+@Dao
+interface CartDao {
+    @Query("SELECT * FROM shopping_cart")
+    fun getAllCartItems(): List<ShoppingCartEntity>
+
+    @Upsert
+    fun upsertCartItem(cartItem: ShoppingCartEntity)
+
+    @Query("DELETE FROM shopping_cart WHERE productId = :productId")
+    fun deleteCartItem(productId: Long)
+}
