f61be59aded86574497c3f83bdca80052ef1b9fa
devfeijoa <devfeijoa@gmail.com>
2025-05-29 21:30:13 +0900

    feat: ShoppingCartDatabase 구현

---
+++ app/src/main/java/woowacourse/shopping/data/ShoppingCartDatabase.kt
@@ -0,0 +1,27 @@
+package woowacourse.shopping.data
+
+import android.content.Context
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+abstract class ShoppingCartDatabase : RoomDatabase() {
+    abstract fun cartDao(): CartDao
+
+    companion object {
+        private const val DATABASE_NAME = "shopping_cart_database"
+
+        @Volatile
+        private var instance: ShoppingCartDatabase? = null
+
+        fun getDataBase(context: Context): ShoppingCartDatabase =
+            instance ?: synchronized(this) {
+                Room
+                    .databaseBuilder(
+                        context.applicationContext,
+                        ShoppingCartDatabase::class.java,
+                        DATABASE_NAME,
+                    ).build()
+                    .also { instance = it }
+            }
+    }
+}
