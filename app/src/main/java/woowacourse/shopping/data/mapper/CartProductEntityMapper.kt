495b43534e453b09ba7cdf7468c0aaf0f6543553
ijh1298 <imjh0417@gmail.com>
2025-05-30 16:50:23 +0900

    refactor: 불필요한 with 제거

--- app/src/main/java/woowacourse/shopping/data/mapper/CartProductEntityMapper.kt
+++ app/src/main/java/woowacourse/shopping/data/mapper/CartProductEntityMapper.kt
@@ -4,23 +4,19 @@ import woowacourse.shopping.data.entity.CartProductEntity
 import woowacourse.shopping.product.catalog.ProductUiModel
 
 fun CartProductEntity.toUiModel(): ProductUiModel =
-    with(this) {
-        ProductUiModel(
-            uid,
-            imageUrl,
-            name,
-            price,
-            quantity,
-        )
-    }
+    ProductUiModel(
+        uid,
+        imageUrl,
+        name,
+        price,
+        quantity,
+    )
 
 fun ProductUiModel.toEntity(): CartProductEntity =
-    with(this) {
-        CartProductEntity(
-            id,
-            imageUrl,
-            name,
-            price,
-            quantity,
-        )
-    }
+    CartProductEntity(
+        id,
+        imageUrl,
+        name,
+        price,
+        quantity,
+    )
