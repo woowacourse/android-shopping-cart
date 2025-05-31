d54eba5b44b6c5fee5743173d82c447305506bf2
gahyunkim <mapledt001@naver.com>
2025-05-31 22:55:48 +0900

    feat: PagingData에 page, hasPrevious 추가

--- app/src/main/java/woowacourse/shopping/product/catalog/model/PagingData.kt
+++ app/src/main/java/woowacourse/shopping/product/catalog/model/PagingData.kt
@@ -4,5 +4,7 @@ import woowacourse.shopping.product.catalog.ProductUiModel
 
 data class PagingData(
     val products: List<ProductUiModel>,
+    val page: Int,
     val hasNext: Boolean,
+    val hasPrevious: Boolean,
 )
