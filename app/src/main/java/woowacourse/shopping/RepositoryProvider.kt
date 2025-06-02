82bd85536f981831b77147db0fc7420a8d8326a5
cucumber99 <dlsguq1@gmail.com>
2025-06-02 15:41:56 +0900

    refactor: RepositoryProvider 개선
    - 메소드 파라미터로 Context 타입을 받습니다
    - Repository 변수를 외부에서 변경할 수 없습니다

--- app/src/main/java/woowacourse/shopping/RepositoryProvider.kt
+++ app/src/main/java/woowacourse/shopping/RepositoryProvider.kt
@@ -1,6 +1,6 @@
 package woowacourse.shopping
 
-import android.app.Application
+import android.content.Context
 import woowacourse.shopping.data.goods.repository.GoodsRepositoryImpl
 import woowacourse.shopping.data.recentgoods.database.RecentGoodsDatabase
 import woowacourse.shopping.data.recentgoods.repository.RecentGoodsRepositoryImpl
@@ -12,12 +12,15 @@ import woowacourse.shopping.domain.repository.ShoppingCartRepository
 
 object RepositoryProvider {
     lateinit var goodsRepository: GoodsRepository
+        private set
     lateinit var shoppingCartRepository: ShoppingCartRepository
+        private set
     lateinit var recentGoodsRepository: RecentGoodsRepository
+        private set
 
-    fun init(application: Application) {
-        val shoppingCartDatabase = ShoppingCartDatabase.getDatabase(application)
-        val recentGoodsDatabase = RecentGoodsDatabase.getDatabase(application)
+    fun init(context: Context) {
+        val shoppingCartDatabase = ShoppingCartDatabase.getDatabase(context)
+        val recentGoodsDatabase = RecentGoodsDatabase.getDatabase(context)
         goodsRepository = GoodsRepositoryImpl()
         shoppingCartRepository = ShoppingCartRepositoryImpl(shoppingCartDatabase.shoppingCartDao())
         recentGoodsRepository = RecentGoodsRepositoryImpl(recentGoodsDatabase.recentGoodsDao())
