ada7aa9dbccfab20a1d66fc9166d12ac14c1452b
m6z1 <sonmyungj1zz@gmail.com>
2025-06-03 16:03:59 +0900

    refactor: Local DB 하나로 수정

--- app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
+++ app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
@@ -2,35 +2,24 @@ package woowacourse.shopping
 
 import android.app.Application
 import androidx.room.Room
-import woowacourse.shopping.data.product.local.database.ProductDatabase
+import woowacourse.shopping.data.common.database.AppDatabase
 import woowacourse.shopping.data.product.repository.DefaultProductsRepository
-import woowacourse.shopping.data.shoppingCart.local.database.ShoppingCartDatabase
 import woowacourse.shopping.data.shoppingCart.repository.DefaultShoppingCartRepository
 
 class ShoppingApplication : Application() {
-    private val shoppingCartDatabase: ShoppingCartDatabase by lazy {
+    private val appDatabase: AppDatabase by lazy {
         Room
             .databaseBuilder(
                 applicationContext,
-                ShoppingCartDatabase::class.java,
-                "shoppingCart",
-            ).fallbackToDestructiveMigration()
-            .build()
-    }
-
-    private val productDatabase: ProductDatabase by lazy {
-        Room
-            .databaseBuilder(
-                applicationContext,
-                ProductDatabase::class.java,
-                "recent_watching",
+                AppDatabase::class.java,
+                "database",
             ).fallbackToDestructiveMigration()
             .build()
     }
 
     override fun onCreate() {
         super.onCreate()
-        DefaultShoppingCartRepository.initialize(shoppingCartDatabase.shoppingCartDao())
-        DefaultProductsRepository.initialize(productDatabase.recentWatchingDao())
+        DefaultShoppingCartRepository.initialize(appDatabase.shoppingCartDao())
+        DefaultProductsRepository.initialize(appDatabase.recentWatchingDao())
     }
 }
---
+++ app/src/main/java/woowacourse/shopping/data/common/database/AppDatabase.kt
@@ -0,0 +1,23 @@
+package woowacourse.shopping.data.common.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import woowacourse.shopping.data.product.local.dao.RecentWatchingDao
+import woowacourse.shopping.data.product.local.entity.ProductEntity
+import woowacourse.shopping.data.product.local.entity.RecentWatchingEntity
+import woowacourse.shopping.data.shoppingCart.local.dao.ShoppingCartDao
+import woowacourse.shopping.data.shoppingCart.local.entity.ShoppingCartProductEntity
+
+@Database(
+    entities = [
+        ShoppingCartProductEntity::class,
+        ProductEntity::class,
+        RecentWatchingEntity::class,
+    ],
+    version = 2,
+)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun recentWatchingDao(): RecentWatchingDao
+
+    abstract fun shoppingCartDao(): ShoppingCartDao
+}
