a6764f128eb7cd0b6e947cd7d717f7db736cd949
지원 <cheerfulboys@naver.com>
2025-06-03 16:46:24 +0900

    refactor: 최근 상품 레이아웃 매니저 수정

--- app/src/main/java/woowacourse/shopping/ui/fashionlist/RecentProductLayoutViewHolder.kt
+++ app/src/main/java/woowacourse/shopping/ui/fashionlist/RecentProductLayoutViewHolder.kt
@@ -1,6 +1,6 @@
 package woowacourse.shopping.ui.fashionlist
 
-import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import woowacourse.shopping.databinding.ItemRecentLayoutBinding
 import woowacourse.shopping.domain.product.Product
@@ -11,11 +11,6 @@ class RecentProductLayoutViewHolder(
     fun bind(item: List<Product>) {
         binding.rvRecentProduct.adapter = RecentProductAdapter(item)
         binding.rvRecentProduct.layoutManager =
-            GridLayoutManager(
-                binding.root.context,
-                1,
-                GridLayoutManager.HORIZONTAL,
-                false,
-            )
+            LinearLayoutManager(binding.root.context, LinearLayoutManager.HORIZONTAL, false)
     }
 }
