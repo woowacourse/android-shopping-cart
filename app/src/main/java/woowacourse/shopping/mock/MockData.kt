70ea7687a0b5347eaed2cf3c9801967759a2bcb6
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2026-04-29 00:01:43 +0900

    feat: 가짜 상품 데이터를 생성

---
+++ app/src/main/java/woowacourse/shopping/mock/MockData.kt
@@ -0,0 +1,31 @@
+package woowacourse.shopping.mock
+
+import woowacourse.shopping.domain.product.ImageUrl
+import woowacourse.shopping.domain.product.Price
+import woowacourse.shopping.domain.product.Product
+import woowacourse.shopping.domain.product.ProductName
+
+object MockData {
+    val productInfo =
+        Product(
+            name = ProductName("케로로"),
+            imageUrl =
+                ImageUrl(
+                    "https://img1.daumcdn.net/thumb/R1280x0.fwebp/?fname=http://t1.daumcdn.net/brunch/service/user/cnoC/image/81kyXbEZD1IOwgNjto1sFm7PPfI",
+                ),
+            price = Price(10000),
+        )
+
+    val products =
+        List(100) { index ->
+            Product(
+                name = ProductName("케로로 ${index + 1}"),
+                imageUrl =
+                    ImageUrl(
+                        "https://img1.daumcdn.net/thumb/R1280x0.fwebp/?fname=http://t1.daumcdn.net/brunch/service/user/cnoC/image/81kyXbEZD1IOwgNjto1sFm7PPfI",
+                    ),
+                price = Price(10000),
+                id = index.toString(),
+            )
+        }
+}
