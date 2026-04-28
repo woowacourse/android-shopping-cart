f1684b3444072d59ea64955a0a3d119c4494fda9
aksworns22 <wkdeogks17@gmail.com>
2026-04-28 15:47:18 +0900

    feat: 장바구니 화면 추가 및 상품 목록에서 장바구니 이동 기능 구현

--- app/src/main/java/woowacourse/shopping/MainActivity.kt
+++ app/src/main/java/woowacourse/shopping/MainActivity.kt
@@ -5,21 +5,35 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import woowacourse.shopping.ui.theme.AndroidshoppingTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        val intent = Intent(this, DetailProductActivity::class.java)
         setContent {
             AndroidshoppingTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    topBar = this::TopBar,
+                    modifier = Modifier.fillMaxSize(),
+                ) { innerPadding ->
                     ProductItem(
                         product =
                             Product(
@@ -30,11 +44,34 @@ class MainActivity : ComponentActivity() {
                             ),
                         modifier =
                             Modifier.padding(innerPadding).clickable(onClick = {
-                                startActivity(intent)
+                                startActivity(Intent(this, DetailProductActivity::class.java))
                             }),
                     )
                 }
             }
         }
     }
+
+    @Composable
+    private fun TopBar() {
+        TopAppBar(
+            title = { Text(text = "Shopping", color = Color.White) },
+            actions = {
+                IconButton(onClick = {
+                    startActivity(Intent(this@MainActivity, ShoppingCartActivity::class.java))
+                }) {
+                    Image(
+                        painter = painterResource(R.drawable.shopping_cart_icon),
+                        contentDescription = "장바구니 아이콘",
+                        modifier = Modifier.size(22.dp),
+                    )
+                }
+            },
+            colors =
+                TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color.Gray,
+                ),
+        )
+    }
+
 }
---
+++ app/src/main/java/woowacourse/shopping/ShoppingCartActivity.kt
@@ -0,0 +1,29 @@
+package woowacourse.shopping
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.ui.Modifier
+
+class ShoppingCartActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                TopAppBar(
+                    title = { Text(text = "장바구니") },
+                    modifier = Modifier.padding(innerPadding),
+                )
+            }
+        }
+    }
+}
