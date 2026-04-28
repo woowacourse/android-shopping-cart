e53b95680615bf9125d72ec8ded0480a46534397
todays-sun-day <178suna@gmail.com>
2026-04-28 21:14:44 +0900

    feat: 상품 상세 화면 UI 구현 및 관련 컴포넌트 추가

--- app/src/main/java/woowacourse/shopping/ui/shopping/component/ProductGroup.kt
+++ app/src/main/java/woowacourse/shopping/ui/shopping/component/ProductGroup.kt
@@ -8,13 +8,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import woowacourse.shopping.model.Product
 import woowacourse.shopping.model.Products
 import woowacourse.shopping.repository.inmemory.InMemoryProductRepository
 
 @Composable
 fun ProductGroup(
     products: Products,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    onProductClick: (Product) -> Unit
 ) {
     LazyVerticalGrid(
         columns = GridCells.Adaptive(minSize = 154.dp),
@@ -22,8 +24,8 @@ fun ProductGroup(
         verticalArrangement = Arrangement.spacedBy(12.dp),
         horizontalArrangement = Arrangement.spacedBy(12.dp)
     ) {
-        items(products.toList()) { product ->
-            ProductUnit(product)
+        items(items = products.toList(), key = { it.id }) { product ->
+            ProductUnit(product, onClick = { onProductClick(product) })
         }
     }
 }
@@ -32,6 +34,7 @@ fun ProductGroup(
 @Preview(showBackground = true)
 private fun ProductGroupPreview() {
     ProductGroup(
-        products = InMemoryProductRepository.products
+        products = InMemoryProductRepository.products,
+        onProductClick = {},
     )
 }
