8bc9dd59042de28cda17f76832f9124cac4e5608
joon0447 <joon0447@naver.com>
2026-04-28 15:46:58 +0900

    feat: CartItem 도메인 구현 - 테스트 코드, 도메인 작성

--- app/src/main/java/woowacourse/shopping/core/model/Money.kt
+++ app/src/main/java/woowacourse/shopping/core/model/Money.kt
@@ -7,4 +7,6 @@ value class Money(
     init {
         require(amount >= 0) { "금액은 0원 이상이어야 합니다." }
     }
+
+    operator fun times(time: Int): Money = Money(this.amount * time)
 }
---
+++ app/src/test/java/woowacourse/shopping/feature/cart/model/CartItemTest.kt
@@ -0,0 +1,37 @@
+package woowacourse.shopping.feature.cart.model
+
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.Test
+import woowacourse.shopping.core.model.Money
+import woowacourse.shopping.core.model.Product
+import woowacourse.shopping.core.model.ProductName
+
+class CartItemTest {
+    private val product =
+        Product(
+            name = ProductName(name = "상품"),
+            price = Money(amount = 5000),
+            imageUrl = "",
+        )
+
+    @Test
+    fun `해당 상품의 개수에 따른 가격을 계산한다`() {
+        val item =
+            CartItem(
+                product = product,
+                quantity = 2,
+            )
+        assertThat(item.getTotalPrice()).isEqualTo(Money(10000))
+    }
+
+    @Test
+    fun `상품 개수가 0 이하일 시 예외를 반환한다`() {
+        assertThatThrownBy {
+            CartItem(
+                product = product,
+                quantity = 0,
+            )
+        }.isInstanceOf(IllegalArgumentException::class.java)
+    }
+}
