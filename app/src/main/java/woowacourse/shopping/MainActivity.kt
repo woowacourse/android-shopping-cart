485eb7d9a6539da8c9d9ce69aad2c17eed52015a
ujeong <qnf323@naver.com>
2026-04-28 21:40:59 +0900

    feat: 상품 목록 화면, 상품 목록 앱바 구현

--- app/src/main/java/woowacourse/shopping/MainActivity.kt
+++ app/src/main/java/woowacourse/shopping/MainActivity.kt
@@ -1,21 +1,18 @@
 package woowacourse.shopping
 
+import ProductListAppBar
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Icon
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import woowacourse.shopping.constants.MockData
 import woowacourse.shopping.ui.theme.AndroidshoppingTheme
 
@@ -31,20 +28,13 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProductListScreen() {
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         topBar = {
-            Box {
-                Row {
-                    Text(stringResource(R.string.app_name))
-                    Icon(
-                        painter = painterResource(R.drawable.cart_icon),
-                        contentDescription = stringResource(R.string.cart_description),
-                    )
-                }
-            }
+            ProductListAppBar()
         },
     ) { innerPadding ->
         Box(
