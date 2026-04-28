f936f0b638f2d24d18ca605a543074d508fcf436
todays-sun-day <178suna@gmail.com>
2026-04-29 00:55:08 +0900

    feat: 공통 타이포그래피 추가

---
+++ app/src/main/java/woowacourse/shopping/ui/ShoppingTypography.kt
@@ -0,0 +1,22 @@
+package woowacourse.shopping.ui
+
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+object ShoppingTypography {
+    val titleMedium = TextStyle(
+        fontSize = 20.sp,
+        fontWeight = FontWeight.W500
+    )
+
+    val productName = TextStyle(
+        fontSize = 18.sp,
+        fontWeight = FontWeight.W700
+    )
+
+    val productPrice = TextStyle(
+        fontSize = 16.sp,
+        fontWeight = FontWeight.W400
+    )
+}
