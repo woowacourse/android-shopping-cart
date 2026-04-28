8ef4e2c5264ca3d5868a224c529c29f8952d2c88
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2026-04-28 23:59:03 +0900

    refactor: product의 상태를 위한 객체들의 프로퍼티의 가시성을 변경

--- app/src/main/java/woowacourse/shopping/domain/product/Price.kt
+++ app/src/main/java/woowacourse/shopping/domain/product/Price.kt
@@ -2,7 +2,7 @@ package woowacourse.shopping.domain.product
 
 @JvmInline
 value class Price(
-    private val value: Int,
+    val value: Int,
 ) {
     init {
         require(value >= 0) { "가격이 음수입니다." }
