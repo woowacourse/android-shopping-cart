d8caa6f7d6f25c3a2bd61e466c9f2872d1ee4f12
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2026-04-29 18:07:05 +0900

    feat: 장바구니 도메인에 페이징 로직 추가

--- app/src/main/java/woowacourse/shopping/domain/cart/Cart.kt
+++ app/src/main/java/woowacourse/shopping/domain/cart/Cart.kt
@@ -3,7 +3,17 @@ package woowacourse.shopping.domain.cart
 data class Cart(
     val cartItems: CartItems,
 ) {
+    val totalCount: Int = cartItems.size()
+
     fun addCartItem(cartItem: CartItem): Cart = copy(cartItems = cartItems.addCartItem(cartItem))
 
     fun removeCartItem(cartItem: CartItem): Cart = copy(cartItems = cartItems.removeCartItem(cartItem))
+
+    fun getPage(
+        page: Int,
+        pageSize: Int,
+    ): List<CartItem> {
+        val fromIndex = page * pageSize
+        return cartItems.subList(fromIndex, fromIndex + pageSize)
+    }
 }
--- app/src/main/java/woowacourse/shopping/domain/cart/CartItems.kt
+++ app/src/main/java/woowacourse/shopping/domain/cart/CartItems.kt
@@ -1,11 +1,18 @@
 package woowacourse.shopping.domain.cart
 
 class CartItems(
-    private val value: List<CartItem>,
+    private val value: List<CartItem> = emptyList(),
 ) {
     fun addCartItem(cartItem: CartItem): CartItems = CartItems(value + cartItem)
 
     fun removeCartItem(cartItem: CartItem): CartItems = CartItems(value.filter { !it.isSameCartItem(cartItem) })
 
     fun searchCartItem(cartItem: CartItem): Boolean = value.any { it.isSameCartItem(cartItem) }
+
+    fun subList(
+        fromIndex: Int,
+        toIndex: Int,
+    ): List<CartItem> = value.subList(fromIndex, minOf(toIndex, value.size))
+
+    fun size(): Int = value.size
 }
