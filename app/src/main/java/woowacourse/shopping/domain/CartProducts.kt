21e4e38959061e005c736750c696e9636a5dc828
Redish03 <pluto0303@pukyong.ac.kr>
2026-04-29 18:24:11 +0900

    feat: products의 사이즈 계산 로직 추가

--- app/src/main/java/woowacourse/shopping/domain/CartProducts.kt
+++ app/src/main/java/woowacourse/shopping/domain/CartProducts.kt
@@ -5,6 +5,7 @@ import java.util.UUID
 class CartProducts(
     val products: List<Product> = emptyList()
 ){
+    fun size() = products.size
     fun add(product: Product) = CartProducts(products + product)
     fun remove(id: UUID): CartProducts {
         val product = findWithId(id) ?: return this
