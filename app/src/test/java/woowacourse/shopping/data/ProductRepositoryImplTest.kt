cb3ef39b86aae114080a4efdb9b5017ed6a267d2
cksckckcks <chan679999@naver.com>
2026-04-29 19:56:49 +0900

    refactor: 테스트 편의성을 위한 가짜 객체 도입

--- app/src/test/java/woowacourse/shopping/data/ProductRepositoryImplTest.kt
+++ app/src/test/java/woowacourse/shopping/data/ProductRepositoryImplTest.kt
@@ -4,12 +4,14 @@ import io.kotest.matchers.equals.shouldEqual
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import woowacourse.shopping.data.repository.ProductRepositoryImpl
+import woowacourse.shopping.data.source.ProductDataSource
+import woowacourse.shopping.domain.Product
+import woowacourse.shopping.fixture.ShoppingFixture
 
 class ProductRepositoryImplTest {
     @Test
     fun `특정 상품을 id를 통해 가져올 수 있다`() {
-        val productManager =
-            ProductRepositoryImpl()
+        val productManager = ProductRepositoryImpl(FakeProductDataSource())
 
         productManager.getProductById(id = "1").name shouldEqual "bolt"
     }
@@ -17,10 +19,13 @@ class ProductRepositoryImplTest {
     @Test
     fun `존재하지 않는 id일 경우 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
-            val productManager =
-                ProductRepositoryImpl()
+            val productManager = ProductRepositoryImpl(FakeProductDataSource())
 
             productManager.getProductById(id = "아아아아")
         }
     }
+}
+
+class FakeProductDataSource : ProductDataSource {
+    override val products: List<Product> = listOf(ShoppingFixture.getProduct(id = "1"))
 }
