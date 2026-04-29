627e202222623c67037e452e6dc7078d12c1aca9
todays-sun-day <178suna@gmail.com>
2026-04-29 17:53:22 +0900

    feat: 장바구니 페이지네이션 기능 구현 및 CartPaging 컴포넌트 추가

--- app/src/main/java/woowacourse/shopping/ui/cart/CartScreen.kt
+++ app/src/main/java/woowacourse/shopping/ui/cart/CartScreen.kt
@@ -1,10 +1,17 @@
 package woowacourse.shopping.ui.cart
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -13,6 +20,9 @@ import woowacourse.shopping.model.Product
 import woowacourse.shopping.repository.inmemory.InMemoryProductRepository
 import woowacourse.shopping.ui.cart.component.CartHeader
 import woowacourse.shopping.ui.cart.component.CartItemGroup
+import woowacourse.shopping.ui.cart.component.CartPaging
+
+private const val PAGE_SIZE = 5
 
 @Composable
 fun CartScreen(
@@ -21,17 +31,42 @@ fun CartScreen(
     onBackClick: () -> Unit,
     onDeleteClick: (Product) -> Unit
 ) {
+    val cartItemsSize = cart.items.size
+    val totalPages = (cartItemsSize - 1) / PAGE_SIZE + 1
+
+    var currentPage by remember { mutableIntStateOf(1) }
+    val pagedItems = cart.items.toList()
+        .drop((currentPage - 1) * PAGE_SIZE)
+        .take(PAGE_SIZE)
+        .toMap()
+
+
+    LaunchedEffect(totalPages) {
+        currentPage = currentPage.coerceAtMost(totalPages)
+    }
+
     Column(
         modifier = modifier.fillMaxSize()
     ) {
         CartHeader(onBackClick = onBackClick)
 
-        Box(
-            modifier = Modifier.padding(top = 24.dp, start = 18.dp, end = 18.dp)
-        ) {
-            CartItemGroup(
-                cart = cart,
-                onDeleteClick = onDeleteClick
+        CartItemGroup(
+            cart = Cart(pagedItems),
+            modifier = Modifier
+                .padding(top = 8.dp, start = 18.dp, end = 18.dp)
+                .weight(1f),
+            onDeleteClick = onDeleteClick
+        )
+
+        if (cartItemsSize >= PAGE_SIZE) {
+            Spacer(modifier = Modifier.height(60.dp))
+
+            CartPaging(
+                currentPage = currentPage,
+                totalPages = totalPages,
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+                onPreviousClick = { currentPage -= 1 },
+                onNextClick = { currentPage += 1 }
             )
         }
     }
@@ -40,14 +75,6 @@ fun CartScreen(
 @Composable
 @Preview(showBackground = true)
 private fun CartScreenPreview() {
-    CartScreen(
-        cart = Cart(
-            mapOf(
-                Pair(InMemoryProductRepository.APPLE, 1),
-                Pair(InMemoryProductRepository.BBOYAMI, 1)
-            )
-        ),
-        onBackClick = {},
-        onDeleteClick = {}
-    )
+    val cart = Cart(InMemoryProductRepository.products.associateWith { 1 })
+    CartScreen(cart = cart, onBackClick = {}, onDeleteClick = {})
 }
