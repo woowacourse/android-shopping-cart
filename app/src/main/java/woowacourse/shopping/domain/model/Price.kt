cc70ce42942b411f6356fa26738da256c3483638
hanminjae <hanminjae131@gmail.com>
2026-04-29 15:33:33 +0900

    feat: 상품 및 가격 관련 도메인 모델 구현
    - 빈 값 검증 로직을 포함한 ProductTitle 값 클래스 추가
    - 음수 값 검증 로직을 포함한 Price 값 클래스 추가
    - 상품 정보를 담는 Product 데이터 클래스 구현
    - 중복된 상품을 제외하여 관리하는 ProductItems 일급 컬렉션 구현

---
+++ app/src/main/java/woowacourse/shopping/domain/model/Price.kt
@@ -0,0 +1,8 @@
+package woowacourse.shopping.domain.model
+
+@JvmInline
+value class Price(val value: Int) {
+    init {
+        require(value >= 0) { "가격은 음수일 수 없습니다." }
+    }
+}
---
+++ app/src/main/java/woowacourse/shopping/domain/model/product/ProductItems.kt
@@ -0,0 +1,8 @@
+package woowacourse.shopping.domain.model.product
+
+data class ProductItems(
+    private val _items: List<Product>
+) {
+    val items: List<Product>
+        get() = _items.distinctBy { it.id }
+}
