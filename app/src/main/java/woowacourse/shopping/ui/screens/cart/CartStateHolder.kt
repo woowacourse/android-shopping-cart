6b07b7c70409e8e9fc3cff21e25a965433bc5465
cksckckcks <chan679999@naver.com>
2026-04-29 18:14:04 +0900

    feat: 장바구니 스테이트 홀더 구현

---
+++ app/src/main/java/woowacourse/shopping/ui/screens/cart/CartStateHolder.kt
@@ -0,0 +1,14 @@
+package woowacourse.shopping.ui.screens.cart
+
+import woowacourse.shopping.data.CartRepositoryImpl
+import woowacourse.shopping.domain.repository.CartRepository
+
+class CartStateHolder(
+    private val cartRepository: CartRepository = CartRepositoryImpl()
+) {
+    val cartItems = cartRepository.getCartItems()
+
+    fun deleteCartItem(id: String) {
+        cartRepository.deleteItem(id)
+    }
+}
