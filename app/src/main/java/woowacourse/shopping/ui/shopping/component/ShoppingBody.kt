63a46fcb4d19364d1345fd546e53fbac943532f8
todays-sun-day <178suna@gmail.com>
2026-04-29 23:06:58 +0900

    feat: 상품 목록 더보기 기능 구현 및 UI 추가

--- app/src/main/java/woowacourse/shopping/ui/shopping/component/ShoppingBody.kt
+++ app/src/main/java/woowacourse/shopping/ui/shopping/component/ShoppingBody.kt
@@ -1,10 +1,14 @@
 package woowacourse.shopping.ui.shopping.component
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -13,28 +17,43 @@ import woowacourse.shopping.model.Products
 import woowacourse.shopping.repository.inmemory.InMemoryProductRepository
 
 @Composable
-fun ProductGroup(
+fun ShoppingBody(
     products: Products,
+    showMoreButton: Boolean,
     modifier: Modifier = Modifier,
-    onProductClick: (Product) -> Unit
+    onProductClick: (Product) -> Unit,
+    onMoreClick: () -> Unit
 ) {
     LazyVerticalGrid(
         columns = GridCells.Adaptive(minSize = 154.dp),
         modifier = modifier,
         verticalArrangement = Arrangement.spacedBy(12.dp),
-        horizontalArrangement = Arrangement.spacedBy(12.dp)
+        horizontalArrangement = Arrangement.spacedBy(12.dp),
     ) {
         items(items = products.toList(), key = { it.id }) { product ->
             ProductUnit(product, onClick = { onProductClick(product) })
+        }
+
+        if (showMoreButton) {
+            item(span = { GridItemSpan(maxLineSpan) } ) {
+                Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
+                    MoreButton(
+                        modifier = Modifier,
+                        onClick = onMoreClick,
+                    )
+                }
+            }
         }
     }
 }
 
 @Composable
 @Preview(showBackground = true)
-private fun ProductGroupPreview() {
-    ProductGroup(
+private fun ShoppingBodyPreview() {
+    ShoppingBody(
         products = InMemoryProductRepository.products,
+        showMoreButton = true,
         onProductClick = {},
+        onMoreClick = {}
     )
 }
