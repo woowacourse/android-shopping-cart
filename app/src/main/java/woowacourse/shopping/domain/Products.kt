599b10579f25ede0b6282246f326ed1f761b5620
chohs4164 <josh100as@gmail.com>
2026-04-30 16:54:36 +0900

    fix: Products 도메인 비즈니스 로직 추가

--- app/src/main/java/woowacourse/shopping/domain/Products.kt
+++ app/src/main/java/woowacourse/shopping/domain/Products.kt
@@ -1,12 +1,26 @@
 package woowacourse.shopping.domain
 
 import woowacourse.shopping.ProductFixture
+import kotlin.math.min
 import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
 
+val SHOPPING_PAGE_SIZE = 20
 data class Products(
     val products: List<Product> = emptyList()
 ) {
+    fun getProducts(page:Int, pageSize:Int = SHOPPING_PAGE_SIZE):List<Product>{
+        val fromIndex = page * pageSize
+        val toIndex = min(fromIndex + SHOPPING_PAGE_SIZE, products.size)
+        return products.subList(0,toIndex)
+    }
+
+    fun hasNextPage(currentPage:Int,pageSize:Int = SHOPPING_PAGE_SIZE) : Boolean{
+        return getProducts(page = currentPage).size >= SHOPPING_PAGE_SIZE &&
+                (currentPage+1) * pageSize < products.size
+    }
     @OptIn(ExperimentalUuidApi::class)
     fun findProductById(productId: Uuid): Product? = ProductFixture.productList.firstOrNull { it.productId == productId }
+
+
 }
