368ee2a53680e83749393173d54111aca3038724
joon0447 <joon0447@naver.com>
2026-04-30 16:26:14 +0900

    refactor: 사용하지 않는 import 제거

--- app/src/main/java/woowacourse/shopping/feature/shopping/ui/ShoppingScreen.kt
+++ app/src/main/java/woowacourse/shopping/feature/shopping/ui/ShoppingScreen.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.minus
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
