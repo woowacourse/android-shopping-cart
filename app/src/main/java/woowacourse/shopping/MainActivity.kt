4b033a735f710de0b04c92a5308487f9d5a11371
cksckckcks <chan679999@naver.com>
2026-04-30 16:05:31 +0900

    refactor: MainActivity가 라우팅만 하도록 코드 수정

--- app/src/main/java/woowacourse/shopping/MainActivity.kt
+++ app/src/main/java/woowacourse/shopping/MainActivity.kt
@@ -1,17 +1,17 @@
 package woowacourse.shopping
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.enableEdgeToEdge
 import woowacourse.shopping.ui.screens.product.ProductActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
 
         val intent = Intent(this, ProductActivity::class.java)
         startActivity(intent)
+
+        finish()
     }
 }
