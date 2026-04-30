d4437c79cc65f16d1c60c0907aec9a9758d6fc37
todays-sun-day <178suna@gmail.com>
2026-04-30 15:23:06 +0900

    test: 장바구니 상태 관리 테스트를 CartRepositoryTest로 이전 및 README 반영

--- app/src/test/java/woowacourse/shopping/repository/CartRepositoryTest.kt
+++ app/src/test/java/woowacourse/shopping/repository/CartRepositoryTest.kt
@@ -1,11 +1,16 @@
 @file:Suppress("NonAsciiCharacters")
 
-package woowacourse.shopping.model
+package woowacourse.shopping.repository
 
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import woowacourse.shopping.model.Money
+import woowacourse.shopping.model.Product
+import woowacourse.shopping.repository.inmemory.InMemoryCartRepository
 
-class CartTest {
+class CartRepositoryTest {
+    val repo: CartRepository = InMemoryCartRepository
+
     val product1 = Product(
         name = "새우깡",
         price = Money(3100),
@@ -21,23 +26,21 @@ class CartTest {
 
     @Test
     fun `정상적인 상품 객체를 장바구니에 추가하면, 장바구니 내부 목록에 해당 상품이 올바르게 반영된다`() {
-        val cart = Cart(items).add(product2)
-        val result1 = cart.showAll().contains(product1)
-        val result2 = cart.showAll().contains(product2)
+        repo.add(product1)
 
-        assert(result1 && result2)
+        assert(repo.showAll().items.contains(product1))
     }
 
     @Test
     fun `장바구니에 존재하는 상품을 삭제 요청하면, 목록에서 해당 상품이 정상적으로 제거된다`() {
-        val cart = Cart(items).delete(product1)
-        val result = cart.showAll().containsKey(product1)
+        repo.add(product1)
+        repo.delete(product1)
 
-        assert(!result)
+        assert(!repo.showAll().items.contains(product1))
     }
 
     @Test
     fun `존재하지 않는 상품의 삭제를 시도할 경우, 상태가 변경되지 않거나 올바르게 무시,예외 처리된다`() {
-        assertThrows<IllegalArgumentException> { Cart(items).delete(product2) }
+        assertThrows<IllegalArgumentException> { repo.delete(product1) }
     }
 }
