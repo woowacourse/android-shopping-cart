e37317de9c1d19839b2d6c6dce7e021fb3aeeecd
joon0447 <joon0447@naver.com>
2026-05-02 00:51:52 +0900

    test: CartRepository의 다른 테스트에 영향이 가지 않도록 코드 수정

--- app/src/test/java/woowacourse/shopping/domain/repository/CartRepositoryTest.kt
+++ app/src/test/java/woowacourse/shopping/domain/repository/CartRepositoryTest.kt
@@ -8,13 +8,6 @@ import woowacourse.shopping.domain.model.Product
 import woowacourse.shopping.domain.model.ProductName
 
 class CartRepositoryTest {
-    private val product =
-        Product(
-            id = "1",
-            name = ProductName("상품"),
-            price = Money(2000),
-            imageUrl = "ds",
-        )
 
     @Test
     fun `등록되지 않은 상품을 등록하면 NewAdded를 반환한다`() {
@@ -31,6 +24,11 @@ class CartRepositoryTest {
 
     @Test
     fun `이미 등록된 상품을 다시 등록하면 DuplicateItem을 반환한다`() {
+        val product = Product(
+            name = ProductName("상품2"),
+            price = Money(2000),
+            imageUrl = "ds",
+        )
         CartRepository.addItem(product = product)
         assertThat(
             CartRepository.addItem(product = product),
