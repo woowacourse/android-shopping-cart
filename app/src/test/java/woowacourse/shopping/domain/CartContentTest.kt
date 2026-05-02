d3252a07d7048cfdac9349371765efb36521b3d4
ujeong <qnf323@naver.com>
2026-05-02 21:43:18 +0900

    refactor: normalProduct 생성에 id를 입력받을 수 있도록 수정하여 id를 직접 비교할 수 있도록 수정

--- app/src/test/java/woowacourse/shopping/domain/CartContentTest.kt
+++ app/src/test/java/woowacourse/shopping/domain/CartContentTest.kt
@@ -1,5 +1,6 @@
 package woowacourse.shopping.domain
 
+import java.util.UUID
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
@@ -8,13 +9,13 @@ class CartContentTest {
     fun `입력받은 상품의 id가 CartItem 의 상품 id 와 같으면 true를 반환한다`() {
 
         // given : 상품과 CartItem이 주어진다
-        val product = normalProduct("임시")
+        val product = normalProduct("임시", id = "1")
 
         val cartContent = CartContent(product, Quantity(1))
 
         // when : 다른 상품을 입력받아 비교할 때
         val result = cartContent.hasProductId(
-            product.id,
+            "1",
         )
 
         // then : true를 반환한다
@@ -25,8 +26,8 @@ class CartContentTest {
     fun `입력받은 상품의 id가 CartItem 의 상품 id 와 다르면 false를 반환한다`() {
 
         // given : 상품과 CartItem이 주어진다
-        val product = normalProduct("임시")
-        val other = normalProduct("임시2")
+        val product = normalProduct("임시", "1")
+        val other = normalProduct("임시2", "2")
 
         val cartContent = CartContent(product, Quantity(1))
 
@@ -39,7 +40,11 @@ class CartContentTest {
         assertEquals(false, result)
     }
 
-    private fun normalProduct(title: String): Product = Product(
+    private fun normalProduct(
+        title: String,
+        id: String = UUID.randomUUID().toString(),
+    ): Product = Product(
+        id = id,
         name = title,
         price = Money(1000),
         imageUrl = "",
