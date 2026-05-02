7bdbbb5635479882d2022107e4802c4412e67eac
nadajinny <jinsun23007@gmail.com>
2026-05-03 01:38:12 +0900

    test: suspend 함수로 인한 테스트 오류 해결

--- app/src/test/java/woowacourse/shopping/repository/CartRepositoryTest.kt
+++ app/src/test/java/woowacourse/shopping/repository/CartRepositoryTest.kt
@@ -2,6 +2,10 @@
 
 package woowacourse.shopping.repository
 
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import woowacourse.shopping.model.Money
@@ -24,25 +28,44 @@ class CartRepositoryTest {
             price = Money(1300),
             imageUrl = "",
         )
-    val items = mapOf(product1 to 1)
+    @AfterEach
+    fun tearDown() =
+        runBlocking {
+            clearProduct(product1)
+            clearProduct(product2)
+        }
 
     @Test
-    fun `정상적인 상품 객체를 장바구니에 추가하면, 장바구니 내부 목록에 해당 상품이 올바르게 반영된다`() {
-        repo.add(product1)
+    fun `정상적인 상품 객체를 장바구니에 추가하면, 장바구니 내부 목록에 해당 상품이 올바르게 반영된다`() =
+        runBlocking {
+            repo.add(product1)
 
-        assert(repo.showAll().items.contains(product1))
-    }
+            val actual = repo.getCartItems(0, repo.count())
 
-    @Test
-    fun `장바구니에 존재하는 상품을 삭제 요청하면, 목록에서 해당 상품이 정상적으로 제거된다`() {
-        repo.add(product1)
-        repo.delete(product1)
-
-        assert(!repo.showAll().items.contains(product1))
-    }
+            assertTrue(actual.contains(product1))
+        }
 
     @Test
-    fun `존재하지 않는 상품의 삭제를 시도할 경우, 상태가 변경되지 않거나 올바르게 무시,예외 처리된다`() {
-        assertThrows<IllegalArgumentException> { repo.delete(product1) }
+    fun `장바구니에 존재하는 상품을 삭제 요청하면, 목록에서 해당 상품이 정상적으로 제거된다`() =
+        runBlocking {
+            repo.add(product1)
+            repo.delete(product1)
+
+            val actual = repo.getCartItems(0, repo.count())
+
+            assertFalse(actual.contains(product1))
+        }
+
+    @Test
+    fun `존재하지 않는 상품의 삭제를 시도할 경우, 상태가 변경되지 않거나 올바르게 무시,예외 처리된다`() =
+        runBlocking {
+            assertThrows<IllegalArgumentException> { runBlocking { repo.delete(product1) } }
+        }
+
+    private suspend fun clearProduct(product: Product) {
+        while (true) {
+            runCatching { repo.delete(product) }
+                .onFailure { return }
+        }
     }
 }
--- app/src/test/java/woowacourse/shopping/repository/ProductRepositoryTest.kt
+++ app/src/test/java/woowacourse/shopping/repository/ProductRepositoryTest.kt
@@ -2,6 +2,8 @@
 
 package woowacourse.shopping.repository
 
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import woowacourse.shopping.repository.inmemory.InMemoryProductRepository
 
@@ -9,24 +11,26 @@ class ProductRepositoryTest {
     val repo: ProductRepository = InMemoryProductRepository
 
     @Test
-    fun `특정 오프셋과 사이즈(20개)를 요청했을 때, 정확히 해당 구간의 데이터를 반환한다`() {
-        val size = 20
-        val offset = 0
+    fun `특정 오프셋과 사이즈(20개)를 요청했을 때, 정확히 해당 구간의 데이터를 반환한다`() =
+        runBlocking {
+            val size = 20
+            val offset = 0
 
-        val actual = repo.getProducts(offset, size).count()
-        val expected = 20
+            val actual = repo.getProducts(offset, size).count()
+            val expected = 20
 
-        assert(expected == actual)
-    }
+            assertEquals(expected, actual)
+        }
 
     @Test
-    fun `전체 데이터 개수보다 큰 범위(남은 데이터가 20개 미만)를 요청했을 때, 예외 없이 남은 개수만큼만 정상 반환한다`() {
-        val totalSize = repo.size
-        val moreThanTotalSize = totalSize + 1
+    fun `전체 데이터 개수보다 큰 범위(남은 데이터가 20개 미만)를 요청했을 때, 예외 없이 남은 개수만큼만 정상 반환한다`() =
+        runBlocking {
+            val totalSize = repo.size
+            val moreThanTotalSize = totalSize + 1
 
-        val actual = repo.getProducts(0, moreThanTotalSize).count()
-        val expected = repo.size
+            val actual = repo.getProducts(0, moreThanTotalSize).count()
+            val expected = repo.size
 
-        assert(expected == actual)
-    }
+            assertEquals(expected, actual)
+        }
 }
