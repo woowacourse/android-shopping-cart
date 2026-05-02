183f0061d367da173b6f6509c89cc91b7e8b28a7
joon0447 <joon0447@naver.com>
2026-05-02 10:36:11 +0900

    test: CartRepositoryTest를 InMemoryCartRepositoryTest로 변경

--- app/src/test/java/woowacourse/shopping/data/InMemoryCartRepositoryTest.kt
+++ app/src/test/java/woowacourse/shopping/data/InMemoryCartRepositoryTest.kt
@@ -1,5 +1,6 @@
-package woowacourse.shopping.domain.repository
+package woowacourse.shopping.data
 
+import kotlinx.coroutines.test.runTest
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import woowacourse.shopping.domain.model.AddItemResult
@@ -7,31 +8,36 @@ import woowacourse.shopping.domain.model.Money
 import woowacourse.shopping.domain.model.Product
 import woowacourse.shopping.domain.model.ProductName
 
-class CartRepositoryTest {
+class InMemoryCartRepositoryTest {
+
+    private val repository = InMemoryCartRepository
+
     @Test
-    fun `등록되지 않은 상품을 등록하면 NewAdded를 반환한다`() {
+    fun `등록되지 않은 상품을 등록하면 NewAdded를 반환한다`() = runTest {
         val uniqueProduct =
             Product(
                 name = ProductName("상품"),
                 price = Money(2000),
                 imageUrl = "ds",
             )
-        assertThat(
-            CartRepository.addItem(product = uniqueProduct),
-        ).isInstanceOf(AddItemResult.NewAdded::class.java)
+
+        val result = repository.addItem(product = uniqueProduct)
+
+        assertThat(result).isInstanceOf(AddItemResult.NewAdded::class.java)
     }
 
     @Test
-    fun `이미 등록된 상품을 다시 등록하면 DuplicateItem을 반환한다`() {
+    fun `이미 등록된 상품을 다시 등록하면 DuplicateItem을 반환한다`() = runTest {
         val product =
             Product(
                 name = ProductName("상품2"),
                 price = Money(2000),
                 imageUrl = "ds",
             )
-        CartRepository.addItem(product = product)
-        assertThat(
-            CartRepository.addItem(product = product),
-        ).isEqualTo(AddItemResult.DuplicateItem)
+        repository.addItem(product = product)
+
+        val result = repository.addItem(product = product)
+
+        assertThat(result).isEqualTo(AddItemResult.DuplicateItem)
     }
 }
