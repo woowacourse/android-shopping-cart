a9952d10b97284ba762bc589b7e7716c831bd805
조은경 <108224432+eungyeong12@users.noreply.github.com>
2026-05-03 01:17:57 +0900

    refactor: map과 copy를 사용하는 방식으로 수정

--- app/src/main/java/woowacourse/shopping/domain/model/cart/Cart.kt
+++ app/src/main/java/woowacourse/shopping/domain/model/cart/Cart.kt
@@ -10,16 +10,22 @@ data class Cart(
     val cartItems: List<CartItem> = emptyList(),
 ) {
     fun addProductToCart(product: Product): Cart {
-        val index = cartItems.indexOfFirst { product.productId == it.product.productId }
-        if (index != -1) {
-            val existProductAndCount = cartItems[index]
-            val newProductAndCount = existProductAndCount.increaseQuantity()
-            val updated = cartItems.toMutableList()
-            updated[index] = newProductAndCount
-            return copy(cartItems = updated)
-        } else {
+        val exists = cartItems.any { product.productId == it.product.productId }
+
+        if (!exists) {
             return copy(cartItems = cartItems + CartItem(product, 1))
         }
+
+        return copy(
+            cartItems =
+                cartItems.map {
+                    if (product.productId == it.product.productId) {
+                        it.increaseQuantity()
+                    } else {
+                        it
+                    }
+                },
+        )
     }
 
     fun deleteProductFromCart(productId: Uuid): Cart =
