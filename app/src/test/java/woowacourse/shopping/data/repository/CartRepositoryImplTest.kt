0eaff8062d7df2c26ac5d2cad29da4df5a987eef
조은경 <108224432+eungyeong12@users.noreply.github.com>
2026-05-03 21:25:58 +0900

    test: CartRepositoryImpl 테스트 추가

---
+++ app/src/test/java/woowacourse/shopping/data/repository/CartRepositoryImplTest.kt
@@ -0,0 +1,53 @@
+package woowacourse.shopping.data.repository
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import woowacourse.shopping.domain.model.product.Price
+import woowacourse.shopping.domain.model.product.Product
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
+
+class CartRepositoryImplTest {
+    @OptIn(ExperimentalUuidApi::class)
+    val product = Product(
+        productId = Uuid.random(),
+        imageUrl = "",
+        productName = "동원 스위트콘",
+        price = Price(99800),
+    )
+
+    @Test
+    fun `page가 음수이면 빈 Cart를 반환한다`() {
+        val cart = CartRepositoryImpl.getPagingItems(page = -1, pageSize = 10)
+        assertThat(cart.cartItems).isEmpty()
+    }
+
+    @Test
+    fun `pageSize가 0 이하이면 빈 Cart를 반환한다`() {
+        val cart = CartRepositoryImpl.getPagingItems(page = 0, pageSize = 0)
+        assertThat(cart.cartItems).isEmpty()
+    }
+
+    @Test
+    fun `장바구니가 비어 있으면 빈 Cart를 반환한다`() {
+        val cart = CartRepositoryImpl.getPagingItems(page = 0, pageSize = 10)
+        assertThat(cart.cartItems).isEmpty()
+    }
+
+    @Test
+    fun `범위를 벗어난 페이지이면 빈 Cart를 반환한다`() {
+        val cart = CartRepositoryImpl.getPagingItems(page = 3, pageSize = 10)
+        assertThat(cart.cartItems).isEmpty()
+    }
+
+    @OptIn(ExperimentalUuidApi::class)
+    @Test
+    fun `상품을 추가하면 장바구니에 저장된다`() {
+        CartRepositoryImpl.addProduct(product)
+    }
+
+    @Test
+    fun `상품을 삭제하면 장바구니에서 삭제된다`() {
+        CartRepositoryImpl.addProduct(product)
+    }
+}
