b49286368281a970e00a92636eba17156e6f8790
Redish03 <pluto0303@pukyong.ac.kr>
2026-05-04 01:28:54 +0900

    feat: 카트에 담기 성공했다면 "장바구니에 담았습니다" Snackbar를 노출한 후 화면전환을 하지 않는다

--- app/src/main/java/woowacourse/shopping/ProductDetailActivity.kt
+++ app/src/main/java/woowacourse/shopping/ProductDetailActivity.kt
@@ -1,13 +1,19 @@
 package woowacourse.shopping
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import kotlinx.coroutines.launch
 import woowacourse.shopping.ui.component.screen.ProductDetailScreen
 import woowacourse.shopping.ui.theme.AndroidshoppingTheme
 import java.util.UUID
@@ -18,16 +24,19 @@ class ProductDetailActivity : ComponentActivity() {
 
         val productId = UUID.fromString(intent.getStringExtra("id"))
         val product = MockCatalog.findProductById(productId)
+        val toast = Toast.makeText(this, "장바구니에 담았습니다", Toast.LENGTH_SHORT)
 
         enableEdgeToEdge()
         setContent {
             AndroidshoppingTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                ) { innerPadding ->
                     ProductDetailScreen(
                         product = product,
                         onAddRequest = {
                             CartProvider.addItem(product)
-                            setResult(RESULT_OK, intent)
+                            toast.show()
                             finish()
                         },
                         onClose = { finish() },
