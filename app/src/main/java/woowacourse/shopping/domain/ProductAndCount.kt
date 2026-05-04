9bfc3d3aa9e9fb4682b0a7069be7c0853f7c0010
chohs4164 <josh100as@gmail.com>
2026-05-04 14:54:44 +0900

    refactor: ProductAndCount 도메인 수정
    - count < 0이 될 경우를 고려하여 생성 시점에서 count>=0을 보장할 수 있도록 해주었습니다.

--- app/src/main/java/woowacourse/shopping/domain/ProductAndCount.kt
+++ app/src/main/java/woowacourse/shopping/domain/ProductAndCount.kt
@@ -8,6 +8,10 @@ data class ProductAndCount(
     val product: Product,
     private val count: Int,
 ) {
+    init {
+        require(count >= 0) { "상품의 수량은 음수가 될 수 없습니다." }
+    }
+
     fun increaseQuantity(): ProductAndCount = copy(count = count + 1)
 
     fun decreaseQuantity(): ProductAndCount = copy(count = count - 1)
