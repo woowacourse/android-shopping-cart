ce0b09eee35d95ebc425653676faa5e7472e1490
nadajinny <jinsun23007@gmail.com>
2026-05-04 14:36:57 +0900

    test: Cart 수정에 따른 test 수정 진행

--- app/src/test/java/woowacourse/shopping/repository/CartRepositoryTest.kt
+++ app/src/test/java/woowacourse/shopping/repository/CartRepositoryTest.kt
@@ -39,33 +39,33 @@ class CartRepositoryTest {
     @Test
     fun `정상적인 상품 객체를 장바구니에 추가하면, 장바구니 내부 목록에 해당 상품이 올바르게 반영된다`() =
         runBlocking {
-            repo.add(product1)
+            repo.add(product1.id)
 
             val actual = repo.getCartItems(0, repo.count())
 
-            assertTrue(actual.contains(product1))
+            assertTrue(actual.containsKey(product1.id))
         }
 
     @Test
     fun `장바구니에 존재하는 상품을 삭제 요청하면, 목록에서 해당 상품이 정상적으로 제거된다`() =
         runBlocking {
-            repo.add(product1)
-            repo.delete(product1)
+            repo.add(product1.id)
+            repo.delete(product1.id)
 
             val actual = repo.getCartItems(0, repo.count())
 
-            assertFalse(actual.contains(product1))
+            assertFalse(actual.containsKey(product1.id))
         }
 
     @Test
-    fun `존재하지 않는 상품의 삭제를 시도할 경우, 상태가 변경되지 않거나 올바르게 무시,예외 처리된다`() =
+    fun `존재하지 않는 상품의 삭제를 시도할 경우, 상태가 변경되지 않거나 올바르게 무시,예외 처리된다`(): Unit =
         runBlocking {
-            assertThrows<IllegalArgumentException> { runBlocking { repo.delete(product1) } }
+            assertThrows<IllegalArgumentException> { runBlocking { repo.delete(product1.id) } }
         }
 
     private suspend fun clearProduct(product: Product) {
         while (true) {
-            runCatching { repo.delete(product) }
+            runCatching { repo.delete(product.id) }
                 .onFailure { return }
         }
     }
