550fd1a9782888a1eee5168cf6708a59097ee91a
baekcii <bsy8470@naver.com>
2026-05-04 14:50:43 +0900

    test: 상품 레포지토리 테스트 코드 추가 및 수정

--- app/src/test/java/woowacourse/shopping/data/ProductRepositoryImplTest.kt
+++ app/src/test/java/woowacourse/shopping/data/ProductRepositoryImplTest.kt
@@ -1,17 +1,15 @@
 package woowacourse.shopping.data
 
 import io.kotest.matchers.equals.shouldEqual
+import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import woowacourse.shopping.data.repository.ProductRepositoryImpl
-import woowacourse.shopping.data.source.ProductDataSource
-import woowacourse.shopping.domain.Product
-import woowacourse.shopping.fixture.ShoppingFixture
 
 class ProductRepositoryImplTest {
     @Test
     fun `특정 상품을 id를 통해 가져올 수 있다`() {
-        val productManager = ProductRepositoryImpl(FakeProductDataSource())
+        val productManager = ProductRepositoryImpl()
 
         productManager.getProductById(id = "1").name shouldEqual "bolt"
     }
@@ -19,13 +17,32 @@ class ProductRepositoryImplTest {
     @Test
     fun `존재하지 않는 id일 경우 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
-            val productManager = ProductRepositoryImpl(FakeProductDataSource())
+            val productManager = ProductRepositoryImpl()
 
             productManager.getProductById(id = "아아아아")
         }
     }
-}
 
-class FakeProductDataSource : ProductDataSource {
-    override val products: List<Product> = listOf(ShoppingFixture.getProduct(id = "1"))
+    @Test
+    fun `상품을 모두 로드하면 hasNext는 false가 된다`() =
+        runTest {
+            val productRepository = ProductRepositoryImpl()
+            repeat(2) {
+                productRepository.getProducts()
+            }
+            productRepository.hasNext shouldEqual false
+        }
+
+    @Test
+    fun `모든 상품을 로드한 후 추가로 조회하면 빈 리스트를 반환한다`() =
+        runTest {
+            val productRepository = ProductRepositoryImpl()
+
+            repeat(2) {
+                productRepository.getProducts()
+            }
+
+            val result = productRepository.getProducts()
+            result.size shouldEqual 0
+        }
 }
