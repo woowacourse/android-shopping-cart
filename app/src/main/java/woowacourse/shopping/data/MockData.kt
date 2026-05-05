434af31dd05521c50f9492ef17866c739cd31201
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2026-05-06 01:33:08 +0900

    refactor: 사용하지 않는 MockData 제거

--- app/src/main/java/woowacourse/shopping/data/MockData.kt
+++ app/src/main/java/woowacourse/shopping/data/MockData.kt
@@ -7,16 +7,6 @@ import woowacourse.shopping.domain.product.model.Product
 import woowacourse.shopping.domain.product.model.ProductName
 
 object MockData {
-    val productInfo =
-        Product(
-            name = ProductName("케로로"),
-            imageUrl =
-                ImageUrl(
-                    "https://img1.daumcdn.net/thumb/R1280x0.fwebp/?fname=http://t1.daumcdn.net/brunch/service/user/cnoC/image/81kyXbEZD1IOwgNjto1sFm7PPfI",
-                ),
-            price = Price(10000),
-        )
-
     val products =
         listOf(
             Product(
@@ -134,17 +124,4 @@ object MockData {
                     id = index.toString(),
                 )
             }
-
-    val cartItems =
-        List(11) { index ->
-            CartItem(
-                product =
-                    Product(
-                        id = index.toString(),
-                        name = ProductName("케로로 ${index + 1}"),
-                        imageUrl = ImageUrl("https://picsum.photos/seed/product$index/200/200"),
-                        price = Price((index + 1) * 10000),
-                    ),
-            )
-        }
 }
