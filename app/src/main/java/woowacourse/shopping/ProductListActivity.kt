f532b41049b89c846e87b86b521bf3f7f944c5f9
chohs4164 <josh100as@gmail.com>
2026-05-08 01:51:20 +0900

    refactor: ProductListViewModel 생성

--- app/src/main/java/woowacourse/shopping/ProductListActivity.kt
+++ app/src/main/java/woowacourse/shopping/ProductListActivity.kt
@@ -5,8 +5,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import woowacourse.shopping.domain.Products
 import woowacourse.shopping.ui.shopping.screen.ProductListScreen
+import woowacourse.shopping.ui.shopping.viewmodel.ProductListViewModel
 import woowacourse.shopping.ui.theme.AndroidShoppingTheme
 import kotlin.uuid.ExperimentalUuidApi
 
@@ -15,14 +18,21 @@ class ProductListActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val viewModel = ProductListViewModel(
+            AppContainer.createProductRepository(packageName),
+            AppContainer.cartRepository
+        )
+
         setContent {
             AndroidShoppingTheme {
                 ProductListScreen(
+                    viewModel = viewModel,
                     products = Products(ProductFixture.productList(packageName)),
                     onCartClick = { startActivity(Intent(this, CartActivity::class.java)) },
                     onProductClick = { productId ->
                         ProductDetailActivity.start(this, productId)
                     },
+                    modifier = Modifier.testTag("product_list")
                 )
             }
         }
---
+++ app/src/main/java/woowacourse/shopping/ui/shopping/viewmodel/ProductListViewModel.kt
@@ -0,0 +1,52 @@
+package woowacourse.shopping.ui.shopping.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import woowacourse.shopping.domain.PageRequest
+import woowacourse.shopping.domain.Product
+import woowacourse.shopping.domain.SHOPPING_PAGE_SIZE
+import woowacourse.shopping.domain.toPage
+import woowacourse.shopping.repository.cart.CartRepository
+import woowacourse.shopping.repository.cart.InMemoryCartRepository
+import woowacourse.shopping.repository.product.InMemoryProductRepository
+import woowacourse.shopping.repository.product.ProductRepository
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
+
+@OptIn(ExperimentalUuidApi::class)
+class ProductListViewModel(
+    private val productRepository: ProductRepository,
+    private val cartRepository: CartRepository
+) : ViewModel() {
+
+    // productRepository
+    var currentPageIndex by mutableStateOf(0)
+        private set
+
+    fun visibleProducts(): List<Product> =
+        productRepository.getAllProducts()
+            .products
+            .toPage(PageRequest(0, (currentPageIndex + 1) * SHOPPING_PAGE_SIZE))
+            .items
+
+    fun increasePageIndex() {
+        currentPageIndex++
+    }
+
+    // cartRepository
+    fun getProductQuantity(productId: Uuid): Int =
+        cartRepository.getProductQuantity(productId = productId)
+
+    val totalProductQuantity: Int
+        get() = cartRepository.getCartProducts().sumOf { it.quantity }
+
+    fun addProduct(product: Product, quantityToAdd: Int) {
+        cartRepository.addProduct(product = product, quantityToAdd = quantityToAdd)
+    }
+
+    fun decreaseProduct(productId: Uuid, quantityToRemove: Int) {
+        cartRepository.decreaseProduct(productId = productId, quantityToRemove = quantityToRemove)
+    }
+}
