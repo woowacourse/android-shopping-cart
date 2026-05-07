247c82d492e567dfbf86d8ed1add43961a8ea7c2
first-woosun <dbdntjs620@gmail.com>
2026-05-07 23:17:37 +0900

    feat: QuantitySelector CallBack 로직 지정

--- app/src/main/java/woowacourse/shopping/ProductDetailActivity.kt
+++ app/src/main/java/woowacourse/shopping/ProductDetailActivity.kt
@@ -7,8 +7,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import woowacourse.shopping.domain.Product
+import woowacourse.shopping.domain.PurchaseProduct
 import woowacourse.shopping.ui.component.screen.ProductDetailScreen
 import woowacourse.shopping.ui.theme.AndroidshoppingTheme
 
@@ -24,12 +29,21 @@ class ProductDetailActivity : ComponentActivity() {
 
         enableEdgeToEdge()
         setContent {
+            var count by rememberSaveable { mutableIntStateOf(1) }
+
             AndroidshoppingTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     ProductDetailScreen(
                         product = product,
+                        count = count,
+                        onAdd = { count++ },
+                        onMinus = {
+                            if (count != 1) count-- else count
+                        },
                         onAddRequest = {
-                            intent.putExtra(IntentKeys.STORED_PRODUCT_KEY, product)
+                            intent.putExtra(IntentKeys.STORED_PRODUCT_KEY,
+                                PurchaseProduct(product, count)
+                            )
                             setResult(RESULT_OK, intent)
                             finish()
                         },
