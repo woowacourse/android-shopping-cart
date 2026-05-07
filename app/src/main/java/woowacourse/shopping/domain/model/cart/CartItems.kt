871a79442367d26e87c34a3f277658564cc86c33
cbnuLeehyunwoo <henow123@naver.com>
2026-05-07 16:07:41 +0900

    refactor: CartItems 클래스의 내부 리스트 관리 로직 및 프로퍼티 구조 단순화

--- app/src/main/java/woowacourse/shopping/domain/model/cart/CartItems.kt
+++ app/src/main/java/woowacourse/shopping/domain/model/cart/CartItems.kt
@@ -2,31 +2,28 @@ package woowacourse.shopping.domain.model.cart
 
 import woowacourse.shopping.domain.model.Quantity
 
-data class CartItems(
-    private val _items: List<CartItem> = emptyList(),
+class CartItems(
+    items: List<CartItem> = emptyList(),
 ) {
-    val safelist = _items.toList()
-    val items: List<CartItem>
-        get() = safelist
-
+    val items: List<CartItem> = items.toList()
     fun addOrMerge(newItem: CartItem): CartItems {
-        val existingItem = _items.find { it.product.id == newItem.product.id }
+        val existingItem = items.find { it.product.id == newItem.product.id }
 
         val newItems =
             if (existingItem != null) {
-                _items.map { cartItem ->
+                items.map { cartItem ->
                     if (cartItem.product.id != newItem.product.id) return@map cartItem
                     cartItem.copy(quantity = Quantity(cartItem.quantity.value + newItem.quantity.value))
                 }
             } else {
-                _items + newItem
+                items + newItem
             }
 
         return CartItems(newItems)
     }
 
     fun remove(productId: String): CartItems {
-        val newItems = _items.filterNot { it.product.id == productId }
+        val newItems = items.filterNot { it.product.id == productId }
         return CartItems(newItems)
     }
 }
