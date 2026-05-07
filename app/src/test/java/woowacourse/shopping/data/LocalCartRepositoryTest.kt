0d3c72f5267b4df4c194922e7feb7451239042fa
joon0447 <joon0447@naver.com>
2026-05-07 22:08:30 +0900

    style: ktlint 적용

--- app/src/test/java/woowacourse/shopping/data/LocalCartRepositoryTest.kt
+++ app/src/test/java/woowacourse/shopping/data/LocalCartRepositoryTest.kt
@@ -31,63 +31,67 @@ class LocalCartRepositoryTest {
     }
 
     @Test
-    fun `등록되지 않은 상품을 등록하면 NewAdded를 반환한다`() = runTest {
-        coEvery { cartDao.findById(productId) } returns null
-        coEvery { cartDao.addOrIncrement(productId) } returns Unit
-        coEvery { cartDao.getAll() } returns listOf(CartEntity(productId, 1))
-        coEvery { productRepository.getProductById(productId) } returns product
+    fun `등록되지 않은 상품을 등록하면 NewAdded를 반환한다`() =
+        runTest {
+            coEvery { cartDao.findById(productId) } returns null
+            coEvery { cartDao.addOrIncrement(productId) } returns Unit
+            coEvery { cartDao.getAll() } returns listOf(CartEntity(productId, 1))
+            coEvery { productRepository.getProductById(productId) } returns product
 
-        val result = repository.addItem(productId)
+            val result = repository.addItem(productId)
 
-        assertThat(result).isInstanceOf(AddItemResult.NewAdded::class.java)
-    }
-
-    @Test
-    fun `이미 등록된 상품을 등록하면 Increment를 반환한다`() = runTest {
-        val existingEntity = CartEntity(productId, 1)
-        coEvery { cartDao.findById(productId) } returns existingEntity
-        coEvery { cartDao.addOrIncrement(productId) } returns Unit
-        coEvery { cartDao.getAll() } returns listOf(CartEntity(productId, 2))
-        coEvery { productRepository.getProductById(productId) } returns product
-
-        // When
-        val result = repository.addItem(productId)
-
-        assertThat(result).isInstanceOf(AddItemResult.Incremented::class.java)
-    }
+            assertThat(result).isInstanceOf(AddItemResult.NewAdded::class.java)
+        }
 
     @Test
-    fun `수량 감소 시 장바구니에 등록된 상품의 수량을 1 감소시킨다`() = runTest {
-        val existingEntity = CartEntity(productId, 2)
-        coEvery { cartDao.findById(productId) } returns existingEntity
-        coEvery { cartDao.deleteOrDecrement(productId) } returns Unit
-        coEvery { cartDao.getAll() } returns listOf(CartEntity(productId, 1))
-        coEvery { productRepository.getProductById(productId) } returns product
+    fun `이미 등록된 상품을 등록하면 Increment를 반환한다`() =
+        runTest {
+            val existingEntity = CartEntity(productId, 1)
+            coEvery { cartDao.findById(productId) } returns existingEntity
+            coEvery { cartDao.addOrIncrement(productId) } returns Unit
+            coEvery { cartDao.getAll() } returns listOf(CartEntity(productId, 2))
+            coEvery { productRepository.getProductById(productId) } returns product
 
-        val result = repository.decrease(productId)
+            // When
+            val result = repository.addItem(productId)
 
-        assertThat(result).isInstanceOf(RemoveItemResult.Success::class.java)
-        coVerify { cartDao.deleteOrDecrement(productId) }
-
-        val cart = (result as RemoveItemResult.Success).cart
-        assertThat(cart.items).hasSize(1)
-        assertThat(cart.items.first().quantity).isEqualTo(1)
-    }
+            assertThat(result).isInstanceOf(AddItemResult.Incremented::class.java)
+        }
 
     @Test
-    fun `장바구니에 등록된 상품의 수량이 1일 때 수량을 감소시키면 장바구니에서 삭제된다`() = runTest {
-        val existingEntity = CartEntity(productId, 1)
-        coEvery { cartDao.findById(productId) } returns existingEntity
-        coEvery { cartDao.deleteOrDecrement(productId) } returns Unit
-        coEvery { cartDao.getAll() } returns emptyList()
-        coEvery { productRepository.getProductById(productId) } returns product
+    fun `수량 감소 시 장바구니에 등록된 상품의 수량을 1 감소시킨다`() =
+        runTest {
+            val existingEntity = CartEntity(productId, 2)
+            coEvery { cartDao.findById(productId) } returns existingEntity
+            coEvery { cartDao.deleteOrDecrement(productId) } returns Unit
+            coEvery { cartDao.getAll() } returns listOf(CartEntity(productId, 1))
+            coEvery { productRepository.getProductById(productId) } returns product
 
-        val result = repository.decrease(productId)
+            val result = repository.decrease(productId)
 
-        assertThat(result).isInstanceOf(RemoveItemResult.Success::class.java)
-        coVerify { cartDao.deleteOrDecrement(productId) }
+            assertThat(result).isInstanceOf(RemoveItemResult.Success::class.java)
+            coVerify { cartDao.deleteOrDecrement(productId) }
 
-        val cart = (result as RemoveItemResult.Success).cart
-        assertThat(cart.items).hasSize(0)
-    }
+            val cart = (result as RemoveItemResult.Success).cart
+            assertThat(cart.items).hasSize(1)
+            assertThat(cart.items.first().quantity).isEqualTo(1)
+        }
+
+    @Test
+    fun `장바구니에 등록된 상품의 수량이 1일 때 수량을 감소시키면 장바구니에서 삭제된다`() =
+        runTest {
+            val existingEntity = CartEntity(productId, 1)
+            coEvery { cartDao.findById(productId) } returns existingEntity
+            coEvery { cartDao.deleteOrDecrement(productId) } returns Unit
+            coEvery { cartDao.getAll() } returns emptyList()
+            coEvery { productRepository.getProductById(productId) } returns product
+
+            val result = repository.decrease(productId)
+
+            assertThat(result).isInstanceOf(RemoveItemResult.Success::class.java)
+            coVerify { cartDao.deleteOrDecrement(productId) }
+
+            val cart = (result as RemoveItemResult.Success).cart
+            assertThat(cart.items).hasSize(0)
+        }
 }
--- app/src/test/java/woowacourse/shopping/domain/model/CartTest.kt
+++ app/src/test/java/woowacourse/shopping/domain/model/CartTest.kt
@@ -1,24 +1,24 @@
 package woowacourse.shopping.domain.model
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 class CartTest {
+    private val product1 =
+        Product(
+            id = "1",
+            name = ProductName("상품1"),
+            price = Money(5000),
+            imageUrl = "",
+        )
 
-    private val product1 = Product(
-        id = "1",
-        name = ProductName("상품1"),
-        price = Money(5000),
-        imageUrl = ""
-    )
-
-    private val product2 = Product(
-        id = "2",
-        name = ProductName("상품2"),
-        price = Money(1000),
-        imageUrl = ""
-    )
+    private val product2 =
+        Product(
+            id = "2",
+            name = ProductName("상품2"),
+            price = Money(1000),
+            imageUrl = "",
+        )
 
     @Test
     fun `빈 카트는 size는 0이다`() {
@@ -34,25 +34,26 @@ class CartTest {
 
     @Test
     fun `카트의 상품 품목이 2개 들어있다면 size는 2를 반환한다`() {
-        val cart = Cart(
-            listOf(
-                CartItem(product = product1, quantity = 1),
-                CartItem(product = product2, quantity = 30),
+        val cart =
+            Cart(
+                listOf(
+                    CartItem(product = product1, quantity = 1),
+                    CartItem(product = product2, quantity = 30),
+                ),
             )
-        )
         assertThat(cart.size).isEqualTo(2)
     }
 
     @Test
     fun `totalPrice가 상품 전체의 가격을 계산한다`() {
-        val cart = Cart(
-            listOf(
-                CartItem(product = product1, quantity = 1),
-                CartItem(product = product2, quantity = 30),
+        val cart =
+            Cart(
+                listOf(
+                    CartItem(product = product1, quantity = 1),
+                    CartItem(product = product2, quantity = 30),
+                ),
             )
-        )
 
         assertThat(cart.totalPrice).isEqualTo(Money(35000))
     }
-
 }
