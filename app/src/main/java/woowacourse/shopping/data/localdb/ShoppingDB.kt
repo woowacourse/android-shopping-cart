0df9d23bee5edf0c640c4494071c29966ad8ce1e
gykim_kr <k_bright_st@pusan.ac.kr>
2026-05-09 00:36:22 +0900

    feat: ShoppingDB 싱글톤 패턴 구현

--- app/src/main/java/woowacourse/shopping/data/localdb/ShoppingDB.kt
+++ app/src/main/java/woowacourse/shopping/data/localdb/ShoppingDB.kt
@@ -1,6 +1,8 @@
 package woowacourse.shopping.data.localdb
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import woowacourse.shopping.data.localdb.dao.CartItemDao
 import woowacourse.shopping.data.localdb.dao.RecentItemDao
@@ -14,4 +16,22 @@ import woowacourse.shopping.data.localdb.entity.RecentItemEntity
 abstract class ShoppingDB : RoomDatabase() {
     abstract fun cartItemDao(): CartItemDao
     abstract fun recentItemDao(): RecentItemDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: ShoppingDB? = null
+
+        fun getInstance(context: Context): ShoppingDB {
+            return INSTANCE ?: synchronized(this) {
+                INSTANCE ?: Room.databaseBuilder(
+                    context.applicationContext,
+                    ShoppingDB::class.java,
+                    "shopping_db",
+                ).build().also {
+                    INSTANCE = it
+                }
+
+            }
+        }
+    }
 }
