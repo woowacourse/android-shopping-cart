1b4af80aa443c515a7f6c22d38180329855b1058
cbnuLeehyunwoo <henow123@naver.com>
2026-05-09 00:22:12 +0900

    build: Jetpack Compose Runtime 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.compose.runtime)
     testImplementation(libs.junit.jupiter)
     testImplementation(libs.kotest.runner.junit5)
     androidTestImplementation(libs.androidx.espresso.core)
