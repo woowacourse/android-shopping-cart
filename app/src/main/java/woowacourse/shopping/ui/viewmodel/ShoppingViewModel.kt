7cd0399d532e55a30ff958c222dfbab10d48a3d7
first-woosun <dbdntjs620@gmail.com>
2026-05-10 02:37:03 +0900

    feat: 최근 본 상품 관련 기능 구현
    - 최근 본 상품 전체 목록 조회
    - 가장 마지막에 본 상품 조회
    - 상품 조회 기론 업데이트

--- app/src/main/java/woowacourse/shopping/ui/viewmodel/ShoppingViewModel.kt
+++ app/src/main/java/woowacourse/shopping/ui/viewmodel/ShoppingViewModel.kt
@@ -3,21 +3,21 @@ package woowacourse.shopping.ui.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.WhileSubscribed
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
-import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import woowacourse.shopping.data.repository.PurchaseProductsRepository
+import woowacourse.shopping.data.repository.RecentlyViewedProductRepository
 import woowacourse.shopping.domain.Cart
+import woowacourse.shopping.domain.Product
+import woowacourse.shopping.domain.Products
 import woowacourse.shopping.domain.PurchaseProduct
 
 class ShoppingViewModel(
-    private val purchaseProductsRepository: PurchaseProductsRepository
+    private val purchaseProductsRepository: PurchaseProductsRepository,
+    private val recentlyViewedProductRepository: RecentlyViewedProductRepository
 ): ViewModel() {
     val cart: StateFlow<Cart> = purchaseProductsRepository.getCart()
         .stateIn(
@@ -60,15 +60,36 @@ class ShoppingViewModel(
             purchaseProductsRepository.deletePurchaseProduct(id)
         }
     }
+
+    val viewingHistory: StateFlow<Products> = recentlyViewedProductRepository.getAll()
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = Products()
+        )
+
+    val lastViewedProduct: StateFlow<Product?> = recentlyViewedProductRepository.getLatestItem()
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = null
+        )
+
+    fun updateHistory(product: Product) {
+        viewModelScope.launch {
+            recentlyViewedProductRepository.updateList(product)
+        }
+    }
 }
 
 class ShoppingViewModelFactory(
-    private val purchaseProductsRepository: PurchaseProductsRepository
+    private val purchaseProductsRepository: PurchaseProductsRepository,
+    private val recentlyViewedProductRepository: RecentlyViewedProductRepository
 ): ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if(modelClass.isAssignableFrom(ShoppingViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
-            return ShoppingViewModel(purchaseProductsRepository) as T
+            return ShoppingViewModel(purchaseProductsRepository, recentlyViewedProductRepository) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
