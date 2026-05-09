9b14da3f39222c4d7e63c55145a37141e71eb2dc
first-woosun <dbdntjs620@gmail.com>
2026-05-09 15:38:52 +0900

    feat: ProductDetailViewModel 구현

---
+++ app/src/main/java/woowacourse/shopping/ui/viewmodel/ProductDetailViewModel.kt
@@ -0,0 +1,48 @@
+package woowacourse.shopping.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import woowacourse.shopping.ProductDetailActivity
+import woowacourse.shopping.data.repository.PurchaseProductsRepository
+import woowacourse.shopping.domain.PurchaseProduct
+
+class ProductDetailViewModel(
+    private val purchaseProductsRepository: PurchaseProductsRepository
+): ViewModel() {
+    private val _count = MutableStateFlow(1)
+
+    val countState = _count.value
+
+    fun addCount() {
+        _count.update { it + 1 }
+    }
+
+    fun minusCount() {
+        if (countState > 1) {
+            _count.update { it - 1 }
+        }
+    }
+
+    fun addPurchaseProduct(purchaseProduct: PurchaseProduct) {
+        viewModelScope.launch { purchaseProductsRepository.insert(purchaseProduct) }
+    }
+}
+
+class ProductDetailViewModelFactory(
+    private val purchaseProductsRepository: PurchaseProductsRepository
+): ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if(modelClass.isAssignableFrom(ProductDetailViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return ProductDetailViewModel(purchaseProductsRepository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
