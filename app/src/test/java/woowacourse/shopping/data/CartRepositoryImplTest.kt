7146fa21ff1be7fee60e66befbcbe9019e865c83
cksckckcks <chan679999@naver.com>
2026-05-10 01:23:04 +0900

    test: 카트 레포지토리 변경으로 인한 테스트 추가 작성

--- app/src/test/java/woowacourse/shopping/data/CartRepositoryImplTest.kt
+++ app/src/test/java/woowacourse/shopping/data/CartRepositoryImplTest.kt
@@ -47,6 +47,49 @@ class CartRepositoryImplTest {
 
             cartRepository.getCartItemByPage(1).firstOrNull { it.product == product } shouldBe null
         }
+
+    @Test
+    fun `장바구니에 아이템 개수를 반환한다`() {
+        val cartRepository = CartRepositoryImpl(cartDataSource = FakeCartDataSource())
+
+        val product = ShoppingFixture.getProduct(id = "1")
+        cartRepository.addItem(product = product, amount = 1)
+
+        cartRepository.getItemCount(product.id) shouldEqual 1
+    }
+
+    @Test
+    fun `장바구니에 아이템 개수를 증가시키면 1만큼 증가한다`() {
+        val cartRepository = CartRepositoryImpl(cartDataSource = FakeCartDataSource())
+
+        val product = ShoppingFixture.getProduct(id = "1")
+        cartRepository.addItem(product = product, amount = 1)
+        cartRepository.plusItemCount(product = product)
+
+        cartRepository.getItemCount(product.id) shouldEqual 2
+    }
+
+    @Test
+    fun `장바구니에 아이템 개수를 감소시키면 1만큼 감소한다`() {
+        val cartRepository = CartRepositoryImpl(cartDataSource = FakeCartDataSource())
+
+        val product = ShoppingFixture.getProduct(id = "1")
+        cartRepository.addItem(product = product, amount = 2)
+        cartRepository.minusItemCount(productId = product.id)
+
+        cartRepository.getItemCount(product.id) shouldEqual 1
+    }
+
+    @Test
+    fun `장바구니에 아이템 개수가 1개일 때 감소시키면 장바구니에서 제거된다`() {
+        val cartRepository = CartRepositoryImpl(cartDataSource = FakeCartDataSource())
+
+        val product = ShoppingFixture.getProduct(id = "1")
+        cartRepository.addItem(product = product, amount = 1)
+        cartRepository.minusItemCount(productId = product.id)
+
+        cartRepository.getItemCount(product.id) shouldEqual 0
+    }
 }
 
 class FakeCartDataSource : CartDataSource {
@@ -65,5 +108,13 @@ class FakeCartDataSource : CartDataSource {
 
     override fun deleteItem(productId: String) {
         items.removeIf { it.product.id == productId }
+    }
+
+    override fun updateItem(cartItem: CartItem) {
+        val idx = items.indexOfFirst { it.product.id == cartItem.product.id }
+
+        require(idx != -1) { "카트에 존재하지 않는 상품입니다." }
+
+        items[idx] = cartItem
     }
 }
