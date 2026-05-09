17ce520415701d3fbd769d93c182d46b17ec0545
joon0447 <joon0447@naver.com>
2026-05-10 01:33:17 +0900

    test: ShoppingViewModel 테스트 코드 추가 및 FakeRepository 추가

--- app/src/main/java/woowacourse/shopping/data/repository/LocalCartRepository.kt
+++ app/src/main/java/woowacourse/shopping/data/repository/LocalCartRepository.kt
@@ -56,5 +56,5 @@ class LocalCartRepository(
 
     override suspend fun getAllQuantities(): Map<Long, Int> = cartDao.getAll().associate { it.productId to it.quantity }
 
-    override suspend fun getQuantity(id: Long): Int = cartDao.findById(id)?.quantity ?: 1
+    override suspend fun getQuantity(id: Long): Int = cartDao.findById(id)?.quantity ?: 0
 }
