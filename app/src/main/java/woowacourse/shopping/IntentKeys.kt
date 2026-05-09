98bdadecaab5dedd15ad8f9a6099d8a311ae2f55
first-woosun <dbdntjs620@gmail.com>
2026-05-10 02:35:07 +0900

    refactor: intent key 수정

--- app/src/main/java/woowacourse/shopping/IntentKeys.kt
+++ app/src/main/java/woowacourse/shopping/IntentKeys.kt
@@ -2,5 +2,5 @@ package woowacourse.shopping
 
 object IntentKeys {
     const val SELECTED_PRODUCT_KEY = "selected_product"
-    const val RECENTLY_VIEWED_PRODUCT = "recently_viewed_product"
+    const val LATEST_VIEWED_PRODUCT = "latest_viewed_product"
 }
