e9f7361042843252909376b33acc708b5e2d50b9
first-woosun <dbdntjs620@gmail.com>
2026-05-10 02:39:44 +0900

    feat: RecentlyViewedProductRepository 인스턴스 추가 및 버전 마이그레이션

--- app/src/main/java/woowacourse/shopping/data/database/DataBase.kt
+++ app/src/main/java/woowacourse/shopping/data/database/DataBase.kt
@@ -4,15 +4,37 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import woowacourse.shopping.data.dao.PurchaseProductsDao
+import woowacourse.shopping.data.dao.RecentlyViewedProductDao
 import woowacourse.shopping.data.entity.PurchaseProductEntity
+import woowacourse.shopping.data.entity.RecentlyViewedProductEntity
+
+val MIGRATION_2_3 = object : Migration(2, 3) {
+    override fun migrate(db: SupportSQLiteDatabase) {
+        db.execSQL(
+            """
+                CREATE TABLE IF NOT EXISTS `recently_viewed_products` (
+                    `id` TEXT NOT NULL,
+                    `name` TEXT NOT NULL,
+                    `price` INTEGER NOT NULL,
+                    `imageUri` TEXT NOT NULL,
+                    `time_stamp` INTEGER NOT NULL,
+                    PRIMARY KEY(`id`)
+                )
+            """.trimIndent()
+        )
+    }
+}
 
 @Database(
-    entities = [PurchaseProductEntity::class],
-    version = 1
+    entities = [PurchaseProductEntity::class, RecentlyViewedProductEntity::class],
+    version = 3
 )
 abstract class DataBase: RoomDatabase() {
     abstract fun purchaseProductsDao(): PurchaseProductsDao
+    abstract fun recentlyViewedProductDao(): RecentlyViewedProductDao
 
     companion object {
         @Volatile
@@ -23,8 +45,8 @@ abstract class DataBase: RoomDatabase() {
                 Room.databaseBuilder(
                     context = context,
                     klass = DataBase::class.java,
-                    name = "purchase_products_database"
-                ).build().also { instance = it }
+                    name = "shopping_database"
+                ).addMigrations(MIGRATION_2_3).build().also { instance = it }
             }
         }
     }
