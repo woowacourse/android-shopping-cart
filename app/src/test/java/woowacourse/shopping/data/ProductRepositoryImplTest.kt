9df2c2b9e75f689edc74108be1f0c00b02eede7f
baekcii <bsy8470@naver.com>
2026-05-10 17:47:09 +0900

    test: ProductRepositoryImplTest 및 CartItemTest 수정

--- app/src/test/java/woowacourse/shopping/data/ProductRepositoryImplTest.kt
+++ app/src/test/java/woowacourse/shopping/data/ProductRepositoryImplTest.kt
@@ -9,17 +9,17 @@ import woowacourse.shopping.data.repository.ProductRepositoryImpl
 class ProductRepositoryImplTest {
     @Test
     fun `특정 상품을 id를 통해 가져올 수 있다`() {
-        val productManager = ProductRepositoryImpl()
+        val productRepository = ProductRepositoryImpl()
 
-        productManager.getProductById(id = "1").name shouldEqual "bolt"
+        productRepository.getProductById(id = "1").name shouldEqual "bolt"
     }
 
     @Test
     fun `존재하지 않는 id일 경우 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
-            val productManager = ProductRepositoryImpl()
+            val productRepository = ProductRepositoryImpl()
 
-            productManager.getProductById(id = "아아아아")
+            productRepository.getProductById(id = "아아아아")
         }
     }
 
@@ -27,22 +27,7 @@ class ProductRepositoryImplTest {
     fun `상품을 모두 로드하면 hasNext는 false가 된다`() =
         runTest {
             val productRepository = ProductRepositoryImpl()
-            repeat(2) {
-                productRepository.getProducts()
-            }
-            productRepository.hasNext shouldEqual false
-        }
 
-    @Test
-    fun `모든 상품을 로드한 후 추가로 조회하면 빈 리스트를 반환한다`() =
-        runTest {
-            val productRepository = ProductRepositoryImpl()
-
-            repeat(2) {
-                productRepository.getProducts()
-            }
-
-            val result = productRepository.getProducts()
-            result.size shouldEqual 0
+            productRepository.getProducts(offset = 20).hasNext shouldEqual false
         }
 }
