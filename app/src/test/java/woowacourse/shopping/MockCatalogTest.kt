33c2b30743d6d3104d8b176965264a694e02c165
Redish03 <pluto0303@pukyong.ac.kr>
2026-05-10 22:05:38 +0900

    test: 안쓰는 함수 제거 및 테스트 수정

--- app/src/test/java/woowacourse/shopping/MockCatalogTest.kt
+++ app/src/test/java/woowacourse/shopping/MockCatalogTest.kt
@@ -11,8 +11,8 @@ class MockCatalogTest {
         // given
         val pageSize = 5
 
-        // when: 첫 번째 페이지 로드
-        val firstPage = MockCatalog.loadMoreProducts(
+        // when
+        val firstPage = MockCatalog.loadProducts(
             0,
             pageSize
         ).await()
@@ -22,28 +22,22 @@ class MockCatalogTest {
             MockCatalog.catalog[0],
             firstPage[0]
         )
-
-        // when: 두 번째 페이지 로드
-        val secondPage = MockCatalog.loadMoreProducts(1, pageSize).await()
-        // then
-        assertEquals(10, secondPage.size)
-        assertEquals(MockCatalog.catalog[5], secondPage[5])
     }
 
     @Test
     fun `데이터 범위를 벗어난 페이지요청 시 마지막까지만 반환한다`() = runBlocking {
         val totalSize = MockCatalog.catalog.size
         val pageSize = 20
-        val lastPage = (totalSize / pageSize) + 1
+        val lastPage = (totalSize / pageSize)
 
         val result =
-            MockCatalog.loadMoreProducts(
+            MockCatalog.loadProducts(
                 lastPage,
                 pageSize
             ).await()
 
-        assertTrue(
-            result.size == totalSize
+        assertEquals(
+            totalSize % pageSize, result.size
         )
     }
 }
