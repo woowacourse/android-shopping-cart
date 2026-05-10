adf6d292f0e95632808ec9be3566e4f3c1d9e26b
first-woosun <dbdntjs620@gmail.com>
2026-05-10 16:30:02 +0900

    feat: 앱 실행시 MockWebServer 실행

--- app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
+++ app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
@@ -1,9 +1,12 @@
 package woowacourse.shopping
 
 import android.app.Application
+import okhttp3.OkHttpClient
 import woowacourse.shopping.data.local.database.DataBase
 import woowacourse.shopping.data.local.repository.PurchaseProductsRepository
 import woowacourse.shopping.data.local.repository.RecentlyViewedProductRepository
+import woowacourse.shopping.data.remote.mock.MockWebServer
+import woowacourse.shopping.data.remote.repository.WebServerRepository
 
 class ShoppingApplication: Application() {
     val database by lazy { DataBase.getDatabase(this) }
@@ -14,5 +17,17 @@ class ShoppingApplication: Application() {
 
     val recentlyViewedProductRepository by lazy {
         RecentlyViewedProductRepository(database.recentlyViewedProductDao())
+    }
+
+    val client by lazy { OkHttpClient() }
+    val webServerRepository by lazy {
+        WebServerRepository(client, MockWebServer.baseUrl)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        Thread {
+            MockWebServer.start()
+        }
     }
 }
