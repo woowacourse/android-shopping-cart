c6dfcf62953f11396dc5e77d582347bbfe82d292
baekcii <bsy8470@naver.com>
2026-05-11 01:45:13 +0900

    feat: 최근 본 상품 저장소 객체 추가

--- app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
+++ app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
@@ -3,7 +3,9 @@ package woowacourse.shopping
 import android.app.Application
 import woowacourse.shopping.data.local.ShoppingDatabase
 import woowacourse.shopping.data.repository.CartRepositoryImpl
+import woowacourse.shopping.data.repository.ProductRecentRepositoryImpl
 import woowacourse.shopping.data.repository.ProductRepositoryImpl
+import woowacourse.shopping.data.source.ProductDataSourceImpl
 
 class ShoppingApplication : Application() {
     val database by lazy { ShoppingDatabase.getDatabase(this) }
@@ -14,5 +16,9 @@ class ShoppingApplication : Application() {
 
     val productRepository by lazy {
         ProductRepositoryImpl()
+    }
+
+    val productRecentRepository by lazy {
+        ProductRecentRepositoryImpl(database.recentProductDao(), ProductDataSourceImpl)
     }
 }
