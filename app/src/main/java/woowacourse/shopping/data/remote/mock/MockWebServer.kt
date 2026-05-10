c757e9ec1f2b1be918f6970cc56e1b21ddeac89f
first-woosun <dbdntjs620@gmail.com>
2026-05-10 16:28:25 +0900

    feat: MockWebServer 객체 정의

---
+++ app/src/main/java/woowacourse/shopping/data/remote/mock/MockWebServer.kt
@@ -0,0 +1,19 @@
+package woowacourse.shopping.data.remote.mock
+
+import mockwebserver3.MockWebServer
+
+object MockWebServer {
+    private val server = MockWebServer().apply {
+        dispatcher = MockServerDispatcher()
+    }
+
+    val baseUrl: String get() = server.url("/").toString()
+
+    fun start() {
+        server.start()
+    }
+
+    fun stop() {
+        server.close()
+    }
+}
