4cac123f9bf14927c6ef85890add7f22718f0348
first-woosun <dbdntjs620@gmail.com>
2026-05-10 16:32:06 +0900

    test: ROOM db 테스트 시도

---
+++ app/src/androidTest/java/woowacourse/shopping/PurchaseProductDaoTest.kt
@@ -0,0 +1,54 @@
+package woowacourse.shopping
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.runner.AndroidJUnit4
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import woowacourse.shopping.data.local.dao.PurchaseProductsDao
+import woowacourse.shopping.data.local.database.DataBase
+import woowacourse.shopping.data.local.entity.PurchaseProductEntity
+
+@RunWith(AndroidJUnit4::class)
+class PurchaseProductDaoTest {
+    private lateinit var db: DataBase
+    private lateinit var dao: PurchaseProductsDao
+
+    @Before
+    fun createDb() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        db = Room.inMemoryDatabaseBuilder(context, DataBase::class.java).build()
+        dao = db.purchaseProductsDao()
+    }
+
+    @After
+    fun closeDb() {
+        db.close()
+    }
+
+    @Test
+    fun insertAndGetProduct() = runBlocking {
+        //given
+        val entity = PurchaseProductEntity(
+            id = "1",
+            name = "딸기",
+            price = 10000,
+            imageUri = "uri",
+            count = 1
+        )
+
+        //when
+        dao.insertAll(entity)
+
+        //then
+        val allProducts = dao.getAll().first()
+        assert(allProducts.size == 1)
+        assert(allProducts[0].name == "딸기")
+    }
+}
