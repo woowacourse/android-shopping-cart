c9557559b26cb486b41b867cbdc8687e19473bdf
Redish03 <pluto0303@pukyong.ac.kr>
2026-05-10 22:40:49 +0900

    feat: CartRepository.kt 추가 및 구현체 추가

---
+++ app/src/main/java/woowacourse/shopping/repository/CartRepository.kt
@@ -0,0 +1,13 @@
+package woowacourse.shopping.repository
+
+import woowacourse.shopping.domain.Cart
+import woowacourse.shopping.domain.Product
+import java.util.UUID
+
+interface CartRepository {
+    val cart: Cart
+
+    suspend fun addProduct(product: Product, amount: Int = 1)
+    suspend fun decreaseProduct(productId: UUID, amount: Int = 1)
+    suspend fun removeProduct(productId: UUID)
+}
---
+++ app/src/main/java/woowacourse/shopping/repository/InMemoryCartRepository.kt
@@ -0,0 +1,25 @@
+package woowacourse.shopping.repository
+
+import woowacourse.shopping.domain.Cart
+import woowacourse.shopping.domain.CartProducts
+import woowacourse.shopping.domain.Product
+import java.util.UUID
+
+class InMemoryCartRepository : CartRepository {
+    override var cart: Cart = Cart(CartProducts(emptyList()))
+
+    override suspend fun addProduct(
+        product: Product,
+        amount: Int,
+    ) {
+        cart = cart.addProduct(product, amount)
+    }
+
+    override suspend fun decreaseProduct(productId: UUID, amount: Int) {
+        cart = cart.decreaseProduct(productId, amount)
+    }
+
+    override suspend fun removeProduct(productId: UUID) {
+        cart = cart.removeProduct(productId)
+    }
+}
