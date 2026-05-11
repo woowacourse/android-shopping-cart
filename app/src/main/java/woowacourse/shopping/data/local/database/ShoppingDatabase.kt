b6c19c3b7ec73a55c0a38f7b748265ac3757ebe4
조은경 <108224432+eungyeong12@users.noreply.github.com>
2026-05-11 18:38:22 +0900

    fix: ShoppingDatabase의 데이터베이스 버전을 3으로 업데이트

--- app/src/main/java/woowacourse/shopping/data/local/database/ShoppingDatabase.kt
+++ app/src/main/java/woowacourse/shopping/data/local/database/ShoppingDatabase.kt
@@ -12,7 +12,7 @@ import woowacourse.shopping.data.local.entity.RecentlyViewedProductEntity
         CartItemEntity::class,
         RecentlyViewedProductEntity::class,
     ],
-    version = 2,
+    version = 3,
     exportSchema = false,
 )
 abstract class ShoppingDatabase : RoomDatabase() {
