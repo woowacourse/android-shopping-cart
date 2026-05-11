61f0f6dff6b03bc97ec9363c5a5b622cb9e70fe6
cksckckcks <chan679999@naver.com>
2026-05-11 12:44:18 +0900

    fix: 최근 본 상품을 반대로 보여주는 버그 수정

--- app/src/main/java/woowacourse/shopping/data/source/RecentProductSourceImpl.kt
+++ app/src/main/java/woowacourse/shopping/data/source/RecentProductSourceImpl.kt
@@ -3,11 +3,15 @@ package woowacourse.shopping.data.source
 object RecentProductSourceImpl : RecentProductSource {
     private val productIds = mutableListOf("1", "2", "3")
 
-    override fun getRecentProductIds(): List<String> = productIds
+    override fun getRecentProductIds(): List<String> = productIds.reversed()
 
     override fun addRecentProductId(productId: String) {
+        if (productIds.contains(productId)) {
+            productIds.remove(productId)
+        }
+
         productIds.add(productId)
     }
 
-    override fun getLastViewProductId(): String = productIds.last()
+    override fun getLastViewProductId(): String = productIds.first()
 }
