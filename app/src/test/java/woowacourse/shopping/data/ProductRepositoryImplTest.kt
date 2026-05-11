ca2899613d45b1813bfa0fb3335570b52412cbe5
baekcii <bsy8470@naver.com>
2026-05-11 16:26:41 +0900

    test: ProductRepositoryImpl 테스트 코드에 Fake 객체 적용

--- app/src/test/java/woowacourse/shopping/data/ProductRepositoryImplTest.kt
+++ app/src/test/java/woowacourse/shopping/data/ProductRepositoryImplTest.kt
@@ -8,25 +8,27 @@ import woowacourse.shopping.data.repository.ProductRepositoryImpl
 
 class ProductRepositoryImplTest {
     @Test
-    fun `특정 상품을 id를 통해 가져올 수 있다`() {
-        val productRepository = ProductRepositoryImpl()
+    fun `특정 상품을 id를 통해 가져올 수 있다`() =
+        runTest {
+            val productRepository = ProductRepositoryImpl(FakeProductDataSource())
 
-        productRepository.getProductById(id = "1").name shouldEqual "bolt"
-    }
+            productRepository.getProductById(id = "1").name shouldEqual "bolt"
+        }
 
     @Test
-    fun `존재하지 않는 id일 경우 예외가 발생한다`() {
-        assertThrows<IllegalArgumentException> {
-            val productRepository = ProductRepositoryImpl()
+    fun `존재하지 않는 id일 경우 예외가 발생한다`() =
+        runTest {
+            assertThrows<IllegalArgumentException> {
+                val productRepository = ProductRepositoryImpl(FakeProductDataSource())
 
-            productRepository.getProductById(id = "아아아아")
+                productRepository.getProductById(id = "아아아아")
+            }
         }
-    }
 
     @Test
     fun `상품을 모두 로드하면 hasNext는 false가 된다`() =
         runTest {
-            val productRepository = ProductRepositoryImpl()
+            val productRepository = ProductRepositoryImpl(FakeProductDataSource())
 
             productRepository.getProducts(offset = 20).hasNext shouldEqual false
         }
