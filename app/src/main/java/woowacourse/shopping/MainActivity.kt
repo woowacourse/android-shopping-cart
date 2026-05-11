bf51606983f8f8a11e305fe447327ed7e81315b6
Redish03 <pluto0303@pukyong.ac.kr>
2026-05-11 14:23:55 +0900

    feat: 네트워크 연결 여부 확인 토스트 추가

--- app/src/main/java/woowacourse/shopping/MainActivity.kt
+++ app/src/main/java/woowacourse/shopping/MainActivity.kt
@@ -2,13 +2,17 @@ package woowacourse.shopping
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import woowacourse.shopping.ui.component.screen.MainScreen
 import woowacourse.shopping.ui.theme.AndroidshoppingTheme
 
@@ -20,7 +24,18 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             AndroidshoppingTheme {
+                val networkMonitor = NetworkMonitor(this)
+                val isConnected by networkMonitor.isConnected.collectAsStateWithLifecycle(initialValue = true)
+
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    LaunchedEffect(isConnected) {
+                        if(!isConnected) {
+                            Toast.makeText(this@MainActivity, "인터넷 연결을 확인해주세요.", Toast.LENGTH_SHORT).show()
+                        } else {
+                            Toast.makeText(this@MainActivity, "연결되었습니다.", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
                     MainScreen(
                         onItemClick = { id ->
                             val intent = Intent(this, ProductDetailActivity::class.java).apply {
