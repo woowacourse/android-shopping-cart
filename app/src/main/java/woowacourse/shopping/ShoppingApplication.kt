96f4f7ee26d8904f465735297a41f73465aedf30
cksckckcks <chan679999@naver.com>
2026-05-11 15:51:33 +0900

    feat: Application 클래스 내 가짜 서버 시작 및 네트워크 저장소 주입

--- app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
+++ app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
@@ -2,18 +2,28 @@ package woowacourse.shopping
 
 import android.app.Application
 import androidx.room.Room
+import okhttp3.OkHttpClient
 import woowacourse.shopping.data.repository.CartRepositoryImpl
 import woowacourse.shopping.data.repository.ProductRepositoryImpl
 import woowacourse.shopping.data.repository.RecentProductRepositoryImpl
 import woowacourse.shopping.data.source.CartDataSourceImpl
-import woowacourse.shopping.data.source.ProductDataSourceImpl
 import woowacourse.shopping.data.source.RecentProductSourceImpl
 import woowacourse.shopping.data.source.local.ShoppingDataBase
+import woowacourse.shopping.data.source.remote.ProductRemoteDataSourceImpl
+import woowacourse.shopping.data.source.remote.mock.MockServerManager
 import woowacourse.shopping.domain.repository.CartRepository
 import woowacourse.shopping.domain.repository.ProductRepository
 import woowacourse.shopping.domain.repository.RecentProductRepository
 
 class ShoppingApplication : Application() {
+    private val client = OkHttpClient()
+
+    override fun onCreate() {
+        super.onCreate()
+
+        MockServerManager.start()
+    }
+
     private val database by lazy {
         Room
             .databaseBuilder(
@@ -26,7 +36,14 @@ class ShoppingApplication : Application() {
     private val cartItemDao by lazy { database.cartItemDao() }
     private val recentProductDao by lazy { database.recentItemDao() }
 
-    val productRepository: ProductRepository by lazy { ProductRepositoryImpl(ProductDataSourceImpl) }
+    val productRepository: ProductRepository by lazy {
+        ProductRepositoryImpl(
+            ProductRemoteDataSourceImpl(
+                client,
+                MockServerManager.getBaseUrl(),
+            ),
+        )
+    }
     val cartRepository: CartRepository by lazy { CartRepositoryImpl(CartDataSourceImpl(cartItemDao)) }
     val recentProductRepository: RecentProductRepository by lazy {
         RecentProductRepositoryImpl(
