ea65ee9399005f19b6169530f1d934d50ce991d3
baekcii <bsy8470@naver.com>
2026-05-11 15:04:03 +0900

    feat: 상품 DTO 및 매퍼 구현

--- app/src/main/java/woowacourse/shopping/data/util/Mapper.kt
+++ app/src/main/java/woowacourse/shopping/data/util/Mapper.kt
@@ -1,8 +1,12 @@
 package woowacourse.shopping.data.util
 
 import woowacourse.shopping.data.local.CartEntity
+import woowacourse.shopping.data.remote.dto.ProductDto
+import woowacourse.shopping.data.remote.dto.ProductsResponseDto
 import woowacourse.shopping.domain.CartItem
 import woowacourse.shopping.domain.CartItems
+import woowacourse.shopping.domain.Product
+import woowacourse.shopping.domain.Products
 
 fun CartEntity.toDomain(): CartItem =
     CartItem(
@@ -14,4 +18,18 @@ fun List<CartEntity>.toDomain(isLast: Boolean): CartItems =
     CartItems(
         items = map { it.toDomain() },
         isLast = isLast,
+    )
+
+fun ProductDto.toDomain(): Product =
+    Product(
+        id = id,
+        name = name,
+        price = price,
+        imageUrl = imageUrl,
+    )
+
+fun ProductsResponseDto.toDomain(): Products =
+    Products(
+        items = products.map { it.toDomain() },
+        hasNext = !last,
     )
