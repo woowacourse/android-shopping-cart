208ba17a5c1cd284875b0ce2968e50673285fe57
baekcii <bsy8470@naver.com>
2026-05-11 16:18:00 +0900

    feat: 네트워크 연결 상태 모니터링 객체 구현

---
+++ app/src/main/java/woowacourse/shopping/data/util/NetworkMonitor.kt
@@ -0,0 +1,48 @@
+package woowacourse.shopping.data.util
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
+
+class NetworkMonitor(
+    context: Context,
+) {
+    private val connectivityManager =
+        context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+    val isConnected: Flow<Boolean> =
+        callbackFlow {
+            val callback =
+                object : ConnectivityManager.NetworkCallback() {
+                    override fun onAvailable(network: Network) {
+                        trySend(true)
+                    }
+
+                    override fun onLost(network: Network) {
+                        trySend(false)
+                    }
+                }
+
+            val networkRequest =
+                NetworkRequest
+                    .Builder()
+                    .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+                    .build()
+
+            val isCurrentlyConnected =
+                connectivityManager
+                    .getNetworkCapabilities(connectivityManager.activeNetwork)
+                    ?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) ?: false
+            trySend(isCurrentlyConnected)
+
+            connectivityManager.registerNetworkCallback(networkRequest, callback)
+
+            awaitClose { connectivityManager.unregisterNetworkCallback(callback) }
+        }.distinctUntilChanged()
+}
