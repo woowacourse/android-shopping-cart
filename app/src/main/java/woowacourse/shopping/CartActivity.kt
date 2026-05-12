966b13174967ae3bdd45f7021990ba92e9bbf200
chohs4164 <josh100as@gmail.com>
2026-05-13 02:38:36 +0900

    refactor: viewModel 생명주기 이동

--- app/src/main/java/woowacourse/shopping/CartActivity.kt
+++ app/src/main/java/woowacourse/shopping/CartActivity.kt
@@ -4,21 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
 import woowacourse.shopping.ui.cart.screen.CartScreen
 import woowacourse.shopping.ui.cart.viewmodel.CartViewModel
 import woowacourse.shopping.ui.theme.AndroidShoppingTheme
 import kotlin.uuid.ExperimentalUuidApi
 
 class CartActivity : ComponentActivity() {
+    private val viewModel: CartViewModel by viewModels {
+        viewModelFactory {
+            initializer {
+                CartViewModel(
+                    AppContainer.cartRepository,
+                )
+            }
+        }
+    }
+
     @OptIn(ExperimentalUuidApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        val viewModel =
-            CartViewModel(
-                AppContainer.cartRepository,
-            )
-
         @OptIn(ExperimentalUuidApi::class)
         setContent {
             AndroidShoppingTheme {
--- app/src/main/java/woowacourse/shopping/ui/cart/screen/CartScreen.kt
+++ app/src/main/java/woowacourse/shopping/ui/cart/screen/CartScreen.kt
@@ -7,7 +7,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.viewmodel.compose.viewModel
 import woowacourse.shopping.repository.cart.InMemoryCartRepository
 import woowacourse.shopping.ui.cart.component.CartBody
 import woowacourse.shopping.ui.cart.component.CartTopAppBar
@@ -18,7 +17,7 @@ import kotlin.uuid.ExperimentalUuidApi
 @Composable
 fun CartScreen(
     modifier: Modifier = Modifier,
-    viewModel: CartViewModel = viewModel(),
+    viewModel: CartViewModel,
     onClose: () -> Unit,
 ) {
     Scaffold(
