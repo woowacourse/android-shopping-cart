0724760e5b4e4cc9e64ff49a709e4bc2d5fe9e0a
first-woosun <dbdntjs620@gmail.com>
2026-05-12 17:45:19 +0900

    refactor: 테이블 스키마 수정

--- app/src/main/java/woowacourse/shopping/data/local/entity/RecentlyViewedProductEntity.kt
+++ app/src/main/java/woowacourse/shopping/data/local/entity/RecentlyViewedProductEntity.kt
@@ -8,16 +8,5 @@ import woowacourse.shopping.domain.Product
 @Entity(tableName = "recently_viewed_products")
 data class RecentlyViewedProductEntity(
     @PrimaryKey @ColumnInfo(name = "id") val id: String,
-    @ColumnInfo(name = "name") val name: String,
-    @ColumnInfo(name = "price") val price: Int,
-    @ColumnInfo(name = "imageUri") val imageUri: String,
     @ColumnInfo(name = "time_stamp") val timeStamp: Long = System.currentTimeMillis(),
-) {
-    fun toObject(): Product =
-        Product(
-            id = id,
-            imageUri = imageUri,
-            name = name,
-            price = price,
-        )
-}
+)
