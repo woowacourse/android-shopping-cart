c54c6ee4a90556098706b7571a956d7043c13d9e
first-woosun <dbdntjs620@gmail.com>
2026-05-12 18:19:17 +0900

    refactor: 미사용 함수 제거

--- app/src/main/java/woowacourse/shopping/data/local/dao/PurchaseProductsDao.kt
+++ app/src/main/java/woowacourse/shopping/data/local/dao/PurchaseProductsDao.kt
@@ -13,9 +13,6 @@ interface PurchaseProductsDao {
     @Query("SELECT * FROM purchase_products")
     fun getAll(): Flow<List<PurchaseProductEntity>?>
 
-    @Query("SELECT * FROM purchase_products WHERE id = :id")
-    fun findWithId(id: String): Flow<PurchaseProductEntity?>
-
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insertIgnore(entity: PurchaseProductEntity): Long
 
@@ -34,21 +31,6 @@ interface PurchaseProductsDao {
     @Query("DELETE FROM purchase_products WHERE id = :id")
     suspend fun deleteWithId(id: String)
 
-    @Query("SELECT SUM(count) FROM purchase_products")
-    fun getTotalAmount(): Flow<Int>
-
-    @Query("SELECT count FROM purchase_products WHERE id = :id")
-    fun getCountOfSpecificPurchaseProduct(id: String): Flow<Int>
-
-    @Query("SELECT * FROM purchase_products LIMIT :limit OFFSET :offset")
-    fun getPartedPurchaseProducts(
-        limit: Int,
-        offset: Int,
-    ): Flow<List<PurchaseProductEntity>>
-
     @Query("SELECT COUNT(*) FROM purchase_products")
     fun getProductCount(): Flow<Int>
-
-    @Query("SELECT EXISTS (SELECT * FROM purchase_products WHERE id = :id)")
-    fun isContained(id: String): Boolean
 }
