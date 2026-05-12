63cbcdf869fa0c29e46cb97aa4b7d5a41b5a850c
first-woosun <dbdntjs620@gmail.com>
2026-05-12 09:49:43 +0900

    refactor: purchase_products 테이블 정의 변경
    - id, count만 로컬에 저장
    - "count는 0 초과여야 한다" 제약 추가

--- app/src/main/java/woowacourse/shopping/data/local/entity/PurchaseProductEntity.kt
+++ app/src/main/java/woowacourse/shopping/data/local/entity/PurchaseProductEntity.kt
@@ -3,26 +3,16 @@ package woowacourse.shopping.data.local.entity
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import org.jetbrains.annotations.NotNull
 import woowacourse.shopping.domain.Product
 import woowacourse.shopping.domain.PurchaseProduct
 
 @Entity(tableName = "purchase_products")
 data class PurchaseProductEntity(
     @PrimaryKey @ColumnInfo(name = "id") val id: String,
-    @ColumnInfo(name = "name") val name: String,
-    @ColumnInfo(name = "price") val price: Int,
-    @ColumnInfo(name = "imageUri") val imageUri: String,
     @ColumnInfo(name = "count") val count: Int,
 ) {
-    fun toPurchaseProductObject(): PurchaseProduct =
-        PurchaseProduct(
-            product =
-                Product(
-                    id = id,
-                    imageUri = imageUri,
-                    name = name,
-                    price = price,
-                ),
-            count = count,
-        )
+    init {
+        require(count > 0) { "상품의 개수는 1이상이어야 합니다." }
+    }
 }
