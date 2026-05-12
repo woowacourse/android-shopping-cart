8de7d6e40b0d17d26dc49b05cf3bf013d568e1e3
first-woosun <dbdntjs620@gmail.com>
2026-05-12 18:04:55 +0900

    test: PurchaseProductDaoTest 구현

--- app/src/androidTest/java/woowacourse/shopping/PurchaseProductDaoTest.kt
+++ app/src/androidTest/java/woowacourse/shopping/PurchaseProductDaoTest.kt
@@ -6,8 +6,7 @@ import androidx.test.core.app.ApplicationProvider
 import androidx.test.runner.AndroidJUnit4
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
-import org.junit.After
-import org.junit.Before
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 import woowacourse.shopping.data.local.dao.PurchaseProductsDao
@@ -19,33 +18,71 @@ class PurchaseProductDaoTest {
     private lateinit var db: DataBase
     private lateinit var dao: PurchaseProductsDao
 
-    @Before
-    fun createDb() {
+    @Test
+    fun `상품을_저장하고_불러올_수_있다`() = runBlocking {
+        // given
+        createDb()
+        val entity =
+            PurchaseProductEntity(
+                id = "1",
+                count = 1,
+            )
+
+        // when
+        dao.upsert(entity)
+
+        // then
+        val allProducts = dao.getAll().first()
+        assertEquals(1, allProducts?.size)
+        closeDb()
+    }
+
+    @Test
+    fun `동일한_ID를_갖는_상품을_저장하면_count가_증가한다`() = runBlocking {
+        // given
+        createDb()
+        val entity =
+            PurchaseProductEntity(
+                id = "1",
+                count = 1,
+            )
+        dao.upsert(entity)
+
+        // when
+        dao.upsert(entity)
+
+        // then
+        val allProducts = dao.getAll().first()
+        assertEquals(2, allProducts?.first()?.count)
+        closeDb()
+    }
+
+    @Test
+    fun `ID를통해_장바구니에_담긴_상품을_삭제할_수_있다`() = runBlocking {
+        createDb()
+        val entity =
+            PurchaseProductEntity(
+                id = "1",
+                count = 1,
+            )
+        dao.upsert(entity)
+
+        // when
+        dao.deleteWithId(entity.id)
+
+        // then
+        val allProducts = dao.getAll().first()
+        assertEquals(0, allProducts?.size)
+        closeDb()
+    }
+
+    private fun createDb() {
         val context = ApplicationProvider.getApplicationContext<Context>()
         db = Room.inMemoryDatabaseBuilder(context, DataBase::class.java).build()
         dao = db.purchaseProductsDao()
     }
 
-    @After
-    fun closeDb() {
+    private fun closeDb() {
         db.close()
     }
-
-    @Test
-    fun insertAndGetProduct() =
-        runBlocking {
-            // given
-            val entity =
-                PurchaseProductEntity(
-                    id = "1",
-                    count = 1,
-                )
-
-            // when
-            dao.insertAll(entity)
-
-            // then
-            val allProducts = dao.getAll().first()
-            assert(allProducts.size == 1)
-        }
 }
