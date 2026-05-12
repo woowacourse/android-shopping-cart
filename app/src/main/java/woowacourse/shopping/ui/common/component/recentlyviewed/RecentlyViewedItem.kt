b8f77b7e14fb34151a2bd106c48866fb0b7eab56
nadajinny <jinsun23007@gmail.com>
2026-05-12 21:06:53 +0900

    refactor: ktlintFormat 적용

--- app/src/main/java/woowacourse/shopping/ui/common/component/recentlyviewed/RecentlyViewedItem.kt
+++ app/src/main/java/woowacourse/shopping/ui/common/component/recentlyviewed/RecentlyViewedItem.kt
@@ -3,11 +3,11 @@ package woowacourse.shopping.ui.common.component.recentlyviewed
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
--- app/src/main/java/woowacourse/shopping/ui/shopping/component/ProductUnit.kt
+++ app/src/main/java/woowacourse/shopping/ui/shopping/component/ProductUnit.kt
@@ -4,11 +4,11 @@ import android.annotation.SuppressLint
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
