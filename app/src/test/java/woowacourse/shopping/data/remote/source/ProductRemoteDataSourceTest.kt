a21d5f485930146d085f3e49994bdb84a93b7e5d
byunghyunkim0 <bb05qudgus@gmail.com>
2026-05-14 22:41:30 +0900

    test: ProductRemoteDataSource test 코드 작성

--- app/src/test/java/woowacourse/shopping/data/remote/source/ProductRemoteDataSourceTest.kt
+++ app/src/test/java/woowacourse/shopping/data/remote/source/ProductRemoteDataSourceTest.kt
@@ -2,18 +2,20 @@ package woowacourse.shopping.data.remote.source
 
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import woowacourse.shopping.constants.MockData
+import org.junit.jupiter.api.assertNotNull
 import woowacourse.shopping.data.remote.api.ShoppingMockServer
 
 class ProductRemoteDataSourceTest {
-    private val dataSource = ProductRemoteDataSource()
+    private lateinit var dataSource: ProductRemoteDataSource
 
     @BeforeEach
     fun setUp() {
         ShoppingMockServer.start()
+        dataSource = ProductRemoteDataSource()
     }
 
     @AfterEach
@@ -22,20 +24,25 @@ class ProductRemoteDataSourceTest {
     }
 
     @Test
-    fun `전체 상품 목록을 가져온다`() = runBlocking {
-        val products = dataSource.getProducts()
+    fun `페이지에 해당하는 상품 목록을 size만큼 가져온다`() = runBlocking {
+        val result = dataSource.getProducts(page = 1, size = 20)
 
-        Assertions.assertEquals(MockData.MOCK_PRODUCTS_LIST.size, products.size)
-        Assertions.assertEquals("품목1", products[0].name)
+        assertEquals(true, result.isSuccess)
+
+        val products = result.getOrNull()
+        assertNotNull(products)
+        assertTrue(products.isNotEmpty())
     }
 
     @Test
     fun `특정 상품 정보를 가져온다`() = runBlocking {
         val targetId = "1"
+        val result = dataSource.getProduct(targetId)
 
-        val product = dataSource.getProduct(targetId)
+        assertTrue(result.isSuccess)
 
-        Assertions.assertEquals(1, product.id)
-        Assertions.assertEquals("품목1", product.name)
+        val productResponse = result.getOrNull()
+        assertNotNull(productResponse)
+        assertEquals(1, productResponse.id)
     }
 }
